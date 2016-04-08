3683724e4841bfe42b885f768cc090143e3be493
Jason Smith <jason.smith@xamarin.com>
2016-04-07 22:22:51 -0700

    Fix code review issues

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40173.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40173.cs
@@ -1,94 +1,110 @@
-﻿using System;
-
-using Xamarin.Forms.CustomAttributes;
-
+﻿using Xamarin.Forms.CustomAttributes;
 #if UITEST
-using Xamarin.UITest;
 using NUnit.Framework;
 #endif
 
 namespace Xamarin.Forms.Controls
 {
-	[Preserve (AllMembers = true)]
-	[Issue (IssueTracker.Bugzilla, 40173, "Android BoxView/Frame not clickthrough in ListView")]
-	public class Bugzilla40173 : TestContentPage // or TestMasterDetailPage, etc ...
-	{
-		protected override void Init()
-		{
-			// Initialize ui here instead of ctor
-			Content = new Label
-			{
-				AutomationId = "IssuePageLabel",
-				Text = "See if I'm here"
-			};
-
-			// because Im like... really lazy with writing these tests
-			var outputLabel = new Label();
-			var testButton = new Button
-			{
-				Text = "Can't Touch This",
-				AutomationId = "bz40173TestButton1"
-			};
-
-			testButton.Clicked += (sender, args) => outputLabel.Text = "Failed";
-
-			var testGrid = new Grid
-			{
-				Children = {
-					testButton,
-					new BoxView
-					{
-						Color = Color.Pink.MultiplyAlpha(0.5)
-					}
-				}
-			};
-
-			var testListView = new ListView();
-			var items = new[] { "Foo" };
-			testListView.ItemsSource = items;
-			testListView.ItemTemplate = new DataTemplate(() =>
-			{
-				ViewCell result = new ViewCell
-				{
-					View = new Grid
-					{
-						Children =
-						{
-							new BoxView
-							{
-								AutomationId = "listTapTarget",
-								Color = Color.Pink.MultiplyAlpha(0.5)
-							}
-						}
-					}
-				};
-
-
-				return result;
-			});
-
-			testListView.ItemSelected += (sender, args) => outputLabel.Text = "ItemTapped";
-
-			Content = new StackLayout
-			{
-				Children = { outputLabel, testGrid, testListView }
-			};
-		}
+    [Preserve(AllMembers = true)]
+    [Issue(IssueTracker.Bugzilla, 40173, "Android BoxView/Frame not clickthrough in ListView")]
+    public class Bugzilla40173 : TestContentPage // or TestMasterDetailPage, etc ...
+    {
+        const string CantTouchButtonId = "CantTouchButtonId";
+        const string CanTouchButtonId = "CanTouchButtonId";
+        const string ListTapTarget = "ListTapTarget";
+        const string CantTouchFailText = "Failed";
+        const string CanTouchSuccessText = "ButtonTapped";
+        const string ListTapSuccessText = "ItemTapped";
 
 #if UITEST
-		[Test]
-		public void ButtonBlocked ()
-		{
-			RunningApp.Tap(q => q.Marked("bz40173TestButton1"));
-			RunningApp.WaitForNoElement (q => q.Text ("Failed"));
-		}
+        [Test]
+        public void ButtonBlocked()
+        {
+            RunningApp.Tap(q => q.Marked(CantTouchButtonId));
+            RunningApp.WaitForNoElement(q => q.Text(CantTouchFailText));
 
-		[Test]
-		public void ListViewSelectionWorks()
-		{
-			RunningApp.Tap(q => q.Marked("listTapTarget"));
-			RunningApp.WaitForElement (q => q.Text ("ItemTapped"));
-		}
+            RunningApp.Tap(q => q.Marked(CanTouchButtonId));
+            RunningApp.WaitForElement(q => q.Text(CanTouchSuccessText));
+
+            RunningApp.Tap(q => q.Marked(ListTapTarget));
+            RunningApp.WaitForElement(q => q.Text(ListTapSuccessText));
+        }
 #endif
-	}
+
+        protected override void Init()
+        {
+            var outputLabel = new Label();
+            var testButton = new Button
+            {
+                Text = "Can't Touch This",
+                AutomationId = CantTouchButtonId
+            };
+
+            testButton.Clicked += (sender, args) => outputLabel.Text = CantTouchFailText;
+
+            var testGrid = new Grid
+            {
+                Children =
+                {
+                    testButton,
+                    new BoxView
+                    {
+                        Color = Color.Pink.MultiplyAlpha(0.5)
+                    }
+                }
+            };
+
+            // BoxView over Button prevents Button click
+            var testButtonOk = new Button
+            {
+                Text = "Can Touch This",
+                AutomationId = CanTouchButtonId
+            };
+
+            testButtonOk.Clicked += (sender, args) => outputLabel.Text = CanTouchSuccessText;
+
+            var testGridOk = new Grid
+            {
+                Children =
+                {
+                    testButtonOk,
+                    new BoxView
+                    {
+                        Color = Color.Pink.MultiplyAlpha(0.5),
+                        InputTransparent = true
+                    }
+                }
+            };
+
+            var testListView = new ListView();
+            var items = new[] { "Foo" };
+            testListView.ItemsSource = items;
+            testListView.ItemTemplate = new DataTemplate(() =>
+            {
+                var result = new ViewCell
+                {
+                    View = new Grid
+                    {
+                        Children =
+                        {
+                            new BoxView
+                            {
+                                AutomationId = ListTapTarget,
+                                Color = Color.Pink.MultiplyAlpha(0.5)
+                            }
+                        }
+                    }
+                };
+
+                return result;
+            });
+
+            testListView.ItemSelected += (sender, args) => outputLabel.Text = ListTapSuccessText;
+
+            Content = new StackLayout
+            {
+                Children = { outputLabel, testGrid, testGridOk, testListView }
+            };
+        }
+    }
 }
