7656decf0bdbc2ddf477983e58adbf70a9dccc37
Rui Marinho <me@ruimarinho.net>
2016-04-15 12:07:19 +0100

    Fix repo

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla39987.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla39987.cs
@@ -19,5 +19,31 @@ namespace Xamarin.Forms.Controls
 			Children.Add(new CustomMapPage(new CustomMapView(), "Teste 2"));
 			Children.Add(new CustomMapPage(new CustomMapView(), "Teste 3"));
 		}
+
+	}
+
+	public class CustomMapView : View
+	{
+		public CustomMapView()
+		{
+
+		}
+
+	}
+
+	public class CustomMapPage : ContentPage
+	{
+		private CustomMapView _customMapView;
+
+
+		public CustomMapPage(CustomMapView customMapView, string title)
+		{
+			Title = title;
+			_customMapView = customMapView;
+			_customMapView.HorizontalOptions = LayoutOptions.FillAndExpand;
+			_customMapView.VerticalOptions = LayoutOptions.FillAndExpand;
+			Content = _customMapView;
+		}
+
 	}
 }
