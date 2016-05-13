8a9825c13fdba69f4b9935dd7de38ad83dc9e9ce
Daniel Hindrikes <daniel@hindrikes.se>
2016-05-13 09:13:07 +0200

    I have added background color and text color to menu item and created iOS implementation for that.

--- Xamarin.Forms.Core/MenuItem.cs
+++ Xamarin.Forms.Core/MenuItem.cs
@@ -17,7 +17,10 @@ namespace Xamarin.Forms
 
 		public static readonly BindableProperty IconProperty = BindableProperty.Create("Icon", typeof(FileImageSource), typeof(MenuItem), default(FileImageSource));
 
-		internal static readonly BindableProperty IsEnabledProperty = BindableProperty.Create("IsEnabled", typeof(bool), typeof(ToolbarItem), true);
+        public static readonly BindableProperty BackgroundColorProperty = BindableProperty.Create("BackgroundColor", typeof(FileImageSource), typeof(MenuItem), Color.Default);
+        public static readonly BindableProperty TextColorProperty = BindableProperty.Create("TextColor", typeof(FileImageSource), typeof(MenuItem), Color.Default);
+
+        internal static readonly BindableProperty IsEnabledProperty = BindableProperty.Create("IsEnabled", typeof(bool), typeof(ToolbarItem), true);
 
 		public ICommand Command
 		{
@@ -49,7 +52,25 @@ namespace Xamarin.Forms
 			set { SetValue(TextProperty, value); }
 		}
 
-		internal bool IsEnabled
+        /// <summary>
+        /// Setting the background color of the menu item if target platform is iOS.
+        /// </summary>
+        public Color BackgroundColor
+        {
+            get { return (Color)GetValue(BackgroundColorProperty); }
+            set { SetValue(BackgroundColorProperty, value); }
+        }
+
+        /// <summary>
+        /// Setting the text olor of the menu item if target platform is iOS.
+        /// </summary>
+        public Color TextColor
+        {
+            get { return (Color)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+
+        internal bool IsEnabled
 		{
 			get { return (bool)GetValue(IsEnabledProperty); }
 			set { SetValue(IsEnabledProperty, value); }
