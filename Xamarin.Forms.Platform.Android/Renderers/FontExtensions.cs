4a267c98cdc5f467a4fc0c66f2d12762db725513
Roy Vousten <r.vousten@hotmail.com>
2016-06-13 19:36:46 +0200

    Fixed Tabs

--- Xamarin.Forms.Platform.Android/Renderers/FontExtensions.cs
+++ Xamarin.Forms.Platform.Android/Renderers/FontExtensions.cs
@@ -7,101 +7,101 @@ using AApplication = Android.App.Application;
 
 namespace Xamarin.Forms.Platform.Android
 {
-    public static class FontExtensions
-    {
-        private static string[] SupportedFontExtensions = { ".ttf", ".otf" };
+	public static class FontExtensions
+	{
+		private static string[] SupportedFontExtensions = { ".ttf", ".otf" };
 
-        private static readonly Dictionary<Tuple<string, FontAttributes>, Typeface> Typefaces = new Dictionary<Tuple<string, FontAttributes>, Typeface>();
+		private static readonly Dictionary<Tuple<string, FontAttributes>, Typeface> Typefaces = new Dictionary<Tuple<string, FontAttributes>, Typeface>();
 
-        private static Typeface s_defaultTypeface;
+		private static Typeface s_defaultTypeface;
 
-        public static float ToScaledPixel(this Font self)
-        {
-            if (self.IsDefault)
-                return 14;
+		public static float ToScaledPixel(this Font self)
+		{
+			if (self.IsDefault)
+				return 14;
 
-            if (self.UseNamedSize)
-            {
-                switch (self.NamedSize)
-                {
-                    case NamedSize.Micro:
-                        return 10;
+			if (self.UseNamedSize)
+			{
+				switch (self.NamedSize)
+				{
+					case NamedSize.Micro:
+						return 10;
 
-                    case NamedSize.Small:
-                        return 12;
+					case NamedSize.Small:
+						return 12;
 
-                    case NamedSize.Default:
-                    case NamedSize.Medium:
-                        return 14;
+					case NamedSize.Default:
+					case NamedSize.Medium:
+						return 14;
 
-                    case NamedSize.Large:
-                        return 18;
-                }
-            }
+					case NamedSize.Large:
+						return 18;
+				}
+			}
 
-            return (float)self.FontSize;
-        }
+			return (float)self.FontSize;
+		}
 
-        public static Typeface ToTypeface(this Font self)
-        {
-            if (self.IsDefault)
-                return s_defaultTypeface ?? (s_defaultTypeface = Typeface.Default);
+		public static Typeface ToTypeface(this Font self)
+		{
+			if (self.IsDefault)
+				return s_defaultTypeface ?? (s_defaultTypeface = Typeface.Default);
 
-            var key = new Tuple<string, FontAttributes>(self.FontFamily, self.FontAttributes);
-            Typeface result;
-            if (Typefaces.TryGetValue(key, out result))
-                return result;
+			var key = new Tuple<string, FontAttributes>(self.FontFamily, self.FontAttributes);
+			Typeface result;
+			if (Typefaces.TryGetValue(key, out result))
+				return result;
 
-            var style = TypefaceStyle.Normal;
-            if ((self.FontAttributes & (FontAttributes.Bold | FontAttributes.Italic)) == (FontAttributes.Bold | FontAttributes.Italic))
-                style = TypefaceStyle.BoldItalic;
-            else if ((self.FontAttributes & FontAttributes.Bold) != 0)
-                style = TypefaceStyle.Bold;
-            else if ((self.FontAttributes & FontAttributes.Italic) != 0)
-                style = TypefaceStyle.Italic;
+			var style = TypefaceStyle.Normal;
+			if ((self.FontAttributes & (FontAttributes.Bold | FontAttributes.Italic)) == (FontAttributes.Bold | FontAttributes.Italic))
+				style = TypefaceStyle.BoldItalic;
+			else if ((self.FontAttributes & FontAttributes.Bold) != 0)
+				style = TypefaceStyle.Bold;
+			else if ((self.FontAttributes & FontAttributes.Italic) != 0)
+				style = TypefaceStyle.Italic;
 
-            if (SupportedFontExtensions.Any(f => self.FontFamily.EndsWith(f, StringComparison.InvariantCultureIgnoreCase)))
-                result = Typeface.CreateFromAsset(AApplication.Context.Assets, self.FontFamily);
-            else if (self.FontFamily != null)
-                result = Typeface.Create(self.FontFamily, style);
+			if (SupportedFontExtensions.Any(f => self.FontFamily.EndsWith(f, StringComparison.InvariantCultureIgnoreCase)))
+				result = Typeface.CreateFromAsset(AApplication.Context.Assets, self.FontFamily);
+			else if (self.FontFamily != null)
+				result = Typeface.Create(self.FontFamily, style);
 
-            if (result == null)
-                result = Typeface.Create(Typeface.Default, style);
+			if (result == null)
+				result = Typeface.Create(Typeface.Default, style);
 
-            Typefaces[key] = result;
-            return result;
-        }
+			Typefaces[key] = result;
+			return result;
+		}
 
-        internal static bool IsDefault(this IFontElement self)
-        {
-            return self.FontFamily == null && self.FontSize == Device.GetNamedSize(NamedSize.Default, typeof(Label), true) && self.FontAttributes == FontAttributes.None;
-        }
+		internal static bool IsDefault(this IFontElement self)
+		{
+			return self.FontFamily == null && self.FontSize == Device.GetNamedSize(NamedSize.Default, typeof(Label), true) && self.FontAttributes == FontAttributes.None;
+		}
 
-        internal static Typeface ToTypeface(this IFontElement self)
-        {
-            var key = new Tuple<string, FontAttributes>(self.FontFamily, self.FontAttributes);
-            Typeface result;
-            if (Typefaces.TryGetValue(key, out result))
-                return result;
+		internal static Typeface ToTypeface(this IFontElement self)
+		{
+			var key = new Tuple<string, FontAttributes>(self.FontFamily, self.FontAttributes);
+			Typeface result;
+			if (Typefaces.TryGetValue(key, out result))
+				return result;
 
-            var style = TypefaceStyle.Normal;
-            if ((self.FontAttributes & (FontAttributes.Bold | FontAttributes.Italic)) == (FontAttributes.Bold | FontAttributes.Italic))
-                style = TypefaceStyle.BoldItalic;
-            else if ((self.FontAttributes & FontAttributes.Bold) != 0)
-                style = TypefaceStyle.Bold;
-            else if ((self.FontAttributes & FontAttributes.Italic) != 0)
-                style = TypefaceStyle.Italic;
+			var style = TypefaceStyle.Normal;
+			if ((self.FontAttributes & (FontAttributes.Bold | FontAttributes.Italic)) == (FontAttributes.Bold | FontAttributes.Italic))
+				style = TypefaceStyle.BoldItalic;
+			else if ((self.FontAttributes & FontAttributes.Bold) != 0)
+				style = TypefaceStyle.Bold;
+			else if ((self.FontAttributes & FontAttributes.Italic) != 0)
+				style = TypefaceStyle.Italic;
 
-            if (SupportedFontExtensions.Any(f => self.FontFamily.EndsWith(f, StringComparison.InvariantCultureIgnoreCase)))
-                result = Typeface.CreateFromAsset(AApplication.Context.Assets, self.FontFamily);
-            else if (self.FontFamily != null)
-                result = Typeface.Create(self.FontFamily, style);
+			if (SupportedFontExtensions.Any(f => self.FontFamily.EndsWith(f, StringComparison.InvariantCultureIgnoreCase)))
+				result = Typeface.CreateFromAsset(AApplication.Context.Assets, self.FontFamily);
+			else if (self.FontFamily != null)
+				result = Typeface.Create(self.FontFamily, style);
 
-            if (result == null)
-                result = Typeface.Create(Typeface.Default, style);
+			if (result == null)
+				result = Typeface.Create(Typeface.Default, style);
 
-            Typefaces[key] = result;
-            return result;
-        }
-    }
+			Typefaces[key] = result;
+			return result;
+		}
+	}
 }
