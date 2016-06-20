e445ca289cedfdb4c7306815864cfe6adcb45275
scastria <shawn@spcware.com>
2016-06-20 16:04:57 -0500

    Add DataTemplate extensions to allow typesafe binding just like BindableObject

---
+++ Xamarin.Forms.Core.UnitTests/DataTemplateExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Core.UnitTests
+{
+	[TestFixture]
+	public class DataTemplateExtensionTests : BaseTestFixture
+	{
+		[Test]
+		public void SetBindingNull()
+		{
+			Assert.That(() => DataTemplateExtensions.SetBinding(null, MockBindable.TextProperty, "Name"),
+				Throws.InstanceOf<ArgumentNullException>());
+			Assert.That(() => DataTemplateExtensions.SetBinding(new DataTemplate(() => new MockBindable()), null, "Name"),
+				Throws.InstanceOf<ArgumentNullException>());
+			Assert.That(() => DataTemplateExtensions.SetBinding(new DataTemplate(() => new MockBindable()), MockBindable.TextProperty, null),
+				Throws.InstanceOf<ArgumentNullException>());
+
+			Assert.That(() => DataTemplateExtensions.SetBinding<MockViewModel>(null, MockBindable.TextProperty, vm => vm.Text),
+				Throws.InstanceOf<ArgumentNullException>());
+			Assert.That(() => DataTemplateExtensions.SetBinding<MockViewModel>(new DataTemplate(() => new MockBindable()), null, vm => vm.Text),
+				Throws.InstanceOf<ArgumentNullException>());
+			Assert.That(() => DataTemplateExtensions.SetBinding<MockViewModel>(new DataTemplate(() => new MockBindable()), MockBindable.TextProperty, null),
+				Throws.InstanceOf<ArgumentNullException>());
+		}
+	}
+}
--- Xamarin.Forms.Core/DataTemplateExtensions.cs
+++ Xamarin.Forms.Core/DataTemplateExtensions.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Linq.Expressions;
+
 namespace Xamarin.Forms
 {
-	internal static class DataTemplateExtensions
+	public static class DataTemplateExtensions
 	{
-		public static object CreateContent(this DataTemplate self, object item, BindableObject container)
+		internal static object CreateContent(this DataTemplate self, object item, BindableObject container)
 		{
 			var selector = self as DataTemplateSelector;
 			if (selector != null)
@@ -10,6 +13,32 @@ namespace Xamarin.Forms
 				self = selector.SelectTemplate(item, container);
 			}
 			return self.CreateContent();
+		}
+
+		public static void SetBinding(this DataTemplate self, BindableProperty targetProperty, string path, BindingMode mode = BindingMode.Default, IValueConverter converter = null,
+							  string stringFormat = null)
+		{
+			if (self == null)
+				throw new ArgumentNullException("self");
+			if (targetProperty == null)
+				throw new ArgumentNullException("targetProperty");
+
+			var binding = new Binding(path, mode, converter, stringFormat: stringFormat);
+			self.SetBinding(targetProperty, binding);
+		}
+
+		public static void SetBinding<TSource>(this DataTemplate self, BindableProperty targetProperty, Expression<Func<TSource, object>> sourceProperty, BindingMode mode = BindingMode.Default,
+											   IValueConverter converter = null, string stringFormat = null)
+		{
+			if (self == null)
+				throw new ArgumentNullException("self");
+			if (targetProperty == null)
+				throw new ArgumentNullException("targetProperty");
+			if (sourceProperty == null)
+				throw new ArgumentNullException("sourceProperty");
+
+			Binding binding = Binding.Create(sourceProperty, mode, converter, stringFormat: stringFormat);
+			self.SetBinding(targetProperty, binding);
 		}
 	}
 }
