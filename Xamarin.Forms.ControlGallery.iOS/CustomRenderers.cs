9d6e79d80179fdf9a8b408ea2a1ed91ff08bcd8b
Samantha Houts <samantha.houts@xamarin.com>
2016-06-20 16:26:12 -0700

    [Controls] Improve TabbedPage test case
    iOS should default to the color used in a custom renderer instead of to
    the global default color.

--- Xamarin.Forms.ControlGallery.iOS/CustomRenderers.cs
+++ Xamarin.Forms.ControlGallery.iOS/CustomRenderers.cs
@@ -27,7 +27,7 @@ using MonoTouch.CoreLocation;
 [assembly: ExportRenderer(typeof(NativeListView2), typeof(NativeiOSListViewRenderer))]
 [assembly: ExportRenderer(typeof(NativeListView), typeof(NativeListViewRenderer))]
 [assembly: ExportRenderer(typeof(CustomMapView), typeof(CustomIOSMapRenderer))]
-
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(TabbedPageWithCustomBarColorRenderer))]
 namespace Xamarin.Forms.ControlGallery.iOS
 {
 	public class CustomIOSMapRenderer : ViewRenderer<CustomMapView, MKMapView>
@@ -588,5 +588,16 @@ namespace Xamarin.Forms.ControlGallery.iOS
 		}
 	}
 
+	public class TabbedPageWithCustomBarColorRenderer : TabbedRenderer
+	{
+		public TabbedPageWithCustomBarColorRenderer()
+		{
+			TabBar.TintColor = UIColor.White;
+			TabBar.BarTintColor = UIColor.Purple;
+
+			//UITabBar.Appearance.TintColor = UIColor.White;
+			//UITabBar.Appearance.BarTintColor = UIColor.Purple;
+		}
+	}
 }
 
