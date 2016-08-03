4a55504d845a593863c7f3b6b336cc0f82349b97
E.Z. Hart <hartez@gmail.com>
2016-08-03 15:16:43 -0600

    Add test to demonstrate that the solution works even if <head> isn't in the HTML string

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla32033.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla32033.cs
@@ -24,8 +24,12 @@ namespace Xamarin.Forms.Controls
 			var buttonHtmlString = new Button() {Text = "HTML string with links/refs to local files"};
 			buttonHtmlString.Clicked += (sender, args) => Navigation.PushAsync(HtmlString());
 
+			var buttonHtmlStringNoHead = new Button() {Text = "HTML string with links/refs to local files (no <head>)"};
+			buttonHtmlStringNoHead.Clicked += (sender, args) => Navigation.PushAsync(HtmlStringNoHead());
+
 			layout.Children.Add(buttonLocal);
 			layout.Children.Add(buttonHtmlString);
+			layout.Children.Add(buttonHtmlStringNoHead);
 
 			page.Content = layout;
 
@@ -79,6 +83,44 @@ Clicking that link should navigate to a page with the heading 'Xamarin Forms' an
 <head>
 <link rel=""stylesheet"" href=""default.css"">
 </head>
+<body>
+<h1>Xamarin.Forms</h1>
+<p>The CSS and image are loaded from local files!</p>
+<img src='WebImages/XamarinLogo.png'/>
+<p><a href=""local.html"">next page</a></p>
+</body>
+</html>"
+				}
+			};
+
+
+			var layout = new StackLayout {HorizontalOptions = LayoutOptions.Fill, VerticalOptions = LayoutOptions.Fill,  Children = { instructions, webView } };
+			page.Content = layout;
+
+			return page;
+		}
+
+		// This test verifies that the <base> injection solution works even if the HTML string doesn't explicitly include a <head> section
+		static ContentPage HtmlStringNoHead()
+		{
+			var page = new ContentPage();
+
+			var instructions = new Label
+			{
+				Text =
+@"The WebView below should contain the heading 'Xamarin Forms', display the Xamarin logo, and have a link labeled 'next page'.
+Clicking that link should navigate to a page with the heading 'Xamarin Forms' and text reading 'This is a local HTML page'. "
+			};
+
+			var webView = new WebView
+			{
+				WidthRequest = 300,
+				HeightRequest = 500,
+				HorizontalOptions = LayoutOptions.Fill,
+				VerticalOptions = LayoutOptions.Fill,
+				Source = new HtmlWebViewSource
+				{
+					Html = @"<html>
 <body>
 <h1>Xamarin.Forms</h1>
 <p>The CSS and image are loaded from local files!</p>
