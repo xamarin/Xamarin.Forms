d42246b2a8bee4a4f7f8066ed84e07cceb0ff06f
Prashant Cholachagudda <pvc@outlook.com>
2016-08-05 21:39:55 +0530

    Fix bugzilla41559
    # Description of Change
    
    Added a “Done” button to dismiss the Numeric keyboard on iPhone/iPod
    
    # Bugs Fixed
    https://bugzilla.xamarin.com/show_bug.cgi?id=41559
    
    # API Changes
    None.
    
    # PR Checklist
    [x] Has tests (if omitted, state reason in description)
    [] Rebased on top of master at time of PR
    [x] Changes adhere to coding standard
    [] Consolidate commits as makes sense

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzill41559.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Bugzilla, 41559, "In iOS Entry.keyboard=Keyboard.Numeric does not have a done button")]
+	public class Bugzill41559 : TestContentPage
+	{
+		protected override void Init()
+		{
+			var numericEntry = new Entry { Keyboard = Keyboard.Numeric, AutomationId = "NumericEntry" };
+			Content = new StackLayout
+			{
+				Children = {
+					numericEntry
+				}
+			};
+
+			numericEntry.Focus();
+		}
+
+#if UITEST
+		[Test]
+		public void Bugzilla41559Test ()
+		{
+			RunningApp.Tap("NumericEntry");
+			var buttonCount = RunningApp.Query("Done").Count();
+			Assert.GreaterOrEqual(buttonCount, 1, "Done keyboard button is not added");
+		}
+#endif
+	}
+}
+
+
