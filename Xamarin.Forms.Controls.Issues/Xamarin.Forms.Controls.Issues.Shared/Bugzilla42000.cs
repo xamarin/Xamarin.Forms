ee35592c533e9f61bb0712e182b0418aac6e374e
Adrian Knight <adrianknight89@outlook.com>
2016-09-29 21:29:38 -0500

    Adding sample code

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla42000.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.TestCasesPages
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 42000, "Unable to use comma (\", \") as decimal point", PlatformAffected.Android)]
+	public class Bugzilla42000 : ContentPage
+	{
+		public Bugzilla42000()
+		{
+			var flag = false;
+
+			var entry = new Entry { Keyboard = Keyboard.Numeric };
+
+			var button = new Button
+			{
+				Text = "Change keyboard type"
+			};
+			button.Clicked += (sender, args) =>
+			{
+				entry.Keyboard = !flag ? Keyboard.Chat : Keyboard.Numeric;
+
+				flag = !flag;
+			};
+
+			Content = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Children =
+				{
+					entry,
+					button
+				}
+			};
+		}
+	}
+}
