96898864b029dec478d989dc1024bd8dd65756bb
Adrian Knight <adrianknight89@outlook.com>
2016-09-30 21:44:52 -0500

    Mirroring iOS UIBarButtonSystemItem

---
+++ Xamarin.Forms.Core/PlatformConfiguration/iOSSpecific/UIBarButtonSystemItem.cs
@@ -0,0 +1,30 @@
+﻿namespace Xamarin.Forms.PlatformConfiguration.iOSSpecific
+{
+	public enum UIBarButtonSystemItem : long
+	{
+		Done = 0,
+		Cancel = 1,
+		Edit = 2,
+		Save = 3,
+		Add = 4,
+		FlexibleSpace = 5,
+		FixedSpace = 6,
+		Compose = 7,
+		Reply = 8,
+		Action = 9,
+		Organize = 10,
+		Bookmarks = 11,
+		Search = 12,
+		Refresh = 13,
+		Stop = 14,
+		Camera = 15,
+		Trash = 16,
+		Play = 17,
+		Pause = 18,
+		Rewind = 19,
+		FastForward = 20,
+		Undo = 21,
+		Redo = 22,
+		PageCurl = 23
+	}
+}
