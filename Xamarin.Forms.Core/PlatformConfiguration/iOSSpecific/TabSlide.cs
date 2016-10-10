6a541a4d9bfde775da074e0967d17518e256bc11
Adrian Knight <adrianknight89@outlook.com>
2016-10-09 19:10:36 -0500

    Adding models to set tab slide on the tabbed page

---
+++ Xamarin.Forms.Core/PlatformConfiguration/iOSSpecific/TabSlide.cs
@@ -0,0 +1,43 @@
+﻿namespace Xamarin.Forms.PlatformConfiguration.iOSSpecific
+{
+	public class TabSlide
+	{
+		public SlideConfig LeftSlideConfig { get; set; }
+		public SlideConfig RightSlideConfig { get; set; }
+
+		public TabSlide()
+		{
+			LeftSlideConfig = new SlideConfig();
+			RightSlideConfig = new SlideConfig();
+		}
+
+		public TabSlide(SlideConfig leftSlideConfig, SlideConfig rightSlideConfig)
+		{
+			LeftSlideConfig = leftSlideConfig;
+			RightSlideConfig = rightSlideConfig;
+		}
+	}
+
+	public class SlideConfig
+	{
+		public bool IsEnabled { get; set; }
+
+		public double Duration { get; set; }
+
+		public double Velocity { get; set; }
+
+		public SlideConfig()
+		{
+			IsEnabled = true;
+			Duration = .3;
+			Velocity = 1000;
+		}
+
+		public SlideConfig(bool isEnabled, double duration, double velocity)
+		{
+			IsEnabled = isEnabled;
+			Duration = duration;
+			Velocity = velocity;
+		}
+	}
+}
---
+++ Xamarin.Forms.Core/PlatformConfiguration/iOSSpecific/TabbedPage.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace Xamarin.Forms.PlatformConfiguration.iOSSpecific
+{
+	using FormsElement = Forms.TabbedPage;
+
+	public static class TabbedPage
+	{
+		public static readonly BindableProperty TabSlidesProperty = BindableProperty.Create(nameof(TabSlides), typeof(Dictionary<int, TabSlide>), typeof(TabbedPage));
+
+		public static Dictionary<int, TabSlide> GetTabSlides(BindableObject element)
+		{
+			return (Dictionary<int, TabSlide>)element.GetValue(TabSlidesProperty);
+		}
+
+		public static void SetTabSlides(BindableObject element, Dictionary<int, TabSlide> value)
+		{
+			element.SetValue(TabSlidesProperty, value);
+		}
+
+		public static Dictionary<int, TabSlide> TabSlides(this IPlatformElementConfiguration<iOS, FormsElement> config)
+		{
+			return GetTabSlides(config.Element);
+		}
+
+		public static IPlatformElementConfiguration<iOS, FormsElement> SetTabSlides(this IPlatformElementConfiguration<iOS, FormsElement> config, Dictionary<int, TabSlide> value)
+		{
+			SetTabSlides(config.Element, value);
+			return config;
+		}
+
+		public static IPlatformElementConfiguration<iOS, FormsElement> EnableTabSlides(this IPlatformElementConfiguration<iOS, FormsElement> config)
+		{
+			Dictionary<int, TabSlide> tabSlides = GetTabSlides(config.Element);
+			if (tabSlides == null || tabSlides.Count == 0)
+			{
+				if (tabSlides == null)
+				{
+					tabSlides = new Dictionary<int, TabSlide>();
+				}
+
+				for (var i = 0; i < config.Element.Children.Count; i++)
+				{
+					tabSlides.Add(i, new TabSlide());
+				}
+			}
+
+			SetTabSlides(config.Element, tabSlides);
+			return config;
+		}
+
+		public static IPlatformElementConfiguration<iOS, FormsElement> DisableTabSlides(this IPlatformElementConfiguration<iOS, FormsElement> config)
+		{
+			SetTabSlides(config.Element, null);
+			return config;
+		}
+	}
+}
