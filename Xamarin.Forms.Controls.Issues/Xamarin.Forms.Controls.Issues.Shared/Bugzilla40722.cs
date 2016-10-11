2a7a2084d6c25ec0d643707c9e28f3f9bd5524a8
Samantha Houts <samantha.houts@xamarin.com>
2016-10-11 15:08:59 -0700

    Add reproduction for 40722

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40722.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 40722, "Using FormsAppCompatActivity calls OnDisappearing on device sleep")]
+	public class Bugzilla40722 : TestContentPage 
+	{
+		protected override void Init()
+		{
+			Content = new Label
+			{
+				AutomationId = "IssuePageLabel",
+				Text = "Sleep the device. If \"Disappearing!\" is displayed in the Output debug window, this test has failed."
+			};
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			System.Diagnostics.Debug.WriteLine("Appearing!");
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+
+			System.Diagnostics.Debug.WriteLine("Disappearing!");
+		}
+	}
+}
