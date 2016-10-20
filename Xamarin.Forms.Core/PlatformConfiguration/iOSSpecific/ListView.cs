837cbe65c6cfba1301ac0bced10a325237512d5b
Adrian Knight <adrianknight89@outlook.com>
2016-10-20 15:35:41 -0500

    changes after review

--- Xamarin.Forms.Core/PlatformConfiguration/iOSSpecific/ListView.cs
+++ Xamarin.Forms.Core/PlatformConfiguration/iOSSpecific/ListView.cs
@@ -4,38 +4,26 @@
 
 	public static class ListView
 	{
-		public static readonly BindableProperty BouncesProperty = BindableProperty.Create(nameof(Bounces), typeof(bool), typeof(ListView), true);
+		public static readonly BindableProperty IsBounceEnabledProperty = BindableProperty.Create(nameof(IsBounceEnabled), typeof(bool), typeof(ListView), true);
 
-		public static bool GetBounces(BindableObject element)
+		public static bool GetIsBounceEnabled(BindableObject element)
 		{
-			return (bool)element.GetValue(BouncesProperty);
+			return (bool)element.GetValue(IsBounceEnabledProperty);
 		}
 
-		public static void SetBounces(BindableObject element, bool value)
+		public static void SetIsBounceEnabled(BindableObject element, bool value)
 		{
-			element.SetValue(BouncesProperty, value);
+			element.SetValue(IsBounceEnabledProperty, value);
 		}
 
-		public static bool Bounces(this IPlatformElementConfiguration<iOS, FormsElement> config)
+		public static bool IsBounceEnabled(this IPlatformElementConfiguration<iOS, FormsElement> config)
 		{
-			return GetBounces(config.Element);
+			return GetIsBounceEnabled(config.Element);
 		}
 
-		public static IPlatformElementConfiguration<iOS, FormsElement> SetBounces(this IPlatformElementConfiguration<iOS, FormsElement> config, bool value)
+		public static IPlatformElementConfiguration<iOS, FormsElement> SetIsBounceEnabled(this IPlatformElementConfiguration<iOS, FormsElement> config, bool value)
 		{
-			SetBounces(config.Element, value);
-			return config;
-		}
-
-		public static IPlatformElementConfiguration<iOS, FormsElement> EnableBounces(this IPlatformElementConfiguration<iOS, FormsElement> config)
-		{
-			SetBounces(config.Element, true);
-			return config;
-		}
-
-		public static IPlatformElementConfiguration<iOS, FormsElement> DisableBounces(this IPlatformElementConfiguration<iOS, FormsElement> config)
-		{
-			SetBounces(config.Element, false);
+			SetIsBounceEnabled(config.Element, value);
 			return config;
 		}
 	}
