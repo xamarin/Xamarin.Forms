694fa4001ded5702dda2ddb6f57bb5443fc35819
Adrian Knight <adrianknight89@outlook.com>
2016-10-22 17:16:03 -0500

    ui test for isenabled

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla44096.cs
@@ -0,0 +1,123 @@
+﻿using System.Linq;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 44096, "Grid, StackLayout, and ContentView still participate in hit testing on Android after IsEnabled is set to false", PlatformAffected.Android)]
+	public class Bugzilla44096 : TestContentPage
+	{
+		protected override void Init()
+		{
+			var result = new Label
+			{
+				Text = "Success"
+			};
+
+			var grid = new Grid
+			{
+				IsEnabled = false,
+				WidthRequest = 250,
+				HeightRequest = 50,
+				AutomationId = "grid"
+			};
+			AddTapGesture(result, grid);
+
+			var contentView = new ContentView
+			{
+				IsEnabled = false,
+				WidthRequest = 250,
+				HeightRequest = 50,
+				AutomationId = "contentView"
+			};
+			AddTapGesture(result, contentView);
+
+			var stackLayout = new StackLayout
+			{
+				IsEnabled = false,
+				WidthRequest = 250,
+				HeightRequest = 50,
+				AutomationId = "stackLayout"
+			};
+			AddTapGesture(result, stackLayout);
+
+			var color = new Button
+			{
+				Text = "Add colors",
+				Command = new Command(() =>
+				{
+					grid.BackgroundColor = Color.Red;
+					contentView.BackgroundColor = Color.Blue;
+					stackLayout.BackgroundColor = Color.Yellow;
+				}),
+				AutomationId = "color"
+			};
+
+			var parent = new StackLayout
+			{
+				Spacing = 10,
+				Orientation = StackOrientation.Vertical,
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Children =
+				{
+					color,
+					result,
+					grid,
+					contentView,
+					stackLayout
+				}
+			};
+
+			Content = parent;
+		}
+
+		void AddTapGesture(Label result, View view)
+		{
+			var tapGestureRecognizer = new TapGestureRecognizer
+			{
+				Command = new Command(() => { result.Text = "Fail"; })
+			};
+			view.GestureRecognizers.Add(tapGestureRecognizer);
+		}
+
+#if UITEST
+		[Test]
+		public void Test()
+		{
+			RunningApp.WaitForElement(q => q.Marked("grid"));
+			RunningApp.Tap(q => q.Marked("grid"));
+			RunningApp.WaitForElement(q => q.Marked("Success"));
+
+			RunningApp.WaitForElement(q => q.Marked("contentView"));
+			RunningApp.Tap(q => q.Marked("contentView"));
+			RunningApp.WaitForElement(q => q.Marked("Success"));
+
+			RunningApp.WaitForElement(q => q.Marked("stackLayout"));
+			RunningApp.Tap(q => q.Marked("stackLayout"));
+			RunningApp.WaitForElement(q => q.Marked("Success"));
+
+			RunningApp.WaitForElement(q => q.Marked("color"));
+			RunningApp.Tap(q => q.Marked("color"));
+
+			RunningApp.WaitForElement(q => q.Marked("grid"));
+			RunningApp.Tap(q => q.Marked("grid"));
+			RunningApp.WaitForElement(q => q.Marked("Success"));
+
+			RunningApp.WaitForElement(q => q.Marked("contentView"));
+			RunningApp.Tap(q => q.Marked("contentView"));
+			RunningApp.WaitForElement(q => q.Marked("Success"));
+
+			RunningApp.WaitForElement(q => q.Marked("stackLayout"));
+			RunningApp.Tap(q => q.Marked("stackLayout"));
+			RunningApp.WaitForElement(q => q.Marked("Success"));
+		}
+#endif
+	}
+}
