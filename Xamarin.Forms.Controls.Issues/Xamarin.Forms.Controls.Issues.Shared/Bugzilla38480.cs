1c7a4f6295de72a40a5458e2917cf42ebf251a06
Adrian Knight <adrianknight89@outlook.com>
2016-11-01 21:19:09 -0500

    add sample code

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla38480.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 38480, "How to disable cut,copy, paste options for textFields in Xamarin.forms", PlatformAffected.iOS)]
+	public class Bugzilla38480 : TestContentPage
+	{
+		protected override void Init()
+		{
+			// Initialize ui here instead of ctor
+			Content = new Label
+			{
+				AutomationId = "IssuePageLabel",
+				Text = "See if I'm here"
+			};
+		}
+	}
+}
