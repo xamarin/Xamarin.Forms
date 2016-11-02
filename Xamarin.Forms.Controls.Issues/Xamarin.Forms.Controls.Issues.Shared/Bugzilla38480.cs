7eeafae8be1536de557302becb224893f10311c1
Adrian Knight <adrianknight89@outlook.com>
2016-11-01 21:45:50 -0500

    added sample code

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla38480.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla38480.cs
@@ -1,5 +1,8 @@
-﻿using Xamarin.Forms.CustomAttributes;
+﻿using System.Collections.Generic;
+using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 #if UITEST
 using Xamarin.UITest;
@@ -14,12 +17,162 @@ namespace Xamarin.Forms.Controls
 	{
 		protected override void Init()
 		{
-			// Initialize ui here instead of ctor
-			Content = new Label
+			var scrollView = new ScrollView();
+			var stackLayout = new StackLayout
 			{
-				AutomationId = "IssuePageLabel",
-				Text = "See if I'm here"
+				Orientation = StackOrientation.Vertical,
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Spacing = 15
 			};
+
+			var datePicker = new Xamarin.Forms.DatePicker
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+
+			var picker = new Xamarin.Forms.Picker
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+			picker.Items.Add("item1");
+			picker.Items.Add("item2");
+			picker.Items.Add("item3");
+			picker.Items.Add("item4");
+			picker.Items.Add("item5");
+
+			var timePicker = new Xamarin.Forms.TimePicker
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+
+
+			var entry = new Xamarin.Forms.Entry
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+
+			var button = new Button
+			{
+				Text = "Change",
+				Command = new Command(() => { Change(datePicker, picker, timePicker, entry); })
+			};
+
+			stackLayout.Children.Add(button);
+			stackLayout.Children.Add(datePicker);
+			stackLayout.Children.Add(picker);
+			stackLayout.Children.Add(timePicker);
+			stackLayout.Children.Add(entry);
+
+			SetInitialValues(datePicker, picker, timePicker, entry);
+			scrollView.Content = stackLayout;
+
+			Content = scrollView;
+		}
+
+		void SetInitialValues(Xamarin.Forms.DatePicker datePicker, Xamarin.Forms.Picker picker, Xamarin.Forms.TimePicker timePicker, Xamarin.Forms.Entry entry)
+		{
+			datePicker.On<iOS>().SetDisabledSelectorActions(new List<SelectorAction>
+			{
+				SelectorAction.AddShortcut,
+				SelectorAction.Cut,
+				SelectorAction.Copy,
+				SelectorAction.Define,
+				SelectorAction.Delete,
+				SelectorAction.Lookup,
+				SelectorAction.Select,
+				SelectorAction.SelectAll,
+				SelectorAction.Replace,
+				SelectorAction.Share,
+				SelectorAction.Paste
+			});
+
+			picker.On<iOS>().SetDisabledSelectorActions(new List<SelectorAction>
+			{
+				SelectorAction.AddShortcut,
+				SelectorAction.Cut,
+				SelectorAction.Copy,
+				SelectorAction.Define,
+				SelectorAction.Delete,
+				SelectorAction.Lookup,
+				SelectorAction.Select,
+				SelectorAction.SelectAll,
+				SelectorAction.Replace,
+				SelectorAction.Share,
+				SelectorAction.Paste
+			});
+
+			timePicker.On<iOS>().SetDisabledSelectorActions(new List<SelectorAction>
+			{
+				SelectorAction.AddShortcut,
+				SelectorAction.Cut,
+				SelectorAction.Copy,
+				SelectorAction.Define,
+				SelectorAction.Delete,
+				SelectorAction.Lookup,
+				SelectorAction.Select,
+				SelectorAction.SelectAll,
+				SelectorAction.Replace,
+				SelectorAction.Share,
+				SelectorAction.Paste
+			});
+
+			entry.On<iOS>().SetDisabledSelectorActions(new List<SelectorAction>
+			{
+				SelectorAction.AddShortcut,
+				SelectorAction.Cut,
+				SelectorAction.Copy,
+				SelectorAction.Define,
+				SelectorAction.Delete,
+				SelectorAction.Lookup,
+				SelectorAction.Select,
+				SelectorAction.SelectAll,
+				SelectorAction.Replace,
+				SelectorAction.Share,
+				SelectorAction.Paste
+			});
+		}
+
+		void Change(Xamarin.Forms.DatePicker datePicker, Xamarin.Forms.Picker picker, Xamarin.Forms.TimePicker timePicker, Xamarin.Forms.Entry entry)
+		{
+			datePicker.On<iOS>().SetDisabledSelectorActions(new List<SelectorAction>
+			{
+				SelectorAction.AddShortcut,
+				SelectorAction.Cut,
+				SelectorAction.Copy
+			});
+
+			picker.On<iOS>().SetDisabledSelectorActions(new List<SelectorAction>
+			{
+				SelectorAction.Cut,
+				SelectorAction.Replace,
+				SelectorAction.Share,
+				SelectorAction.Paste
+			});
+
+			timePicker.On<iOS>().SetDisabledSelectorActions(new List<SelectorAction>
+			{
+				SelectorAction.Copy,
+				SelectorAction.Define,
+				SelectorAction.Delete,
+				SelectorAction.Replace,
+				SelectorAction.Share,
+				SelectorAction.Paste
+			});
+
+			entry.On<iOS>().SetDisabledSelectorActions(new List<SelectorAction>
+			{
+				SelectorAction.AddShortcut,
+				SelectorAction.Cut,
+				SelectorAction.Lookup, // not the same as "Look Up" which happens to be Define on iOS 10.
+				SelectorAction.Select,
+				SelectorAction.SelectAll,
+				SelectorAction.Share
+			});
 		}
 	}
 }
