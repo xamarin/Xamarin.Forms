fd1294529c1782d8938068ab1e493d32d2cf8b5e
Adrian Knight <adrianknight89@outlook.com>
2016-11-17 00:42:53 -0600

    picker selected index could change when picker view is dismissed

---
+++ Xamarin.Forms.Core/PlatformConfiguration/iOSSpecific/Picker.cs
@@ -0,0 +1,30 @@
+﻿namespace Xamarin.Forms.PlatformConfiguration.iOSSpecific
+{
+	using FormsElement = Forms.Picker;
+
+	public static class Picker
+	{
+		public static readonly BindableProperty ShouldChangeSelectedIndexWhenDoneProperty = BindableProperty.Create(nameof(ShouldChangeSelectedIndexWhenDone), typeof(bool), typeof(Picker), false);
+
+		public static bool GetShouldChangeSelectedIndexWhenDone(BindableObject element)
+		{
+			return (bool)element.GetValue(ShouldChangeSelectedIndexWhenDoneProperty);
+		}
+
+		public static void SetShouldChangeSelectedIndexWhenDone(BindableObject element, bool value)
+		{
+			element.SetValue(ShouldChangeSelectedIndexWhenDoneProperty, value);
+		}
+
+		public static bool ShouldChangeSelectedIndexWhenDone(this IPlatformElementConfiguration<iOS, FormsElement> config)
+		{
+			return GetShouldChangeSelectedIndexWhenDone(config.Element);
+		}
+
+		public static IPlatformElementConfiguration<iOS, FormsElement> SetShouldChangeSelectedIndexWhenDone(this IPlatformElementConfiguration<iOS, FormsElement> config, bool value)
+		{
+			SetShouldChangeSelectedIndexWhenDone(config.Element, value);
+			return config;
+		}
+	}
+}
