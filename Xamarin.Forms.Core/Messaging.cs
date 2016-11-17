bf62bd84f22e5128645fe53febcab5e534f69869
Adrian Knight <adrianknight89@outlook.com>
2016-11-17 01:07:35 -0600

    Add Messaging

--- Xamarin.Forms.Core/Messaging.cs
+++ Xamarin.Forms.Core/Messaging.cs
@@ -1,35 +1,41 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Xamarin.Forms
 {
-	[Obsolete("MessagingCenter is obsolete in favor of Messaging.")]
-	public static class MessagingCenter
+	public class Messaging : IMessaging
 	{
-		static readonly Dictionary<Tuple<string, Type, Type>, List<Tuple<WeakReference, Action<object, object>>>> s_callbacks =
-			new Dictionary<Tuple<string, Type, Type>, List<Tuple<WeakReference, Action<object, object>>>>();
+		readonly Dictionary<Tuple<string, Type, Type>, List<Tuple<WeakReference, Action<object, object>>>> _callbacks = new Dictionary<Tuple<string, Type, Type>, List<Tuple<WeakReference, Action<object, object>>>>();
 
-		public static void Send<TSender, TArgs>(TSender sender, string message, TArgs args) where TSender : class
+		static readonly Lazy<Messaging> s_lazy = new Lazy<Messaging>(() => new Messaging());
+
+		public static Messaging Instance => s_lazy.Value;
+
+		Messaging()
+		{
+		}
+
+		public void Send<TSender, TArgs>(TSender sender, string message, TArgs args) where TSender : class
 		{
 			if (sender == null)
-				throw new ArgumentNullException("sender");
+				throw new ArgumentNullException(nameof(sender));
 			InnerSend(message, typeof(TSender), typeof(TArgs), sender, args);
 		}
 
-		public static void Send<TSender>(TSender sender, string message) where TSender : class
+		public void Send<TSender>(TSender sender, string message) where TSender : class
 		{
 			if (sender == null)
-				throw new ArgumentNullException("sender");
+				throw new ArgumentNullException(nameof(sender));
 			InnerSend(message, typeof(TSender), null, sender, null);
 		}
 
-		public static void Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source = null) where TSender : class
+		public void Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source = null) where TSender : class
 		{
 			if (subscriber == null)
-				throw new ArgumentNullException("subscriber");
+				throw new ArgumentNullException(nameof(subscriber));
 			if (callback == null)
-				throw new ArgumentNullException("callback");
+				throw new ArgumentNullException(nameof(callback));
 
 			Action<object, object> wrap = (sender, args) =>
 			{
@@ -41,12 +47,12 @@ namespace Xamarin.Forms
 			InnerSubscribe(subscriber, message, typeof(TSender), typeof(TArgs), wrap);
 		}
 
-		public static void Subscribe<TSender>(object subscriber, string message, Action<TSender> callback, TSender source = null) where TSender : class
+		public void Subscribe<TSender>(object subscriber, string message, Action<TSender> callback, TSender source = null) where TSender : class
 		{
 			if (subscriber == null)
-				throw new ArgumentNullException("subscriber");
+				throw new ArgumentNullException(nameof(subscriber));
 			if (callback == null)
-				throw new ArgumentNullException("callback");
+				throw new ArgumentNullException(nameof(callback));
 
 			Action<object, object> wrap = (sender, args) =>
 			{
@@ -58,29 +64,29 @@ namespace Xamarin.Forms
 			InnerSubscribe(subscriber, message, typeof(TSender), null, wrap);
 		}
 
-		public static void Unsubscribe<TSender, TArgs>(object subscriber, string message) where TSender : class
+		public void Unsubscribe<TSender, TArgs>(object subscriber, string message) where TSender : class
 		{
 			InnerUnsubscribe(message, typeof(TSender), typeof(TArgs), subscriber);
 		}
 
-		public static void Unsubscribe<TSender>(object subscriber, string message) where TSender : class
+		public void Unsubscribe<TSender>(object subscriber, string message) where TSender : class
 		{
 			InnerUnsubscribe(message, typeof(TSender), null, subscriber);
 		}
 
-		internal static void ClearSubscribers()
+		internal void ClearSubscribers()
 		{
-			s_callbacks.Clear();
+			_callbacks.Clear();
 		}
 
-		static void InnerSend(string message, Type senderType, Type argType, object sender, object args)
+		void InnerSend(string message, Type senderType, Type argType, object sender, object args)
 		{
 			if (message == null)
-				throw new ArgumentNullException("message");
+				throw new ArgumentNullException(nameof(message));
 			var key = new Tuple<string, Type, Type>(message, senderType, argType);
-			if (!s_callbacks.ContainsKey(key))
+			if (!_callbacks.ContainsKey(key))
 				return;
-			List<Tuple<WeakReference, Action<object, object>>> actions = s_callbacks[key];
+			List<Tuple<WeakReference, Action<object, object>>> actions = _callbacks[key];
 			if (actions == null || !actions.Any())
 				return; // should not be reachable
 
@@ -92,41 +98,41 @@ namespace Xamarin.Forms
 			List<Tuple<WeakReference, Action<object, object>>> actionsCopy = actions.ToList();
 			foreach (Tuple<WeakReference, Action<object, object>> action in actionsCopy)
 			{
-				if (action.Item1.Target != null && actions.Contains(action))
+				if (action.Item1.IsAlive && actions.Contains(action))
 					action.Item2(sender, args);
 			}
 		}
 
-		static void InnerSubscribe(object subscriber, string message, Type senderType, Type argType, Action<object, object> callback)
+		void InnerSubscribe(object subscriber, string message, Type senderType, Type argType, Action<object, object> callback)
 		{
 			if (message == null)
-				throw new ArgumentNullException("message");
+				throw new ArgumentNullException(nameof(message));
 			var key = new Tuple<string, Type, Type>(message, senderType, argType);
 			var value = new Tuple<WeakReference, Action<object, object>>(new WeakReference(subscriber), callback);
-			if (s_callbacks.ContainsKey(key))
+			if (_callbacks.ContainsKey(key))
 			{
-				s_callbacks[key].Add(value);
+				_callbacks[key].Add(value);
 			}
 			else
 			{
 				var list = new List<Tuple<WeakReference, Action<object, object>>> { value };
-				s_callbacks[key] = list;
+				_callbacks[key] = list;
 			}
 		}
 
-		static void InnerUnsubscribe(string message, Type senderType, Type argType, object subscriber)
+		void InnerUnsubscribe(string message, Type senderType, Type argType, object subscriber)
 		{
 			if (subscriber == null)
-				throw new ArgumentNullException("subscriber");
+				throw new ArgumentNullException(nameof(subscriber));
 			if (message == null)
-				throw new ArgumentNullException("message");
+				throw new ArgumentNullException(nameof(message));
 
 			var key = new Tuple<string, Type, Type>(message, senderType, argType);
-			if (!s_callbacks.ContainsKey(key))
+			if (!_callbacks.ContainsKey(key))
 				return;
-			s_callbacks[key].RemoveAll(tuple => !tuple.Item1.IsAlive || tuple.Item1.Target == subscriber);
-			if (!s_callbacks[key].Any())
-				s_callbacks.Remove(key);
+			_callbacks[key].RemoveAll(tuple => !tuple.Item1.IsAlive || tuple.Item1.Target == subscriber);
+			if (!_callbacks[key].Any())
+				_callbacks.Remove(key);
 		}
 	}
 }
