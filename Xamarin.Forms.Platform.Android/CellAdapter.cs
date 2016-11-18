6ecd997d8367c95f3f97791c686e081acd3c1294
Adrian Knight <adrianknight89@outlook.com>
2016-11-17 22:15:44 -0600

    remove redundant check

--- Xamarin.Forms.Platform.Android/CellAdapter.cs
+++ Xamarin.Forms.Platform.Android/CellAdapter.cs
@@ -214,7 +214,7 @@ namespace Xamarin.Forms.Platform.Android
 
 		bool HandleContextMode(AView view, int position)
 		{
-			if (view is EditorEditText || view is EditText || view is TextView || view is SearchView)
+			if (view is EditText || view is TextView || view is SearchView)
 				return false;
 
 			Cell cell = GetCellForPosition(position);
