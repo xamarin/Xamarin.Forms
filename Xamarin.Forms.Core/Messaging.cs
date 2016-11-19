fc4dea54142380b25e1c284294dc9fce658fb30c
Adrian Knight <adrianknight89@outlook.com>
2016-11-19 00:23:13 -0600

    actions are weakly-referenced

--- Xamarin.Forms.Core/Messaging.cs
+++ Xamarin.Forms.Core/Messaging.cs
@@ -6,7 +6,7 @@ namespace Xamarin.Forms
 {
 	public class Messaging : IMessaging
 	{
-		readonly Dictionary<Tuple<string, Type, Type>, List<Tuple<WeakReference, Action<object, object>>>> _callbacks = new Dictionary<Tuple<string, Type, Type>, List<Tuple<WeakReference, Action<object, object>>>>();
+		readonly Dictionary<Tuple<string, Type, Type>, List<Tuple<WeakReference, WeakReference>>> _callbacks = new Dictionary<Tuple<string, Type, Type>, List<Tuple<WeakReference, WeakReference>>>();
 
 		static readonly Lazy<Messaging> s_lazy = new Lazy<Messaging>(() => new Messaging());
 
@@ -20,6 +20,9 @@ namespace Xamarin.Forms
 		{
 			if (sender == null)
 				throw new ArgumentNullException(nameof(sender));
+			if(string.IsNullOrWhiteSpace(message))
+				throw new ArgumentException("Message name cannot be empty.");
+
 			InnerSend(message, typeof(TSender), typeof(TArgs), sender, args);
 		}
 
@@ -27,6 +30,9 @@ namespace Xamarin.Forms
 		{
 			if (sender == null)
 				throw new ArgumentNullException(nameof(sender));
+			if (string.IsNullOrWhiteSpace(message))
+				throw new ArgumentException("Message name cannot be empty.");
+
 			InnerSend(message, typeof(TSender), null, sender, null);
 		}
 
@@ -34,6 +40,8 @@ namespace Xamarin.Forms
 		{
 			if (subscriber == null)
 				throw new ArgumentNullException(nameof(subscriber));
+			if (string.IsNullOrWhiteSpace(message))
+				throw new ArgumentException("Message name cannot be empty.");
 			if (callback == null)
 				throw new ArgumentNullException(nameof(callback));
 
@@ -51,6 +59,8 @@ namespace Xamarin.Forms
 		{
 			if (subscriber == null)
 				throw new ArgumentNullException(nameof(subscriber));
+			if (string.IsNullOrWhiteSpace(message))
+				throw new ArgumentException("Message name cannot be empty.");
 			if (callback == null)
 				throw new ArgumentNullException(nameof(callback));
 
@@ -81,56 +91,48 @@ namespace Xamarin.Forms
 
 		void InnerSend(string message, Type senderType, Type argType, object sender, object args)
 		{
-			if (message == null)
-				throw new ArgumentNullException(nameof(message));
 			var key = new Tuple<string, Type, Type>(message, senderType, argType);
+
 			if (!_callbacks.ContainsKey(key))
 				return;
-			List<Tuple<WeakReference, Action<object, object>>> actions = _callbacks[key];
+
+			List<Tuple<WeakReference, WeakReference>> actions = _callbacks[key];
 			if (actions == null || !actions.Any())
 				return; // should not be reachable
 
 			// ok so this code looks a bit funky but here is the gist of the problem. It is possible that in the course
 			// of executing the callbacks for this message someone will subscribe/unsubscribe from the same message in
 			// the callback. This would invalidate the enumerator. To work around this we make a copy. However if you unsubscribe 
-			// from a message you can fairly reasonably expect that you will therefor not receive a call. To fix this we then
+			// from a message you can fairly reasonably expect that you will therefore not receive a call. To fix this we then
 			// check that the item we are about to send the message to actually exists in the live list.
-			List<Tuple<WeakReference, Action<object, object>>> actionsCopy = actions.ToList();
-			foreach (Tuple<WeakReference, Action<object, object>> action in actionsCopy)
+			List<Tuple<WeakReference, WeakReference>> actionsCopy = actions.ToList();
+			foreach (Tuple<WeakReference, WeakReference> action in actionsCopy)
 			{
-				if (action.Item1.IsAlive && actions.Contains(action))
-					action.Item2(sender, args);
+				if (action.Item1.Target != null && actions.Contains(action))
+					((Action<object, object>)action.Item2.Target)?.Invoke(sender, args);
 			}
 		}
 
 		void InnerSubscribe(object subscriber, string message, Type senderType, Type argType, Action<object, object> callback)
 		{
-			if (message == null)
-				throw new ArgumentNullException(nameof(message));
 			var key = new Tuple<string, Type, Type>(message, senderType, argType);
-			var value = new Tuple<WeakReference, Action<object, object>>(new WeakReference(subscriber), callback);
+			var value = new Tuple<WeakReference, WeakReference>(new WeakReference(subscriber), new WeakReference(callback));
+
 			if (_callbacks.ContainsKey(key))
-			{
 				_callbacks[key].Add(value);
-			}
 			else
-			{
-				var list = new List<Tuple<WeakReference, Action<object, object>>> { value };
-				_callbacks[key] = list;
-			}
+				_callbacks[key] = new List<Tuple<WeakReference, WeakReference>> { value };
 		}
 
 		void InnerUnsubscribe(string message, Type senderType, Type argType, object subscriber)
 		{
-			if (subscriber == null)
-				throw new ArgumentNullException(nameof(subscriber));
-			if (message == null)
-				throw new ArgumentNullException(nameof(message));
-
 			var key = new Tuple<string, Type, Type>(message, senderType, argType);
+
 			if (!_callbacks.ContainsKey(key))
 				return;
+
 			_callbacks[key].RemoveAll(tuple => !tuple.Item1.IsAlive || tuple.Item1.Target == subscriber);
+
 			if (!_callbacks[key].Any())
 				_callbacks.Remove(key);
 		}
