be8ac7e2324b0127641bf8385751b795044931af
Adrian Knight <adrianknight89@outlook.com>
2016-11-20 04:24:03 -0600

    add sample code

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla47548.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+// Apply the default category of "Issues" to all of the tests in this assembly
+// We use this as a catch-all for tests which haven't been individually categorized
+#if UITEST
+[assembly: NUnit.Framework.Category("Issues")]
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 47548, "There is no way to skip adding status bar underlay on AppCompat", PlatformAffected.Android)]
+	public class Bugzilla47548 : TestContentPage
+	{
+		protected override void Init()
+		{
+			// Initialize ui here instead of ctor
+			Content = new Label
+			{
+				AutomationId = "IssuePageLabel",
+				Text = "See if I'm here"
+			};
+		}
+	}
+}
