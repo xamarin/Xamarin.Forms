a89c2e344ac6ef61b5921da24fb5e45449c3186d
Adrian Knight <adrianknight89@outlook.com>
2016-12-05 12:26:55 -0600

    new fix

--- Xamarin.Forms.Platform.Android/CellAdapter.cs
+++ Xamarin.Forms.Platform.Android/CellAdapter.cs
@@ -82,6 +82,7 @@ namespace Xamarin.Forms.Platform.Android
 		public bool OnActionItemClicked(ActionMode mode, IMenuItem item)
 		{
 			OnActionItemClickedImpl(item);
+			item.SetEnabled(false);
 			_actionMode?.Finish();
 			return true;
 		}
@@ -89,6 +90,7 @@ namespace Xamarin.Forms.Platform.Android
 		bool global::Android.Support.V7.View.ActionMode.ICallback.OnActionItemClicked(global::Android.Support.V7.View.ActionMode mode, IMenuItem item)
 		{
 			OnActionItemClickedImpl(item);
+			item.SetEnabled(false);
 			_supportActionMode?.Finish();
 			return true;
 		}
@@ -253,9 +255,6 @@ namespace Xamarin.Forms.Platform.Android
 
 		void OnActionItemClickedImpl(IMenuItem item)
 		{
-			if (ActionModeContext == null)
-				return;
-
 			int index = item.ItemId;
 			IMenuItemController action = ActionModeContext.ContextActions[index];
 
