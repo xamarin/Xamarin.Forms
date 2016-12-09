e53cb926241f34b36d162ce219dabd379617c1af
Jason Smith <jason.smith@xamarin.com>
2016-12-09 10:50:55 -0800

    Deprecate versions of iOS older than 8

--- Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
@@ -370,8 +370,7 @@ namespace Xamarin.Forms.Platform.iOS
 			}
 			else
 			{
-				if (Forms.IsiOS7OrNewer)
-					Control.EstimatedRowHeight = 0;
+				Control.EstimatedRowHeight = 0;
 				_estimatedRowHeight = true;
 			}
 		}
@@ -560,10 +559,9 @@ namespace Xamarin.Forms.Platform.iOS
 		void UpdateRowHeight()
 		{
 			var rowHeight = Element.RowHeight;
-			if (Element.HasUnevenRows && rowHeight == -1 && Forms.IsiOS7OrNewer)
+			if (Element.HasUnevenRows && rowHeight == -1)
 			{
-				if (Forms.IsiOS8OrNewer)
-					Control.RowHeight = UITableView.AutomaticDimension;
+				Control.RowHeight = UITableView.AutomaticDimension;
 			}
 			else
 				Control.RowHeight = rowHeight <= 0 ? DefaultRowHeight : rowHeight;
@@ -612,10 +610,7 @@ namespace Xamarin.Forms.Platform.iOS
 
 				if (List.RowHeight == -1 && cell.Height == -1 && cell is ViewCell)
 				{
-					// only doing ViewCell because its the only one that matters (the others dont adjust ANYWAY)
-					if (Forms.IsiOS8OrNewer)
-						return UITableView.AutomaticDimension;
-					return CalculateHeightForCell(tableView, cell);
+					return UITableView.AutomaticDimension;
 				}
 
 				var renderHeight = cell.RenderHeight;
@@ -681,7 +676,7 @@ namespace Xamarin.Forms.Platform.iOS
 			{
 				_uiTableViewController = uiTableViewController;
 				_uiTableView = uiTableViewController.TableView;
-				_defaultSectionHeight = Forms.IsiOS8OrNewer ? DefaultRowHeight : _uiTableView.SectionHeaderHeight;
+				_defaultSectionHeight = DefaultRowHeight;
 				List = list;
 				List.ItemSelected += OnItemSelected;
 				UpdateShortNameListener();
