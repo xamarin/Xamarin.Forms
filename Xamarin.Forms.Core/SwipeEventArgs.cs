34605e7b2c744e784edbf859bfde0135b63b89eb
Sean Davies <spdavies2@live.com>
2016-12-20 15:22:53 +0000

    Add files via upload

---
+++ Xamarin.Forms.Core/SwipeEventArgs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Xamarin.Forms
+{
+	public class SwipeLeftEventArgs : EventArgs
+	{
+		public SwipeLeftEventArgs(object parameter)
+		{
+			Parameter = parameter;
+		}
+
+		public object Parameter { get; private set; }
+	}
+
+	public class SwipeRightEventArgs : EventArgs
+	{
+		public SwipeRightEventArgs(object parameter)
+		{
+			Parameter = parameter;
+		}
+
+		public object Parameter { get; private set; }
+	}
+
+	public class SwipeUpEventArgs : EventArgs
+	{
+		public SwipeUpEventArgs(object parameter)
+		{
+			Parameter = parameter;
+		}
+
+		public object Parameter { get; private set; }
+	}
+
+	public class SwipeDownEventArgs : EventArgs
+	{
+		public SwipeDownEventArgs(object parameter)
+		{
+			Parameter = parameter;
+		}
+
+		public object Parameter { get; private set; }
+	}
+
+
+}
---
+++ Xamarin.Forms.Core/SwipeGestureRecognizer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Input;
+using System.Linq;
+
+namespace Xamarin.Forms
+{
+	public class SwipeGestureRecognizer : GestureRecognizer
+	{
+		public static readonly BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(ICommand), typeof(SwipeGestureRecognizer), null);
+
+		public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create("CommandParameter", typeof(object), typeof(SwipeGestureRecognizer), null);
+
+		public SwipeGestureRecognizer()
+		{
+
+		}
+
+		public ICommand Command
+		{
+			get { return (ICommand)GetValue(CommandProperty); }
+			set { SetValue(CommandProperty, value); }
+		}
+
+		public object CommandParameter
+		{
+			get { return GetValue(CommandParameterProperty); }
+			set { SetValue(CommandParameterProperty, value); }
+		}
+
+
+		public event EventHandler SwipeLeft;
+
+		public event EventHandler SwipeRight;
+
+		public event EventHandler SwipeDown;
+
+		public event EventHandler SwipeUp;
+
+		//public event EventHandler SwipeCompleted;
+
+		internal void HandleRightSwipe(View sender)
+		{
+			ICommand cmd = Command;
+			if (cmd != null && cmd.CanExecute(CommandParameter))
+				cmd.Execute(CommandParameter);
+
+			EventHandler handler = SwipeRight;
+			if (handler != null)
+				handler(sender, new SwipeRightEventArgs(CommandParameter));
+		}
+
+		internal void HandleLeftSwipe(View sender)
+		{
+			ICommand cmd = Command;
+			if (cmd != null && cmd.CanExecute(CommandParameter))
+				cmd.Execute(CommandParameter);
+
+			EventHandler handler = SwipeLeft;
+			if (handler != null)
+				handler(sender, new SwipeLeftEventArgs(CommandParameter));
+		}
+
+		internal void HandleDownSwipe(View sender)
+		{
+			ICommand cmd = Command;
+			if (cmd != null && cmd.CanExecute(CommandParameter))
+				cmd.Execute(CommandParameter);
+
+			EventHandler handler = SwipeDown;
+			if (handler != null)
+				handler(sender, new SwipeDownEventArgs(CommandParameter));
+		}
+
+		internal void HandleUpSwipe(View sender)
+		{
+			ICommand cmd = Command;
+			if (cmd != null && cmd.CanExecute(CommandParameter))
+				cmd.Execute(CommandParameter);
+
+			EventHandler handler = SwipeUp;
+			if (handler != null)
+				handler(sender, new SwipeDownEventArgs(CommandParameter));
+		}
+
+	}
+
+
+}
