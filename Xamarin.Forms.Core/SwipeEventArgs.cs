29f2aac8787162708cecf471a1b799a200a66f5f
Sean Davies <spdavies2@live.com>
2016-12-21 15:35:59 +0000

    Update SwipeEventArgs.cs

--- Xamarin.Forms.Core/SwipeEventArgs.cs
+++ Xamarin.Forms.Core/SwipeEventArgs.cs
@@ -4,11 +4,22 @@ namespace Xamarin.Forms
 {
 	public class SwipeEventArgs : EventArgs
 	{
-		public SwipeEventArgs(object parameter)
+		public SwipeEventArgs(object parameter, SwipeDirection direction)
 		{
 			Parameter = parameter;
+			Direction = direction;
 		}
 
 		public object Parameter { get; private set; }
+
+		public SwipeDirection Direction { get; private set; }
+	}
+
+	public enum SwipeDirection
+	{
+		Left,
+		Up,
+		Right,
+		Down
 	}
 }
