9fe796b4825caf700b99a7c1425d94302a98ac44
Stephane Delcroix <stephane@delcroix.org>
2016-12-22 10:28:01 +0100

    blind fix windows platforms

--- Xamarin.Forms.Platform.WinRT/WindowsBasePlatformServices.cs
+++ Xamarin.Forms.Platform.WinRT/WindowsBasePlatformServices.cs
@@ -117,6 +117,8 @@ namespace Xamarin.Forms.Platform.WinRT
 
 		public bool IsInvokeRequired => !CoreApplication.MainView.CoreWindow.Dispatcher.HasThreadAccess;
 
+		public string RuntimePlatform => Device.Windows;
+
 		public void OpenUriAction(Uri uri)
 		{
 			Launcher.LaunchUriAsync(uri).WatchForError();
