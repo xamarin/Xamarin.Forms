d337d7484a32f35d7075ff9837afe424eb515949
E.Z. Hart <hartez@gmail.com>
2017-02-24 18:47:56 -0700

    Cache the Alpha value and queue up restoration after the visibility is changed

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla51238.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla51238.cs
@@ -2,44 +2,17 @@
 using Xamarin.Forms.Internals;
 
 #if UITEST
-using Xamarin.UITest;
 using NUnit.Framework;
 #endif
 
 namespace Xamarin.Forms.Controls.Issues
 {
 	[Preserve(AllMembers = true)]
-	[Issue(IssueTracker.Bugzilla, 51238, "Transparent Grid causes Java.Lang.IllegalStateException: Unable to create layer for Platform_DefaultRenderer", PlatformAffected.Android)]
+	[Issue(IssueTracker.Bugzilla, 51238,
+		"Transparent Grid causes Java.Lang.IllegalStateException: Unable to create layer for Platform_DefaultRenderer",
+		PlatformAffected.Android)]
 	public class Bugzilla51238 : TestContentPage
 	{
-		protected override void Init()
-		{
-			var grid = new Grid();
-			grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Star });
-			grid.RowDefinitions.Add(new RowDefinition() { Height = 50 });
-
-			var transparentLayer = new Grid();
-			transparentLayer.IsVisible = false;
-			transparentLayer.BackgroundColor = Color.Lime;
-			transparentLayer.Opacity = 0.5;
-
-			Grid.SetRow(transparentLayer, 0);
-
-			var button = new Button() { Text = "Tap Me!", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center };
-
-			Grid.SetRow(button, 0);
-
-			button.Clicked += (sender, args) =>
-			{
-				transparentLayer.IsVisible = !transparentLayer.IsVisible;
-			};
-
-			grid.Children.Add(transparentLayer);
-			grid.Children.Add(button);
-
-			Content = grid;
-		}
-
 #if UITEST
 		[Test]
 		public void Issue1Test()
@@ -49,5 +22,44 @@ namespace Xamarin.Forms.Controls.Issues
 			RunningApp.WaitForElement("Tap Me!");
 		}
 #endif
+
+		protected override void Init()
+		{
+			var grid = new Grid();
+			grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+			grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
+
+			var transparentLayer = new Grid();
+			transparentLayer.IsVisible = false;
+			transparentLayer.BackgroundColor = Color.Lime;
+			transparentLayer.Opacity = 0.5;
+
+			var label = new Label
+			{
+				Text = "Foo",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			Grid.SetRow(label, 0);
+			Grid.SetRow(transparentLayer, 0);
+
+			var button = new Button
+			{
+				Text = "Tap Me!",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			Grid.SetRow(button, 1);
+
+			button.Clicked += (sender, args) => { transparentLayer.IsVisible = !transparentLayer.IsVisible; };
+
+			grid.Children.Add(label);
+			grid.Children.Add(transparentLayer);
+			grid.Children.Add(button);
+
+			Content = grid;
+		}
 	}
 }
