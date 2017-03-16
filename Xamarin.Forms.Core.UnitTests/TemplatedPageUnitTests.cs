7da691d6ae8db12694cced7deda819e1c4f9ab43
wplong11 <wplong11@naver.com>
2017-03-17 01:23:50 +0900

    Add missing test cases

---
+++ Xamarin.Forms.Core.UnitTests/TemplatedPageUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Core.UnitTests
+{
+    [TestFixture]
+	public class TemplatedPageUnitTests : BaseTestFixture 
+	{
+        [Test]
+        public void TemplatedPage_should_have_the_InternalChildren_correctly_when_ControlTemplate_changed()
+        {
+            var sut = new TemplatedPage();
+            IList<Element> internalChildren = ((IControlTemplated)sut).InternalChildren;
+            internalChildren.Add(new VisualElement());
+            internalChildren.Add(new VisualElement());
+            internalChildren.Add(new VisualElement());
+
+            sut.ControlTemplate = new ControlTemplate();
+
+            Assert.AreEqual(1, internalChildren.Count);
+        }
+	}
+}
