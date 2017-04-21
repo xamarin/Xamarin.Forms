265ca612f3f5699010c437ce9bfacc6757918a0a
Sean Davies <sean.davies@kingspan.com>
2017-04-21 09:58:23 +0100

    uitest

---
+++ Xamarin.Forms.Core.UnitTests/SwipeGestureRecognizerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Core.UnitTests
+{
+	[TestFixture]
+	public class SwipeGestureRecognizerTests : BaseTestFixture
+	{
+		[Test]
+		public void Constructor()
+		{
+			var swipe = new SwipeGestureRecognizer();
+
+			Assert.AreEqual(null, swipe.Command);
+			Assert.AreEqual(null, swipe.CommandParameter);
+			Assert.AreEqual(100, swipe.Threshold);
+		}
+
+		[Test]
+		public void CallbackPassesParameter()
+		{
+			var view = new View();
+			var swipe = new SwipeGestureRecognizer();
+			swipe.CommandParameter = "Hello";
+
+			object result = null;
+			swipe.Command = new Command(o => result = o);
+
+			swipe.SendSwiped(view, SwipeDirection.Left);
+			Assert.AreEqual(result, swipe.CommandParameter);
+		}
+	}
+}
