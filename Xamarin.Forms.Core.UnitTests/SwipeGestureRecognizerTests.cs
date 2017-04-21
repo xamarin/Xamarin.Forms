f56a56d31d1d43063b6316d05936e699dfd10c6a
Matthew Richardson <matthew@velocitysystems.co.nz>
2017-04-21 18:33:21 +0100

    Added additional test methods for Swipe unit tests.

--- Xamarin.Forms.Core.UnitTests/SwipeGestureRecognizerTests.cs
+++ Xamarin.Forms.Core.UnitTests/SwipeGestureRecognizerTests.cs
@@ -29,5 +29,43 @@ namespace Xamarin.Forms.Core.UnitTests
 			swipe.SendSwiped(view, SwipeDirection.Left);
 			Assert.AreEqual(result, swipe.CommandParameter);
 		}
+
+		[Test]
+		public void SwipedEventDirectionMatchesTotalXTest()
+		{
+			var view = new View();
+			var swipe = new SwipeGestureRecognizer();
+
+			SwipeDirection direction = SwipeDirection.Up;
+			swipe.Swiped += (object sender, SwipedEventArgs e) =>
+			{
+				direction = e.Direction;
+			};
+
+			((ISwipeGestureController)swipe).SendSwipe(view, totalX: -150, totalY: 10);
+			Assert.AreEqual(SwipeDirection.Left, direction);
+
+			((ISwipeGestureController)swipe).SendSwipe(view, totalX: 150, totalY: 10);
+			Assert.AreEqual(SwipeDirection.Right, direction);
+		}
+
+		[Test]
+		public void SwipedEventDirectionMatchesTotalYTest()
+		{
+			var view = new View();
+			var swipe = new SwipeGestureRecognizer();
+
+			SwipeDirection direction = SwipeDirection.Left;
+			swipe.Swiped += (object sender, SwipedEventArgs e) =>
+			{
+				direction = e.Direction;
+			};
+
+			((ISwipeGestureController)swipe).SendSwipe(view, totalX: 10, totalY: -150);
+			Assert.AreEqual(SwipeDirection.Up, direction);
+
+			((ISwipeGestureController)swipe).SendSwipe(view, totalX: 10, totalY: 150);
+			Assert.AreEqual(SwipeDirection.Down, direction);
+		}
 	}
 }
