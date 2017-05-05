2432f5744f7d61b13f0a6e9f3d9b0ae3362e70de
kadiryazgan <kadiryazgan@msn.com>
2017-05-05 22:43:34 +0300

    make OnShouldReturn protected
    There are situations where we don't want to dismiss the keyboard when return key is pressed.
    Making OnShouldReturn protected will let custom renderers to disable ResignFirstResponder behaviour.

--- Xamarin.Forms.Platform.iOS/Renderers/EntryRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/EntryRenderer.cs
@@ -127,7 +127,7 @@ namespace Xamarin.Forms.Platform.iOS
 			ElementController.SetValueFromRenderer(VisualElement.IsFocusedPropertyKey, false);
 		}
 
-		bool OnShouldReturn(UITextField view)
+		protected bool OnShouldReturn(UITextField view)
 		{
 			Control.ResignFirstResponder();
 			((IEntryController)Element).SendCompleted();
