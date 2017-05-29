432bde28745cfe7bcfc8defb52f394bb303ad605
Rui Marinho <me@ruimarinho.net>
2017-05-29 16:59:58 +0100

    [Android] Add a button background tracker to handle background and border properties and be shared between button renderers

--- Xamarin.Forms.Platform.Android/Renderers/ButtonRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/ButtonRenderer.cs
@@ -1,8 +1,6 @@
 using System;
 using System.ComponentModel;
-using Android.Content.Res;
 using Android.Graphics;
-using Android.Graphics.Drawables;
 using Android.Util;
 using static System.String;
 using AButton = Android.Widget.Button;
@@ -15,12 +13,10 @@ namespace Xamarin.Forms.Platform.Android
 {
 	public class ButtonRenderer : ViewRenderer<Button, AButton>, AView.IOnAttachStateChangeListener
 	{
-		ButtonDrawable _backgroundDrawable;
+		ButtonBackgroundTracker _backgroundTracker;
 		TextColorSwitcher _textColorSwitcher;
-		Drawable _defaultDrawable;
 		float _defaultFontSize;
 		Typeface _defaultTypeface;
-		bool _drawableEnabled;
 		bool _isDisposed;
 		int _imageHeight = -1;
 
@@ -73,11 +69,7 @@ namespace Xamarin.Forms.Platform.Android
 
 			if (disposing)
 			{
-				if (_backgroundDrawable != null)
-				{
-					_backgroundDrawable.Dispose();
-					_backgroundDrawable = null;
-				}
+				_backgroundTracker?.Dispose();
 			}
 
 			base.Dispose(disposing);
@@ -104,18 +96,11 @@ namespace Xamarin.Forms.Platform.Android
 					SetNativeControl(button);
 					_textColorSwitcher = new TextColorSwitcher(button.TextColors);
 					button.AddOnAttachStateChangeListener(this);
+					if (_backgroundTracker == null)
+						_backgroundTracker = new ButtonBackgroundTracker(Element, Control);
 				}
 			}
-			else
-			{
-				if (_drawableEnabled)
-				{
-					_drawableEnabled = false;
-					_backgroundDrawable.Reset();
-					_backgroundDrawable = null;
-				}
-			}
-
+			
 			UpdateAll();
 		}
 
@@ -135,21 +120,13 @@ namespace Xamarin.Forms.Platform.Android
 				UpdateBitmap();
 			else if (e.PropertyName == VisualElement.IsVisibleProperty.PropertyName)
 				UpdateText();
-
-			if (_drawableEnabled &&
-				(e.PropertyName == VisualElement.BackgroundColorProperty.PropertyName || e.PropertyName == Button.BorderColorProperty.PropertyName || e.PropertyName == Button.BorderRadiusProperty.PropertyName ||
-				 e.PropertyName == Button.BorderWidthProperty.PropertyName))
-			{
-				_backgroundDrawable.Reset();
-				Control.Invalidate();
-			}
-
+			
 			base.OnElementPropertyChanged(sender, e);
 		}
 
 		protected override void UpdateBackgroundColor()
 		{
-			// Do nothing, the drawable handles this now
+			_backgroundTracker.UpdateBackgroundColor();
 		}
 
 		void UpdateAll()
@@ -220,34 +197,7 @@ namespace Xamarin.Forms.Platform.Android
 
 		void UpdateDrawable()
 		{
-			if (Element.BackgroundColor == Color.Default)
-			{
-				if (!_drawableEnabled)
-					return;
-
-				if (_defaultDrawable != null)
-					Control.SetBackground(_defaultDrawable);
-
-				_drawableEnabled = false;
-			}
-			else
-			{
-				if (_backgroundDrawable == null)
-					_backgroundDrawable = new ButtonDrawable();
-
-				_backgroundDrawable.Button = Element;
-
-				if (_drawableEnabled)
-					return;
-
-				if (_defaultDrawable == null)
-					_defaultDrawable = Control.Background;
-
-				Control.SetBackground(_backgroundDrawable);
-				_drawableEnabled = true;
-			}
-
-			Control.Invalidate();
+			_backgroundTracker.UpdateDrawable();
 		}
 
 		void UpdateEnabled()
