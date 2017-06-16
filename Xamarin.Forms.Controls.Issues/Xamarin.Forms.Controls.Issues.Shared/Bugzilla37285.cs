40ae7ba96e26cb9e749f139b1d75e3b6e1e7ca6b
Samantha Houts <samantha.houts@xamarin.com>
2017-06-15 18:39:48 -0700

    Add repro

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla37285.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 9937285, "Possible to enter text into Picker control", PlatformAffected.iOS)]
+	public class Bugzilla37285 : TestContentPage
+	{
+		protected override void Init()
+		{
+			var picker = new Picker { ItemsSource = Enumerable.Range(0, 100).Select(c=> c.ToString()).ToList() };
+
+			var stack = new StackLayout { Children = { picker } };
+
+			Content = stack;
+		}
+	}
+}
