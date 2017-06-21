34eafd4a87a33a877b11b07026e2324bf4cead02
E.Z. Hart <hartez@gmail.com>
2017-06-21 11:11:15 -0600

    Automated test for maps modal crash

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/MapsModalCrash.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/MapsModalCrash.cs
@@ -1,20 +1,32 @@
-﻿using Xamarin.Forms.CustomAttributes;
+﻿using System;
+using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Maps;
 #if UITEST
+using Xamarin.Forms.Core.UITests;
 using Xamarin.UITest;
 using NUnit.Framework;
 #endif
 
 namespace Xamarin.Forms.Controls.Issues
 {
+
+#if UITEST
+	[Category(UITestCategories.Maps)]
+	[Category(UITestCategories.ManualReview)]
+#endif
+
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.None, 1701, "Modal Page over Map crashes application", PlatformAffected.Android)]
-	public class MapsModalCrash : TestContentPage 
+	public class MapsModalCrash : TestContentPage
 	{
+		const string StartTest = "Start Test";
+		const string DisplayModal = "Click Me";
+		const string Success = "SuccessLabel";
+
 		protected override void Init()
 		{
-			var button = new Button { Text = "Start Test" };
+			var button = new Button { Text = StartTest };
 			button.Clicked += (sender, args) =>
 			{
 				Application.Current.MainPage = MapPage();
@@ -34,7 +46,7 @@ namespace Xamarin.Forms.Controls.Issues
 		{
 			var map = new Map();
 
-			var button = new Button { Text = "Click Me" };
+			var button = new Button { Text = DisplayModal };
 			button.Clicked += (sender, args) => button.Navigation.PushModalAsync(new NavigationPage(SuccessPage()));
 
 			return new ContentPage
@@ -54,16 +66,20 @@ namespace Xamarin.Forms.Controls.Issues
 		{
 			return new ContentPage
 			{
-				Content = new Label { Text = "If you're seeing this, then the test was a success.", AutomationId = "SuccessLabel" }
+				Content = new Label { Text = "If you're seeing this, then the test was a success.", AutomationId = Success }
 			};
 		}
 
 #if UITEST
-		//[Test]
-		//public void _$BZ$Test()
-		//{
-		//	//RunningApp.WaitForElement(q => q.Marked(""));
-		//}
+		[Test]
+		public void CanDisplayModalOverMap()
+		{
+			RunningApp.WaitForElement(StartTest);
+			RunningApp.Tap(StartTest);
+			RunningApp.WaitForElement(DisplayModal);
+			RunningApp.Tap(DisplayModal);
+			RunningApp.WaitForElement(Success);
+		}
 #endif
 	}
 }
