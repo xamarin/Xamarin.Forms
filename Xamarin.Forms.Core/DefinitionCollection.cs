14127deb1f4939a7b2bbc7ef5d14e2a9d2b0bc2a
Julius Zint <julius.zint@awin-software.de>
2017-06-26 20:04:28 +0200

    index check for negative values

--- Xamarin.Forms.Core/DefinitionCollection.cs
+++ Xamarin.Forms.Core/DefinitionCollection.cs
@@ -83,7 +83,7 @@ namespace Xamarin.Forms
 			get { return _internalList[index]; }
 			set
 			{
-				if(index < _internalList.Count && _internalList[index] != null)
+				if(index < _internalList.Count && _internalList[index] != null && index >= 0)
 					_internalList[index].SizeChanged -= OnItemSizeChanged;
 
 				_internalList[index] = value;
