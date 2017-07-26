d0bedb2a601b8fdf0280c8ad95ebb5b49bd563bc
Joe Manke <JoeM@innovsys.com>
2017-07-26 14:46:16 -0500

    Added UI test for pin tap gesture

---
+++ Xamarin.Forms.Core.UITests.Shared/Tests/MapUITests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Core.UITests
+{
+#if __ANDROID__ || __WINDOWS__
+	[Ignore("Test only meant for Mac and iOS, besides needing API keys for Android and Windows")]
+#endif
+	[TestFixture]
+	[Category(UITestCategories.Maps)]
+    internal class MapUITests : BaseTestFixture
+    {
+	    protected override void NavigateToGallery()
+	    {
+		    App.NavigateToGallery(GalleryQueries.MapGalleryLegacy);
+	    }
+
+		[Test]
+		public void MapGalleryPinClicked()
+		{
+			App.Tap(PlatformQueries.Pin);
+			App.Tap(c => c.Text("Colosseum"));
+			App.Screenshot("Alert displayed as result of PinClicked event");
+		}
+    }
+}
