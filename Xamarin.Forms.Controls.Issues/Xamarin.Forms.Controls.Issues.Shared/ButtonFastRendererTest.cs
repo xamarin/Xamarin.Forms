12830f4ec0f91b8d47ab48d52256b794bada1763
Rui Marinho <me@ruimarinho.net>
2017-08-15 00:58:57 +0100

    [Controls] New test for elevation with fast renderer button

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/ButtonFastRendererTest.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/ButtonFastRendererTest.cs
@@ -13,14 +13,20 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.None, 9999, "Button FastRenderers", PlatformAffected.All)]
 	public class ButtonFastRendererTest : TestContentPage
 	{
+		const string Running = "Running...";
+		const string Success = "Success";
+		const string Failure = "Failure";
+		const string btnId = "btnHello";
 		protected override void Init()
 		{
+			var label = new Label { Text = Running };
 			var img = new Image { Source = "cover1.jpg", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center };
-			var btn = new Button { AutomationId = "btnHello", Text = "hello", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center };
-			btn.Clicked += (sender, e) => { DisplayAlert("hello", "message", "ok"); };
+			var btn = new Button { AutomationId = btnId, Text = "hello", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center };
+			btn.Clicked += (sender, e) => { label.Text = Success; };
 			var grd = new Grid();
 			grd.Children.Add(btn);
 			grd.Children.Add(img);
+			grd.Children.Add(label);
 			Content = grd;
 		}
 
@@ -28,7 +34,9 @@ namespace Xamarin.Forms.Controls.Issues
 		[Test]
 		public void TestButtonUsingElevation ()
 		{
-			RunningApp.WaitForNoElement("btnHello");
+			RunningApp.WaitForElement(Running);
+			RunningApp.Tap(btnId);
+			RunningApp.WaitForNoElement(Success);
 		}
 #endif
 	}
