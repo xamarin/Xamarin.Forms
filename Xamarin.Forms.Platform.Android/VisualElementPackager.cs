fea87e35acd00b5c9ad5a2a82009b5484a511c0b
Rui Marinho <me@ruimarinho.net>
2017-08-14 15:50:49 +0100

    [Android] Set elevation in Android Views if needed

--- Xamarin.Forms.Platform.Android/VisualElementPackager.cs
+++ Xamarin.Forms.Platform.Android/VisualElementPackager.cs
@@ -120,6 +120,7 @@ namespace Xamarin.Forms.Platform.Android
 					(_renderer.View as ViewGroup)?.BringChildToFront(r.View);
 				}
 			}
+			(_renderer as Platform.DefaultRenderer)?.InvalidateMinimumElevation();
 		}
 
 		void OnChildAdded(object sender, ElementEventArgs e)
