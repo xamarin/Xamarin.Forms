9560d873622f72f378e8b491a9e5619f0cecc997
Stephane Delcroix <stephane@delcroix.org>
2017-09-29 21:23:16 +0200

    [Xaml] Chain op_implicit for OnPlatform (if needed)
    For OnPlatform<T>, if a conversion for T to the destination type exists,
    first convert OnPlatform<T> to T, the T to the dest type
    
    Also unify the way we look for op_implicit operators

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Bz59818.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UnitTests;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public partial class Bz59818 : ContentPage
+	{
+		public Bz59818()
+		{
+			InitializeComponent();
+		}
+
+		public Bz59818(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		[TestFixture]
+		class Tests
+		{
+			[SetUp]
+			public void Setup()
+			{
+				Device.PlatformServices = new MockPlatformServices();
+			}
+
+			[TearDown]
+			public void TearDown()
+			{
+				Device.PlatformServices = null;
+			}
+
+			[TestCase(true)]
+			[TestCase(false)]
+			public void Bz59818(bool useCompiledXaml)
+			{
+				((MockPlatformServices)Device.PlatformServices).RuntimePlatform = Device.iOS;
+				if (useCompiledXaml)
+					MockCompiler.Compile(typeof(Bz59818));
+				var layout = new Bz59818(useCompiledXaml);
+				Assert.That(layout.grid.ColumnDefinitions[0].Width, Is.EqualTo(new GridLength(100)));
+			}
+		}
+	}
+}
