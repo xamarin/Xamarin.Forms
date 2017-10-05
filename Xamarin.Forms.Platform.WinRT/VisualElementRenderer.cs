f016485d90b42706f9b1b385419a82bd7a681bc0
Brad Chase <brad.chase@argus.aero>
2017-10-05 17:14:39 -0400

    [UWP] Issue where in some weird circumstances(Custom controls) grabbing a desired size could cause a null reference exception.

--- Xamarin.Forms.Platform.WinRT/VisualElementRenderer.cs
+++ Xamarin.Forms.Platform.WinRT/VisualElementRenderer.cs
@@ -105,7 +105,7 @@ namespace Xamarin.Forms.Platform.WinRT
 
 		public virtual SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
 		{
-			if (Children.Count == 0)
+			if (Children.Count == 0 || Control == null)
 				return new SizeRequest();
 
 			var constraint = new Windows.Foundation.Size(widthConstraint, heightConstraint);
