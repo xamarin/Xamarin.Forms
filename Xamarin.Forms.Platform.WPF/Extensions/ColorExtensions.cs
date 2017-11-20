5c439d5427d655db8dc56d068c225bb4c99d2e06
Brad Chase <brad.chase@argus.aero>
2017-11-20 15:18:33 -0500

    WPF_Public_Color_Extensions

--- Xamarin.Forms.Platform.WPF/Extensions/ColorExtensions.cs
+++ Xamarin.Forms.Platform.WPF/Extensions/ColorExtensions.cs
@@ -7,7 +7,7 @@ using System.Windows.Media;
 
 namespace Xamarin.Forms.Platform.WPF
 { 
-	internal static class ColorExtensions
+	public static class ColorExtensions
 	{
 		public static Brush ToBrush(this Color color)
 		{
