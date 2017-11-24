c33dc09fc0cbc23d7d67a8cf7c5e7674698b6650
Rui Marinho <me@ruimarinho.net>
2017-11-24 16:21:20 +0000

    [UWP] Fix maps UWP

--- Xamarin.Forms.Platform.UAP/NativeBindingService.cs
+++ Xamarin.Forms.Platform.UAP/NativeBindingService.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Reflection;
+using Windows.UI.Xaml;
 using Xamarin.Forms.Xaml.Internals;
 
 
--- Xamarin.Forms.Platform.UAP/NativeValueConverterService.cs
+++ Xamarin.Forms.Platform.UAP/NativeValueConverterService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Windows.UI.Xaml;
 using Xamarin.Forms.Xaml.Internals;
 
