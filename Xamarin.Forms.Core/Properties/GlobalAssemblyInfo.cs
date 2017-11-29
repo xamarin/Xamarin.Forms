09231ea1b037c263514270c11b9ec290fcbd4502
Rui Marinho <me@ruimarinho.net>
2017-11-29 15:38:14 +0000

    Fix docs

--- Xamarin.Forms.Core/Properties/GlobalAssemblyInfo.cs
+++ Xamarin.Forms.Core/Properties/GlobalAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("Xamarin.Forms")]
 [assembly: AssemblyCopyright("Copyright © Xamarin Inc. 2013-2017")]
 [assembly: AssemblyTrademark("")]
-//[assembly: AssemblyVersion("2.0.0.0")]
-//[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
