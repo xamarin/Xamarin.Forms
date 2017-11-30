5175ac92e90aa38ca7e7f70b390de99cd6354934
Samantha Houts <samantha.houts@xamarin.com>
2017-11-29 16:25:14 -0800

    [Android] Update instrumentation

--- Xamarin.Forms.Platform.Android/Renderers/ListViewAdapter.cs
+++ Xamarin.Forms.Platform.Android/Renderers/ListViewAdapter.cs
@@ -186,7 +186,8 @@ namespace Xamarin.Forms.Platform.Android
 		{
 			Cell cell = null;
 
-			Performance.Start();
+			var reference = Guid.NewGuid().ToString();
+			Performance.Start(reference);
 
 			ListViewCachingStrategy cachingStrategy = Controller.CachingStrategy;
 			var nextCellIsHeader = false;
@@ -270,13 +271,13 @@ namespace Xamarin.Forms.Platform.Android
 				else
 					UnsetSelectedBackground(layout);
 
-				Performance.Stop();
+				Performance.Stop(reference);
 				return layout;
 			}
 
 			AView view = CellFactory.GetCell(cell, convertView, parent, _context, _listView);
 
-			Performance.Start("AddView");
+			Performance.Start(reference, "AddView");
 
 			if (cellIsBeingReused)
 			{
@@ -289,7 +290,7 @@ namespace Xamarin.Forms.Platform.Android
 			else
 				layout.AddView(view, 0);
 
-			Performance.Stop("AddView");
+			Performance.Stop(reference, "AddView");
 
 			bool isHeader = cell.GetIsGroupHeader<ItemsView<Cell>, Cell>();
 
@@ -306,7 +307,7 @@ namespace Xamarin.Forms.Platform.Android
 
 			layout.ApplyTouchListenersToSpecialCells(cell);
 
-			Performance.Stop();
+			Performance.Stop(reference);
 
 			return layout;
 		}
