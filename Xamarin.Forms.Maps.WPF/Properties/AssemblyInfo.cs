cbae0b38e9f86811c3c280ed5b61eda7b3a8bac0
Mohamed CHOUCHANE <mohamed.chouchane1990@gmail.com>
2017-12-06 23:18:45 +0100

    Remove french comment in AssemblyInfo.cs

--- Xamarin.Forms.Maps.WPF/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Maps.WPF/Properties/AssemblyInfo.cs
@@ -7,9 +7,6 @@ using Xamarin.Forms.Maps;
 using Xamarin.Forms.Maps.WPF;
 using Xamarin.Forms.Platform.WPF;
 
-// Les informations générales relatives à un assembly dépendent de
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
 [assembly: AssemblyTitle("Xamarin.Forms.Maps.WPF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -19,23 +16,10 @@ using Xamarin.Forms.Platform.WPF;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
-// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
-// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
 [assembly: ExportRenderer(typeof(Map), typeof(MapRenderer))]
 
-
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
-// en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
