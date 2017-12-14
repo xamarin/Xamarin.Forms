fa4f23eb3371d761b174192d1077b8f4d7ef57c1
E.Z. Hart <hartez@gmail.com>
2017-12-13 18:34:23 -0700

    Make new methods internal

--- Xamarin.Forms.Platform.Android/VisualElementExtensions.cs
+++ Xamarin.Forms.Platform.Android/VisualElementExtensions.cs
@@ -15,7 +15,7 @@ namespace Xamarin.Forms.Platform.Android
 			return renderer;
 		}
 
-		public static bool UseLegacyColorManagement<T>(this T element) where T : VisualElement, IElementConfiguration<T>
+		internal static bool UseLegacyColorManagement<T>(this T element) where T : VisualElement, IElementConfiguration<T>
 		{
 			// Determine whether we're letting the VSM handle the colors or doing it the old way
 			// or disabling the legacy color management and doing it the old-old (pre 2.0) way
--- Xamarin.Forms.Platform.iOS/Extensions/VisualElementExtensions.cs
+++ Xamarin.Forms.Platform.iOS/Extensions/VisualElementExtensions.cs
@@ -2,9 +2,9 @@ using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace Xamarin.Forms.Platform.iOS
 {
-	public static class VisualElementExtensions
+	internal static class VisualElementExtensions
 	{
-		public static bool UseLegacyColorManagement<T>(this T element) where T : VisualElement, IElementConfiguration<T>
+		internal static bool UseLegacyColorManagement<T>(this T element) where T : VisualElement, IElementConfiguration<T>
 		{
 			// Determine whether we're letting the VSM handle the colors or doing it the old way
 			// or disabling the legacy color management and doing it the old-old (pre 2.0) way
