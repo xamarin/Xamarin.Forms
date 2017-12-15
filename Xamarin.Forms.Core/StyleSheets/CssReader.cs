Sourcephy/Xamarin.Forms-1

[*] Support for CSS StyleSheets
 Parsing, loading, and applying of StyleSheets

Xamarin.Forms.Core/StyleSheets/CssReader.cs
@@ -1,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Xamarin.Forms.StyleSheets
+{
+	sealed class CssReader : TextReader
+	{
+		readonly TextReader _reader;
+
+		public CssReader(TextReader reader)
+		{
+			if (reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			_reader = reader;
+		}
+
+		readonly Queue<char> _cache = new Queue<char>();
+
+		//skip comments
+		//TODO unescape escaped sequences
+		public override int Peek()
+		{
+			if (_cache.Count > 0)
+				return _cache.Peek();
+
+			int p = _reader.Peek();
+			if (p <= 0)
+				return p;
+			if (unchecked((char)p) != '/')
+				return p;
+
+			_cache.Enqueue(unchecked((char)_reader.Read()));
+			p = _reader.Peek();
+			if (p <= 0)
+				return _cache.Peek();
+			if (unchecked((char)p) != '*')
+				return _cache.Peek();
+
+			_cache.Clear();
+			_reader.Read(); //consume the '*'
+
+			bool hasStar = false;
+			while (true) {
+				var next = _reader.Read();
+				if (next <= 0)
+					return next;
+				if (unchecked((char)next) == '*')
+					hasStar = true;
+				else if (hasStar && unchecked((char)next) == '/')
+					return Peek(); //recursively call self for comments followign comments
+				else
+					hasStar = false;
+			}
+		}
+
+		//skip comments
+		//TODO unescape escaped sequences
+		public override int Read()
+		{
+			if (_cache.Count > 0)
+				return _cache.Dequeue();
+
+			int p = _reader.Read();
+			if (p <= 0)
+				return p;
+			var c = unchecked((char)p);
+			if (c != '/')
+				return p;
+
+			_cache.Enqueue(c);
+			p = _reader.Read();
+			if (p <= 0)
+				return _cache.Dequeue();
+			c = unchecked((char)p);
+			if (c != '*')
+				return _cache.Dequeue();
+
+			_cache.Clear();
+			_reader.Read(); //consume the '*'
+
+			bool hasStar = false;
+			while (true) {
+				var next = _reader.Read();
+				if (next <= 0)
+					return next;
+				if (unchecked((char)next) == '*')
+					hasStar = true;
+				else if (hasStar && unchecked((char)next) == '/')
+					return Read(); //recursively call self for comments followign comments
+				else
+					hasStar = false;
+			}
+		}
+	}
+}

Xamarin.Forms.Core/StyleSheets/StyleSheet.cs
@@ -1,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.StyleSheets
+{
+	public sealed class StyleSheet : IStyle
+	{
+		StyleSheet()
+		{
+		}
+
+		internal IDictionary<Selector, Style> Styles { get; set; } = new Dictionary<Selector, Style>();
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static StyleSheet Parse(Assembly assembly, string resourceId, IXmlLineInfo lineInfo = null)
+		{
+			using (var stream = assembly.GetManifestResourceStream(resourceId)) {
+				if (stream == null)
+					throw new XamlParseException($"No resource found for '{resourceId}'.", lineInfo);
+				using (var reader = new StreamReader(stream)) {
+					return Parse(reader);
+				}
+			}
+		}
+
+		internal static StyleSheet Parse(TextReader reader)
+		{
+			if (reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			return Parse(new CssReader(reader));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		static StyleSheet Parse(CssReader reader)
+		{
+			var sheet = new StyleSheet();
+
+			Style style = null;
+			var selector = Selector.All;
+
+			int p;
+			bool inStyle = false;
+			reader.SkipWhiteSpaces();
+			while ((p = reader.Peek()) > 0) {
+				switch ((char)p) {
+				case '@':
+					throw new NotSupportedException("AT-rules not supported");
+				case '{':
+					reader.Read();
+					style = Style.Parse(reader, '}');
+					inStyle = true;
+					break;
+				case '}':
+					reader.Read();
+					if (!inStyle)
+						throw new Exception();
+					inStyle = false;
+					sheet.Styles.Add(selector, style);
+					style = null;
+					selector = Selector.All;
+					break;
+				default:
+					selector = Selector.Parse(reader, '{');
+					break;
+				}
+			}
+			return sheet;
+		}
+
+		Type IStyle.TargetType
+			=> typeof(VisualElement);
+
+		void IStyle.Apply(BindableObject bindable)
+		{
+			var styleable = bindable as VisualElement;
+			if (styleable == null)
+				return;
+			Apply(styleable);
+		}
+
+		void Apply(VisualElement styleable)
+		{
+			ApplyCore(styleable);
+			foreach (var child in styleable.LogicalChildrenInternal)
+				((IStyle)this).Apply(child);
+		}
+
+		void ApplyCore(VisualElement styleable)
+		{
+			foreach (var kvp in Styles) {
+				var selector = kvp.Key;
+				var style = kvp.Value;
+				if (!selector.Matches(styleable))
+					continue;
+				style.Apply(styleable);
+			}
+		}
+
+		void IStyle.UnApply(BindableObject bindable)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

xamarin/Xamarin.Forms

 see https://github.com/xamarin/Xamarin.Forms/pull/1207#issue-266464280
 for complete description.

Xamarin.Forms.Core/StyleSheets/CssReader.cs
@@ -1,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Xamarin.Forms.StyleSheets
+{
+	sealed class CssReader : TextReader
+	{
+		readonly TextReader _reader;
+
+		public CssReader(TextReader reader)
+		{
+			if (reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			_reader = reader;
+		}
+
+		readonly Queue<char> _cache = new Queue<char>();
+
+		//skip comments
+		//TODO unescape escaped sequences
+		public override int Peek()
+		{
+			if (_cache.Count > 0)
+				return _cache.Peek();
+
+			int p = _reader.Peek();
+			if (p <= 0)
+				return p;
+			if (unchecked((char)p) != '/')
+				return p;
+
+			_cache.Enqueue(unchecked((char)_reader.Read()));
+			p = _reader.Peek();
+			if (p <= 0)
+				return _cache.Peek();
+			if (unchecked((char)p) != '*')
+				return _cache.Peek();
+
+			_cache.Clear();
+			_reader.Read(); //consume the '*'
+
+			bool hasStar = false;
+			while (true) {
+				var next = _reader.Read();
+				if (next <= 0)
+					return next;
+				if (unchecked((char)next) == '*')
+					hasStar = true;
+				else if (hasStar && unchecked((char)next) == '/')
+					return Peek(); //recursively call self for comments following comments
+				else
+					hasStar = false;
+			}
+		}
+
+		//skip comments
+		//TODO unescape escaped sequences
+		public override int Read()
+		{
+			if (_cache.Count > 0)
+				return _cache.Dequeue();
+
+			int p = _reader.Read();
+			if (p <= 0)
+				return p;
+			var c = unchecked((char)p);
+			if (c != '/')
+				return p;
+
+			_cache.Enqueue(c);
+			p = _reader.Read();
+			if (p <= 0)
+				return _cache.Dequeue();
+			c = unchecked((char)p);
+			if (c != '*')
+				return _cache.Dequeue();
+
+			_cache.Clear();
+			_reader.Read(); //consume the '*'
+
+			bool hasStar = false;
+			while (true) {
+				var next = _reader.Read();
+				if (next <= 0)
+					return next;
+				if (unchecked((char)next) == '*')
+					hasStar = true;
+				else if (hasStar && unchecked((char)next) == '/')
+					return Read(); //recursively call self for comments following comments
+				else
+					hasStar = false;
+			}
+		}
+	}
+}

Xamarin.Forms.Core/StyleSheets/StyleSheet.cs
@@ -1,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.StyleSheets
+{
+	public sealed class StyleSheet : IStyle
+	{
+		StyleSheet()
+		{
+		}
+
+		internal IDictionary<Selector, Style> Styles { get; set; } = new Dictionary<Selector, Style>();
+
+		public static StyleSheet FromAssemblyResource(Assembly assembly, string resourceId, IXmlLineInfo lineInfo = null)
+		{
+			using (var stream = assembly.GetManifestResourceStream(resourceId)) {
+				if (stream == null)
+					throw new XamlParseException($"No resource found for '{resourceId}'.", lineInfo);
+				using (var reader = new StreamReader(stream)) {
+					return FromReader(reader);
+				}
+			}
+		}
+
+		//used by code generated by XamlC. Has to stay public
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static StyleSheet FromString(string stylesheet)
+		{
+			if (stylesheet == null)
+				throw new ArgumentNullException(nameof(stylesheet));
+			using (var reader = new StringReader(stylesheet))
+				return FromReader(reader);
+		}
+
+		public static StyleSheet FromReader(TextReader reader)
+		{
+			if (reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			return Parse(new CssReader(reader));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		static StyleSheet Parse(CssReader reader)
+		{
+			var sheet = new StyleSheet();
+
+			Style style = null;
+			var selector = Selector.All;
+
+			int p;
+			bool inStyle = false;
+			reader.SkipWhiteSpaces();
+			while ((p = reader.Peek()) > 0) {
+				switch ((char)p) {
+				case '@':
+					throw new NotSupportedException("AT-rules not supported");
+				case '{':
+					reader.Read();
+					style = Style.Parse(reader, '}');
+					inStyle = true;
+					break;
+				case '}':
+					reader.Read();
+					if (!inStyle)
+						throw new Exception();
+					inStyle = false;
+					sheet.Styles.Add(selector, style);
+					style = null;
+					selector = Selector.All;
+					break;
+				default:
+					selector = Selector.Parse(reader, '{');
+					break;
+				}
+			}
+			return sheet;
+		}
+
+		Type IStyle.TargetType
+			=> typeof(VisualElement);
+
+		void IStyle.Apply(BindableObject bindable)
+		{
+			var styleable = bindable as VisualElement;
+			if (styleable == null)
+				return;
+			Apply(styleable);
+		}
+
+		void Apply(VisualElement styleable)
+		{
+			ApplyCore(styleable);
+			foreach (var child in styleable.LogicalChildrenInternal)
+				((IStyle)this).Apply(child);
+		}
+
+		void ApplyCore(VisualElement styleable)
+		{
+			foreach (var kvp in Styles) {
+				var selector = kvp.Key;
+				var style = kvp.Value;
+				if (!selector.Matches(styleable))
+					continue;
+				style.Apply(styleable);
+			}
+		}
+
+		void IStyle.UnApply(BindableObject bindable)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}