a6779067cf51e235a03c521701b58dcc7a304cb2
Michael Dwan <m@dwan.io>
2018-01-10 17:29:53 -0600

    Use ScrollForElement

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue1414.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue1414.cs
@@ -24,26 +24,25 @@ namespace Xamarin.Forms.Controls.Issues
 					new ColumnDefinition { Width = GridLength.Star },
 					new ColumnDefinition { Width = GridLength.Star }
 				},
-				AutomationId = $"Grid:{sectionIndex}:{cellIndex}"
+				AutomationId = $"Row-{sectionIndex}-{cellIndex}"
 			};
 
 			if (cellIndex % 2 == 0)
 			{
-				grid.AddChild(new Label { Text = $"Label {cellIndex}" }, 0, 0);
-				grid.AddChild(new Label { Text = $"Label {cellIndex}" }, 1, 0);
+				grid.AddChild(new Label { Text = $"Cell {sectionIndex}-{cellIndex}" }, 0, 0);
+				grid.AddChild(new Label { Text = $"Label" }, 1, 0);
 				grid.BackgroundColor = Color.Fuchsia;
 			}
 			else
 			{
-				grid.AddChild(new Label { Text = $"Label {cellIndex}" }, 0, 0);
-				grid.AddChild(new Entry { Text = $"Entry {cellIndex}" }, 1, 0);
+				grid.AddChild(new Label { Text = $"Cell {sectionIndex}-{cellIndex}" }, 0, 0);
+				grid.AddChild(new Entry { Text = $"Entry" }, 1, 0);
 				grid.BackgroundColor = Color.Yellow;
 			}
 
 			return new ViewCell
 			{
 				View = grid,
-				AutomationId = $"Cell:{sectionIndex}:{cellIndex}"
 			};
 		}
 
@@ -78,9 +77,11 @@ namespace Xamarin.Forms.Controls.Issues
 		{
 			RunningApp.Screenshot("Start G1414");
 			RunningApp.WaitForElement(q => q.Marked("TableView"));
-			RunningApp.ScrollDownTo("Grid:4:24", strategy: ScrollStrategy.Gesture, swipeSpeed: 1000, swipePercentage: 0.9);
+
+			var tableFrame = RunningApp.Query(q => q.Marked("TableView"))[0].Rect;
+			RunningApp.ScrollForElement("* marked:'Row-4-24'", new Drag(tableFrame, Drag.Direction.BottomToTop, Drag.DragLength.Long));
 			RunningApp.Screenshot("Scrolled to end without crashing!");
-			RunningApp.ScrollUpTo("Grid:0:0", strategy: ScrollStrategy.Gesture, swipeSpeed: 1000, swipePercentage: 0.9);
+			RunningApp.ScrollForElement("* marked:'Row-0-0'", new Drag(tableFrame, Drag.Direction.TopToBottom, Drag.DragLength.Long));
 			RunningApp.Screenshot("Scrolled to top without crashing!");
 		}
 #endif
