75e2fd08864b7141e7ddc95c7e38dd332ac6f60d
Samantha Houts <samantha.houts@xamarin.com>
2018-02-20 16:41:54 -0800

    Add repro for 1909

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue1909.cs
@@ -0,0 +1,50 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 1909, "Xamarin.forms 2.5.0.280555 and android circle button issue", PlatformAffected.Android)]
+	public class Issue1909 : TestContentPage 
+	{
+		public class FlatButton : Button { }
+		protected override void Init()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label{ Text = "The following buttons should be perfectly round. If they are not, this test has failed." },
+					new Button
+					{
+						BackgroundColor = Color.Red,
+						CornerRadius = 32,
+						BorderWidth = 0,
+						FontSize = 36,
+						HeightRequest = 64,
+						HorizontalOptions = LayoutOptions.Center,
+						TextColor = Color.White,
+						VerticalOptions = LayoutOptions.Center,
+						WidthRequest = 64
+					},
+					new FlatButton
+					{
+						BackgroundColor = Color.Red,
+						CornerRadius = 32,
+						BorderWidth = 0,
+						FontSize = 36,
+						HeightRequest = 64,
+						HorizontalOptions = LayoutOptions.Center,
+						TextColor = Color.White,
+						VerticalOptions = LayoutOptions.Center,
+						WidthRequest = 64
+					}
+				}
+			};
+		}
+	}
+}
