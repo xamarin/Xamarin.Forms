be90fbcf0feb4e74d321f84d0057c7afba74207b
Samantha Houts <samantha.houts@xamarin.com>
2018-02-20 17:09:22 -0800

    [Core] Add PS for removing the native padding from Android buttons

---
+++ Xamarin.Forms.Core/PlatformConfiguration/AndroidSpecific/Button.cs
@@ -0,0 +1,30 @@
+﻿namespace Xamarin.Forms.PlatformConfiguration.AndroidSpecific
+{
+	using FormsElement = Forms.Button;
+
+	public static class Button
+	{
+		public static readonly BindableProperty UseNativePaddingProperty = BindableProperty.Create("UseNativePadding", typeof(bool), typeof(Button), true);
+
+		public static bool GetUseNativePadding(BindableObject element)
+		{
+			return (bool)element.GetValue(UseNativePaddingProperty);
+		}
+
+		public static void SetUseNativePadding(BindableObject element, bool value)
+		{
+			element.SetValue(UseNativePaddingProperty, value);
+		}
+
+		public static bool UseNativePadding(this IPlatformElementConfiguration<Android, FormsElement> config)
+		{
+			return GetUseNativePadding(config.Element);
+		}
+
+		public static IPlatformElementConfiguration<Android, FormsElement> SetUseNativePadding(this IPlatformElementConfiguration<Android, FormsElement> config, bool value)
+		{
+			SetUseNativePadding(config.Element, value);
+			return config;
+		}
+	}
+}
