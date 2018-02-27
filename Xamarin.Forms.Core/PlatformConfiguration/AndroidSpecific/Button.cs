79f5ada72465f2b6df64364340f9074d4b3eeb2a
Samantha Houts <samantha.houts@xamarin.com>
2018-02-27 11:57:23 -0800

    [Core] Add UseDefaultShadow and UseDefaultPadding platform specifics

--- Xamarin.Forms.Core/PlatformConfiguration/AndroidSpecific/Button.cs
+++ Xamarin.Forms.Core/PlatformConfiguration/AndroidSpecific/Button.cs
@@ -4,27 +4,54 @@
 
 	public static class Button
 	{
-		public static readonly BindableProperty UseNativePaddingProperty = BindableProperty.Create("UseNativePadding", typeof(bool), typeof(Button), true);
+		#region UseDefaultPadding
+		public static readonly BindableProperty UseDefaultPaddingProperty = BindableProperty.Create("UseDefaultPadding", typeof(bool), typeof(Button), false);
 
-		public static bool GetUseNativePadding(BindableObject element)
+		public static bool GetUseDefaultPadding(BindableObject element)
 		{
-			return (bool)element.GetValue(UseNativePaddingProperty);
+			return (bool)element.GetValue(UseDefaultPaddingProperty);
 		}
 
-		public static void SetUseNativePadding(BindableObject element, bool value)
+		public static void SetUseDefaultPadding(BindableObject element, bool value)
 		{
-			element.SetValue(UseNativePaddingProperty, value);
+			element.SetValue(UseDefaultPaddingProperty, value);
 		}
 
-		public static bool UseNativePadding(this IPlatformElementConfiguration<Android, FormsElement> config)
+		public static bool UseDefaultPadding(this IPlatformElementConfiguration<Android, FormsElement> config)
 		{
-			return GetUseNativePadding(config.Element);
+			return GetUseDefaultPadding(config.Element);
 		}
 
-		public static IPlatformElementConfiguration<Android, FormsElement> SetUseNativePadding(this IPlatformElementConfiguration<Android, FormsElement> config, bool value)
+		public static IPlatformElementConfiguration<Android, FormsElement> SetUseDefaultPadding(this IPlatformElementConfiguration<Android, FormsElement> config, bool value)
 		{
-			SetUseNativePadding(config.Element, value);
+			SetUseDefaultPadding(config.Element, value);
 			return config;
 		}
+		#endregion
+
+		#region UseDefaultShadow
+		public static readonly BindableProperty UseDefaultShadowProperty = BindableProperty.Create("UseDefaultShadow", typeof(bool), typeof(Button), false);
+
+		public static bool GetUseDefaultShadow(BindableObject element)
+		{
+			return (bool)element.GetValue(UseDefaultShadowProperty);
+		}
+
+		public static void SetUseDefaultShadow(BindableObject element, bool value)
+		{
+			element.SetValue(UseDefaultShadowProperty, value);
+		}
+
+		public static bool UseDefaultShadow(this IPlatformElementConfiguration<Android, FormsElement> config)
+		{
+			return GetUseDefaultShadow(config.Element);
+		}
+
+		public static IPlatformElementConfiguration<Android, FormsElement> SetUseDefaultShadow(this IPlatformElementConfiguration<Android, FormsElement> config, bool value)
+		{
+			SetUseDefaultShadow(config.Element, value);
+			return config;
+		}
+		#endregion
 	}
 }
