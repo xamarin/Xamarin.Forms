327c62ca0f8c44c79640b223377a31c6a497e267
Rui Marinho <me@ruimarinho.net>
2018-03-14 12:37:35 +0000

    [Controls] Set UINavigationBar LargeTitleText appearance

--- Xamarin.Forms.ControlGallery.iOS/AppDelegate.cs
+++ Xamarin.Forms.ControlGallery.iOS/AppDelegate.cs
@@ -169,6 +169,13 @@ namespace Xamarin.Forms.ControlGallery.iOS
 				}
 			};
 
+			// 'Large' Title bar text
+			UINavigationBar.Appearance.LargeTitleTextAttributes = new UIStringAttributes
+			{
+				ForegroundColor = UIColor.FromRGB(0xE7, 0x63, 0x3B), // e7963b dark
+				Font = UIFont.FromName("GillSans-Italic", 40)
+			};
+
 			var app = new App();
 			_app = app;
 
