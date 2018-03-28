2fee0450ae5ee43f497452a9ebb24d9fb759eefe
lateralusX <lateralusx.github@gmail.com>
2018-03-28 13:34:49 +0200

    Add animated image Android implementation.
    Also include GIF image parser base class + android parser sub class.

--- Xamarin.Forms.Platform.Android/Extensions/ImageViewExtensions.cs
+++ Xamarin.Forms.Platform.Android/Extensions/ImageViewExtensions.cs
@@ -6,8 +6,55 @@ using AImageView = Android.Widget.ImageView;
 
 namespace Xamarin.Forms.Platform.Android
 {
+
 	internal static class ImageViewExtensions
 	{
+		public static void Reset(this AnimationDrawable animation)
+		{
+			if (!animation.IsDisposed())
+			{
+				animation.Stop();
+				int frameCount = animation.NumberOfFrames;
+				for (int i = 0; i < frameCount; i++)
+				{
+					var currentFrame = animation.GetFrame(i);
+					if (currentFrame is BitmapDrawable bitmapDrawable)
+					{
+						var bitmap = bitmapDrawable.Bitmap;
+						if (bitmap != null)
+						{
+							if (!bitmap.IsRecycled)
+							{
+								bitmap.Recycle();
+							}
+							bitmap.Dispose();
+							bitmap = null;
+						}
+						bitmapDrawable.Dispose();
+						bitmapDrawable = null;
+					}
+					currentFrame = null;
+				}
+				animation = null;
+			}
+		}
+
+		public static void Reset(this AImageView imageView)
+		{
+			if (!imageView.IsDisposed())
+			{
+				if (imageView.Drawable is AnimationDrawable animation)
+				{
+					imageView.SetImageDrawable(null);
+					animation.Reset();
+					animation.Dispose();
+					animation = null;
+				}
+
+				imageView.SetImageResource(global::Android.Resource.Color.Transparent);
+			}
+		}
+
 		// TODO hartez 2017/04/07 09:33:03 Review this again, not sure it's handling the transition from previousImage to 'null' newImage correctly
 		public static async Task UpdateBitmap(this AImageView imageView, Image newImage, ImageSource source, Image previousImage = null, ImageSource previousImageSource = null)
 		{
@@ -29,16 +76,17 @@ namespace Xamarin.Forms.Platform.Android
 
 			(imageView as IImageRendererController)?.SkipInvalidate();
 
-			imageView.SetImageResource(global::Android.Resource.Color.Transparent);
+			imageView.Reset();
 
 			Bitmap bitmap = null;
+			AnimationDrawable animation = null;
 			Drawable drawable = null;
+			IImageSourceHandlerEx handler;
+			bool useAnimation = newImage.IsSet(Image.AnimationPlayBehaviorProperty) || newImage.IsSet(Image.IsAnimationPlayingProperty);
 
-			IImageSourceHandler handler;
-
-			if (source != null && (handler = Internals.Registrar.Registered.GetHandlerForObject<IImageSourceHandler>(source)) != null)
+			if (source != null && (handler = Internals.Registrar.Registered.GetHandlerForObject<IImageSourceHandlerEx>(source)) != null)
 			{
-				if (handler is FileImageSourceHandler)
+				if (handler is FileImageSourceHandler && !useAnimation)
 				{
 					drawable = imageView.Context.GetDrawable((FileImageSource)source);
 				}
@@ -47,7 +95,10 @@ namespace Xamarin.Forms.Platform.Android
 				{
 					try
 					{
-						bitmap = await handler.LoadImageAsync(source, imageView.Context);
+						if (!useAnimation)
+							bitmap = await handler.LoadImageAsync(source, imageView.Context);
+						else
+							animation = await handler.LoadImageAnimationAsync(source, imageView.Context);
 					}
 					catch (TaskCanceledException)
 					{
@@ -57,22 +108,27 @@ namespace Xamarin.Forms.Platform.Android
 			}
 
 			// Check if the source on the new image has changed since the image was loaded
-			if (newImage != null && !Equals(newImage.Source, source))
+			if ((newImage != null && !Equals(newImage.Source, source)) || imageView.IsDisposed())
 			{
 				bitmap?.Dispose();
+				animation?.Reset();
+				animation?.Dispose();
 				return;
 			}
 
-			if (!imageView.IsDisposed())
+			if (drawable != null)
 			{
-				if (bitmap == null && drawable != null)
-				{
-					imageView.SetImageDrawable(drawable);
-				}
-				else
-				{
-					imageView.SetImageBitmap(bitmap);
-				}
+				imageView.SetImageDrawable(drawable);
+			}
+			else if (bitmap != null)
+			{
+				imageView.SetImageBitmap(bitmap);
+			}
+			else if (animation != null)
+			{
+				imageView.SetImageDrawable(animation);
+				if ((Image.AnimationPlayBehaviorValue)newImage.GetValue(Image.AnimationPlayBehaviorProperty) == Image.AnimationPlayBehaviorValue.OnLoad)
+					animation.Start();
 			}
 
 			bitmap?.Dispose();
--- Xamarin.Forms.Platform.Android/Renderers/FileImageSourceHandler.cs
+++ Xamarin.Forms.Platform.Android/Renderers/FileImageSourceHandler.cs
@@ -1,13 +1,18 @@
+using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Android;
 using Android.Content;
+using Android.Content.Res;
 using Android.Graphics;
-using Xamarin.Forms.Internals;
+using Android.Graphics.Drawables;
+using Android.Util;
 
 namespace Xamarin.Forms.Platform.Android
 {
-	public sealed class FileImageSourceHandler : IImageSourceHandler
+	public sealed class FileImageSourceHandler : IImageSourceHandlerEx
 	{
 		// This is set to true when run under designer context
 		internal static bool DecodeSynchronously {
@@ -26,10 +31,34 @@ namespace Xamarin.Forms.Platform.Android
 
 			if (bitmap == null)
 			{
-				Log.Warning(nameof(FileImageSourceHandler), "Could not find image or image file was invalid: {0}", imagesource);
+				Internals.Log.Warning(nameof(FileImageSourceHandler), "Could not find image or image file was invalid: {0}", imagesource);
 			}
 
 			return bitmap;
+		}
+
+		public async Task<AnimationDrawable> LoadImageAnimationAsync(ImageSource imagesource, Context context, CancellationToken cancelationToken = default(CancellationToken))
+		{
+			string file = ((FileImageSource)imagesource).File;
+
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.InJustDecodeBounds = true;
+
+			if (!DecodeSynchronously)
+				await BitmapFactory.DecodeResourceAsync(context.Resources, ResourceManager.GetDrawableByName(file), options);
+			else
+				BitmapFactory.DecodeResource(context.Resources, ResourceManager.GetDrawableByName(file), options);
+
+			var decoder = new AndroidGIFImageParser(context, options.InDensity, options.InTargetDensity);
+			using (var stream = context.Resources.OpenRawResource(ResourceManager.GetDrawableByName(file)))
+			{
+				if (!DecodeSynchronously)
+					await decoder.ParseAsync(stream);
+				else
+					decoder.ParseAsync(stream).Wait();
+			}
+
+			return decoder.Animation;
 		}
 	}
 }
