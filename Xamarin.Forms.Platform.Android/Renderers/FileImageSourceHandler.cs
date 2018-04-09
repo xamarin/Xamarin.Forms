5f51054f979cda1987a0bf3e20e18f37ee89fc15
lateralusX <lateralusx.github@gmail.com>
2018-04-09 11:32:33 +0200

    Reduce number of temporary bitmaps when scaling.

--- Xamarin.Forms.Platform.Android/Renderers/FileImageSourceHandler.cs
+++ Xamarin.Forms.Platform.Android/Renderers/FileImageSourceHandler.cs
@@ -46,10 +46,9 @@ namespace Xamarin.Forms.Platform.Android
 			else
 				BitmapFactory.DecodeResource(context.Resources, ResourceManager.GetDrawableByName(file), options);
 
-			using (var stream = context.Resources.OpenRawResource(ResourceManager.GetDrawableByName(file)))
+			using (var stream = context.Resources.OpenRawResource(ResourceManager.GetDrawableByName (file)))
+			using (var decoder = new AndroidGIFImageParser(context, options.InDensity, options.InTargetDensity))
 			{
-				var decoder = new AndroidGIFImageParser(context, options.InDensity, options.InTargetDensity);
-
 				try
 				{
 					if (!DecodeSynchronously)
