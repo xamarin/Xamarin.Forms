e56b01a277bc8d25f8eca7bbcf53d3c5c927fb2e
E.Z. Hart <hartez@gmail.com>
2018-05-09 13:44:11 -0600

    Call ChangeVisualState when new states are added so element is in correct state when loaded from XAML; fixes #2625

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2625.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 2625, "VisualStateManager attached to Button seems to not work on Android", PlatformAffected.Android)]
+	public partial class Issue2625 : ContentPage
+	{
+		public Issue2625 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
