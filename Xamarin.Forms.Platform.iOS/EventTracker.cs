73a59090ff3c88a857ef822620b93a7e9e22214a
Sean Davies <sean.davies@kingspan.com>
2018-05-14 08:44:03 +0100

    Added ShouldRecognizeSimultaneously

--- Xamarin.Forms.Platform.iOS/EventTracker.cs
+++ Xamarin.Forms.Platform.iOS/EventTracker.cs
@@ -313,7 +313,7 @@ namespace Xamarin.Forms.Platform.MacOS
 			var result = new UISwipeGestureRecognizer();
 			result.NumberOfTouchesRequired = (uint)numFingers;
 			result.Direction = (UISwipeGestureRecognizerDirection)direction;
-			result.ShouldRecognizeSimultaneously = true;
+			result.ShouldRecognizeSimultaneously = (g, o) => true;
 			result.AddTarget(() => action(direction));
 			return result;
 		}
