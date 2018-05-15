ee4afe68eb360d7c93201e83cdb5860a03e40140
Sean Davies <sean.davies@kingspan.com>
2018-05-15 15:27:55 +0100

    rebase

--- Xamarin.Forms.Platform.Android/GestureManager.cs
+++ Xamarin.Forms.Platform.Android/GestureManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using Android.Content;
@@ -121,7 +122,13 @@ namespace Xamarin.Forms.Platform.Android
 		GestureDetector InitializeTapAndPanAndSwipeDetector()
 		{
 			var context = Control.Context;
-			var listener = new InnerGestureListener(new TapGestureHandler(() => View),
+			var listener = new InnerGestureListener(new TapGestureHandler(() => View, () =>
+			{
+				if (Element is View view)
+					return view.GetChildElements(Point.Zero) ?? new List<GestureElement>();
+
+				return new List<GestureElement>();
+			}),
 				new PanGestureHandler(() => View, context.FromPixels),
 				new SwipeGestureHandler(() => View, context.FromPixels));
 
