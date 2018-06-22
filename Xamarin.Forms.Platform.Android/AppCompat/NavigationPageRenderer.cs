f8ccc9e859120ae632029db12914cafa9d240c30
Samantha Houts <samantha.houts@xamarin.com>
2018-06-22 15:35:03 -0700

    [Android] Consolidate redundant check
    combine my check with the one added in #3021 for great success

--- Xamarin.Forms.Platform.Android/AppCompat/NavigationPageRenderer.cs
+++ Xamarin.Forms.Platform.Android/AppCompat/NavigationPageRenderer.cs
@@ -46,8 +46,6 @@ namespace Xamarin.Forms.Platform.Android.AppCompat
 		MasterDetailPage _masterDetailPage;
 		bool _toolbarVisible;
 		bool _isAttachedToWindow;
-		bool _didInitialPushPages;
-
 
 		// The following is based on https://android.googlesource.com/platform/frameworks/support.git/+/4a7e12af4ec095c3a53bb8481d8d92f63157c3b7/v4/java/android/support/v4/app/FragmentManager.java#677
 		// Must be overriden in a custom renderer to match durations in XML animation resource files
@@ -930,19 +928,15 @@ namespace Xamarin.Forms.Platform.Android.AppCompat
 		
 		void PushCurrentPages()
 		{
-			if (_didInitialPushPages)
+			if (_fragmentStack.Count > 0)
 				return;
 
 			var navController = (INavigationPageController)Element;
 
 			foreach (Page page in navController.Pages)
 			{
-				if (GetPageFragment(page) != null)
-					continue;
-
 				PushViewAsync(page, false);
 			}
-			_didInitialPushPages = true;
 		}
 
 		bool IsAttachedToRoot()
