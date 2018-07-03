6d6949df97236423d73699ce2e091484ecd310f9
John VanSickle <jwvansickle@icloud.com>
2018-07-02 20:10:47 -0400

    Add basic application key shortcuts
    A Xam.Forms standard NSApplication override is necessary to allow basic keyboard shortcuts while not using any application menus. The FormsApplication extends NSApplication while overriding the SendEvent method to be able to intercept key events to enable basic keyboard command functionality for text fields and views. The FormsApplication is used as the Principal class attribute in a project Info.plist. Until the option is added as a VS for mac item, the Info.plist file must be opened using XCode to alter the value

---
+++ Xamarin.Forms.Platform.MacOS/FormsApplication.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using AppKit;
+using Foundation;
+
+namespace Xamarin.Forms.Platform.macOS
+{
+	[Register("FormsApplication")]
+	public class FormsApplication : NSApplication
+	{
+		public FormsApplication(NSCoder coder) : base(coder)
+		{
+		}
+
+		protected FormsApplication(NSObjectFlag t) : base(t)
+		{
+		}
+
+		protected internal FormsApplication(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void SendEvent(NSEvent theEvent)
+		{
+			if (theEvent.Type == NSEventType.KeyDown)
+			{
+				if ((theEvent.ModifierFlags & NSEventModifierMask.DeviceIndependentModifierFlagsMask) == NSEventModifierMask.CommandKeyMask)
+					switch (theEvent.KeyCode)
+					{
+						case (ushort)NSKey.V:
+							if (SendAction(new ObjCRuntime.Selector("paste:"), null, this))
+								return;
+							break;
+						case (ushort)NSKey.C:
+							if (SendAction(new ObjCRuntime.Selector("copy:"), null, this))
+								return;
+							break;
+						case (ushort)NSKey.X:
+							if (SendAction(new ObjCRuntime.Selector("cut:"), null, this))
+								return;
+							break;
+						case (ushort)NSKey.Z:
+							if (SendAction(new ObjCRuntime.Selector("undo:"), null, this))
+								return;
+							break;
+						case (ushort)NSKey.A:
+							if (SendAction(new ObjCRuntime.Selector("selectAll:"), null, this))
+								return;
+							break;
+					}
+				else if ((theEvent.ModifierFlags & NSEventModifierMask.DeviceIndependentModifierFlagsMask) == (NSEventModifierMask.ShiftKeyMask | NSEventModifierMask.CommandKeyMask))
+				{
+					switch (theEvent.KeyCode)
+					{
+						case (ushort)NSKey.Z:
+							if (SendAction(new ObjCRuntime.Selector("redo:"), null, this))
+								return;
+							break;
+					}
+				}
+			}
+
+			base.SendEvent(theEvent);
+		}
+	}
+}
