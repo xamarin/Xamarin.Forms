9a411ec84c131f94acd66cf5ad8cc2e18f8dac2f
Vitaly Knyazev <vitaly.knyazev@outlook.com>
2018-07-12 22:07:20 +0100

    Prevent crash when a leaked cell in ListView with Recycle mode has MenuItem with bindings and framework like Xamarin.Forms sets BindingContext to null after leaving the page

--- Xamarin.Forms.Platform.iOS/ContextActionCell.cs
+++ Xamarin.Forms.Platform.iOS/ContextActionCell.cs
@@ -632,6 +632,9 @@ namespace Xamarin.Forms.Platform.iOS
 
 		void SetupSelection(UITableView table)
 		{
+			if (table.GestureRecognizers == null)
+				return;
+
 			for (var i = 0; i < table.GestureRecognizers.Length; i++)
 			{
 				var r = table.GestureRecognizers[i] as SelectGestureRecognizer;
