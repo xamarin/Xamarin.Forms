307632ca10a93b780b27b3f3cb45fb54972f0aa0
Pavel Yakovlev (Akvelon) <v-payako@microsoft.com>
2018-07-23 09:19:16 +0300

    test improvement

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue1469.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue1469.cs
@@ -1,6 +1,12 @@
 ﻿using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 using System.Linq;
+using System.Threading.Tasks;
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
 
 namespace Xamarin.Forms.Controls.Issues
 {
@@ -10,31 +16,43 @@ namespace Xamarin.Forms.Controls.Issues
 	{
 		protected override void Init()
 		{
-			var statusLabel = new Label();
+			var statusLabel = new Label() { FontSize = 40 };
 			var _items = Enumerable.Range(1, 4).Select(i => $"Item {i}").ToArray();
 			var list = new ListView()
 			{
 				ItemsSource = _items
 			};
-			list.ItemSelected += (_, e) =>
+
+			list.ItemSelected += async (_, e) =>
 			{
 				if (e.SelectedItem == null)
-				{
-					statusLabel.Text = "Success"; // last selected item must be null
 					return;
-				}
-				statusLabel.Text = "Fail";
 				list.SelectedItem = null;
+
+				statusLabel.Text = "One moment please...";
+				await Task.Delay(500);
+				statusLabel.Text = list.SelectedItem == null ? "Success" : "Fail";
 			};
+
 			Content = new StackLayout
 			{
 				Children = {
-					statusLabel,
+					new Label { Text = "If you click an item in the list it should not become selected" },
 					new Button { Text = "Select 3rd item", Command = new Command(() => list.SelectedItem = _items[2]) },
 					new Button { Text = "Clear selection", Command = new Command(() => list.SelectedItem = list.SelectedItem = null) },
+					statusLabel,
 					list
 				}
 			};
 		}
+
+#if UITEST
+		[Test]
+		public void Issue1469Test()
+		{
+			RunningApp.Tap("Select 3rd item");
+			RunningApp.WaitForElement("Success");
+		}
+#endif
 	}
 }
