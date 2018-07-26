e504e83169ba382598c8d9cf8aaa503177477ddd
Pavel Yakovlev (Akvelon) <v-payako@microsoft.com>
2018-07-26 18:29:37 +0300

    improve uitest

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue3273.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue3273.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
 
 namespace Xamarin.Forms.Controls.Issues
 {
@@ -10,6 +15,8 @@ namespace Xamarin.Forms.Controls.Issues
 	{
 		protected override void Init ()
 		{
+			var statusLabel = new Label();
+			var actionLabel = new Label();
 			var Items = new ObservableCollection<string>
 			{
 				"apple",
@@ -19,7 +26,11 @@ namespace Xamarin.Forms.Controls.Issues
 			};
 			BindingContext = Items;
 
-			Items.CollectionChanged += (_, e) => System.Diagnostics.Debug.WriteLine($"{e.Action} action fired.");
+			Items.CollectionChanged += (_, e) =>
+			{
+				statusLabel.Text = "Success";
+				actionLabel.Text = $"<{DateTime.Now.ToLongTimeString()}> {e.Action} action fired.";
+			};
 			Items.RemoveAt(3);
 			Items.Move(0, 1);
 
@@ -29,13 +40,29 @@ namespace Xamarin.Forms.Controls.Issues
 			Content = new StackLayout
 			{
 				Children = {
+					statusLabel,
+					actionLabel,
 					new Button {
 						Text = "Move items",
-						Command = new Command(() => Items.Move(0, 1))
+						Command = new Command(() =>
+						{
+							actionLabel.Text = string.Empty;
+							statusLabel.Text = "Failed";
+							Items.Move(0, 1); })
 					},
 					listView
 				}
 			};
 		}
+
+#if UITEST
+		[Test]
+		public void Issue3273Test()
+		{
+			RunningApp.WaitForElement("Move items");
+			RunningApp.Tap("Move items");
+			RunningApp.WaitForElement("Success");
+		}
+#endif
 	}
 }
