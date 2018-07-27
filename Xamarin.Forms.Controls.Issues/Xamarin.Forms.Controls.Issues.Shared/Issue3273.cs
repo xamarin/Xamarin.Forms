331c5dc5d1bd8635c1f80ef1d1503cd4b4894212
Pavel Yakovlev (Akvelon) <v-payako@microsoft.com>
2018-07-27 16:17:13 +0300

    added a sortable custom renderer for UItest

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue3273.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue3273.cs
@@ -13,45 +13,53 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue (IssueTracker.Github, 3273, "Drag and drop reordering not firing CollectionChanged", PlatformAffected.UWP)]
 	public class Issue3273 : TestContentPage
 	{
+		public class SortableListView : ListView
+		{
+		}
+
 		protected override void Init ()
 		{
 			var statusLabel = new Label();
 			var actionLabel = new Label();
 			var Items = new ObservableCollection<string>
 			{
-				"apple",
-				"orange",
-				"pear",
-				"trash"
+				"drag",
+				"and",
+				"drop",
+				"me",
+				"please"
 			};
 			BindingContext = Items;
 
 			Items.CollectionChanged += (_, e) =>
 			{
 				statusLabel.Text = "Success";
-				actionLabel.Text = $"<{DateTime.Now.ToLongTimeString()}> {e.Action} action fired.";
+				var log = $"<{DateTime.Now.ToLongTimeString()}> {e.Action} action fired.";
+				actionLabel.Text += $"{log}{Environment.NewLine}";
+				System.Diagnostics.Debug.WriteLine(log);
 			};
-			Items.RemoveAt(3);
+			Items.RemoveAt(4);
 			Items.Move(0, 1);
 
-			var listView = new ListView();
+			var listView = new SortableListView();
 			listView.SetBinding(ListView.ItemsSourceProperty, ".");
 
 			Content = new StackLayout
 			{
 				Children = {
 					statusLabel,
-					actionLabel,
 					new Button {
 						Text = "Move items",
 						Command = new Command(() =>
 						{
 							actionLabel.Text = string.Empty;
 							statusLabel.Text = "Failed";
-							Items.Move(0, 1); })
+							Items.Move(0, 1);
+						})
 					},
 					listView,
-					new ListView()
+					new ListView(),
+					actionLabel
 				}
 			};
 		}
