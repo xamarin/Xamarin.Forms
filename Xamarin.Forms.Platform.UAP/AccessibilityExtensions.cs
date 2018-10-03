46947d947126974c86ae74a852f660335f4f39f0
Rui Marinho <me@ruimarinho.net>
2018-10-03 17:25:27 +0100

    [UWP]  Set Accessibility properties on ToolbarItems

--- Xamarin.Forms.Platform.UAP/AccessibilityExtensions.cs
+++ Xamarin.Forms.Platform.UAP/AccessibilityExtensions.cs
@@ -91,5 +91,37 @@ namespace Xamarin.Forms.Platform.UWP
 			return _defaultAutomationPropertiesLabeledBy;
 		}
 
+		// TODO: This is not having any effect on anything I've tested yet. See if we need it  
+		// after we test the MDP and NP w/ back button explicitly enabled. 
+		public static void SetBackButtonTitle(this PageControl Control, Element Element)
+		{
+			if (Element == null)
+				return;
+
+			var elemValue = ConcatenateNameAndHint(Element);
+
+			Control.BackButtonTitle = elemValue;
+		}
+
+		static string ConcatenateNameAndHint(Element Element)
+		{
+			string separator;
+			
+			var name = (string)Element.GetValue(AutomationProperties.NameProperty);
+
+			var hint = (string)Element.GetValue(AutomationProperties.HelpTextProperty);
+
+
+			if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(hint))
+				separator = "";
+
+			else
+				separator = ". ";
+
+
+			return string.Join(separator, name, hint);
+
+		}
+
 	}
 }
