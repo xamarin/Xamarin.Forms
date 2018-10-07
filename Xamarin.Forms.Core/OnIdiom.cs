121019e05933c7e71f73446b1b7162e0a062532a
Martin Zikmund <martinzikmund@sphereline.com>
2018-10-07 12:58:37 +0200

    Bringing OnIdiom in line with OnPlatform, now supports default value, multiple idioms per setting and gracefully falls back to legacy

--- Xamarin.Forms.Core/OnIdiom.cs
+++ Xamarin.Forms.Core/OnIdiom.cs
@@ -1,33 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms.Xaml;
+
 namespace Xamarin.Forms
 {
+	[ContentProperty("Idioms")]
 	public class OnIdiom<T>
 	{
-		public T Phone { get; set; }
+		readonly Dictionary<TargetIdiom, T> _legacyValues = new Dictionary<TargetIdiom, T>();
 
-		public T Tablet { get; set; }
+		bool _hasDefault;		
+
+		T _default;
 		
-		public T Desktop { get; set; }
 
-		public T TV { get; set; }
+		public OnIdiom()
+		{
+			Idioms = new List<OnIdiomSetting>();
+		}
 
-		public T Watch { get; set; }
+		public T Phone
+		{
+			get => GetLegacyValue(TargetIdiom.Phone);
+			set => SetLegacyValue(TargetIdiom.Phone, value);
+		}
+
+		public T Tablet
+		{
+			get => GetLegacyValue(TargetIdiom.Tablet);
+			set => SetLegacyValue(TargetIdiom.Tablet, value);
+		}
+
+		public T Desktop
+		{
+			get => GetLegacyValue(TargetIdiom.Desktop);
+			set => SetLegacyValue(TargetIdiom.Desktop, value);
+		}
+
+		public T TV
+		{
+			get => GetLegacyValue(TargetIdiom.TV);
+			set => SetLegacyValue(TargetIdiom.TV, value);
+		}
+
+		public T Watch
+		{
+			get => GetLegacyValue(TargetIdiom.Watch);
+			set => SetLegacyValue(TargetIdiom.Watch, value);
+		}
+
+		public T Default
+		{
+			get { return _default; }
+			set
+			{
+				_hasDefault = true;
+				_default = value;
+			}
+		}
+
+		public IList<OnIdiomSetting> Idioms { get; private set; }
+
+#pragma warning disable RECS0108 // Warns about static fields in generic types
+		static readonly IValueConverterProvider s_valueConverter = DependencyService.Get<IValueConverterProvider>();
+#pragma warning restore RECS0108 // Warns about static fields in generic types
 
 		public static implicit operator T(OnIdiom<T> onIdiom)
 		{
-			switch (Device.Idiom)
+			var currentIdiom = Device.Idiom.ToString("G");
+			foreach (var idiomSetting in onIdiom.Idioms)
 			{
-				default:
-				case TargetIdiom.Phone:
-					return onIdiom.Phone;
-				case TargetIdiom.Tablet:
-					return onIdiom.Tablet;
-				case TargetIdiom.Desktop:
-					return onIdiom.Desktop;
-				case TargetIdiom.TV:
-					return onIdiom.TV;
-				case TargetIdiom.Watch:
-					return onIdiom.Watch;
+				if (idiomSetting.Idiom?.FirstOrDefault(idiom =>
+					    idiom.Equals(currentIdiom, StringComparison.OrdinalIgnoreCase)) == null)
+					continue;
+				if (s_valueConverter == null)
+					continue;
+				return (T)s_valueConverter.Convert(idiomSetting.Value, typeof(T), null, null);
 			}
+
+			//has any legacy value been set?
+			if (onIdiom._legacyValues.Count == 0)
+				return onIdiom._hasDefault ? onIdiom._default : default(T);
+
+			//legacy fallback
+#pragma warning disable 0618, 0612
+			if (!onIdiom._legacyValues.TryGetValue(Device.Idiom, out var legacyValue))
+			{
+				legacyValue = onIdiom._hasDefault ? onIdiom._default : default(T);
+			}
+			return legacyValue;
+#pragma warning restore 0618, 0612
 		}
+
+
+		T GetLegacyValue( TargetIdiom idiom )
+		{
+			return _legacyValues.TryGetValue(idiom, out var value) 
+				? value : default(T);
+		}
+
+		void SetLegacyValue(TargetIdiom idiom, T value)
+		{
+			_legacyValues[idiom] = value;
+		}
+	}
+
+	[ContentProperty("Value")]
+	public class OnIdiomSetting
+	{
+		[TypeConverter(typeof(ListStringTypeConverter))]
+		public IList<string> Idiom { get; set; }
+		public object Value { get; set; }
 	}
 }
