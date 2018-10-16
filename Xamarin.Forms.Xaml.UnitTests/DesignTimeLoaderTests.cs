87050415dedb8df9dd6a4b571ea49889ae8cb73e
Stephane Delcroix <stephane@delcroix.org>
2018-10-16 13:51:59 +0200

    [Xaml] add FallbackTypeResolver

--- Xamarin.Forms.Xaml.UnitTests/DesignTimeLoaderTests.cs
+++ Xamarin.Forms.Xaml.UnitTests/DesignTimeLoaderTests.cs
@@ -1,26 +1,67 @@
-﻿//
-// DesignTimeLoaderTests.cs
-//
-// Author:
-//       Stephane Delcroix <stdelc@microsoft.com>
-//
-// Copyright (c) 2018 
-using System;
+﻿using System;
 
+using NUnit.Framework;
 using Xamarin.Forms;
+using Xamarin.Forms.Core.UnitTests;
 
 namespace Xamarin.Forms.Xaml.UnitTests
 {
-	public class DesignTimeLoaderTests : ContentPage
+	[TestFixture]
+	public class DesignTimeLoaderTests
 	{
-		public DesignTimeLoaderTests()
+		[SetUp]
+		public void Setup()
 		{
-			Content = new StackLayout {
-				Children = {
-					new Label { Text = "Hello ContentPage" }
-				}
-			};
+			Device.PlatformServices = new MockPlatformServices();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			XamlLoader.FallbackTypeResolver = null;
+			Device.PlatformServices = null;
+		}
+
+		[Test]
+		public void ContenPageWithMissingClass()
+		{
+			var xaml = @"
+				<ContentPage xmlns=""http://xamarin.com/schemas/2014/forms""
+					xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+					x:Class=""Xamarin.Forms.Xaml.UnitTests.CustomView""
+				/>";
+
+			Assert.That(XamlLoader.Create(xaml, true), Is.TypeOf<ContentPage>());
+		}
+
+		[Test]
+		public void ViewWithMissingClass()
+		{
+			var xaml = @"
+				<ContentView xmlns=""http://xamarin.com/schemas/2014/forms""
+					xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+					x:Class=""Xamarin.Forms.Xaml.UnitTests.CustomView""
+				/>";
+
+			Assert.That(XamlLoader.Create(xaml, true), Is.TypeOf<ContentView>());
+		}
+
+		[Test]
+		public void ContenPageWithMissingType()
+		{
+			XamlLoader.FallbackTypeResolver = (p, type) => type ?? typeof(MockView);
+
+			var xaml = @"
+				<ContentPage xmlns=""http://xamarin.com/schemas/2014/forms""
+					xmlns:local=""clr-namespace:MissingNamespace;assembly=MissingAssembly""
+					xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+					<ContentPage.Content>
+						<local:MyCustomButton />
+					</ContentPage.Content>
+				</ContentPage>";
+
+			var page = (ContentPage) XamlLoader.Create(xaml, true);
+			Assert.That(page.Content, Is.TypeOf<MockView>());
 		}
 	}
 }
-
