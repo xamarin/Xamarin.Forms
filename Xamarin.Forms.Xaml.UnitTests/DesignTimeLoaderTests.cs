79cf14beda7462e68ffa8564b8d3404e6618b445
Stephane Delcroix <stephane@delcroix.org>
2018-10-16 13:38:50 +0200

    [Xaml] un-obsolete XamlLoader.Create
    now that we plan to load Xaml files without compiling the ctor, keeping
    XamlLoader.Create makes sense.

---
+++ Xamarin.Forms.Xaml.UnitTests/DesignTimeLoaderTests.cs
@@ -0,0 +1,26 @@
+﻿//
+// DesignTimeLoaderTests.cs
+//
+// Author:
+//       Stephane Delcroix <stdelc@microsoft.com>
+//
+// Copyright (c) 2018 
+using System;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public class DesignTimeLoaderTests : ContentPage
+	{
+		public DesignTimeLoaderTests()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
