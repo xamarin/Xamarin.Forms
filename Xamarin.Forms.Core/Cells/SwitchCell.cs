d298b16afee622cdf4520fe4836a3af1b8bec981
Nathaniel Nunes <manutdkid77@gmail.com>
2018-10-20 12:40:18 +0530

    Used nameof on the OnColor property

--- Xamarin.Forms.Core/Cells/SwitchCell.cs
+++ Xamarin.Forms.Core/Cells/SwitchCell.cs
@@ -12,7 +12,7 @@ namespace Xamarin.Forms
 
 		public static readonly BindableProperty TextProperty = BindableProperty.Create("Text", typeof(string), typeof(SwitchCell), default(string));
 
-		public static readonly BindableProperty OnColorProperty = BindableProperty.Create("OnColor", typeof(Color), typeof(SwitchCell), Color.Default);
+		public static readonly BindableProperty OnColorProperty = BindableProperty.Create(nameof(OnColor), typeof(Color), typeof(SwitchCell), Color.Default);
 
 		public Color OnColor
 		{
