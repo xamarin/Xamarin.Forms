aeb61396a1728f0b7a73ab13bba3130a4e37d68c
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2018-10-25 02:27:23 +0300

    updated DS and sample

--- Xamarin.Forms.Core/DependencyService.cs
+++ Xamarin.Forms.Core/DependencyService.cs
@@ -10,7 +10,9 @@ namespace Xamarin.Forms
 	{
 		static bool s_initialized;
 
-		static readonly object SyncLock = new object();
+		static readonly object s_dependencyLock = new object();
+		static readonly object s_initializeLock = new object();
+
 		static readonly List<Type> DependencyTypes = new List<Type>();
 		static readonly Dictionary<Type, DependencyData> DependencyImplementations = new Dictionary<Type, DependencyData>();
 
@@ -26,7 +28,7 @@ namespace Xamarin.Forms
 			Initialize();
 
 			DependencyData dependencyImplementation;
-			lock (SyncLock)
+			lock (s_dependencyLock)
 			{
 				Type targetType = typeof(T);
 				if (!DependencyImplementations.ContainsKey(targetType))
@@ -44,7 +46,13 @@ namespace Xamarin.Forms
 			{
 				if (dependencyImplementation.GlobalInstance == null)
 				{
-					dependencyImplementation.GlobalInstance = Activator.CreateInstance(dependencyImplementation.ImplementorType);
+					lock (dependencyImplementation)
+					{
+						if (dependencyImplementation.GlobalInstance == null)
+						{
+							dependencyImplementation.GlobalInstance = Activator.CreateInstance(dependencyImplementation.ImplementorType);
+						}
+					}
 				}
 				return (T)dependencyImplementation.GlobalInstance;
 			}
@@ -65,10 +73,8 @@ namespace Xamarin.Forms
 			if (!DependencyTypes.Contains(targetType))
 				DependencyTypes.Add(targetType);
 
-			lock (SyncLock)
-			{
+			lock (s_dependencyLock)
 				DependencyImplementations[targetType] = new DependencyData { ImplementorType = implementorType };
-			}
 		}
 
 		static Type FindImplementor(Type target)
@@ -79,57 +85,65 @@ namespace Xamarin.Forms
 		static void Initialize()
 		{
 			if (s_initialized)
-			{
 				return;
-			}
 
-			Assembly[] assemblies = Device.GetAssemblies();
-			if (Internals.Registrar.ExtraAssemblies != null)
+			lock (s_initializeLock)
 			{
-				assemblies = assemblies.Union(Internals.Registrar.ExtraAssemblies).ToArray();
-			}
+				if (s_initialized)
+					return;
 
-			Initialize(assemblies);
+				Assembly[] assemblies = Device.GetAssemblies();
+				if (Internals.Registrar.ExtraAssemblies != null)
+				{
+					assemblies = assemblies.Union(Internals.Registrar.ExtraAssemblies).ToArray();
+				}
+
+				Initialize(assemblies);
+			}
 		}
 
 		internal static void Initialize(Assembly[] assemblies)
 		{
 			if (s_initialized)
-			{
 				return;
-			}
 
-			Type targetAttrType = typeof(DependencyAttribute);
-
-			// Don't use LINQ for performance reasons
-			// Naive implementation can easily take over a second to run
-			foreach (Assembly assembly in assemblies)
+			lock (s_initializeLock)
 			{
-				Attribute[] attributes;
-				try
-				{
-					attributes = assembly.GetCustomAttributes(targetAttrType).ToArray();
-				}
-				catch (System.IO.FileNotFoundException)
-				{
-					// Sometimes the previewer doesn't actually have everything required for these loads to work
-					Log.Warning(nameof(Registrar), "Could not load assembly: {0} for Attibute {1} | Some renderers may not be loaded", assembly.FullName, targetAttrType.FullName);
-					continue;
-				}
-				
-				if (attributes.Length == 0)
-					continue;
+				if (s_initialized)
+					return;
 
-				foreach (DependencyAttribute attribute in attributes)
+				Type targetAttrType = typeof(DependencyAttribute);
+
+				// Don't use LINQ for performance reasons
+				// Naive implementation can easily take over a second to run
+				foreach (Assembly assembly in assemblies)
 				{
-					if (!DependencyTypes.Contains(attribute.Implementor))
+					Attribute[] attributes;
+					try
 					{
-						DependencyTypes.Add(attribute.Implementor);
+						attributes = assembly.GetCustomAttributes(targetAttrType).ToArray();
+					}
+					catch (System.IO.FileNotFoundException)
+					{
+						// Sometimes the previewer doesn't actually have everything required for these loads to work
+						Log.Warning(nameof(Registrar), "Could not load assembly: {0} for Attibute {1} | Some renderers may not be loaded", assembly.FullName, targetAttrType.FullName);
+						continue;
+					}
+
+					if (attributes.Length == 0)
+						continue;
+
+					foreach (DependencyAttribute attribute in attributes)
+					{
+						if (!DependencyTypes.Contains(attribute.Implementor))
+						{
+							DependencyTypes.Add(attribute.Implementor);
+						}
 					}
 				}
-			}
 
-			s_initialized = true;
+				s_initialized = true;
+			}
 		}
 
 		class DependencyData
