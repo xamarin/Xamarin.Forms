f4e74225796561d9ac9a204f9b8173e65d74c1c9
Florian Auer <FlorianAuer@users.noreply.github.com>
2018-10-24 19:29:10 +0200

    #issue 4203 Supply actual build version for all Xamarin.Forms* assemblies
    Added AssemblyVersion and AssemblyFileVersion

--- Xamarin.Forms.Controls/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Controls/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo ("Xamarin.Forms.ControlGallery.Android")]
+
+[assembly: AssemblyVersion("3.3.0.912540")]
+[assembly: AssemblyFileVersion("3.3.0.912540")]
