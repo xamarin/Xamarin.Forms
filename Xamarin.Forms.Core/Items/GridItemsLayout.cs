22bf65b7a195c0653d7a4d9dd13d4602ee40946c
E.Z. Hart <hartez@gmail.com>
2018-11-05 13:40:15 -0700

    Move classes into their own files

---
+++ Xamarin.Forms.Core/Items/GridItemsLayout.cs
@@ -0,0 +1,28 @@
+﻿namespace Xamarin.Forms
+{
+	public class GridItemsLayout : ItemsLayout
+	{
+		public static readonly BindableProperty SpanProperty =
+			BindableProperty.Create(nameof(Span), typeof(int), typeof(GridItemsLayout), 1, 
+				validateValue: (bindable, value) => (int)value >= 1);
+
+		public int Span
+		{
+			get => (int)GetValue(SpanProperty);
+			set => SetValue(SpanProperty, value);
+		}
+
+		public GridItemsLayout([Parameter("Span")] int span, [Parameter("Orientation")] ItemsLayoutOrientation orientation) :
+			base(orientation)
+		{
+			Span = span;
+		}
+
+		public override string ToString()
+		{
+			var orientation = Orientation == ItemsLayoutOrientation.Horizontal ? "Horizontal Grid" : "Vertical Grid";
+			
+			return $"{orientation}, {nameof(Span)} {Span}";
+		}
+	}
+}
---
+++ Xamarin.Forms.Core/Items/PropertyChangedEventArgsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace Xamarin.Forms
+{
+	// TODO hartez 2018/06/23 13:42:22 Trying this out for a nicer read in OnElementPropertyChanged, not sure if I like it yet	
+	public static class PropertyChangedEventArgsExtensions
+	{
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static bool Is(this PropertyChangedEventArgs args, BindableProperty property)
+		{
+			CollectionView.VerifyCollectionViewFlagEnabled();
+			return args.PropertyName == property.PropertyName;
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static bool IsOneOf(this PropertyChangedEventArgs args, params BindableProperty[] properties)
+		{
+			CollectionView.VerifyCollectionViewFlagEnabled();
+			foreach (BindableProperty property in properties)
+			{
+				if (args.PropertyName == property.PropertyName)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
