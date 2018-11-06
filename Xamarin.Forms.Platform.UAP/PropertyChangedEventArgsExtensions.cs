9e782b6ed7aae5cbbebc8c70a965f0f24c20da22
E.Z. Hart <hartez@gmail.com>
2018-11-06 10:47:09 -0700

    Move PropertyChangedEventArgsExtensions to internal on each platform

--- Xamarin.Forms.Platform.UAP/PropertyChangedEventArgsExtensions.cs
+++ Xamarin.Forms.Platform.UAP/PropertyChangedEventArgsExtensions.cs
@@ -1,21 +1,16 @@
 ﻿using System.ComponentModel;
 
-namespace Xamarin.Forms
+namespace Xamarin.Forms.Platform.UWP
 {
-	// TODO hartez 2018/06/23 13:42:22 Trying this out for a nicer read in OnElementPropertyChanged, not sure if I like it yet	
-	public static class PropertyChangedEventArgsExtensions
+	internal static class PropertyChangedEventArgsExtensions
 	{
-		[EditorBrowsable(EditorBrowsableState.Never)]
 		public static bool Is(this PropertyChangedEventArgs args, BindableProperty property)
 		{
-			CollectionView.VerifyCollectionViewFlagEnabled();
 			return args.PropertyName == property.PropertyName;
 		}
 
-		[EditorBrowsable(EditorBrowsableState.Never)]
 		public static bool IsOneOf(this PropertyChangedEventArgs args, params BindableProperty[] properties)
 		{
-			CollectionView.VerifyCollectionViewFlagEnabled();
 			foreach (BindableProperty property in properties)
 			{
 				if (args.PropertyName == property.PropertyName)
