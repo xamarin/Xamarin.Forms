94a6563bee4ce82ac3c45702a835fac284495314
Gerald Versluis <gerald@verslu.is>
2018-11-15 17:38:46 +0100

    Implemented reporting index for all ListView EventArgs

--- Xamarin.Forms.Core/ItemVisibilityEventArgs.cs
+++ Xamarin.Forms.Core/ItemVisibilityEventArgs.cs
@@ -4,11 +4,14 @@ namespace Xamarin.Forms
 {
 	public sealed class ItemVisibilityEventArgs : EventArgs
 	{
-		public ItemVisibilityEventArgs(object item)
+		public ItemVisibilityEventArgs(object item, int itemIndex)
 		{
 			Item = item;
+			ItemIndex = itemIndex;
 		}
 
 		public object Item { get; private set; }
+
+		public int ItemIndex { get; private set; }
 	}
 }
--- Xamarin.Forms.Core/SelectedItemChangedEventArgs.cs
+++ Xamarin.Forms.Core/SelectedItemChangedEventArgs.cs
@@ -4,11 +4,15 @@ namespace Xamarin.Forms
 {
 	public class SelectedItemChangedEventArgs : EventArgs
 	{
-		public SelectedItemChangedEventArgs(object selectedItem)
+		public SelectedItemChangedEventArgs(object selectedItem, int selectedItemIndex)
 		{
 			SelectedItem = selectedItem;
+			SelectedItemIndex = selectedItemIndex;
 		}
 
 		public object SelectedItem { get; private set; }
+
+		public int SelectedItemIndex { get; private set; }
+
 	}
 }
