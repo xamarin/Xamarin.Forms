e6caac68ddcf63ee1ef91c590e6158ab14443dfe
Rui Marinho <me@ruimarinho.net>
2018-11-15 17:09:13 +0000

    [Controls]Update issue 2580 with more info

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2580.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2580.cs
@@ -20,23 +20,24 @@ namespace Xamarin.Forms.Controls.Issues
 		{
 			var instructions = new Label { Text = "Manual check that both Label 1 have the same size, if the 1st is bigger than this test failed." };
 			var label = new Label { Text = "Label 1", BackgroundColor = Color.Red };
-			AutomationProperties.SetHelpText(label, "This is a The longer this is, the worse the problem");
+			AutomationProperties.SetHelpText(label, "The longer this label hit is, the worse the problem");
+			var image = new Image { Source = "bank.png", BackgroundColor = Color.Red };
+			AutomationProperties.SetHelpText(image, "The longer this image hint is, the worse the problem");
+			var image2 = new Image { Source = "bank.png", BackgroundColor = Color.Red };
 			var label2 = new Label { Text = "Label 1", BackgroundColor = Color.Red };
 			var horizontalLayout = new StackLayout
 			{
 				Orientation = StackOrientation.Horizontal,
-				Children = { label
-					//,new Label { Text = "label 2" }, new Label { Text = "label 3" }, new Label { Text = "label 4" } }
-				}
+				Children = { label, image, new Label { Text = "label 2" }, new Label { Text = "label 3" }, new Label { Text = "label 4" } }
 			};
 			Content = new StackLayout
 			{
 				Children = {
-					//instructions,
-					horizontalLayout
-					//, new StackLayout { Orientation = StackOrientation.Horizontal, Children = { label2, new Label { Text = "label 2" }, new Label { Text = "label 3" }, new Label { Text = "label 4" } } } } };
-			}
+					instructions,
+					horizontalLayout,
+					new StackLayout { Orientation = StackOrientation.Horizontal, Children = { label2, image2, new Label { Text = "label 2" }, new Label { Text = "label 3" }, new Label { Text = "label 4" } } } }
 			};
+
 		}
 	}
 }
