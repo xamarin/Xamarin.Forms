0b98185c3a098e76330dd87622d2b30ed7d6f308
Gerald Versluis <gerald@verslu.is>
2018-11-16 11:13:50 +0100

    Added back current ctors marked as obsolete

--- Xamarin.Forms.Core/SelectedItemChangedEventArgs.cs
+++ Xamarin.Forms.Core/SelectedItemChangedEventArgs.cs
@@ -4,6 +4,13 @@ namespace Xamarin.Forms
 {
 	public class SelectedItemChangedEventArgs : EventArgs
 	{
+		[Obsolete("Please use the constructor that reports the items index")]
+		public SelectedItemChangedEventArgs(object selectedItem)
+			: this(selectedItem, -1)
+		{
+
+		}
+
 		public SelectedItemChangedEventArgs(object selectedItem, int selectedItemIndex)
 		{
 			SelectedItem = selectedItem;
