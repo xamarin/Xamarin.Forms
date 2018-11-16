4c06bb05f85f5ebf9ff78100eb0ccbecc7f2ccdf
Rui Marinho <me@ruimarinho.net>
2018-11-16 18:41:27 +0000

    [Controls]Add instructions for issue #4026

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4026.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4026.cs
@@ -16,21 +16,16 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.Github, 4026, "Bindable Span height Issue", PlatformAffected.All)]
 	public class Issue4026 : TestContentPage
 	{
-		public Issue4026()
-		{
-			BindingContext = this;
-		}
 		protected override void Init()
 		{
+			Padding = new Thickness(0, 40, 0, 0);
 			BackgroundColor = Color.Aquamarine;
-			var layout = new StackLayout { VerticalOptions = LayoutOptions.Start, BackgroundColor = Color.CadetBlue, Padding = new Thickness(10) };
-			var btn = new Button { Text = "Add More Text to Span", Command = new Command(() => Title += " More Text") };
-			var spanBindable = new Span { TextColor = Color.Blue };
+			var btn = new Button { Text = "Add More Text to Span", Command = new Command(() => Title += " More Text is here") };
+			var spanBindable = new Span { TextColor = Color.Blue, };
 			spanBindable.SetBinding(Span.TextProperty, new Binding(nameof(Title), BindingMode.OneWay));
-			var label = new Label { BackgroundColor = Color.Red, FormattedText = new FormattedString { Spans = { new Span { Text = "Span Test Span Test Span Test Span Test" }, spanBindable } } };
-			layout.Children.Add(btn);
-			layout.Children.Add(label);
-			Content = layout;
+			var label = new Label { LineBreakMode = LineBreakMode.WordWrap, VerticalTextAlignment = TextAlignment.Center, BackgroundColor = Color.Red, FormattedText = new FormattedString { Spans = { new Span { Text = "Span Test Span Test Span Test Span Test" }, spanBindable } } };
+			BindingContext = this;
+			Content = new StackLayout { VerticalOptions = LayoutOptions.Start, BackgroundColor = Color.CadetBlue, Padding = new Thickness(10), Children = { new Label { Text = "When you add new spans, all of them should appear, they should not be cut." }, btn, label } };
 		}
 	}
 }
