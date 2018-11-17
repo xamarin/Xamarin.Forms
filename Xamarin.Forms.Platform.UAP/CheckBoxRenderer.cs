870f9ec73c729dc45dfe9dfbc021dc85b61bdcc9
James Montemagno <james.montemagno@gmail.com>
2018-11-16 16:50:53 -0800

    Initial UWP implementation, no colors yet.

---
+++ Xamarin.Forms.Platform.UAP/CheckBoxRenderer.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using Windows.UI.Xaml;
+
+using WindowsCheckbox = Windows.UI.Xaml.Controls.CheckBox;
+
+namespace Xamarin.Forms.Platform.UWP
+{
+	public class CheckBoxRenderer : ViewRenderer<CheckBox, WindowsCheckbox>
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<CheckBox> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement != null)
+			{
+				if (Control == null)
+				{
+					var control = new WindowsCheckbox();
+					control.Checked += OnNativeChecked;
+					control.Unchecked += OnNativeChecked;
+					//control.ClearValue(WindowsCheckbox.IsCheckedProperty);
+
+					SetNativeControl(control);
+				}
+
+				Control.IsChecked = Element.IsChecked;
+				
+				UpdateFlowDirection();
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == CheckBox.IsCheckedProperty.PropertyName)
+			{
+				Control.IsChecked = Element.IsChecked;
+			}
+			else if (e.PropertyName == VisualElement.FlowDirectionProperty.PropertyName)
+			{
+				UpdateFlowDirection();
+			}
+		}
+
+		protected override bool PreventGestureBubbling { get; set; } = true;
+
+		void OnNativeChecked(object sender, RoutedEventArgs routedEventArgs)
+		{
+			((IElementController)Element).SetValueFromRenderer(CheckBox.IsCheckedProperty, Control.IsChecked);
+		}
+
+		void UpdateFlowDirection()
+		{
+			Control.UpdateFlowDirection(Element);
+		}
+	}
+}
