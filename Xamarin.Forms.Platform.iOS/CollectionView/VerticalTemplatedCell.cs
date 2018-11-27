76a6a2ae6245841d6dc10d62176c13a60b28e624
E.Z. Hart <hartez@gmail.com>
2018-11-27 16:30:27 -0700

    [Android] ItemContentView should use all available space; fixes #4406 Fix inconsistent/inaccurate class naming for cells/views.

--- Xamarin.Forms.Platform.iOS/CollectionView/VerticalTemplatedCell.cs
+++ Xamarin.Forms.Platform.iOS/CollectionView/VerticalTemplatedCell.cs
@@ -6,12 +6,12 @@ using UIKit;
 
 namespace Xamarin.Forms.Platform.iOS
 {
-	internal sealed class TemplatedVerticalListCell : TemplatedCell
+	internal sealed class VerticalTemplatedCell : TemplatedCell
 	{
 		public static NSString ReuseId = new NSString("Xamarin.Forms.Platform.iOS.TemplatedVerticalListCell");
 
 		[Export("initWithFrame:")]
-		public TemplatedVerticalListCell(CGRect frame) : base(frame)
+		public VerticalTemplatedCell(CGRect frame) : base(frame)
 		{
 		}
 
