a98dc7a01c9d37870a5f6f30f6fd0a1954745860
Yaser Moradi <ysmoradi@outlook.com>
2018-12-11 11:05:14 +0330

    Make exception types public. See #4685

--- Xamarin.Forms.Core/InvalidNavigationException.cs
+++ Xamarin.Forms.Core/InvalidNavigationException.cs
@@ -5,7 +5,7 @@ namespace Xamarin.Forms
 #if NETSTANDARD2_0
 	[Serializable]
 #endif
-	internal class InvalidNavigationException : Exception
+	public class InvalidNavigationException : Exception
 	{
 		public InvalidNavigationException(string message) : base(message)
 		{
