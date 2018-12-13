7cb860a69e4c021d913d08d403a4415323cd4393
Rui Marinho <me@ruimarinho.net>
2018-12-13 13:37:30 +0000

    [iOS] Fix InvalidateCache and clear estimatedRowHeight

--- Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
@@ -809,7 +809,7 @@ namespace Xamarin.Forms.Platform.iOS
 				return CalculateHeightForCell(table, firstCell);
 			}
 
-			internal override void InvalidatePrototypicalCellCache()
+			internal override void InvalidatingPrototypicalCellCache()
 			{
 				ClearPrototype();
 				_prototypicalCellByTypeOrDataTemplate.Clear();
@@ -974,9 +974,14 @@ namespace Xamarin.Forms.Platform.iOS
 
 			UIColor DefaultBackgroundColor => UIColor.Clear;
 
-			internal virtual void InvalidatePrototypicalCellCache()
+			internal void InvalidatePrototypicalCellCache()
 			{
 				_estimatedRowHeight = false;
+				InvalidatingPrototypicalCellCache();
+			}
+
+			internal virtual void InvalidatingPrototypicalCellCache()
+			{
 			}
 
 			public override void DraggingEnded(UIScrollView scrollView, bool willDecelerate)
