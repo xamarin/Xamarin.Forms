648d0c88e1ea67f48c78a8c8460d6f6cf485d096
ShaneN <shane94@hotmail.com>
2018-12-18 15:28:05 -0700

    add ui test

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue3244.cs
@@ -0,0 +1,50 @@
+﻿
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 3244, "[Android] ImageSource.FromFile does not pick up images if they are stored in a mipmap folder",
+		PlatformAffected.Android)]
+#if UITEST
+	[NUnit.Framework.Category(UITestCategories.Image)]
+#endif
+	public class Issue3244 : TestContentPage
+	{
+		string _imageId = "imageid";
+		protected override void Init()
+		{
+			Content = new StackLayout()
+			{
+				Children =
+				{
+					new Label()
+					{
+						Text = "Android only test"
+					},
+					new Image()
+					{
+						Source = ImageSource.FromFile("mipmapicon"),
+						AutomationId = _imageId
+					}
+				}
+			};
+		}
+
+#if UITEST && __ANDROID__
+		[Test]
+		public void MipMapImageLoaded()
+		{
+			var element = RunningApp.WaitForElement(_imageId);
+			Assert.IsTrue(element[0].Rect.Height > 0);
+			Assert.IsTrue(element[0].Rect.Width > 0);
+		}
+#endif
+	}
+}
