62df74614be694bb38fc62d1c5ab3cbfe004c69f
Pavel Yakovlev <v-payako@microsoft.com>
2018-12-19 17:26:46 +0300

    [UWP] performance optimization of the rendering of icons font - the minimum Dpi of the icon is 300

--- Xamarin.Forms.Platform.UAP/FontImageSourceHandler.cs
+++ Xamarin.Forms.Platform.UAP/FontImageSourceHandler.cs
@@ -3,46 +3,39 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Text;
-using Windows.Storage.Streams;
-using Windows.UI.Xaml.Media.Imaging;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 
 namespace Xamarin.Forms.Platform.UWP
 {
 	public sealed class FontImageSourceHandler : IImageSourceHandler
 	{
-		public async Task<Windows.UI.Xaml.Media.ImageSource> LoadImageAsync(ImageSource imagesource,
+		float _minimumDpi = 300;
+
+		public Task<Windows.UI.Xaml.Media.ImageSource> LoadImageAsync(ImageSource imagesource,
 			CancellationToken cancelationToken = default(CancellationToken))
 		{
-			BitmapSource image = null;
-			if (imagesource is FontImageSource fontsource)
+			if (!(imagesource is FontImageSource fontsource))
+				return null;
+
+			var device = CanvasDevice.GetSharedDevice();
+			var dpi = Math.Max(_minimumDpi, Windows.Graphics.Display.DisplayInformation.GetForCurrentView().LogicalDpi);
+			var canvasSize = (float)fontsource.Size + 2;
+
+			var imageSource = new CanvasImageSource(device, canvasSize, canvasSize, dpi);
+			using (var ds = imageSource.CreateDrawingSession(Windows.UI.Colors.Transparent))
 			{
-				var device = CanvasDevice.GetSharedDevice();
-				var localDpi = Windows.Graphics.Display.DisplayInformation.GetForCurrentView().LogicalDpi;
-				var canvasSize = (float)fontsource.Size + 2;
-				var renderTarget = new CanvasRenderTarget(device, canvasSize, canvasSize, localDpi);
-
-				using (var ds = renderTarget.CreateDrawingSession())
+				var textFormat = new CanvasTextFormat
 				{
-					ds.Clear(Windows.UI.Colors.Transparent);
-					var textFormat = new CanvasTextFormat
-					{
-						FontFamily = fontsource.FontFamily,
-						FontSize = (float)fontsource.Size,
-						HorizontalAlignment = CanvasHorizontalAlignment.Center,
-						Options = CanvasDrawTextOptions.Default
-					};
-					var iconcolor = (fontsource.Color != Color.Default ? fontsource.Color : Color.White).ToWindowsColor();
-					ds.DrawText(fontsource.Glyph.ToString(), textFormat.FontSize / 2, 0, iconcolor, textFormat);
-				}
-
-				using (var stream = new InMemoryRandomAccessStream())
-				{
-					await renderTarget.SaveAsync(stream, CanvasBitmapFileFormat.Png);
-					image = new BitmapImage();
-					await image.SetSourceAsync(stream);
-				}
+					FontFamily = fontsource.FontFamily,
+					FontSize = (float)fontsource.Size,
+					HorizontalAlignment = CanvasHorizontalAlignment.Center,
+					Options = CanvasDrawTextOptions.Default,
+				};
+				var iconcolor = (fontsource.Color != Color.Default ? fontsource.Color : Color.White).ToWindowsColor();
+				ds.DrawText(fontsource.Glyph.ToString(), textFormat.FontSize / 2, 0, iconcolor, textFormat);
 			}
-			return image;
+
+			return Task.FromResult((Windows.UI.Xaml.Media.ImageSource)imageSource);
 		}
 	}
 }
