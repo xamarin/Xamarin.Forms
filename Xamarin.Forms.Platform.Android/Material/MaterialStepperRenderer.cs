db4e1a98d3721a941581a31be4a005c24b377bb3
Matthew Leibowitz <mattleibow@live.com>
2019-01-19 18:09:27 +0200

    [Android] [Material] Switching back to a traditional renderer for now to get tabstops working

--- Xamarin.Forms.Platform.Android/Material/MaterialStepperRenderer.cs
+++ Xamarin.Forms.Platform.Android/Material/MaterialStepperRenderer.cs
@@ -1,157 +1,69 @@
 #if __ANDROID_28__
-using System;
 using System.ComponentModel;
 using Android.Content;
-using Android.Support.V4.View;
 using Android.Views;
 using Android.Widget;
 using Xamarin.Forms;
-using Xamarin.Forms.Platform.Android.FastRenderers;
 using Xamarin.Forms.Platform.Android.Material;
 using AButton = Android.Widget.Button;
-using AView = Android.Views.View;
 using MButton = Android.Support.Design.Button.MaterialButton;
 
 [assembly: ExportRenderer(typeof(Xamarin.Forms.Stepper), typeof(MaterialStepperRenderer), new[] { typeof(VisualRendererMarker.Material) })]
 
 namespace Xamarin.Forms.Platform.Android.Material
 {
-	public class MaterialStepperRenderer : LinearLayout,
-		IStepperRenderer, IVisualElementRenderer, IViewRenderer, ITabStop
+	public class MaterialStepperRenderer : ViewRenderer<Stepper, LinearLayout>, IStepperRenderer
 	{
 		const int DefaultButtonSpacing = 4;
-
-		int? _defaultLabelFor;
-
-		bool _disposed;
-
-		Stepper _element;
-
-		VisualElementTracker _visualElementTracker;
-		VisualElementRenderer _visualElementRenderer;
-		MotionEventHelper _motionEventHelper;
 
 		MButton _downButton;
 		MButton _upButton;
 
-		public event EventHandler<VisualElementChangedEventArgs> ElementChanged;
-		public event EventHandler<PropertyChangedEventArgs> ElementPropertyChanged;
-
-		public MaterialStepperRenderer(Context context)
-			: base(MaterialContextThemeWrapper.Create(context))
+		public MaterialStepperRenderer(Context context) : base(context)
 		{
 			VisualElement.VerifyVisualFlagEnabled();
 
-			_visualElementRenderer = new VisualElementRenderer(this);
-			_motionEventHelper = new MotionEventHelper();
-
-			Orientation = Orientation.Horizontal;
-			Focusable = true;
-			DescendantFocusability = DescendantFocusability.AfterDescendants;
-
-			StepperRendererManager.CreateStepperButtons(this, out _downButton, out _upButton);
-			AddView(_downButton, new LinearLayout.LayoutParams(LayoutParams.WrapContent, LayoutParams.MatchParent));
-			AddView(_upButton, new LinearLayout.LayoutParams(LayoutParams.WrapContent, LayoutParams.MatchParent)
-			{
-				// because the buttons have no inset, we add spacing in the form of a margin to one button
-				LeftMargin = (int)Context.ToPixels(DefaultButtonSpacing)
-			});
+			AutoPackage = false;
 		}
 
-		protected LinearLayout Control => this;
-
-		protected Stepper Element
+		protected override LinearLayout CreateNativeControl()
 		{
-			get { return _element; }
-			set
+			return new LinearLayout(Context)
 			{
-				if (_element == value)
-					return;
-
-				var oldElement = _element;
-				_element = value;
-
-				OnElementChanged(new ElementChangedEventArgs<Stepper>(oldElement, _element));
-
-				_element?.SendViewInitialized(this);
-
-				_motionEventHelper.UpdateElement(_element);
-			}
+				Orientation = Orientation.Horizontal,
+				Focusable = true,
+				DescendantFocusability = DescendantFocusability.AfterDescendants
+			};
 		}
 
-		protected override void Dispose(bool disposing)
+		protected override void OnElementChanged(ElementChangedEventArgs<Stepper> e)
 		{
-			if (_disposed)
-				return;
-			_disposed = true;
+			base.OnElementChanged(e);
 
-			if (disposing)
+			if (e.OldElement == null)
 			{
-				_visualElementTracker?.Dispose();
-				_visualElementTracker = null;
-
-				_visualElementRenderer?.Dispose();
-				_visualElementRenderer = null;
-
-				if (Element != null)
+				if (Control == null)
 				{
-					Element.PropertyChanged -= OnElementPropertyChanged;
-
-					if (Platform.GetRenderer(Element) == this)
-						Element.ClearValue(Platform.RendererProperty);
+					var layout = CreateNativeControl();
+					StepperRendererManager.CreateStepperButtons(this, out _downButton, out _upButton);
+					layout.AddView(_downButton, new LinearLayout.LayoutParams(LayoutParams.WrapContent, LayoutParams.MatchParent));
+					layout.AddView(_upButton, new LinearLayout.LayoutParams(LayoutParams.WrapContent, LayoutParams.MatchParent)
+					{
+						// because the buttons have no inset, we add spacing in the form of a margin to one button
+						LeftMargin = (int)Context.ToPixels(DefaultButtonSpacing)
+					});
+					SetNativeControl(layout);
 				}
 			}
 
-			base.Dispose(disposing);
+			StepperRendererManager.UpdateButtons(this, null, _downButton, _upButton);
 		}
 
-		protected virtual void OnElementChanged(ElementChangedEventArgs<Stepper> e)
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
 		{
-			ElementChanged?.Invoke(this, new VisualElementChangedEventArgs(e.OldElement, e.NewElement));
-
-			if (e.OldElement != null)
-			{
-				e.OldElement.PropertyChanged -= OnElementPropertyChanged;
-			}
-
-			if (e.NewElement != null)
-			{
-				this.EnsureId();
-
-				if (_visualElementTracker == null)
-					_visualElementTracker = new VisualElementTracker(this);
-
-				e.NewElement.PropertyChanged += OnElementPropertyChanged;
-
-				StepperRendererManager.UpdateButtons(this, null, _downButton, _upButton);
-				UpdateBackgroundColor();
-
-				ElevationHelper.SetElevation(this, e.NewElement);
-			}
-		}
-
-		protected virtual void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			ElementPropertyChanged?.Invoke(this, e);
-
-			if (e.PropertyName == VisualElement.BackgroundColorProperty.PropertyName)
-				UpdateBackgroundColor();
+			base.OnElementPropertyChanged(sender, e);
 
 			StepperRendererManager.UpdateButtons(this, e, _downButton, _upButton);
-		}
-
-		public override bool OnTouchEvent(MotionEvent e)
-		{
-			if (_visualElementRenderer.OnTouchEvent(e) || base.OnTouchEvent(e))
-				return true;
-
-			return _motionEventHelper.HandleMotionEvent(Parent, e);
-		}
-
-		private void UpdateBackgroundColor()
-		{
-			var currentColor = Element?.BackgroundColor ?? Color.Default;
-			SetBackgroundColor(currentColor.ToAndroid());
 		}
 
 		// IStepperRenderer
@@ -172,45 +84,6 @@ namespace Xamarin.Forms.Platform.Android.Material
 
 			return button;
 		}
-
-		// IVisualElementRenderer
-
-		VisualElement IVisualElementRenderer.Element => Element;
-
-		VisualElementTracker IVisualElementRenderer.Tracker => _visualElementTracker;
-
-		ViewGroup IVisualElementRenderer.ViewGroup => null;
-
-		AView IVisualElementRenderer.View => this;
-
-		SizeRequest IVisualElementRenderer.GetDesiredSize(int widthConstraint, int heightConstraint)
-		{
-			Measure(widthConstraint, heightConstraint);
-			return new SizeRequest(new Size(Control.MeasuredWidth, Control.MeasuredHeight), new Size());
-		}
-
-		void IVisualElementRenderer.SetElement(VisualElement element) =>
-			Element = (element as Stepper) ?? throw new ArgumentException($"Element must be of type {nameof(Stepper)}.");
-
-		void IVisualElementRenderer.SetLabelFor(int? id)
-		{
-			if (_defaultLabelFor == null)
-				_defaultLabelFor = ViewCompat.GetLabelFor(this);
-
-			ViewCompat.SetLabelFor(this, (int)(id ?? _defaultLabelFor));
-		}
-
-		void IVisualElementRenderer.UpdateLayout() =>
-			_visualElementTracker?.UpdateLayout();
-
-		// IViewRenderer
-
-		void IViewRenderer.MeasureExactly() =>
-			ViewRenderer.MeasureExactly(this, Element, Context);
-
-		// ITabStop
-
-		AView ITabStop.TabStop => this;
 	}
 }
 #endif
