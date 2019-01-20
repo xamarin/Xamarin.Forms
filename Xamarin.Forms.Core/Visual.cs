ef19f64f85468312e5930d4df985ef033b0903c0
ShaneN <shane94@hotmail.com>
2019-01-19 21:33:03 -0700

    [core] fix platform messaging

--- Xamarin.Forms.Core/Visual.cs
+++ Xamarin.Forms.Core/Visual.cs
@@ -5,6 +5,7 @@ namespace Xamarin.Forms
 	public static class VisualMarker
 	{
 		static bool _isMaterialRegistered = false;
+		static bool _warnedAboutMaterial = false;
 
 		public static IVisual MatchParent { get; } = new VisualRendererMarker.MatchParent();
 		public static IVisual Default { get; } = new VisualRendererMarker.Default();
@@ -13,10 +14,14 @@ namespace Xamarin.Forms
 		internal static void RegisterMaterial() => _isMaterialRegistered = true;
 		internal static void MaterialCheck()
 		{
-			if (_isMaterialRegistered)
+			if (_isMaterialRegistered || _warnedAboutMaterial)
 				return;
 
-			Internals.Log.Warning("Visual", "Material needs to be registered on iOS by calling FormsMaterial.Init() after Forms.Init()");
+			_warnedAboutMaterial = true;
+			if (Device.RuntimePlatform == Device.iOS)
+				Internals.Log.Warning("Visual", "Material needs to be registered on iOS by calling FormsMaterial.Init() after the Xamarin.Forms.Forms.Init method call.");
+			else if (Device.RuntimePlatform != Device.Android)
+				Internals.Log.Warning("Visual", $"Material is currently not support on {Device.RuntimePlatform}.");
 		}
 	}
 
