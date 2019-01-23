7e69207c16162a27dbf4a24c50a595bd52b174fd
Rui Marinho <me@ruimarinho.net>
2019-01-23 03:16:46 +0000

    [iOS] IndicatorsViewRenderer and CarouselVieRenderer draft

--- Xamarin.Forms.Platform.iOS/CollectionView/CarouselViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/CollectionView/CarouselViewRenderer.cs
@@ -1,10 +1,21 @@
-namespace Xamarin.Forms.Platform.iOS
+﻿namespace Xamarin.Forms.Platform.iOS
 {
-	public class CarouselViewRenderer : ItemsViewRenderer
+	public class CarouselViewRenderer : CollectionViewRenderer
 	{
 		public CarouselViewRenderer()
 		{
 			CollectionView.VerifyCollectionViewFlagEnabled(nameof(CarouselViewRenderer));
+		}
+
+		protected override ItemsViewLayout SelectLayout(IItemsLayout layoutSpecification)
+		{
+			if (layoutSpecification is ListItemsLayout listItemsLayout)
+			{
+				return new CarouselViewLayout(listItemsLayout);
+			}
+
+			// Fall back to horizontal list
+			return new CarouselViewLayout(new ListItemsLayout(ItemsLayoutOrientation.Horizontal));
 		}
 	}
 }
