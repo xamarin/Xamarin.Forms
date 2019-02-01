3dce83c73efb85009224e7cbd88c734d9348b2de
Stephane Delcroix <stephane@delcroix.org>
2019-02-01 14:07:41 +0100

    Think about the nullable

--- Xamarin.Forms.Build.Tasks/NodeILExtensions.cs
+++ Xamarin.Forms.Build.Tasks/NodeILExtensions.cs
@@ -55,6 +55,8 @@ namespace Xamarin.Forms.Build.Tasks
 			}
 
 			//check if it's assignable from a string
+			if (targetTypeRef.ResolveCached().FullName == "System.Nullable`1")
+				targetTypeRef = ((GenericInstanceType)targetTypeRef).GenericArguments[0];
 			if (targetTypeRef.ResolveCached().BaseType != null && targetTypeRef.ResolveCached().BaseType.FullName == "System.Enum")
 				return true;
 			if (targetTypeRef.FullName == "System.Char")
