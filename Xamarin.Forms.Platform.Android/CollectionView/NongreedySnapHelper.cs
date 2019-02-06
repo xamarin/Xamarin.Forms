1ba848abfe96d6095482129dabfd1f9dd4062412
E.Z. Hart <hartez@gmail.com>
2019-02-06 11:56:07 -0700

    Remove underscore in method name

--- Xamarin.Forms.Platform.Android/CollectionView/NongreedySnapHelper.cs
+++ Xamarin.Forms.Platform.Android/CollectionView/NongreedySnapHelper.cs
@@ -19,7 +19,7 @@ namespace Xamarin.Forms.Platform.Android
 
 			if (_recyclerView != null)
 			{
-				_recyclerView.ScrollChange += RecyclerView_ScrollChange;
+				_recyclerView.ScrollChange += RecyclerViewScrollChange;
 			}
 		}
 
@@ -36,14 +36,14 @@ namespace Xamarin.Forms.Platform.Android
 			{
 				if (_recyclerView != null)
 				{
-					_recyclerView.ScrollChange -= RecyclerView_ScrollChange;
+					_recyclerView.ScrollChange -= RecyclerViewScrollChange;
 				}
 			}
 
 			base.Dispose(disposing);
 		}
 
-		void RecyclerView_ScrollChange(object sender, global::Android.Views.View.ScrollChangeEventArgs e)
+		void RecyclerViewScrollChange(object sender, global::Android.Views.View.ScrollChangeEventArgs e)
 		{
 			CanSnap = true;
 		}
