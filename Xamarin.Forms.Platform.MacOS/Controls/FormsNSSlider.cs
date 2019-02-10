faa46ff29f36588dc9df9e5cebf4d010b89b73c7
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2019-02-10 03:24:01 +0300

    fix https://github.com/xamarin/Xamarin.Forms/issues/3610

---
+++ Xamarin.Forms.Platform.MacOS/Controls/FormsNSSlider.cs
@@ -0,0 +1,20 @@
+﻿using AppKit;
+using CoreGraphics;
+
+namespace Xamarin.Forms.Platform.macOS.Controls
+{
+	internal class FormsNSSlider : NSSlider
+	{
+		readonly CGSize _fitSize;
+
+		internal FormsNSSlider() : base(CGRect.Empty)
+		{
+			Continuous = true;
+			SizeToFit();
+			var size = Bounds.Size;
+			_fitSize = new CGSize(size.Width > 0 ? size.Width : 100, size.Height > 0 ? size.Height : 20);
+		}
+
+		public override CGSize SizeThatFits(CGSize size) => _fitSize;
+	}
+}
