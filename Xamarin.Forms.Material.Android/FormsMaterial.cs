663ac7e4d3ba81f188c53ad52dfb3feb431604fa
ShaneN <shane94@hotmail.com>
2019-02-27 13:40:07 -0700

    - add context to Init for future cases

--- Xamarin.Forms.Material.Android/FormsMaterial.cs
+++ Xamarin.Forms.Material.Android/FormsMaterial.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Android.Content;
+using Android.OS;
+
 namespace Xamarin.Forms
 {
     public static class FormsMaterial
     {
-        public static void Init()
+        public static void Init(Context context, Bundle bundle)
         {
 			// my only purpose is to exist so when called
 			// this dll doesn't get removed
