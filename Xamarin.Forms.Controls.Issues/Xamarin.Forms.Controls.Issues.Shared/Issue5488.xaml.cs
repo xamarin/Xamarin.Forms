997efc9f59dfbb597baf1a4e97900b07c782360d
Mason <mason.yunchen@gmail.com>
2019-03-14 17:02:34 +1300

    fix for issue5488

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5488.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 5488, "CarouselView on iOS in XamarinForms 3.6: System.InvalidCastException: Specified cast is not valid.", PlatformAffected.iOS)]
+	public partial class Issue5488 : ContentPage
+	{
+		public Issue5488()
+		{
+			InitializeComponent();
+		}
+	}
+
+	public class TestPageViewModel
+	{
+		public ObservableCollection<User> Users { get; set; }
+		public TestPageViewModel()
+		{
+			Users = new ObservableCollection<User>(Enumerable.Range(1, 10).Select(_ =>
+				new User
+				{
+					Id = _,
+					Name = $"User_{_}"
+				}
+			));
+		}
+	}
+
+	public class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
+
