73dab21c66c41571ed56c8c50546b640abf64fa8
ShaneN <shane94@hotmail.com>
2019-03-26 22:09:16 -0600

    - suggested changes

--- Xamarin.Forms.Material.Android/MaterialFormsTextInputLayoutBase.cs
+++ Xamarin.Forms.Material.Android/MaterialFormsTextInputLayoutBase.cs
@@ -142,7 +142,7 @@ namespace Xamarin.Forms.Material.Android
 			if (!_isDisposed)
 			{
 				_isDisposed = true;
-				if (EditText != null && !EditText.IsDisposed())
+				if (EditText != null &&)
 					EditText.FocusChange -= OnFocusChange;
 			}
 
