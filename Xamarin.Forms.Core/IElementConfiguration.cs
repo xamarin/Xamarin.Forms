055deaed257adde1ae586bc007de1961b9b6b8e3
ShaneN <shane94@hotmail.com>
2019-04-01 13:30:30 -0600

    - remove confusing comment

--- Xamarin.Forms.Core/IElementConfiguration.cs
+++ Xamarin.Forms.Core/IElementConfiguration.cs
@@ -1,7 +1,6 @@
 
 namespace Xamarin.Forms
 {
-	// Don't make this generic covariant as it causes UWP performance to tank
 	public interface IElementConfiguration<out TElement> where TElement : Element
 	{
 		IPlatformElementConfiguration<T, TElement> On<T>() where T : IConfigPlatform;
