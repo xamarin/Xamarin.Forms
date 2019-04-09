9f9d4920feee89d40959b14e2f2e0789bff437dd
Adrian Knight <adrianknight89@outlook.com>
2019-04-08 22:57:35 -0500

    update existing template

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/_Template.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/_Template.cs
@@ -14,27 +14,45 @@ namespace Xamarin.Forms.Controls.Issues
 #endif
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 1, "Issue Description", PlatformAffected.Default)]
-	public class Issue1 : TestContentPage // or TestMasterDetailPage, etc ...
+	public class Github1 : TestContentPage // or TestMasterDetailPage, etc ...
 	{
 		protected override void Init()
 		{
 			// Initialize ui here instead of ctor
 			Content = new Label
 			{
-				AutomationId = "IssuePageLabel",
+				AutomationId = "Github1Label",
 				Text = "See if I'm here"
 			};
 		}
 
 #if UITEST
 		[Test]
-		public void Issue1Test() 
+		public void Github1Test() 
 		{
 			// Delete this and all other UITEST sections if there is no way to automate the test. Otherwise, be sure to rename the test and update the Category attribute on the class. Note that you can add multiple categories.
-			RunningApp.Screenshot ("I am at Issue 1");
-			RunningApp.WaitForElement (q => q.Marked ("IssuePageLabel"));
-			RunningApp.Screenshot ("I see the Label");
+			RunningApp.Screenshot("I am at Github 1");
+			RunningApp.WaitForElement(q => q.Marked("Github1Label"));
+			RunningApp.Screenshot("I see the Label");
 		}
 #endif
+	}
+
+	[Preserve(AllMembers = true)]
+	public class ViewModelGithub1
+	{
+		public ViewModelGithub1()
+		{
+
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class ModelGithub1
+	{
+		public ModelGithub1()
+		{
+
+		}
 	}
 }
