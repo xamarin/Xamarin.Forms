a0ea014d6bbd6782af0bd7af3bee7992fab71637
Pedro Jesus <pedro.pj.souza@hotmail.com>
2019-04-10 23:24:43 -0300

    - Automated Bugzilla40092

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40092.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40092.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 
 #if UITEST
+using Xamarin.Forms.Core.UITests;
 using Xamarin.UITest;
 using NUnit.Framework;
 #endif
@@ -11,14 +12,21 @@ using NUnit.Framework;
 namespace Xamarin.Forms.Controls.Issues
 {
     [Preserve(AllMembers = true)]
-    [Issue(IssueTracker.Bugzilla, 40092, "Ensure android devices with fractional scale factors (3.5) don't have a white line around the border", PlatformAffected.Android)]
-    public class Bugzilla40092 : TestContentPage
+    [Issue(IssueTracker.Bugzilla, 40092, "Ensure android devices with fractional scale factors (3.5) don't have a white line around the border"
+		, PlatformAffected.Android)]
+#if UITEST
+	[Category(UITestCategories.BoxView)]
+#endif
+	public class Bugzilla40092 : TestContentPage
     {
+		const string Black = "black";
+		const string White = "white";
         protected override void Init()
         {
             AbsoluteLayout mainLayout = new AbsoluteLayout()
             {
-                BackgroundColor = Color.White
+                BackgroundColor = Color.White,
+				AutomationId = White
             };
 
 
@@ -31,12 +39,22 @@ namespace Xamarin.Forms.Controls.Issues
 
             BoxView view = new BoxView()
             {
-                Color = Color.Black
+                Color = Color.Black,
+				AutomationId = Black
             };
 
             mainLayout.Children.Add(view, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.All);
             Content = thePage;
 
         }
-    }
+
+
+#if UITEST
+		[Test]
+		public void AllScreenIsBlack()
+		{
+			RunningApp.WaitForElement(Black);
+		}
+#endif
+	}
 }
