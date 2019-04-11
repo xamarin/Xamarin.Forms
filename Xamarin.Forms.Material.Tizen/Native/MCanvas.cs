797d874fb4636bf767f34aab4b0eb313b189f5d5
Kangho Hur <rookiejava@gmail.com>
2019-04-11 10:52:17 +0900

    Fix MCanvas build issue

--- Xamarin.Forms.Material.Tizen/Native/MCanvas.cs
+++ Xamarin.Forms.Material.Tizen/Native/MCanvas.cs
@@ -6,7 +6,7 @@ using Tizen.NET.MaterialComponents;
 
 namespace Xamarin.Forms.Material.Tizen.Native
 {
-	public class MCanvas : MBox, IContainable<EvasObject>
+	public class MCanvas : MCard, IContainable<EvasObject>
 	{
 		readonly ObservableCollection<EvasObject> _children = new ObservableCollection<EvasObject>();
 
