c3d7d57ab7f2dbed71f70c392b6214cf37018163
Pedro Jesus <pedro.pj.souza@hotmail.com>
2019-04-10 22:28:02 -0300

    - Automated and Add instructions Bugzilla40408

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40408.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40408.cs
@@ -3,6 +3,7 @@ using Xamarin.Forms.Internals;
 using System;
 
 #if UITEST
+using Xamarin.Forms.Core.UITests;
 using Xamarin.UITest;
 using NUnit.Framework;
 #endif
@@ -11,14 +12,26 @@ namespace Xamarin.Forms.Controls.Issues
 {
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Bugzilla, 40408, "MasterDetailPage and TabbedPage only firing Appearing once", PlatformAffected.WinRT)]
+#if UITEST
+	[Category(UITestCategories.Navigation)]
+#endif
 	public class Bugzilla40408 : TestNavigationPage
 	{
+		const string Page1 = "Page 1";
+		const string Page2 = "Page 2";
+		const string Appearing = "Appearing";
+		const string Disappearing = "Disappearing";
+		const string Page3 = "Page 3";
+		const string Content = "View Content";
+		const string MasterDetailPage = "View Master Detail";
+		const string TabbedPage2 = "View TabbedPage";
+		const string Ok = "OK";
 		protected override void Init()
 		{
 			BarBackgroundColor = Color.Red;
 			
 			var contentPage2 = new ContentPage();
-			contentPage2.Title = "Page 2";
+			contentPage2.Title = Page2;
 			contentPage2.BackgroundColor = Color.Green;
 
 			contentPage2.Appearing += ContentPage2_Appearing;
@@ -28,12 +41,12 @@ namespace Xamarin.Forms.Controls.Issues
 			tabbedPage1.Appearing += TabbedPage1_Appearing;
 			tabbedPage1.Disappearing += TabbedPage1_Disappearing;
 
-			var contentPage3 = new ContentPage() { Title = "Page 3" };
+			var contentPage3 = new ContentPage() { Title = Page3 };
 			contentPage3.BackgroundColor = Color.Pink;
 			tabbedPage1.Children.Add(contentPage3);
 			
 			var masterDetailPage1 = new MasterDetailPage();
-			masterDetailPage1.Title = "Page 3";
+			masterDetailPage1.Title = Page3;
 			var master1 = new ContentPage();
 			master1.BackgroundColor = Color.Yellow;
 			master1.Title = "Master 1";
@@ -50,45 +63,80 @@ namespace Xamarin.Forms.Controls.Issues
 			var contentPage1 = new ContentPage();
 			SetHasBackButton(contentPage1, true);
 			contentPage1.BackgroundColor = Color.Blue;
-			contentPage1.Title = "Page 1";
+			contentPage1.Title = Page1;
 			var stack = new StackLayout();
 			contentPage1.Content = stack;
 
-			stack.Children.Add(new Button() { Text = "View Content", Command = new Command(() => PushAsync(contentPage2)) });
-			stack.Children.Add(new Button() { Text = "View Master Detail", Command = new Command(() => PushAsync(masterDetailPage1)) });
-			stack.Children.Add(new Button() { Text = "View Tabbed Page", Command = new Command(() => PushAsync(tabbedPage1)) });
+			stack.Children.Add(new Button() { Text = Content, Command = new Command(() => PushAsync(contentPage2)) });
+			stack.Children.Add(new Button() { Text = MasterDetailPage, Command = new Command(() => PushAsync(masterDetailPage1)) });
+			stack.Children.Add(new Button() { Text = TabbedPage2, Command = new Command(() => PushAsync(tabbedPage1)) });
+			stack.Children.Add(new Label()
+			{
+				Text = "Navigate to each page 2 times, and make sure the Display Alert shows up all the times.",
+				TextColor = Color.White,
+				HorizontalTextAlignment = TextAlignment.Center
+			});
 			
 			PushAsync(contentPage1);
 		}
 
 		void ContentPage2_Disappearing(object sender, EventArgs e)
 		{
-			DisplayAlert("Disappearing", "ContentPage", "OK");
+			DisplayAlert(Disappearing, "ContentPage", Ok);
 		}
 
 		void TabbedPage1_Disappearing(object sender, EventArgs e)
 		{
-			DisplayAlert("Disappearing", "TabbedPage", "OK");
+			DisplayAlert(Disappearing, "TabbedPage", Ok);
 		}
 
 		void MasterDetailPage1_Disappearing(object sender, EventArgs e)
 		{
-			DisplayAlert("Disappearing", "MasterDetailPage", "OK");
+			DisplayAlert(Disappearing, "MasterDetailPage", Ok);
 		}
 
 		void TabbedPage1_Appearing(object sender, EventArgs e)
 		{
-			DisplayAlert("Appearing", "TabbedPage", "OK");
+			DisplayAlert(Appearing, "TabbedPage", Ok);
 		}
 
 		void ContentPage2_Appearing(object sender, EventArgs e)
 		{
-			DisplayAlert("Appearing", "ContentPage", "OK");
+			DisplayAlert(Appearing, "ContentPage", Ok);
 		}
 
 		void MasterDetailPage1_Appearing(object sender, EventArgs e)
 		{
-			DisplayAlert("Appearing", "MasterDetailPage", "OK");
+			DisplayAlert(Appearing, "MasterDetailPage", Ok);
 		}
+
+
+#if UITEST
+		[Test]
+		public void OnAppearingEvents()
+		{
+			NavigateTo(Content, Page2);
+			NavigateTo(Content, Page2);
+			NavigateTo(TabbedPage2, Page3);
+			NavigateTo(TabbedPage2, Page3);
+
+			// This one fails in UWP
+
+			NavigateTo(MasterDetailPage, Page3);
+			NavigateTo(MasterDetailPage, Page3);
+		}
+
+		void NavigateTo(string goTo, string destination)
+		{
+			RunningApp.WaitForElement(Page1);
+			RunningApp.Tap(goTo);
+			RunningApp.WaitForElement(Appearing);
+			RunningApp.Tap(Ok);
+			RunningApp.WaitForElement(destination);
+			RunningApp.NavigateBack();
+			RunningApp.WaitForElement(Disappearing);
+			RunningApp.Tap(Ok);
+		}
+#endif
 	}
 }
