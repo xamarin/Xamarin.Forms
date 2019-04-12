0bcaf8a407c5f6bb66263731a931f5e49781234a
Rui Marinho <me@ruimarinho.net>
2019-04-12 19:52:57 +0100

    [Controls] Reuse dynamic gallery for SearchHandler

--- Xamarin.Forms.Controls/XamStore/Views/SearchHandlerPage.cs
+++ Xamarin.Forms.Controls/XamStore/Views/SearchHandlerPage.cs
@@ -6,6 +6,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using static Xamarin.Forms.Controls.DynamicViewGallery;
 using static Xamarin.Forms.Controls.XamStore.BasePage;
 
 namespace Xamarin.Forms.Controls.XamStore
@@ -13,85 +14,55 @@ namespace Xamarin.Forms.Controls.XamStore
 	[Preserve(AllMembers = true)]
 	public class SearchHandlerPage : ContentPage
 	{
-		HashSet<string> _exceptProperties = new HashSet<string>
-		{
-			AutomationIdProperty.PropertyName,
-			ClassIdProperty.PropertyName,
-			"StyleId",
-		};
-		CustomSearchHandler _searchHandler;
+		SearchHandler _searchHandler;
 		StackLayout _propertyLayout;
+		const string SearchHandlerKey = nameof(SearchHandler);
+
 		public SearchHandlerPage()
 		{
 			On<iOS>().SetUseSafeArea(true);
-			_searchHandler = AddSearchHandler();
+
+			var searchHandlerKey = nameof(SearchHandler);
+
+			TestedTypes.Add(SearchHandlerKey, (AddSearchHandler, new NamedAction[] { new NamedAction { Name = nameof(Focus), Action = FocusUnfocusSearchHandler } }));
+
+			_searchHandler = TestedTypes[SearchHandlerKey].ctor() as SearchHandler;
+
 			_propertyLayout = new StackLayout
 			{
 				Spacing = 10,
-				Padding = 10
+				Padding = 10,
+				Children = { new Button { Text = "Show/Hide SearchHandler", Command = new Command(()=> ShowHideSearchHandler()) }}
 			};
 			Content = new StackLayout
 			{
 				Children = {
-					new Label { Text = "Test SearchHandler", HorizontalTextAlignment = TextAlignment.Center },
-					new ScrollView { Content = new StackLayout {
-														Children = {
-																	new Button { Text = "Show/Hide SearchHandler", Command = new Command(()=> ShowHideSearchHandler()) },
-																	new Button { Text = "Focus/Unfocus SearchHandler", Command = new Command(()=> FocusUnfocusSearchHandler()) },
-
-																	_propertyLayout
-																	}
-															}
-									}
+					new Label { Text = "Test SearchHandler on IOs that this text appears", HorizontalTextAlignment = TextAlignment.Center },
+					new ScrollView { Content = _propertyLayout }
 				}
 			};
 
+			GetProperties(_searchHandler, typeof(SearchHandler), _propertyLayout);
 
-			var publicProperties = typeof(SearchHandler)
-				.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-				.Where(p => p.CanRead && p.CanWrite && !_exceptProperties.Contains(p.Name));
-
-			foreach (var property in publicProperties)
-			{
-				if (property.PropertyType == typeof(Color))
-				{
-					var colorPicker = new ColorPicker
-					{
-						Title = property.Name,
-						Color = (Color)property.GetValue(_searchHandler)
-					};
-					colorPicker.ColorPicked += (_, e) => property.SetValue(_searchHandler, e.Color);
-					_propertyLayout.Children.Add(colorPicker);
-				}
-				if (property.PropertyType == typeof(TextAlignment))
-				{
-					var alignmentPicker = new Picker
-					{
-						Title = property.Name,
-						Items = { TextAlignment.Center.ToString(), TextAlignment.End.ToString(), TextAlignment.Start.ToString() }
-					};
-					alignmentPicker.SelectedIndexChanged += (_, e) => property.SetValue(_searchHandler, Enum.Parse(typeof(TextAlignment), alignmentPicker.SelectedItem.ToString()));
-					_propertyLayout.Children.Add(alignmentPicker);
-				}
-			}
 		}
 
-		void FocusUnfocusSearchHandler()
+		void FocusUnfocusSearchHandler(object searchHandler)
 		{
-			if (_searchHandler == null)
+			var sh = searchHandler as SearchHandler;
+			if (sh == null)
 				return;
 
-			if (_searchHandler.IsFocused)
-				_searchHandler.Unfocus();
+			if (sh.IsFocused)
+				sh.Unfocus();
 			else
-				_searchHandler.Focus();
+				sh.Focus();
 		}
 
 		void ShowHideSearchHandler()
 		{
 			if (_searchHandler == null)
 			{
-				_searchHandler = AddSearchHandler();
+				_searchHandler = TestedTypes[SearchHandlerKey].ctor() as SearchHandler;
 			}
 			else
 			{
