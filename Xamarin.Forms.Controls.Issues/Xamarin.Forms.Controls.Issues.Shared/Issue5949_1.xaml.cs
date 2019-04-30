d2d346c9e49e67a2dbc65a6b63267540aab29c85
E.Z. Hart <hartez@gmail.com>
2019-04-30 15:53:42 -0600

    Reproduction

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5949_1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Preserve(AllMembers = true)]
+	public partial class Issue5949_1 : MasterDetailPage
+	{
+		public Issue5949_1()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5949_2.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Preserve(AllMembers = true)]
+	public partial class Issue5949_2 : ContentPage
+	{
+		const string BackButton = "5949GoBack";
+
+		public Issue5949_2()
+		{
+#if APP
+			InitializeComponent();
+			BindingContext = new _5949ViewModel();
+#endif
+		}
+
+		[Preserve(AllMembers = true)]
+		class _5949ViewModel
+		{
+			public _5949ViewModel()
+			{
+				Items = new List<string>
+				{
+					"one", "two", "three"
+				};
+			}
+
+			public List<string> Items { get; set; }
+		}
+
+		void ToolbarItem_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(LoginPage());
+		}
+
+		ContentPage LoginPage()
+		{
+			var page = new ContentPage
+			{
+				Title = "Issue 5949"
+			};
+
+			var button = new Button { Text = "Back", AutomationId = BackButton };
+
+			button.Clicked += ButtonClicked;
+
+			page.Content = button;
+
+			return page;
+		}
+
+		private void ButtonClicked(object sender, EventArgs e)
+		{
+			Application.Current.MainPage = new Issue5949_1();
+		}
+	}
+}
