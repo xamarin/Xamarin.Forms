4a5543e9a8aa13c04b8ca4f7d4368e292bd40c00
Rui Marinho <me@ruimarinho.net>
2019-05-01 17:06:31 +0100

    Revert "{Android]Allow set colors and customize text on Shell SearchHandler"
    This reverts commit 344e1cba82469a856bd1ab52741321ddce1c0d0e.

--- Xamarin.Forms.Platform.Android/Renderers/ShellSearchView.cs
+++ Xamarin.Forms.Platform.Android/Renderers/ShellSearchView.cs
@@ -10,6 +10,7 @@ using Java.Lang;
 using System;
 using System.ComponentModel;
 using System.Threading.Tasks;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Platform.Android.FastRenderers;
 using AColor = Android.Graphics.Color;
 using AView = Android.Views.View;
@@ -41,8 +42,6 @@ namespace Xamarin.Forms.Platform.Android
 		void IShellSearchView.LoadView()
 		{
 			LoadView(SearchHandler);
-			if(_searchHandlerAppearanceTracker == null)
-				_searchHandlerAppearanceTracker = new SearchHandlerAppearanceTracker(this);
 		}
 
 		#endregion IShellSearchView
@@ -90,10 +89,8 @@ namespace Xamarin.Forms.Platform.Android
 		AImageButton _clearButton;
 		AImageButton _clearPlaceholderButton;
 		AImageButton _searchButton;
-		//AButton _cancelButton;
 		AppCompatAutoCompleteTextView _textBlock;
 		bool _disposed;
-		SearchHandlerAppearanceTracker _searchHandlerAppearanceTracker;
 
 		public ShellSearchView(Context context, IShellContext shellContext) : base(context)
 		{
@@ -125,7 +122,6 @@ namespace Xamarin.Forms.Platform.Android
 			{
 				_disposed = true;
 
-				_searchHandlerAppearanceTracker?.Dispose();
 				SearchHandler.PropertyChanged -= OnSearchHandlerPropertyChanged;
 
 				_textBlock.ItemClick -= OnTextBlockItemClicked;
@@ -153,7 +149,6 @@ namespace Xamarin.Forms.Platform.Android
 			_cardView = null;
 			_clearPlaceholderButton = null;
 			_shellContext = null;
-			_searchHandlerAppearanceTracker = null;
 
 			SearchHandler = null;
 		}
@@ -209,9 +204,6 @@ namespace Xamarin.Forms.Platform.Android
 			// A note on accessibility. The _textBlocks hint is what android defaults to reading in the screen
 			// reader. Therefor we do not need to set something else.
 
-			//_cancelButton = new AButton(context);
-			//_cancelButton.Text = "Cancel";
-
 			_clearButton = CreateImageButton(context, clearImage, Resource.Drawable.abc_ic_clear_material, 0, padding);
 			_clearPlaceholderButton = CreateImageButton(context, clearPlaceholderImage, -1, 0, padding);
 
@@ -219,7 +211,6 @@ namespace Xamarin.Forms.Platform.Android
 			linearLayout.AddView(_textBlock);
 			linearLayout.AddView(_clearButton);
 			linearLayout.AddView(_clearPlaceholderButton);
-			//linearLayout.AddView(_cancelButton);
 
 			UpdateClearButtonState();
 
@@ -230,17 +221,11 @@ namespace Xamarin.Forms.Platform.Android
 			_clearButton.Click += OnClearButtonClicked;
 			_clearPlaceholderButton.Click += OnClearPlaceholderButtonClicked;
 			_searchButton.Click += OnSearchButtonClicked;
-			//_cancelButton.Click += CancelButtonClicked;
 
 			AddView(_cardView);
 
 			linearLayout.Dispose();
 		}
-
-		//protected virtual void CancelButtonClicked(object sender, EventArgs e)
-		//{
-		//	throw new NotImplementedException();
-		//}
 
 		protected virtual void OnSearchHandlerPropertyChanged(object sender, PropertyChangedEventArgs e)
 		{
