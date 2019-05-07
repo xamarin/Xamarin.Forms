6814e0fd2f41ab7300db1e04b7846f2ab063e2b6
Nico Vermeir <nico_vermeir@hotmail.com>
2019-05-07 09:15:34 +0200

    collapses header area on hide nav

--- Xamarin.Forms.Platform.UAP/Shell/ShellItemRenderer.cs
+++ Xamarin.Forms.Platform.UAP/Shell/ShellItemRenderer.cs
@@ -22,7 +22,7 @@ namespace Xamarin.Forms.Platform.UWP
 
 		public ShellItemRenderer()
 		{
-			RowDefinitions.Add(new Windows.UI.Xaml.Controls.RowDefinition() { Height = new Windows.UI.Xaml.GridLength(40, Windows.UI.Xaml.GridUnitType.Pixel) });
+			RowDefinitions.Add(new Windows.UI.Xaml.Controls.RowDefinition() { Height = new Windows.UI.Xaml.GridLength(1, Windows.UI.Xaml.GridUnitType.Auto) });
 			RowDefinitions.Add(new Windows.UI.Xaml.Controls.RowDefinition() { Height = new Windows.UI.Xaml.GridLength(1, Windows.UI.Xaml.GridUnitType.Star) });
 			RowDefinitions.Add(new Windows.UI.Xaml.Controls.RowDefinition() { Height = new Windows.UI.Xaml.GridLength(1, Windows.UI.Xaml.GridUnitType.Auto) });
 
@@ -33,7 +33,7 @@ namespace Xamarin.Forms.Platform.UWP
 				TextTrimming = Windows.UI.Xaml.TextTrimming.CharacterEllipsis,
 				TextWrapping = Windows.UI.Xaml.TextWrapping.NoWrap
 			};
-			_HeaderArea = new Windows.UI.Xaml.Controls.Grid() { Padding = new Windows.UI.Xaml.Thickness(10,0,10,0) };
+			_HeaderArea = new Windows.UI.Xaml.Controls.Grid() { Height=40, Padding = new Windows.UI.Xaml.Thickness(10,0,10,0) };
 			_HeaderArea.ColumnDefinitions.Add(new Windows.UI.Xaml.Controls.ColumnDefinition() { Width = new Windows.UI.Xaml.GridLength(1, Windows.UI.Xaml.GridUnitType.Star) });
 			_HeaderArea.ColumnDefinitions.Add(new Windows.UI.Xaml.Controls.ColumnDefinition() { Width = new Windows.UI.Xaml.GridLength(1, Windows.UI.Xaml.GridUnitType.Auto) });
 			_HeaderArea.Children.Add(_Title);
