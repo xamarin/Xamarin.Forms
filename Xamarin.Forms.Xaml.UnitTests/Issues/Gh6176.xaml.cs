83770757fc3a924f3199ea42a6469beab117a8f5
Stephane Delcroix <stephane@delcroix.org>
2019-05-14 17:47:51 -0700

    [XamlC] fix Debug harness generation code

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Gh6176.xaml.cs
@@ -0,0 +1,22 @@
+﻿//
+// Gh6176.xaml.cs
+//
+// Author:
+//       Stephane Delcroix <stdelc@microsoft.com>
+//
+// Copyright (c) 2019 
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public partial class Gh6176 : ContentPage
+	{
+		public Gh6176()
+		{
+			InitializeComponent();
+		}
+	}
+}
