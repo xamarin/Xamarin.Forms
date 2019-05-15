c4a46cf525662064c080411ee190a57982ffe08f
Stephane Delcroix <stephane@delcroix.org>
2019-05-14 17:50:56 -0700

    [X] resolve event handlers in base generic types
    - fixes #6176

--- Xamarin.Forms.Xaml.UnitTests/Issues/Gh6176.xaml.cs
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Gh6176.xaml.cs
@@ -1,22 +1,39 @@
-﻿//
-// Gh6176.xaml.cs
-//
-// Author:
-//       Stephane Delcroix <stdelc@microsoft.com>
-//
-// Copyright (c) 2019 
-using System;
+﻿using System;
 using System.Collections.Generic;
-
+using NUnit.Framework;
 using Xamarin.Forms;
+using Xamarin.Forms.Core.UnitTests;
 
 namespace Xamarin.Forms.Xaml.UnitTests
 {
-	public partial class Gh6176 : ContentPage
+	public class Gh6176VM
 	{
-		public Gh6176()
+	}
+
+	public class Gh6176Base<TVM> : ContentPage where TVM: class
+	{
+		public TVM ViewModel => BindingContext as TVM;
+		protected void ShowMenu(object sender, EventArgs e) { }
+	}
+
+	public partial class Gh6176
+	{
+		public Gh6176() => InitializeComponent();
+		public Gh6176(bool useCompiledXaml)
 		{
-			InitializeComponent();
+			//this stub will be replaced at compile time
+		}
+
+		[TestFixture] class Tests
+		{
+			[SetUp] public void Setup() => Device.PlatformServices = new MockPlatformServices();
+			[TearDown] public void TearDown() => Device.PlatformServices = null;
+
+			[Test]
+			public void XamlCDoesntFail([Values(false, true)]bool useCompiledXaml)
+			{
+				var layout = new Gh6176(useCompiledXaml);
+			}
 		}
 	}
 }
