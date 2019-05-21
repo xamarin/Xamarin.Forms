29e21f054f87b8ecd25fee4daea2939a3fa9a80e
Rui Marinho <me@ruimarinho.net>
2019-05-21 17:57:11 +0100

    [Build] make sure build tasks gets info

--- Xamarin.Forms.Build.Tasks/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Build.Tasks/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Xamarin.Forms.Build.Tasks")]
-[assembly: AssemblyDescription("")]
-
 #if DEBUG
 [assembly:InternalsVisibleTo("Xamarin.Forms.Xaml.UnitTests")]
 #endif
