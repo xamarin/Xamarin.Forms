f7cdffda7165c18f85d6bc4ac6787f4aee451e6b
Rui Marinho <me@ruimarinho.net>
2019-05-22 00:10:19 +0100

    [Test] Refactor test for gitinfo on vsts

--- Xamarin.Forms.Xaml.UnitTests/AssemblyInfoTests.cs
+++ Xamarin.Forms.Xaml.UnitTests/AssemblyInfoTests.cs
@@ -23,7 +23,7 @@ namespace Xamarin.Forms.Xaml.UnitTests
 
 		const string s_company = "Microsoft";
 
-		string gitInfo => File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", "..", "GitInfo.txt"));
+		const string s_gitInfoFile = "GitInfo.txt";
 
 		[Test, TestCaseSource("references")]
 		public void AssemblyTitle(string assemblyName)
@@ -37,7 +37,7 @@ namespace Xamarin.Forms.Xaml.UnitTests
 		{
 			Assembly testAssembly = System.Reflection.Assembly.Load(assemblyName);			
 			Version version = testAssembly.GetName().Version;
-			Version gitInfoVersion = Version.Parse(gitInfo);
+			Version gitInfoVersion = Version.Parse(GetFileFromRoot(s_gitInfoFile));
 			Assert.AreEqual(version.Major, gitInfoVersion.Major);
 			Assert.AreEqual(version.Minor, gitInfoVersion.Minor);
 			Assert.AreEqual(version.Build, gitInfoVersion.Build);
@@ -48,10 +48,12 @@ namespace Xamarin.Forms.Xaml.UnitTests
 		{
 			Assembly testAssembly = System.Reflection.Assembly.Load(assemblyName);
 			FileVersionInfo version = FileVersionInfo.GetVersionInfo(testAssembly.Location);
-			Version gitInfoVersion = Version.Parse(gitInfo);
+			Version gitInfoVersion = Version.Parse(GetFileFromRoot(s_gitInfoFile));
 			Assert.AreEqual(version.FileMajorPart, gitInfoVersion.Major);
 			Assert.AreEqual(version.FileMinorPart, gitInfoVersion.Minor);
 			Assert.AreEqual(version.FileBuildPart, gitInfoVersion.Build);
+			//We need to enable this
+		//	Assert.AreEqual(version.FilePrivatePart, ThisAssembly.Git.Commits);
 			Assert.AreEqual(version.ProductName, s_productName);
 			Assert.AreEqual(version.CompanyName, s_company);
 		}
@@ -63,6 +65,30 @@ namespace Xamarin.Forms.Xaml.UnitTests
 			FileVersionInfo version = FileVersionInfo.GetVersionInfo(testAssembly.Location);
 			Assert.AreEqual(version.ProductName, s_productName);
 			Assert.AreEqual(version.CompanyName, s_company);
+		}
+
+		static string GetFileFromRoot(string file)
+		{
+			var gitInfoFile = Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", "..", file);
+			if (!File.Exists(gitInfoFile))
+			{
+				//NOTE: VSTS may be running tests in a staging directory, so we can use an environment variable to find the source
+				//	https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?view=vsts&tabs=batch#buildsourcesdirectory
+				var sourcesDirectory = Environment.GetEnvironmentVariable("BUILD_SOURCESDIRECTORY");
+				if (!string.IsNullOrEmpty(sourcesDirectory))
+				{
+					gitInfoFile = Path.Combine(sourcesDirectory, file);
+					if (!File.Exists(gitInfoFile))
+					{
+						Assert.Fail($"Unable to find {file} at path: {gitInfoFile}");
+					}
+				}
+				else
+				{
+					Assert.Fail($"Unable to find {file} at path: {gitInfoFile}");
+				}
+			}
+			return File.ReadAllText(gitInfoFile);
 		}
 	}
 }
