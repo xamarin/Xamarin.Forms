c3db0f620228bfca3dd911120dc7bb674f90efe8
Daniel <daniel.luberda@gmail.com>
2019-05-24 13:43:10 +0200

    Mac Animated NSImage support

--- Xamarin.Forms.Platform.iOS/Renderers/ImageElementManager.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ImageElementManager.cs
@@ -115,11 +115,17 @@ namespace Xamarin.Forms.Platform.MacOS
 			var imageController = imageElement as IImageController;
 
 			var source = imageElement.Source;
-		
+#if __MOBILE__
 			if (Control.Image?.Images != null && Control.Image.Images.Length > 1)
 			{
 				renderer.SetImage(null);
 			} 
+#else
+			if (Control.Image != null && Control.Image.Representations().Length > 1)
+			{
+				renderer.SetImage(null);
+			}
+#endif
 			else if (oldElement != null)
 			{
 				var oldSource = oldElement.Source;
