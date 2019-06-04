afb77fd2ea98f0fd847b1bb2e6cce6555f58c7a6
Andrew Hoefling <andrew@hoefling.me>
2019-06-03 21:11:58 -0400

    Added unit tests to cover TabBar which is used in Shell

---
+++ Xamarin.Forms.Xaml.UnitTests/FontImageExtension.xaml.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+using Xamarin.Forms.Core.UnitTests;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public partial class FontImageExtension : TabBar
+	{
+		public FontImageExtension() => InitializeComponent();
+		public FontImageExtension(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		public static string FontFamily => "MyFontFamily";
+		public static string Glyph => "MyGlyph";
+
+		[TestFixture]
+		class Tests
+		{
+			[SetUp] public void Setup() => Device.PlatformServices = new MockPlatformServices();
+			[TearDown] public void TearDown() => Device.PlatformServices = null;
+
+			[TestCase(true), TestCase(false)]
+			public void FontImageExtension_Positive(bool useCompiledXaml)
+			{
+				var layout = new FontImageExtension(useCompiledXaml);
+				var tabs = layout.AllChildren;
+
+				foreach (var tab in tabs)
+				{
+					Tab myTab = (Tab)tab;
+					if (myTab == null)
+						continue;
+
+					Assert.That(myTab.Icon, Is.TypeOf<FontImageSource>());
+				}
+			}
+
+			[TestCase(true), TestCase(false)]
+			public void FontImageExtension_Negative(bool useCompiledXaml)
+			{
+				var layout = new FontImageExtension(useCompiledXaml);
+				var tabs = layout.AllChildren;
+
+				foreach (var tab in tabs)
+				{
+					Tab myTab = (Tab)tab;
+					if (myTab == null)
+						continue;
+
+					Assert.That(myTab.Icon, Is.Not.TypeOf<ImageSource>());
+				}
+			}
+		}
+	}
+}
