85615ddf790a76c0b6003d2fa04198981c10b08f
Andrei Nitescu <nitescua@yahoo.com>
2019-06-25 18:41:57 +0300

    DefaultDataTemplateSelector (#6649)

---
+++ Xamarin.Forms.Core/DefaultDataTemplateSelector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Xamarin.Forms
+{
+	public class DefaultDataTemplateSelector : DataTemplateSelector
+	{
+		readonly Func<object, BindableObject, DataTemplate> _func;
+
+		public DefaultDataTemplateSelector(Func<object, BindableObject, DataTemplate> func) => _func = func;
+
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container) => _func(item, container);
+	}
+}
