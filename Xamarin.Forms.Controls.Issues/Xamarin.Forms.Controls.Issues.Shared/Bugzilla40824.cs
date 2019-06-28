72915cf91a16104f44be18b4ce1aad865e2cdf8a
Pedro Jesus <pedro.pj.souza@hotmail.com>
2019-06-28 19:30:17 -0300

    - Revert the Bugzilla40824

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40824.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40824.cs
@@ -5,7 +5,6 @@ using Xamarin.Forms.Internals;
 using System.Collections.Generic;
 
 #if UITEST
-using Xamarin.Forms.Core.UITests;
 using Xamarin.UITest;
 using NUnit.Framework;
 #endif
@@ -13,28 +12,18 @@ using NUnit.Framework;
 namespace Xamarin.Forms.Controls.Issues
 {
 	[Preserve(AllMembers = true)]
-	[Issue(IssueTracker.Bugzilla, 40824, "ListView item's contextual action menu not being closed upon navigation in AppCompat"
-		, PlatformAffected.Android)]
-#if UITEST
-	[Category(UITestCategories.ListView)]
-#endif
+	[Issue(IssueTracker.Bugzilla, 40824, "ListView item's contextual action menu not being closed upon navigation in AppCompat")]
 	public class Bugzilla40824 : TestContentPage
 	{
-		const string Cat = "Cat";
-		const string Menu = "Action";
-		const string Navigate = "Go to next page";
-		const string PageTitle = "Next Page";
 		protected override void Init()
 		{
 			var list = new ListView
 			{
 				ItemsSource = new List<string>
 				{
-					Cat,
+					"Cat",
 					"Dog",
-					"Rat",
-					"The contextual action should disapper when",
-					"navigating to another page"
+					"Rat"
 				},
 				ItemTemplate = new DataTemplate(() =>
 				{
@@ -42,8 +31,7 @@ namespace Xamarin.Forms.Controls.Issues
 					cell.SetBinding(TextCell.TextProperty, ".");
 					cell.ContextActions.Add(new MenuItem
 					{
-						Text = Menu,
-						AutomationId = Menu,
+						Text = "Action",
 						IconImageSource = "icon",
 						IsDestructive = true,
 						Command = new Command(() => DisplayAlert("TITLE", "Context action invoked", "Ok")),
@@ -58,25 +46,12 @@ namespace Xamarin.Forms.Controls.Issues
 				{
 					new Button
 					{
-						Text = Navigate,
-						Command = new Command(() => Navigation.PushAsync(new ContentPage { Title = PageTitle, Content = new Label { Text = "Here" } }))
+						Text = "Go to next page",
+						Command = new Command(() => Navigation.PushAsync(new ContentPage { Title = "Next Page", Content = new Label { Text = "Here" } }))
 					},
 					list
 				}
 			};
 		}
-
-#if UITEST && __ANDROID__
-		[Test]
-		public void NavigationWithContextualAction()
-		{
-			RunningApp.WaitForElement(Cat);
-			RunningApp.TouchAndHold(Cat);
-			RunningApp.WaitForElement(Menu);
-			RunningApp.Tap(Navigate);
-			RunningApp.WaitForElement(PageTitle);
-			RunningApp.WaitForNoElement(Menu);
-		}
-#endif
 	}
 }
