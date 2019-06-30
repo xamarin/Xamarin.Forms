b8895e90e0d6258f5b20431f7652d4b9d1115516
Keith Dahlby <keith@newbo.co>
2019-06-30 01:40:51 -0500

    [Core] Extract DeviceUnitTests

---
+++ Xamarin.Forms.Core.UnitTests/DeviceUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Core.UnitTests
+{
+	[TestFixture]
+	public class DeviceUnitTests : BaseTestFixture
+	{
+		[Test]
+		public void TestBeginInvokeOnMainThread()
+		{
+			Device.PlatformServices = new MockPlatformServices(invokeOnMainThread: action => action());
+
+			bool invoked = false;
+			Device.BeginInvokeOnMainThread(() => invoked = true);
+
+			Assert.True(invoked);
+		}
+
+		[Test]
+		public void InvokeOnMainThreadThrowsWhenNull()
+		{
+			Device.PlatformServices = null;
+			Assert.Throws<InvalidOperationException>(() => Device.BeginInvokeOnMainThread(() => { }));
+		}
+	}
+}
