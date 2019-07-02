90bb42fd8d4090dc847d532b2d3e593c702f56be
Mike Schwörer <mailport@mikescher.de>
2019-07-02 19:11:31 +0200

    Fixed #5046 - Button CornerRadius doesn't work in explicit style

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5046.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if APP
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 5046, "CornerRadius doesn't work in explicit style when implicit style exists", 
+	       PlatformAffected.Android | PlatformAffected.iOS)]
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Issue5046 : ContentPage
+	{
+		public Issue5046()
+		{
+			InitializeComponent();
+		}
+
+		void Button_Clicked(object sender, System.EventArgs e)
+		{
+			this.DisplayAlert("CornerRadius", "CornerRadius = " + ((Button)sender).CornerRadius, "OK");
+		}
+	}
+#endif
+}
