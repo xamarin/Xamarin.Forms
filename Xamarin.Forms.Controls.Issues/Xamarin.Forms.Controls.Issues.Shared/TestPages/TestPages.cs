f9be721b990679d5961d27d0f7bfac65e5905e4a
Rui Marinho <me@ruimarinho.net>
2019-07-25 10:26:34 +0100

    [Tests]Update to Xamarin.UITest 3.0 and NUnit 3.0 (#6332)
    * [UITests]Update to Xamarin.UITest 3.0
    
    * [Tests] Move tests to Xamarin.UITests and NUnit 3.0
    
    * [UITests] Use OneTimeSetUp
    
    * suppress CS0436

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TestPages/TestPages.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TestPages/TestPages.cs
@@ -713,7 +713,7 @@ namespace Xamarin.Forms.Controls.Issues
 	[SetUpFixture]
 	public class IssuesSetup
 	{
-		[SetUp]
+		[OneTimeSetUp]
 		public void RunBeforeAnyTests()
 		{
 			AppSetup.RunningApp = AppSetup.Setup(null);
