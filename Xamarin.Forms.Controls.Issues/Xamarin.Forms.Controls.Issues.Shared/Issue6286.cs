4f3f73409c26bbebc982a4d17eddec0078be6d2b
shane <shneuvil@microsoft.com>
2019-07-30 13:40:32 -0600

    Modify 6286 test to await push and pop

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6286.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6286.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
+using System.Threading.Tasks;
 
 #if UITEST
 using Xamarin.Forms.Core.UITests;
@@ -19,33 +20,51 @@ namespace Xamarin.Forms.Controls.Issues
 	{
 		WebView webview;
 		WebView webview2;
+		int count;
+		ContentPage page1;
+		ContentPage page2;
+
 		protected override void Init()
 		{
-			webview = new WebView { Source = "http://microsoft.com" };
+			webview = new WebView { Source = "https://microsoft.com" };
 			webview.Navigated += Webview_Navigated;
-			var page1 = new ContentPage { Content = webview };
+			page1 = new ContentPage { Content = webview };
 
-			webview2 = new WebView { Source = "http://xamarin.com" };
+			webview2 = new WebView { Source = "https://xamarin.com" };
 			webview2.Navigated += Webview_Navigated;
-			var page2 = new ContentPage { Content = webview2 };
+			page2 = new ContentPage { Content = webview2 };
 
-			int count = 0;
+			count = 0;
 			Navigation.PushAsync(page1);
+			RunTest();
+		}
+
+		void RunTest()
+		{
 			Device.StartTimer(TimeSpan.FromSeconds(2), () =>
 			{
-				webview.Source = "http://xamarin.com";
-				Navigation.PushAsync(page2);
-
-				webview2.Source = "http://microsoft.com";
-				Navigation.PopAsync();
-
-				var done = count++ < 3;
-
-				if (done)
-					page1.Content = new Label { Text = "success", AutomationId = "success" };
-
-				return done;
+				PushPopPages();
+				return false;
 			});
+		}
+
+		async Task PushPopPages()
+		{
+			webview.Source = "https://xamarin.com";
+			await Navigation.PushAsync(page2);
+
+			webview2.Source = "https://microsoft.com";
+			await Navigation.PopAsync();
+
+			var done = count++ < 3;
+
+			if (done)
+				page1.Content = new Label { Text = "success", AutomationId = "success" };
+
+			if(!done)
+			{
+				RunTest();
+			}
 		}
 
 		void Webview_Navigated(object sender, WebNavigatedEventArgs e)
