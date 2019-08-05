134320d348a3812e44507ae0b50459c8f43478e9
E.Z. Hart <hartez@gmail.com>
2019-08-05 11:46:46 -0600

    Add mock Dispatcher and DispatcherProvider for XAML unit tests

---
+++ Xamarin.Forms.Xaml.UnitTests/MockDispatcherProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml.UnitTests;
+
+[assembly: Dependency(typeof(MockDispatcherProvider))]
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public class MockDispatcherProvider : IDispatcherProvider
+	{
+		public IDispatcher GetDispatcher(object context)
+		{
+			return new MockDispatcher();
+		}
+	}
+
+	public class MockDispatcher : IDispatcher
+	{
+		public void BeginInvokeOnMainThread(Action action)
+		{
+			Device.BeginInvokeOnMainThread(action);
+		}
+
+		bool IDispatcher.IsInvokeRequired => Device.IsInvokeRequired;
+	}
+}
