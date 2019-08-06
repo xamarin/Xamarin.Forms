e6478ac4e49d7fd2fba82484af88b44a27de5945
Happypig375 <hadrianwttang@outlook.com>
2019-08-06 16:26:36 +0800

    Address comments

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7049.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7049.cs
@@ -12,17 +12,22 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue (IssueTracker.Github, 7049, "Null reference exception on some Android devices - Xamarin.Forms.Platform.Android.PageRenderer.Xamarin.Forms.Platform.Android.IOrderedTraversalController.UpdateTraversalOrder", PlatformAffected.Android)]
 	public class Issue7049 : TestContentPage
 	{
+		private const string ContinueButton = "ContinueButton"
+		private const string View1 = "View1"
+		private const string View2 = "View2"
+		private const string View3 = "View3"
+		
 		private bool _flag = true;
 
 		protected override void Init()
 		{
 
-			var button = new Button { Text = "Continue" };
+			var button = new Button { AutomationId = ContinueButton, Text = "Continue" };
 			button.Clicked += (_, __) =>
 			{
 				var view = _flag ?
-					(View)new Entry { AutomationId = "View2", Text = "Press 1 time to crash" } :
-					new Label { AutomationId = "View3", Text = "I'm shown, bug fixed!" };
+					(View)new Entry { AutomationId = View2, Text = "Press 1 time to crash" } :
+					new Label { AutomationId = View3, Text = "I'm shown, bug fixed!" };
 				_flag ^= true;
 				(Content as StackLayout).Children[1] = view;
 			};
@@ -31,22 +36,22 @@ namespace Xamarin.Forms.Controls.Issues
 				Children =
 				{
 					button,
-					new Label { AutomationId = "View1", Text = "Press 2 times to crash" }
+					new Label { AutomationId = View1, Text = "Press 2 times to crash" }
 				}
 			};
 		}
 
-#if UITEST
+#if UITEST && __ANDROID__
 		[Test]
 		[Description ("Test null reference in IOrderedTraversalController.UpdateTraversalOrder of Android PageRenderer")]
 		public void Issue7049TestsNullRefInUpdateTraversalOrder()
 		{
-			RunningApp.WaitForElement("View1");
-			RunningApp.Tap("View");
-			RunningApp.WaitForElement("View2");
-			RunningApp.Tap("View");
-			RunningApp.WaitForElement("View3");
-			RunningApp.Tap("View");
+			RunningApp.WaitForElement(View1);
+			RunningApp.Tap(ContinueButton);
+			RunningApp.WaitForElement(View2);
+			RunningApp.Tap(ContinueButton);
+			RunningApp.WaitForElement(View3);
+			RunningApp.Tap(ContinueButton);
 		}
 #endif
 	}
