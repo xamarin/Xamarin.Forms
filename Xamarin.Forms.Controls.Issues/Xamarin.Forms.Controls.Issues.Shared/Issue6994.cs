b4e3f5190f3eccc7d206934c5d94f027a287c316
Samantha Houts <samantha.houts@xamarin.com>
2019-08-15 10:10:39 -0700

    await

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6994.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6994.cs
@@ -74,9 +74,7 @@ namespace Xamarin.Forms.Controls.Issues
 						await Task.Delay(20);
 						source.Clear();
 						await Task.Delay(2000);
-#pragma warning disable CS4014
-						Device.InvokeOnMainThreadAsync(() => instructions.Text = "Success");
-#pragma warning restore CS4014
+						await Device.InvokeOnMainThreadAsync(() => instructions.Text = "Success");
 					});
 
 				stackLayout.Children.Remove(instructions);
