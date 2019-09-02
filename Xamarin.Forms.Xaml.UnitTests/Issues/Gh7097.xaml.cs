fc7099ca1cf2c0c6cdd5c1e7aa7748dc200a7b94
Stephane Delcroix <stephane@delcroix.org>
2019-09-02 21:29:53 +0200

    [X] Reuse baseclass namescope if it exists
    If a xaml control derives from another xaml control, thy both should share the same
    NameScope. This add a check if the xaml root already has a NameScope (set by the base)
    and reuses it.
    
    This fixes 2 regressions introduced by #7040 and some other unreported edge cases.
    
    - fixes #7097
    - fixes #7286

--- Xamarin.Forms.Xaml.UnitTests/Issues/Gh7097.xaml.cs
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Gh7097.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Input;
 using NUnit.Framework;
 using Xamarin.Forms;
 using Xamarin.Forms.Core.UnitTests;
@@ -9,7 +10,7 @@ namespace Xamarin.Forms.Xaml.UnitTests
 	public partial class Gh7097 : Gh7097Base
 	{
 		public Gh7097() => InitializeComponent();
-		public Gh7097(bool useCompiledXaml) : base()
+		public Gh7097(bool useCompiledXaml) : base(useCompiledXaml)
 		{
 			//this stub will be replaced at compile time
 		}
@@ -36,10 +37,14 @@ namespace Xamarin.Forms.Xaml.UnitTests
 			[Test]
 			public void CanXReferenceRoot([Values(false, true)]bool useCompiledXaml)
 			{
-				var layout = new Gh7097(useCompiledXaml);
+				var layout = new Gh7097(useCompiledXaml) { BindingContext = new {
+						Button1Command = new MockCommand(),
+						Button2Command = new MockCommand(),
+					} };
 				var cv = layout.Content as CollectionView;
-				var content = cv.ItemTemplate.CreateContent();
-				//Assert.That((layout.Content as Label).Text, Is.EqualTo("Foo"));
+				var content = cv.ItemTemplate.CreateContent() as StackLayout;
+				var btn1 = content.Children[0] as Button;
+				Assert.That(btn1.Command, Is.TypeOf<MockCommand>());
 			}
 
 			[Test]
@@ -47,8 +52,18 @@ namespace Xamarin.Forms.Xaml.UnitTests
 			public void RegisteringXNameOnSubPages([Values(false, true)]bool useCompiledXaml)
 			{
 				var layout = new Gh7097(useCompiledXaml);
+				var s = layout.FindByName("self");
 				Assert.That(layout.self, Is.Not.Null);
 				Assert.That(layout.collectionview, Is.Not.Null);
+			}
+
+			class MockCommand : ICommand
+			{
+#pragma warning disable 0067
+				public event EventHandler CanExecuteChanged;
+#pragma warning restore 0067
+				public bool CanExecute(object parameter) => true;
+				public void Execute(object parameter) => throw new NotImplementedException();
 			}
 		}
 	}
