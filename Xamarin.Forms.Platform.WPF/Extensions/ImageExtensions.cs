ba12b95d943d1122a4580f3a61bd93da77973002
neolithos <neolithos@outlook.com>
2019-09-10 10:06:15 +0200

    Wpf api changes #7445

--- Xamarin.Forms.Platform.WPF/Extensions/ImageExtensions.cs
+++ Xamarin.Forms.Platform.WPF/Extensions/ImageExtensions.cs
@@ -10,8 +10,12 @@ using WImageSource = System.Windows.Media.ImageSource;
 
 namespace Xamarin.Forms.Platform.WPF
 {
-	internal static class ImageExtensions
+	/// <summary>Helper to convert xamarin image stuff to wpf image stuff.</summary>
+	public static class ImageExtensions
 	{
+		/// <summary>Convert xamarin stretch to wpf aspect.</summary>
+		/// <param name="aspect"></param>
+		/// <returns></returns>
 		public static Stretch ToStretch(this Aspect aspect)
 		{
 			switch (aspect)
@@ -26,12 +30,11 @@ namespace Xamarin.Forms.Platform.WPF
 			}
 		}
 
-		public static WImageSource ToWindowsImageSource(this ImageSource source)
-		{
-			return source.ToWindowsImageSourceAsync().GetAwaiter().GetResult();
-		}
-
-		public static async Task<WImageSource> ToWindowsImageSourceAsync(this ImageSource source, CancellationToken cancellationToken = default(CancellationToken))
+		/// <summary>Convert a xamarin image source to a wpf image source.</summary>
+		/// <param name="source">Xamarin forms image source</param>
+		/// <param name="cancellationToken">Cancellation token for the operation.</param>
+		/// <returns>Wpf image source</returns>
+		public static async Task<WImageSource> ToWindowsImageSourceAsync(this ImageSource source, CancellationToken cancellationToken = default)
 		{
 			if (source == null || source.IsEmpty)
 				return null;
