6ff3e47318496fd96207955772246ce9efec6290
Pieter Nijs <pieternijs@live.be>
2019-09-13 15:20:06 +0200

    Updated renderer with additional ShadowView

--- Xamarin.Forms.Platform.iOS/Renderers/FrameRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/FrameRenderer.cs
@@ -1,11 +1,15 @@
 using System.ComponentModel;
 using System.Drawing;
+using CoreAnimation;
+using CoreGraphics;
 using UIKit;
 
 namespace Xamarin.Forms.Platform.iOS
 {
 	public class FrameRenderer : VisualElementRenderer<Frame>
 	{
+		ShadowView _shadowView;
+
 		protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
 		{
 			base.OnElementChanged(e);
@@ -39,16 +43,6 @@ namespace Xamarin.Forms.Platform.iOS
 			else
 				Layer.BackgroundColor = Element.BackgroundColor.ToCGColor();
 
-			if (Element.HasShadow)
-			{
-				Layer.ShadowRadius = 5;
-				Layer.ShadowColor = UIColor.Black.CGColor;
-				Layer.ShadowOpacity = 0.8f;
-				Layer.ShadowOffset = new SizeF();
-			}
-			else
-				Layer.ShadowOpacity = 0;
-
 			if (Element.BorderColor == Color.Default)
 				Layer.BorderColor = UIColor.Clear.CGColor;
 			else
@@ -57,8 +51,76 @@ namespace Xamarin.Forms.Platform.iOS
 				Layer.BorderWidth = 1;
 			}
 
+			if (Element.HasShadow)
+			{
+				if (_shadowView == null)
+				{
+					_shadowView = new ShadowView(Layer);
+					SetNeedsLayout();
+				}
+				_shadowView.Layer.CornerRadius = Layer.CornerRadius;
+				_shadowView.Layer.BorderColor = Layer.BorderColor;
+			}
+			else
+			{
+				if (_shadowView != null)
+				{
+					_shadowView.RemoveFromSuperview();
+					_shadowView.Dispose();
+					_shadowView = null;
+				}
+			}
+
 			Layer.RasterizationScale = UIScreen.MainScreen.Scale;
 			Layer.ShouldRasterize = true;
+		}
+
+		public override void LayoutSubviews()
+		{
+			if (_shadowView != null)
+			{
+				if (_shadowView.Superview == null)
+					Superview.InsertSubviewBelow(_shadowView, this);
+
+				_shadowView?.SetNeedsLayout();
+			}
+			base.LayoutSubviews();
+		}
+
+		class ShadowView : UIView
+		{
+			CALayer _shadowee;
+			CGRect _previousBounds;
+			CGRect _previousFrame;
+
+			public ShadowView(CALayer shadowee)
+			{
+				_shadowee = shadowee;
+				BackgroundColor = UIColor.Clear;
+				Layer.ShadowRadius = 5;
+				Layer.ShadowColor = UIColor.Black.CGColor;
+				Layer.ShadowOpacity = 0.8f;
+				Layer.ShadowOffset = new SizeF();
+				Layer.ShadowOpacity = 0.8f;
+				Layer.BorderWidth = 1;
+			}
+
+			public override void LayoutSubviews()
+			{
+				if (_shadowee.Bounds != _previousBounds || _shadowee.Frame != _previousFrame)
+				{
+					base.LayoutSubviews();
+					SetBounds();
+				}
+			}
+
+			void SetBounds()
+			{
+				Layer.Frame = _shadowee.Frame;
+				Layer.Bounds = _shadowee.Bounds;
+				_previousBounds = _shadowee.Bounds;
+				_previousFrame = _shadowee.Frame;
+			}
 		}
 	}
 }
