dotnet/maui

commit 4f2803570587086ee6857a9d8574b5e83802a07e

visualDiagnostics (#7474)

Xamarin.Forms.Xaml/ApplyPropertiesVisitor.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Xml;
 using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml.Diagnostics;
 using Xamarin.Forms.Xaml.Internals;
 
 using static System.String;
@@ -344,16 +345,25 @@ namespace Xamarin.Forms.Xaml
 				return;
 
 			//If it's a BindableProberty, SetValue
-			if (xpe == null && TrySetValue(xamlelement, property, attached, value, lineInfo, serviceProvider, out xpe))
+			if (xpe == null && TrySetValue(xamlelement, property, attached, value, lineInfo, serviceProvider, out xpe)) {
+				VisualDiagnostics.RegisterSourceInfo(value, null, ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
+				VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
 				return;
+			}
 
 			//If we can assign that value to a normal property, let's do it
-			if (xpe == null && TrySetProperty(xamlelement, localName, value, lineInfo, serviceProvider, context, out xpe))
+			if (xpe == null && TrySetProperty(xamlelement, localName, value, lineInfo, serviceProvider, context, out xpe)) {
+				VisualDiagnostics.RegisterSourceInfo(value, null, ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
+				VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
 				return;
+			}
 
 			//If it's an already initialized property, add to it
-			if (xpe == null && TryAddToProperty(xamlelement, propertyName, value, xKey, lineInfo, serviceProvider, context, out xpe))
+			if (xpe == null && TryAddToProperty(xamlelement, propertyName, value, xKey, lineInfo, serviceProvider, context, out xpe)) {
+				VisualDiagnostics.RegisterSourceInfo(value, null, ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
+				VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
 				return;
+			}
 
 			xpe = xpe ?? new XamlParseException($"Cannot assign property \"{localName}\": Property does not exist, or is not assignable, or mismatching type between value and property", lineInfo);
 			if (context.ExceptionHandler != null)

Xamarin.Forms.Xaml/Diagnostics/DebuggerHelper.cs
@@ -1,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System;
+using System.Diagnostics;
+
+namespace Xamarin.Forms.Xaml.Diagnostics
+{
+	static class DebuggerHelper
+	{
+		internal static bool _mockDebuggerIsAttached;
+		public static bool DebuggerIsAttached => _mockDebuggerIsAttached || Debugger.IsAttached;
+	}
+}

Xamarin.Forms.Xaml/Diagnostics/VisualDiagnostics.cs
@@ -1,0 +1,71 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Xamarin.Forms.Xaml.Diagnostics
+{
+	class VisualDiagnostics
+	{
+		static ConditionalWeakTable<object, XamlSourceInfo> sourceInfos = new ConditionalWeakTable<object, XamlSourceInfo>();
+		internal static void RegisterSourceInfo(object target, Uri uri, int lineNumber, int linePosition)
+		{
+			if (DebuggerHelper.DebuggerIsAttached)
+				sourceInfos.Add(target, new XamlSourceInfo(uri, lineNumber, linePosition));
+		}
+
+		internal static void SendVisualTreeChanged(object parent, object child)
+		{
+			if (DebuggerHelper.DebuggerIsAttached)
+				VisualTreeChanged?.Invoke(parent, new VisualTreeChangeEventArgs(parent, child, -1, VisualTreeChangeType.Add));
+		}
+
+		public static event EventHandler<VisualTreeChangeEventArgs> VisualTreeChanged;
+		public static XamlSourceInfo GetXamlSourceInfo(object obj) => sourceInfos.TryGetValue(obj, out var sourceinfo) ? sourceinfo : null;
+	}
+
+	class XamlSourceInfo
+	{
+		public XamlSourceInfo(Uri sourceUri, int lineNumber, int linePosition)
+		{
+			SourceUri = sourceUri;
+			LineNumber = lineNumber;
+			LinePosition = linePosition;
+		}
+
+		public Uri SourceUri { get; }
+		public int LineNumber { get; }
+		public int LinePosition { get; }
+
+		public void Deconstruct(out Uri sourceUri, out int lineNumber, out int linePosition)
+		{
+			sourceUri = SourceUri;
+			lineNumber = LineNumber;
+			linePosition = LinePosition;
+		}
+	}
+
+	class VisualTreeChangeEventArgs : EventArgs
+	{
+		public VisualTreeChangeEventArgs(object parent, object child, int childIndex, VisualTreeChangeType changeType)
+		{
+			Parent = parent;
+			Child = child;
+			ChildIndex = childIndex;
+			ChangeType = changeType;
+		}
+
+		public object Parent { get; }
+		public object Child { get; }
+		public int ChildIndex { get; }
+		public VisualTreeChangeType ChangeType { get; }
+	}
+
+	enum VisualTreeChangeType
+	{
+		Add = 0,
+		Remove = 1
+	}
+}

Xamarin.Forms.Xaml/Diagnostics/ResourceDictionaryDiagnostics.cs
@@ -4,7 +4,11 @@ namespace Xamarin.Forms.Xaml.Diagnostics
 	internal static class ResourceDictionaryDiagnostics
 	{
 		internal static void OnStaticResourceResolved(ResourceDictionary resourceDictionary, string key, object targetObject, object targetProperty)
-			=> StaticResourceResolved?.Invoke(resourceDictionary, new StaticResourceResolvedEventArgs(resourceDictionary, key, targetObject, targetProperty));
+		{
+			if (DebuggerHelper.DebuggerIsAttached)
+				StaticResourceResolved?.Invoke(resourceDictionary, new StaticResourceResolvedEventArgs(resourceDictionary, key, targetObject, targetProperty));
+		}
+
 		public static event EventHandler<StaticResourceResolvedEventArgs> StaticResourceResolved;
 	}
 }

Xamarin.Forms.Xaml/MarkupExtensions/StaticResourceExtension.cs
@@ -10,8 +10,6 @@ namespace Xamarin.Forms.Xaml
 	[ContentProperty(nameof(Key))]
 	public sealed class StaticResourceExtension : IMarkupExtension
 	{
-		internal static bool _mockDebuggerIsAttached; //for unit testing
-
 		public string Key { get; set; }
 		public object ProvideValue(IServiceProvider serviceProvider)
 		{
@@ -28,8 +26,7 @@ namespace Xamarin.Forms.Xaml
 				&& !TryGetApplicationLevelResource(Key, out resource, out resourceDictionary))
 				throw new XamlParseException($"StaticResource not found for key {Key}", xmlLineInfo);
 
-			if (System.Diagnostics.Debugger.IsAttached || _mockDebuggerIsAttached)
-				Diagnostics.ResourceDictionaryDiagnostics.OnStaticResourceResolved(resourceDictionary, Key, valueProvider.TargetObject, valueProvider.TargetProperty);
+			Diagnostics.ResourceDictionaryDiagnostics.OnStaticResourceResolved(resourceDictionary, Key, valueProvider.TargetObject, valueProvider.TargetProperty);
 
 			return CastTo(resource, valueProvider.TargetProperty);
 		}

xamarin/Xamarin.Forms

commit 8395416724d9f9630d7b4a826ed1b87fcb2651a7

[X] avoid throwing in VisualDiagnostics
this fixes a regression introduced in #7474

- fixes #7570

Xamarin.Forms.Xaml/ApplyPropertiesVisitor.cs
@@ -346,22 +346,28 @@ namespace Xamarin.Forms.Xaml
 
 			//If it's a BindableProberty, SetValue
 			if (xpe == null && TrySetValue(xamlelement, property, attached, value, lineInfo, serviceProvider, out xpe)) {
-				VisualDiagnostics.RegisterSourceInfo(value, null, ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
-				VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
+				if (!(node is ValueNode) && value != null && !value.GetType().GetTypeInfo().IsValueType && XamlFilePathAttribute.GetFilePathForObject(context.RootElement) is string path) {
+					VisualDiagnostics.RegisterSourceInfo(value, new Uri(path, UriKind.Relative), ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
+					VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
+				}
 				return;
 			}
 
 			//If we can assign that value to a normal property, let's do it
 			if (xpe == null && TrySetProperty(xamlelement, localName, value, lineInfo, serviceProvider, context, out xpe)) {
-				VisualDiagnostics.RegisterSourceInfo(value, null, ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
-				VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
+				if (!(node is ValueNode) && value != null && !value.GetType().GetTypeInfo().IsValueType && XamlFilePathAttribute.GetFilePathForObject(context.RootElement) is string path) {
+					VisualDiagnostics.RegisterSourceInfo(value, new Uri(path, UriKind.Relative), ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
+					VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
+				}
 				return;
 			}
 
 			//If it's an already initialized property, add to it
 			if (xpe == null && TryAddToProperty(xamlelement, propertyName, value, xKey, lineInfo, serviceProvider, context, out xpe)) {
-				VisualDiagnostics.RegisterSourceInfo(value, null, ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
-				VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
+				if (!(node is ValueNode) && value != null && !value.GetType().GetTypeInfo().IsValueType && XamlFilePathAttribute.GetFilePathForObject(context.RootElement) is string path) {
+					VisualDiagnostics.RegisterSourceInfo(value, new Uri(path, UriKind.Relative), ((IXmlLineInfo)node).LineNumber, ((IXmlLineInfo)node).LinePosition);
+					VisualDiagnostics.SendVisualTreeChanged(xamlelement, value);
+				}
 				return;
 			}
 

Xamarin.Forms.Xaml/Diagnostics/DebuggerHelper.cs
@@ -7,6 +7,10 @@ namespace Xamarin.Forms.Xaml.Diagnostics
 {
 	static class DebuggerHelper
 	{
+#if DEBUG
+		static DebuggerHelper() => _mockDebuggerIsAttached = true;
+#endif
+
 		internal static bool _mockDebuggerIsAttached;
 		public static bool DebuggerIsAttached => _mockDebuggerIsAttached || Debugger.IsAttached;
 	}

Xamarin.Forms.Xaml/Diagnostics/VisualDiagnostics.cs
@@ -12,7 +12,7 @@ namespace Xamarin.Forms.Xaml.Diagnostics
 		static ConditionalWeakTable<object, XamlSourceInfo> sourceInfos = new ConditionalWeakTable<object, XamlSourceInfo>();
 		internal static void RegisterSourceInfo(object target, Uri uri, int lineNumber, int linePosition)
 		{
-			if (DebuggerHelper.DebuggerIsAttached)
+			if (DebuggerHelper.DebuggerIsAttached && !sourceInfos.TryGetValue(target, out _))
 				sourceInfos.Add(target, new XamlSourceInfo(uri, lineNumber, linePosition));
 		}
 