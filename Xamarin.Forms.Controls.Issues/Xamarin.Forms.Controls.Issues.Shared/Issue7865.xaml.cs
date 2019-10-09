f31cca63c2f979588e4bb458164135400afe0aa9
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-10-09 11:58:29 +0200

    Added sample file

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7865.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms.Internals;
+using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.CarouselView)]
+#endif
+#if APP
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 7865, "[iOS] CarouselView setting Position has dif behavior than ScrollTo")]
+	public partial class Issue7865 : TestContentPage
+	{
+		public Issue7865()
+		{
+#if APP
+			Device.SetFlags(new List<string> { CollectionView.CollectionViewExperimental });
+			Title = "Issue 7865";
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+		
+		}
+	}
+}
