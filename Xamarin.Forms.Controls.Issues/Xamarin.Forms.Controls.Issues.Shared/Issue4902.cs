d01399b9c253afa8038d522cbc338fcdff6aad66
Konrad Mueller <k.l.mueller@outlook.com>
2019-10-11 23:56:04 +0200

    Add ui test

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4902.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4902.cs
@@ -10,26 +10,28 @@ using NUnit.Framework;
 namespace Xamarin.Forms.Controls.Issues
 {
 #if UITEST
-	[Category(UITestCategories.ManualReview)]
+	[Category(UITestCategories.Slider)]
 #endif
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 4902, "Slider converts Value to minimum when minimum is greater than zero", PlatformAffected.WPF)]
 	public class Issue4902 : TestContentPage
 	{
-		readonly Slider _slider = new Slider(10, 20, 18);
+		readonly Slider _slider = new Slider(10, 20, 18)
+		{
+			AutomationId = "testslider"
+		};
 		readonly Label _valueLabel = new Label
 		{
 			Text = "Min: 10, Max: 20, Value: 18"
 		};
 
-		readonly Button _setValueButton = new Button { Text = "Set value to 15" };
+		readonly Button _setValueButton = new Button { Text = "Set value to 15", AutomationId = "testbutton"};
 
 		protected override void Init()
 		{
 			// Initialize ui here instead of ctor
 
-			Content = new StackLayout { Children = { _valueLabel, _slider, _setValueButton } };
-			//new Label { AutomationId = "Issue1Label", Text = "See if I'm here" };}
+			Content = new StackLayout { Children = { _valueLabel, _slider, _setValueButton } }; 
 
 			_slider.ValueChanged += (sender, args) =>
 			{
@@ -38,29 +40,21 @@ namespace Xamarin.Forms.Controls.Issues
 			_setValueButton.Clicked += (sender, args) =>
 			{
 				_slider.Value = 15;
-			};
-
-			BindingContext = new ViewModelIssue4902();
+			}; 
 		}
 
 #if UITEST
 		[Test]
-		public void Issue1Test() 
-		{
-			// Delete this and all other UITEST sections if there is no way to automate the test. Otherwise, be sure to rename the test and update the Category attribute on the class. Note that you can add multiple categories.
-			RunningApp.Screenshot("I am at Issue1");
-			RunningApp.WaitForElement(q => q.Marked("Issue1Label"));
-			RunningApp.Screenshot("I see the Label");
+		public void Issue4902Test() 
+		{ 
+			RunningApp.Screenshot("I am at Issue 4902");
+			RunningApp.WaitForElement(q => q.Marked("testslider"));
+			Assert.AreEqual(18, _slider.Value);
+			RunningApp.Screenshot("The value is initially set to the correct value");
+			RunningApp.Tap("testbutton");
+			Assert.AreEqual(15, _slider.Value);
+			RunningApp.Screenshot("The value changed to the correct value");
 		}
 #endif
-	}
-
-	[Preserve(AllMembers = true)]
-	public class ViewModelIssue4902
-	{
-		public ViewModelIssue4902()
-		{
-
-		}
-	}
+	} 
 }
