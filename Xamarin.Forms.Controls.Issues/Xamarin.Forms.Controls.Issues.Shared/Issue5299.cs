1c70edd12b520559f9bfbd945fcac8e7083dc695
Emmanuel Van Meerbeeck <emmanuel.vanmeerbeeck@3factr.be>
2019-10-14 11:08:04 +0200

    added issue #5299

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5299.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 5299, "Disabled Button Appearance", PlatformAffected.iOS | PlatformAffected.Android)]
+	public class Issue5299 : TestContentPage
+	{
+		protected override void Init()
+		{
+			Content = new StackLayout
+			{
+				Children =
+				{
+					 new Button
+					 {
+						Text = "This text should be disabled and have a custom disabled text color.",
+						TextColor = Color.Black,
+						DisabledTextColor = Color.Gray,
+						IsEnabled = false
+					 }
+				}
+			};
+		}
+	}
+}
