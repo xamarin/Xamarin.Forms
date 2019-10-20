f4c856e710693c4677104d9072c48ce942bda38b
Rastislav Novotný <rasto@duracellko.net>
2019-10-20 10:14:45 +0800

    Convert HorizontalGrid and VerticalGrid strings to ItemsLayout (#5577)

--- Xamarin.Forms.Core/Items/ItemsLayoutTypeConverter.cs
+++ Xamarin.Forms.Core/Items/ItemsLayoutTypeConverter.cs
@@ -1,23 +1,51 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Xamarin.Forms
 {
 	[Xaml.TypeConversion(typeof(IItemsLayout))]
 	public class ItemsLayoutTypeConverter : TypeConverter
 	{
+		private const string HorizontalGridPrefix = "HorizontalGrid,";
+		private const string VerticalGridPrefix = "VerticalGrid,";
+
 		public override object ConvertFromInvariantString(string value)
 		{
-			if (value == "HorizontalList")
+			if (value == null)
 			{
-				return LinearItemsLayout.Horizontal;
+				throw new ArgumentNullException(nameof(value));
 			}
 
-			if (value == "VerticalList")
+			switch (value)
 			{
-				return LinearItemsLayout.Vertical;
+				case "HorizontalList":
+					return LinearItemsLayout.Horizontal;
+				case "VerticalList":
+					return LinearItemsLayout.Vertical;
+				case "HorizontalGrid":
+					return new GridItemsLayout(ItemsLayoutOrientation.Horizontal);
+				case "VerticalGrid":
+					return new GridItemsLayout(ItemsLayoutOrientation.Vertical);
+			}
+
+			if (value.StartsWith(HorizontalGridPrefix, StringComparison.Ordinal))
+			{
+				var span = ParseGridSpan(value, HorizontalGridPrefix);
+				return new GridItemsLayout(span, ItemsLayoutOrientation.Horizontal);
+			}
+			else if (value.StartsWith(VerticalGridPrefix, StringComparison.Ordinal))
+			{
+				var span = ParseGridSpan(value, VerticalGridPrefix);
+				return new GridItemsLayout(span, ItemsLayoutOrientation.Vertical);
 			}
 
 			throw new InvalidOperationException($"Cannot convert \"{value}\" into {typeof(IItemsLayout)}");
+		}
+
+		private static int ParseGridSpan(string value, string prefix)
+		{
+			var argument = value.Substring(prefix.Length);
+			return int.Parse(argument, NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite, CultureInfo.InvariantCulture);
 		}
 	}
 }
