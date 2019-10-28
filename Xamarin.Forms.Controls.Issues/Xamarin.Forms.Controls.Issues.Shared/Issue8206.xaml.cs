3a5e080f278b958f8345f01fd8220ed208e0e146
Daniel A. Monetelli L <danielmonetelli@hotmail.com>
2019-10-28 10:31:18 -0500

    FlyoutBackgroundImage is not displayed in UWP

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8206.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 8206, "FlyoutBackgroundImage is not displayed in UWP", PlatformAffected.UWP)]
+	public partial class Issue8206 : Shell
+    {
+        public Issue8206()
+        {
+#if APP
+			InitializeComponent();
+#endif 
+        }
+    }
+}
