b77dec744a00ca6ef97eba66d73f1f4886bc37e9
basdecort <bas.decort@infosupport.com>
2019-10-29 17:35:05 +0100

    Added label to test case

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5807.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5807.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Xamarin.Forms.CustomAttributes;
+﻿using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 
 namespace Xamarin.Forms.Controls.Issues
 {
 	[Preserve(AllMembers = true)]
-	[Issue(IssueTracker.Github, 5807, "[Visual] [iOS] Material entry text caret miss aligned", PlatformAffected.iOS)]
+	[Issue(IssueTracker.Github, 5807, "[Visual][iOS] Material entry text caret miss aligned", PlatformAffected.iOS)]
 	public class Issue5807 : TestContentPage
 	{
 		protected override void Init()
@@ -14,6 +13,11 @@ namespace Xamarin.Forms.Controls.Issues
 			{
 				Children =
 				{
+					new Label
+					{
+						Text = "Tap on the Entry and verify that the Caret is in the correct (center) position.",
+						Margin = new Thickness(30)
+					},
 					new Entry
 					{
 						HorizontalTextAlignment = TextAlignment.Center,
