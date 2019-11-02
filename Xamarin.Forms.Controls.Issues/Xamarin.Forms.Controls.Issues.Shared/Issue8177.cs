cbad80e1ed99a2525f6ac4ac1a874a03dacd1325
E.Z. Hart <hartez@gmail.com>
2019-11-02 13:05:04 -0600

    Make character spacing work for UWP Picker without breaking ItemsSource Fixes #8177

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8177.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 8177, "[Bug] Picker does not update when it's underlying list changes content",
+		PlatformAffected.UWP)]
+#if UITEST
+	[NUnit.Framework.Category(UITestCategories.Picker)]
+#endif
+	public class Issue8177 : TestContentPage
+	{
+		protected override void Init()
+		{
+			var layout = new StackLayout();
+
+			var button = new Button { Text = "Change Picker Contents " };
+			
+			var originalList = new List<string> { "one", "two", "three" };
+			var picker = new Picker { ItemsSource = originalList };
+
+			var newList = new List<string> { "uno", "dos", "tres", "quatro" };
+			button.Clicked += (sender, args) => { picker.ItemsSource = newList; };
+
+			layout.Children.Add(button);
+			layout.Children.Add(picker);
+
+			Content = layout;
+		}
+	}
+}
