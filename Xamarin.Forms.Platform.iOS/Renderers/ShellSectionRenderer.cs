fcd4ba0d115d0cdac823242ceb2032a6a5a81670
Shane Neuville <shneuvil@microsoft.com>
2019-11-06 22:01:09 -0700

    - fix ios to propagate nav bar on page push

--- Xamarin.Forms.Platform.iOS/Renderers/ShellSectionRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ShellSectionRenderer.cs
@@ -207,11 +207,8 @@ namespace Xamarin.Forms.Platform.iOS
 			if (_displayedPage != null)
 			{
 				_displayedPage.PropertyChanged += OnDisplayedPagePropertyChanged;
-				if (!ShellSection.Stack.Contains(_displayedPage))
-				{
-					UpdateNavigationBarHidden();
-					UpdateNavigationBarHasShadow();
-				}
+				UpdateNavigationBarHidden();
+				UpdateNavigationBarHasShadow();
 			}
 		}
 
