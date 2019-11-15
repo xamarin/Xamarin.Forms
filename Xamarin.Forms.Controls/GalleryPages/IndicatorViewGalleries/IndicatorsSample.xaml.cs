cfd6e7e6d8cfb5df8653f812b215bb1d7ba23dd0
Rui Marinho <me@ruimarinho.net>
2019-11-15 20:25:56 +0000

    Add  IndicatorSample

---
+++ Xamarin.Forms.Controls/GalleryPages/IndicatorViewGalleries/IndicatorsSample.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IndicatorsSample : ContentPage
+	{
+		public IndicatorsSample()
+		{
+			Device.SetFlags(new[] { ExperimentalFlags.CarouselViewExperimental, ExperimentalFlags.IndicatorViewExperimental });
+			InitializeComponent();
+			BindingContext = new GalleryPages.CollectionViewGalleries.CarouselViewGalleries.CarouselItemsGalleryViewModel();
+			indicators.SetItemsSourceBy(carousel);
+			//indicatorsForms.Visual = VisualMarker.Forms;
+			indicatorsForms.SetItemsSourceBy(carousel);
+		}
+	}
+}
