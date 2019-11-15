a321da8b7fc82a8415223f61039d659483737030
Jon Goldberger <jgolde@microsoft.com>
2019-11-15 12:09:19 -1000

    Fixes Issue #6187 - Replaces TextColor with Placeholdercolor for setting the underline color in the relavant Material renderers (or base classes if appropriate)

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6187.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 6187, "Visual Material Entry Underline color should reference PlaceholderColor",
+		PlatformAffected.All)]
+#if UITEST
+	[NUnit.Framework.Category(UITestCategories.Entry)]
+#endif
+	public class Issue6187 : TestContentPage
+	{
+		protected override void Init()
+		{
+			Visual = VisualMarker.Material;
+
+			Entry entry = new Entry
+			{
+				Placeholder = "Enter text...",
+				PlaceholderColor = Color.Green,
+				TextColor = Color.Red
+			};
+
+			StackLayout layout = new StackLayout
+			{
+				Padding = new Thickness(0, 50, 0, 0)
+			};
+
+			layout.Children.Add(entry);
+
+			Content = layout;
+		}
+
+
+
+#if UITEST
+		[Test]
+		public void ImageShouldLayoutOnTopOfButton()
+		{
+			
+		}
+#endif
+	}
+}
