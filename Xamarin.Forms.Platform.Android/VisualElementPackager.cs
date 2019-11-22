4cd633cb593526aa3ce189fe2f405818910367bc
Kevin Petit <Kevin.Petit@equadex.net>
2019-11-22 15:24:47 +0100

    Fix unbalanced performance start/stop call.

--- Xamarin.Forms.Platform.Android/VisualElementPackager.cs
+++ Xamarin.Forms.Platform.Android/VisualElementPackager.cs
@@ -156,9 +156,9 @@ namespace Xamarin.Forms.Platform.Android
 					_childViews.Add(renderer);
 				}
 				Performance.Stop(reference, "Add view");
-
-				Performance.Stop(reference);
 			}
+
+			Performance.Stop(reference);
 		}
 
 		void EnsureChildOrder() => EnsureChildOrder(false);
