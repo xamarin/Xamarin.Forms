4f601fef30bd75b330ce47b2c673552ce1823a16
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-12-04 11:26:00 +0100

    Added Windows Platform Specific to avoid set the default image directory

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue3228.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 3228, "[UWP] Default Search Directory for UWP Icons (Platform Specific)")]
+	public partial class Issue3228 : TestContentPage
+	{
+		public Issue3228()
+		{
+#if APP
+			InitializeComponent();
+			Application.Current.On<Windows>().SetImageSearchDirectory("Assets");
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
---
+++ Xamarin.Forms.Core/PlatformConfiguration/WindowsSpecific/Application.cs
@@ -0,0 +1,32 @@
+﻿namespace Xamarin.Forms.PlatformConfiguration.WindowsSpecific
+{
+	using FormsElement = Forms.Application;
+
+	public static class Application
+	{
+		public static readonly BindableProperty ImageSearchDirectoryProperty =
+			BindableProperty.Create("ImageSearchDirectory", typeof(string), typeof(FormsElement), string.Empty);
+
+		public static void SetImageSearchDirectory(BindableObject element, string value)
+		{
+			element.SetValue(ImageSearchDirectoryProperty, value);
+		}
+
+		public static string GetImageSearchDirectory(this IPlatformElementConfiguration<Windows, FormsElement> config)
+		{
+			return (string)config.Element.GetValue(ImageSearchDirectoryProperty);
+		}
+
+		public static string GetImageSearchDirectory(BindableObject element)
+		{
+			return (string)element.GetValue(ImageSearchDirectoryProperty);
+		}
+
+		public static IPlatformElementConfiguration<Windows, FormsElement> SetImageSearchDirectory(
+			this IPlatformElementConfiguration<Windows, FormsElement> config, string value)
+		{
+			config.Element.SetValue(ImageSearchDirectoryProperty, value);
+			return config;
+		}
+	}
+}
