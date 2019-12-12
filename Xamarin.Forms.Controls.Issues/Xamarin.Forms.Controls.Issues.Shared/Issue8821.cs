12dbf3e694f8674aac114323666f485f2d6f2b24
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-12-12 14:33:28 +0100

    Fixed build

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8821.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8821.cs
@@ -4,6 +4,12 @@ using System.Net;
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
 namespace Xamarin.Forms.Controls.Issues
 {
 #if UITEST
