7029326bbb023014aa661dfb2d31ccf6c47db7c7
Samantha Houts <samhouts@users.noreply.github.com>
2019-12-16 15:44:27 -0800

    Update ShellRenderer.cs

--- Xamarin.Forms.Platform.UAP/Shell/ShellRenderer.cs
+++ Xamarin.Forms.Platform.UAP/Shell/ShellRenderer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using Windows.Foundation.Metadata;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
