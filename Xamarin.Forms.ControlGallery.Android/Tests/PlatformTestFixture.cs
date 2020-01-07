aa63b92f2247d944ad214357f6220514de3b3dea
E.Z. Hart <hartez@gmail.com>
2020-01-06 23:19:28 -0700

    Button background color test for Android

--- Xamarin.Forms.ControlGallery.Android/Tests/PlatformTestFixture.cs
+++ Xamarin.Forms.ControlGallery.Android/Tests/PlatformTestFixture.cs
@@ -1,7 +1,12 @@
 ﻿using Android.Content;
 using Android.Content.PM;
+using Android.Support.V7.Widget;
 using Android.Widget;
 using Xamarin.Forms.Platform.Android;
+using AView = Android.Views.View;
+using AColor = Android.Graphics.Color;
+using Android.Graphics;
+using Android.Views;
 
 namespace Xamarin.Forms.ControlGallery.Android.Tests
 {
@@ -46,6 +51,53 @@ namespace Xamarin.Forms.ControlGallery.Android.Tests
 			var renderer = GetRenderer(entry);
 			var viewRenderer = renderer.View as ViewRenderer<Entry, FormsEditText>;
 			return viewRenderer.Control;
+		}
+
+		protected AppCompatButton GetNativeControl(Button button)
+		{
+			var renderer = GetRenderer(button);
+
+			if (renderer is AppCompatButton fastButton)
+			{
+				return fastButton;
+			}
+
+			var viewRenderer = renderer.View as ViewRenderer<Button, AppCompatButton>;
+			return viewRenderer.Control;
+		}
+
+		protected AColor GetColorAtCenter(AView view) 
+		{
+			var rect = new Rect();
+			rect.Set(0, 0, view.Width, view.Height);
+
+			var bitmap = Bitmap.CreateBitmap(rect.Width(), rect.Height(), Bitmap.Config.Argb8888);
+			var canvas = new Canvas(bitmap);
+			canvas.Save();
+			canvas.Translate(rect.Left, rect.Top);
+			view.Draw(canvas);
+			canvas.Restore();
+
+			int pixel = bitmap.GetPixel(rect.CenterX(), rect.CenterY());
+
+			int red = AColor.GetRedComponent(pixel);
+			int blue = AColor.GetBlueComponent(pixel);
+			int green = AColor.GetGreenComponent(pixel);
+
+			return AColor.Rgb(red, green, blue);
+		}
+
+		protected void Layout(VisualElement element, AView nativeView) 
+		{
+			var size = element.Measure(double.PositiveInfinity, double.PositiveInfinity, MeasureFlags.IncludeMargins);
+			var width = size.Request.Width;
+			var height = size.Request.Height;
+			element.Layout(new Rectangle(0, 0, width, height));
+
+			int widthSpec = AView.MeasureSpec.MakeMeasureSpec((int)width, MeasureSpecMode.Exactly);
+			int heightSpec = AView.MeasureSpec.MakeMeasureSpec((int)height, MeasureSpecMode.Exactly);
+			nativeView.Measure(widthSpec, heightSpec);
+			nativeView.Layout(0, 0, (int)width, (int)height);
 		}
 	}
 }
