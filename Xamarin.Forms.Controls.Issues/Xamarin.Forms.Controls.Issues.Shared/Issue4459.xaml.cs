c8bf94b9c5ac3f7b19964ab6bb6f1610a6eccba4
Brian <brian@agspectrum.com>
2020-01-10 09:04:26 -0600

    Make complied xaml conditional

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4459.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4459.xaml.cs
@@ -6,12 +6,16 @@ namespace Xamarin.Forms.Controls.Issues
 {
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 4459, "[UWP] BoxView CornerRadius doesn't work", PlatformAffected.UWP)]
+#if APP
 	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
 	public partial class Issue4459 : ContentPage
 	{
 		public Issue4459()
 		{
+#if APP
 			InitializeComponent();
+#endif
 		}
 
 		void InputView_OnTextChanged(object sender, TextChangedEventArgs e)
