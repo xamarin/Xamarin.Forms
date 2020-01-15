0cf6c33042f76c2634b6be32e0edeb6e426868f9
Vincent Hoogendoorn <vincenth@workstreampeople.com>
2020-01-15 15:34:49 +0100

    Add unit tests for EnumsForGridRowsAndColumns

---
+++ Xamarin.Forms.Core.UnitTests/Markup/EnumsForGridRowsAndColumns.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using NUnit.Framework;
+using static Xamarin.Forms.Markup.EnumsForGridRowsAndColumns;
+
+namespace Xamarin.Forms.Markup.UnitTests
+{
+	[TestFixture]
+	public class EnumsForGridRowsAndColumns : MarkupBaseTestFixture
+	{
+		enum Row { First, Second, Third }
+		enum Col { First, Second, Third, Fourth }
+
+		[Test]
+		public void DefineRows()
+		{
+			var grid = new Grid
+			{
+				RowDefinitions = Rows.Define(
+					(Row.First, Auto),
+					(Row.Second, Star),
+					(Row.Third, 20)
+				)
+			};
+
+			Assert.That(grid.RowDefinitions.Count, Is.EqualTo(3));
+			Assert.That(grid.RowDefinitions[0]?.Height, Is.EqualTo(GridLength.Auto));
+			Assert.That(grid.RowDefinitions[1]?.Height, Is.EqualTo(GridLength.Star));
+			Assert.That(grid.RowDefinitions[2]?.Height, Is.EqualTo(new GridLength(20)));
+		}
+
+		[Test]
+		public void InvalidRowOrder()
+		{
+			Assert.Throws<ArgumentException>(
+				() => Rows.Define((Row.First, 8), (Row.Third, 8)),
+				$"Value of row name Third is not 1. " +
+				"Rows must be defined with enum names whose values form the sequence 0,1,2,..."
+			);
+		}
+
+		[Test]
+		public void DefineColumns()
+		{
+			var grid = new Grid
+			{
+				ColumnDefinitions = Columns.Define(
+					(Col.First, Auto),
+					(Col.Second, Star),
+					(Col.Third, 20),
+					(Col.Fourth, 40)
+				)
+			};
+
+			Assert.That(grid.ColumnDefinitions.Count, Is.EqualTo(4));
+			Assert.That(grid.ColumnDefinitions[0]?.Width, Is.EqualTo(GridLength.Auto));
+			Assert.That(grid.ColumnDefinitions[1]?.Width, Is.EqualTo(GridLength.Star));
+			Assert.That(grid.ColumnDefinitions[2]?.Width, Is.EqualTo(new GridLength(20)));
+			Assert.That(grid.ColumnDefinitions[3]?.Width, Is.EqualTo(new GridLength(40)));
+		}
+
+		[Test]
+		public void InvalidColumnOrder()
+		{
+			Assert.Throws<ArgumentException>(
+				() => Columns.Define((Col.Second, 8), (Col.First, 8)),
+				$"Value of column name Second is not 0. " +
+				"Columns must be defined with enum names whose values form the sequence 0,1,2,..."
+			);
+		}
+
+		[Test]
+		public void AllColumns()
+			=> Assert.That(All<Col>(), Is.EqualTo(4));
+
+		[Test]
+		public void LastRow()
+			=> Assert.That(Last<Row>(), Is.EqualTo(2));
+	}
+}
