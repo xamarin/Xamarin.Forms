f9911a605aa5568508fc4c82899cae8974e0b3af
E.Z. Hart <hartez@gmail.com>
2020-01-15 11:10:06 -0700

    Add scale tests for UWP

--- Xamarin.Forms.Controls/GalleryPages/PlatformTestsGallery/PlatformTestsConsole.xaml.cs
+++ Xamarin.Forms.Controls/GalleryPages/PlatformTestsGallery/PlatformTestsConsole.xaml.cs
@@ -69,8 +69,6 @@ namespace Xamarin.Forms.Controls.GalleryPages.PlatformTestsGallery
 			// e.g. var filter = new TestNameContainsFilter("Bugzilla");
 			// or var filter = new CategoryFilter("Picker");
 
-			var filter = new CategoryFilter("BoxView");
-
 			await Task.Run(() => _runner.Run()).ConfigureAwait(false);
 		}
 
