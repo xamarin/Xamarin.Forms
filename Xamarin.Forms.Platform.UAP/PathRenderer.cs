1abbd091909cc3a7b076d31c11231755f906cfc7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-01-20 13:48:47 +0100

    Implemented PathRenderer on UWP

---
+++ Xamarin.Forms.Platform.UAP/PathRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using WPath = Windows.UI.Xaml.Shapes.Path;
+
+namespace Xamarin.Forms.Platform.UWP
+{
+	public class PathRenderer : ShapeRenderer<Path, WPath>
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Path> args)
+		{
+			if (Control == null && args.NewElement != null)
+			{
+				SetNativeControl(new WPath());
+			}
+
+			base.OnElementChanged(args);
+
+			if (args.NewElement != null)
+			{
+				UpdateData();
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(sender, args);
+
+			if (args.PropertyName == Path.DataProperty.PropertyName)
+				UpdateData();
+		}
+
+		void UpdateData()
+		{
+			Control.Data = Element.Data.ToWindows();
+		}
+	}
+}
---
+++ Xamarin.Forms.Platform.UAP/PointExtensions.cs
@@ -0,0 +1,12 @@
+﻿using WPoint = Windows.Foundation.Point;
+
+namespace Xamarin.Forms.Platform.UWP
+{
+	public static class PointExtensions
+	{
+		public static WPoint ToWindows(this Point point)
+		{
+			return new WPoint(point.X, point.Y);
+		}
+	}
+}
