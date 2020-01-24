5a9a461c37a071f5722f6902de15ae1c1bf15c59
E.Z. Hart <hartez@gmail.com>
2020-01-24 12:58:10 -0700

    Add tolerance for color comparisons

--- Xamarin.Forms.ControlGallery.iOS/Tests/AssertionExtensions.cs
+++ Xamarin.Forms.ControlGallery.iOS/Tests/AssertionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using CoreGraphics;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 using UIKit;
 
 namespace Xamarin.Forms.ControlGallery.iOS.Tests
@@ -68,12 +70,46 @@ namespace Xamarin.Forms.ControlGallery.iOS.Tests
 			return pixel;
 		}
 
+		static bool ARGBEquivalent(UIColor color1, UIColor color2) 
+		{
+			color1.GetRGBA(out nfloat red1, out nfloat green1, out nfloat blue1, out nfloat alpha1);
+			color2.GetRGBA(out nfloat red2, out nfloat green2, out nfloat blue2, out nfloat alpha2);
+
+			const double tolerance = 0.000001;
+
+			return Equal(red1, red2, tolerance)
+				&& Equal(green1, green2, tolerance)
+				&& Equal(blue1, blue2, tolerance) 
+				&& Equal(alpha1, alpha2, tolerance);
+		}
+
+		static bool Equal(nfloat v1, nfloat v2, double tolerance) 
+		{
+			return Math.Abs(v1 - v2) <= tolerance;
+		}
+
 		public static UIImage AssertColorAtPoint(this UIImage bitmap, UIColor expectedColor, int x, int y)
 		{
-			Assert.That(bitmap.ColorAtPoint(x, y), Is.EqualTo(expectedColor),
-				() => bitmap.CreateColorAtPointError(expectedColor, x, y));
+			try
+			{
+				var cap = bitmap.ColorAtPoint(x, y);
 
-			return bitmap;
+				if (!ARGBEquivalent(cap, expectedColor))
+				{
+					System.Diagnostics.Debug.WriteLine("Here");
+				}
+
+				Assert.That(cap, Is.EqualTo(expectedColor).Using<UIColor>(ARGBEquivalent),
+					() => bitmap.CreateColorAtPointError(expectedColor, x, y));
+
+				return bitmap;
+			}
+			catch (Exception ex) 
+			{
+				System.Diagnostics.Debug.WriteLine(ex);
+			}
+
+			return null;
 		}
 
 		public static UIImage AssertColorAtCenter(this UIImage bitmap, UIColor expectedColor)
