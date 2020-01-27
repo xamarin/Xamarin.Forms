d179eafada62e63b09412726bf885cf3fcb3d46d
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-01-27 15:42:50 +0100

    Added an option to choose the ModalPresentationStyles on iOS

--- Xamarin.Forms.Controls/GalleryPages/ShowModalWithTransparentBkgndGalleryPage.cs
+++ Xamarin.Forms.Controls/GalleryPages/ShowModalWithTransparentBkgndGalleryPage.cs
@@ -1,38 +1,91 @@
-﻿using Xamarin.Forms.Internals;
+﻿using System;
+using System.Linq;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace Xamarin.Forms.Controls.GalleryPages
 {
 	class ShowModalWithTransparentBkgndGalleryPage : ContentPage
 	{
+		readonly Picker _modalPresentationStylesPicker;
+		readonly PageWithTransparentBkgnd _pageWithTransparentBkgnd;
+
 		public ShowModalWithTransparentBkgndGalleryPage()
 		{
 			BackgroundColor = Color.LightPink;
 
-			var btn = new Button()
+			var layout = new StackLayout();
+
+			_modalPresentationStylesPicker = new Picker();
+
+			var modalPresentationStyles = Enum.GetNames(typeof(UIModalPresentationStyle)).Select(m => m).ToList();
+
+			_modalPresentationStylesPicker.Title = "Select ModalPresentation Style";
+			_modalPresentationStylesPicker.ItemsSource = modalPresentationStyles;
+			_modalPresentationStylesPicker.SelectedIndex = 2;
+
+			_modalPresentationStylesPicker.SelectedIndexChanged += (sender, args) =>
 			{
-				Text = "Show page",
+				var selected = _modalPresentationStylesPicker.SelectedItem;
+
+				switch (selected)
+				{
+					case "Automatic":
+						_pageWithTransparentBkgnd.On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.Automatic);
+						break;
+					case "FormSheet":
+						_pageWithTransparentBkgnd.On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.FormSheet);
+						break;
+					case "FullScreen":
+						_pageWithTransparentBkgnd.On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.FullScreen);
+						break;
+					case "OverFullScreen":
+						_pageWithTransparentBkgnd.On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.OverFullScreen);
+						break;
+					case "PageSheet":
+						_pageWithTransparentBkgnd.On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.PageSheet);
+						break;
+				}
+			};
+
+			if (Device.RuntimePlatform == Device.iOS)
+			{
+				layout.Children.Add(_modalPresentationStylesPicker);
+			}
+
+			var showTransparentModalPageButton = new Button()
+			{
+				Text = "Show Transparent Modal Page",
 				VerticalOptions = LayoutOptions.Start,
 				HorizontalOptions = LayoutOptions.Center
 			};
 
-			btn.Clicked += ShowModalBtnClicked;
+			showTransparentModalPageButton.Clicked += ShowModalBtnClicked;
 
-			Content = btn;
-		}
+			layout.Children.Add(showTransparentModalPageButton);
 
-		void ShowModalBtnClicked(object sender, System.EventArgs e)
-		{
-			Navigation.PushModalAsync(new PageWithTransparentBkgnd());
+			Content = layout;
+
+			_pageWithTransparentBkgnd = new PageWithTransparentBkgnd();
+
+			_pageWithTransparentBkgnd.On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.OverFullScreen);
 		}
 
 		protected override void OnAppearing()
 		{
 			base.OnAppearing();
+			Console.WriteLine("OnAppearing");
 		}
 
 		protected override void OnDisappearing()
 		{
 			base.OnDisappearing();
+			Console.WriteLine("OnDisappearing");
+		}
+
+		void ShowModalBtnClicked(object sender, EventArgs e)
+		{
+			Navigation.PushModalAsync(_pageWithTransparentBkgnd);
 		}
 	}
 }
