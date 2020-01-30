79f4fbeac6c454da93f1e920cfac6a99f72e5ec7
Jon Robinson Levy <jon.r.levy@gmail.com>
2020-01-30 09:10:05 +0100

    Adding Test Page for Issue 8787

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8787.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 8787, "[Bug] Entry text initially invisible on UWP",
+		PlatformAffected.UWP)]
+	public partial class Issue8787 : TestContentPage
+	{
+		Entry _theEntry;
+
+		string _entryText = "Bound text - not a problem.";
+
+		public string EntryText
+		{
+			get => _entryText;
+			set
+			{
+				_entryText = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public Issue8787()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{			
+		}
+	}
+}
+
