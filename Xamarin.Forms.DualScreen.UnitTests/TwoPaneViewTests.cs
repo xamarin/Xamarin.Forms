8cd8fd31306e71a7d5f42eef6a04d9873a3cabdb
Shane Neuville <shneuvil@microsoft.com>
2020-02-03 16:55:26 -0700

    netcore app tests

--- Xamarin.Forms.DualScreen.UnitTests/TwoPaneViewTests.cs
+++ Xamarin.Forms.DualScreen.UnitTests/TwoPaneViewTests.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using Xamarin.Forms.Core.UnitTests;
 using Xamarin.Forms.Internals;
 
 namespace Xamarin.Forms.DualScreen.UnitTests
