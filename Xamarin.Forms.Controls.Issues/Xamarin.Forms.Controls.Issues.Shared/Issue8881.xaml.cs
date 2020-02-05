9a8e8c342dc89a89cb0a77dfe87553f0978e34e6
Pedro Jesus <pedro.pj.souza@hotmail.com>
2020-02-04 23:24:18 -0300

    Added to control gallery

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8881.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	public partial class Issue8881 : TestShell
+	{
+		public Issue8881()
+		{
+#if APP
+			InitializeComponent(); 
+#endif
+		}
+
+		protected override void Init()
+		{
+		}
+	}
+}
