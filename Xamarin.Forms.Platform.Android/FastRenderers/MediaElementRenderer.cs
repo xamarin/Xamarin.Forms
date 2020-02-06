136704da3c36011729917c23365b8265d03c557a
Shane Neuville <shneuvil@microsoft.com>
2020-02-06 09:05:50 -0700

    Fix UWP volume, mediasource exception, android position

--- Xamarin.Forms.Platform.Android/FastRenderers/MediaElementRenderer.cs
+++ Xamarin.Forms.Platform.Android/FastRenderers/MediaElementRenderer.cs
@@ -99,6 +99,7 @@ namespace Xamarin.Forms.Platform.Android
 			MediaElement.PropertyChanged += OnElementPropertyChanged;
 			MediaElement.SeekRequested += SeekRequested;
 			MediaElement.StateRequested += StateRequested;
+			MediaElement.PositionRequested += OnPositionRequested;
 
 			if (_tracker is null)
 			{
@@ -115,6 +116,9 @@ namespace Xamarin.Forms.Platform.Android
 
 		void StateRequested(object sender, StateRequested e)
 		{
+			if (_view == null)
+				return;
+
 			switch (e.State)
 			{
 				case MediaElementState.Playing:
@@ -142,8 +146,19 @@ namespace Xamarin.Forms.Platform.Android
 			Controller.Position = _view.Position;
 		}
 
+		void OnPositionRequested(object sender, EventArgs e)
+		{
+			if (_view == null)
+				return;
+
+			Controller.Position = _view.Position;
+		}
+
 		void SeekRequested(object sender, SeekRequested e)
 		{
+			if (_view == null)
+				return;
+
 			Controller.Position = _view.Position;
 		}
 
@@ -226,6 +241,9 @@ namespace Xamarin.Forms.Platform.Android
 
 		void MetadataRetrieved(object sender, EventArgs e)
 		{
+			if (_view == null)
+				return;
+
 			Controller.Duration = _view.DurationTimeSpan;
 			Controller.VideoHeight = _view.VideoHeight;
 			Controller.VideoWidth = _view.VideoWidth;
@@ -276,22 +294,34 @@ namespace Xamarin.Forms.Platform.Android
 
 		void UpdateKeepScreenOn()
 		{
+			if (_view == null)
+				return;
+
 			_view.KeepScreenOn = MediaElement.KeepScreenOn;
 		}
 
 		void UpdateShowPlaybackControls()
 		{
+			if (_controller == null)
+				return;
+
 			_controller.Visibility = MediaElement.ShowsPlaybackControls ? ViewStates.Visible : ViewStates.Gone;
 		}
 
 		void UpdateSource()
 		{
+			if (_view == null)
+				return;
+
 			if (MediaElement.Source != null)
 			{
 				if (MediaElement.Source is UriMediaSource uriSource)
 				{
 					if (uriSource.Uri.Scheme == "ms-appx")
 					{
+						if (uriSource.Uri.LocalPath.Length <= 1)
+							return;
+
 						// video resources should be in the raw folder with Build Action set to AndroidResource
 						string uri = "android.resource://" + Context.PackageName + "/raw/" + uriSource.Uri.LocalPath.Substring(1, uriSource.Uri.LocalPath.LastIndexOf('.') - 1).ToLower();
 						_view.SetVideoURI(global::Android.Net.Uri.Parse(uri));
@@ -339,12 +369,18 @@ namespace Xamarin.Forms.Platform.Android
 
 		void MediaPlayer.IOnCompletionListener.OnCompletion(MediaPlayer mp)
 		{
+			if (Controller == null)
+				return;
+
 			Controller.Position = TimeSpan.FromMilliseconds(_mediaPlayer.CurrentPosition);
 			Controller.OnMediaEnded();
 		}
 
 		void MediaPlayer.IOnPreparedListener.OnPrepared(MediaPlayer mp)
 		{
+			if (Controller == null)
+				return;
+
 			Controller.OnMediaOpened();
 
 			UpdateLayoutParameters();
@@ -366,6 +402,9 @@ namespace Xamarin.Forms.Platform.Android
 
 		void UpdateLayoutParameters()
 		{
+			if (_view == null)
+				return;
+
 			if (_view.VideoWidth == 0 || _view.VideoHeight == 0)
 			{
 				_view.LayoutParameters = new FrameLayout.LayoutParams(Width, Height, GravityFlags.Fill);
@@ -442,13 +481,18 @@ namespace Xamarin.Forms.Platform.Android
 
 		bool MediaPlayer.IOnErrorListener.OnError(MediaPlayer mp, MediaError what, int extra)
 		{
+			if (Controller == null)
+				return false;
+
 			Controller.OnMediaFailed();
 			return false;
 		}
 		
 		bool MediaPlayer.IOnInfoListener.OnInfo(MediaPlayer mp, MediaInfo what, int extra)
 		{
-			System.Diagnostics.Debug.WriteLine(what);
+			if (_view == null)
+				return false;
+
 			switch (what)
 			{
 				case MediaInfo.BufferingStart:
