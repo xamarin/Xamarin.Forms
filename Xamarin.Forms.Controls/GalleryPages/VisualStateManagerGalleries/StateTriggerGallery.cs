3362fdd773c8ea1880f51589dee3f35c209f31d9
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-02-06 10:02:28 +0100

    Fixed wrong line break

--- Xamarin.Forms.Controls/GalleryPages/VisualStateManagerGalleries/StateTriggerGallery.cs
+++ Xamarin.Forms.Controls/GalleryPages/VisualStateManagerGalleries/StateTriggerGallery.cs
@@ -11,7 +11,8 @@
 				Children =
 				{
 					GalleryBuilder.NavButton("MinWindowWidth AdaptiveTrigger Gallery", () => new MinWindowWidthAdaptiveTriggerGallery(), Navigation),
-					GalleryBuilder.NavButton("MinWindowHeight AdaptiveTrigger Gallery", () => new MinWindowHeightAdaptiveTriggerGallery(), Navigation),                       GalleryBuilder.NavButton("CompareStateTrigger Gallery", () => new CompareStateTriggerGallery(), Navigation),
+					GalleryBuilder.NavButton("MinWindowHeight AdaptiveTrigger Gallery", () => new MinWindowHeightAdaptiveTriggerGallery(), Navigation),
+					GalleryBuilder.NavButton("CompareStateTrigger Gallery", () => new CompareStateTriggerGallery(), Navigation),
 					GalleryBuilder.NavButton("DeviceStateTrigger Gallery", () => new DeviceStateTriggerGallery(), Navigation),
 					GalleryBuilder.NavButton("OrientationStateTrigger Gallery", () => new OrientationStateTriggerGallery(), Navigation),
 					GalleryBuilder.NavButton("StateTriggers directly on Elements", () => new StateTriggersDirectlyOnElements(), Navigation)
