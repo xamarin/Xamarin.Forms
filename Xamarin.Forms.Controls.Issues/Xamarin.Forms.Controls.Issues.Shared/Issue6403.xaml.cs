85cb609e403c05bf35849c29301c1e0bfb4fa63b
Brian Macomber <brian@agspectrum.com>
2020-02-13 23:43:31 -0600

    pre compiler statements in control gallery

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6403.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6403.xaml.cs
@@ -1,19 +1,24 @@
-﻿using System;
-using Xamarin.Forms.CustomAttributes;
+﻿using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace Xamarin.Forms.Controls.Issues
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+#if APP
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 6403, "Xamarin.Forms UWP Picker collapses on opening Dropdown menu [Bug]", PlatformAffected.UWP)]
 	public partial class Issue6403 : TestContentPage
-    {
-        public Issue6403()
-        {
-            InitializeComponent();
-        }
+	{
+
+		public Issue6403()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
 
 		protected override void Init()
 		{
