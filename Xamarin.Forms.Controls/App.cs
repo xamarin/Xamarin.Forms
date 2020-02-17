23e12e9d6357162f21017c34bd5ecd0301007f88
Rui Marinho <me@ruimarinho.net>
2020-02-17 18:15:41 +0000

    [Controls]Fix sample

--- Xamarin.Forms.Controls/App.cs
+++ Xamarin.Forms.Controls/App.cs
@@ -34,7 +34,7 @@ namespace Xamarin.Forms.Controls
 		{
 			_testCloudService = DependencyService.Get<ITestCloudService>();
 
-			SetMainPage(new IndicatorsSample());
+			SetMainPage(CreateDefaultMainPage());
 
 			//TestMainPageSwitches();
 
--- Xamarin.Forms.Controls/GalleryPages/IndicatorViewGalleries/IndicatorsSample.xaml.cs
+++ Xamarin.Forms.Controls/GalleryPages/IndicatorViewGalleries/IndicatorsSample.xaml.cs
@@ -18,8 +18,7 @@ namespace Xamarin.Forms.Controls
 			Device.SetFlags(new[] { ExperimentalFlags.CarouselViewExperimental, ExperimentalFlags.IndicatorViewExperimental });
 			InitializeComponent();
 			BindingContext = new GalleryPages.CollectionViewGalleries.CarouselViewGalleries.CarouselItemsGalleryViewModel();
-			//IndicatorView.SetItemsSourceBy(indicators, carousel);
-		//	indicators.BackgroundColor = Color.Red;
+			IndicatorView.SetItemsSourceBy(indicators, carousel);
 		}
 	}
 }
