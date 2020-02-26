01470c3f79f744a937d5f2db5f2cd07655904c51
ksemenenko <ksemenenko@live.com>
2020-02-26 11:13:26 +0200

    tests

--- Xamarin.Forms.Core.UnitTests/PageTests.cs
+++ Xamarin.Forms.Core.UnitTests/PageTests.cs
@@ -436,7 +436,7 @@ namespace Xamarin.Forms.Core.UnitTests
 			((IPageController)page).SendAppearing ();
 
 			Assert.True (sent);
-			Assert.True(page.IsAppeared);
+			Assert.True(page.HasAppeared);
 		}
 
 		[Test]
@@ -446,14 +446,14 @@ namespace Xamarin.Forms.Core.UnitTests
 
 			((IPageController)page).SendAppearing ();
 
-			Assert.True(page.IsAppeared);
+			Assert.True(page.HasAppeared);
 			bool sent = false;
 			page.Disappearing += (sender, args) => sent = true;
 
 			((IPageController)page).SendDisappearing ();
 
 			Assert.True (sent);
-			Assert.False(page.IsAppeared);
+			Assert.False(page.HasAppeared);
 		}
 
 		[Test]
