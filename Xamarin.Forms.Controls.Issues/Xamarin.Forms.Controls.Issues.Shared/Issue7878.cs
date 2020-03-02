67f91ed293ee8512c20c2432f7881b7f08b468ff
Gerald Versluis <gerald@verslu.is>
2020-03-02 13:50:37 +0100

    Update Issue7878.cs

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7878.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7878.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace Xamarin.Forms.Controls.Issues
 {
@@ -8,43 +10,77 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.Github, 7878, "Page not popped on iOS 13 FormSheet swipe down", PlatformAffected.iOS)]
 	public class Issue7878 : TestContentPage
 	{
-		ContentPage _modalPage;
-
 		protected override void Init()
 		{
+			var label = new Label
+			{
+				Text = "Both modals should behave the same. With the FormSheet when swiping down the modal should be dismissed properly. And for both modals the Appearing event on this page should be called. If so, this tests succeeded."
+			};
+
+			var modalButtonFormSheet = new Button
+			{
+				Text = "Show Modal FormSheet"
+			};
+
+			modalButtonFormSheet.Clicked += Button_Clicked_FormSheet;
+
 			var modalButton = new Button
 			{
-				Text = "Modal me",
-				HorizontalOptions = LayoutOptions.Center,
-				VerticalOptions = LayoutOptions.Center
+				Text = "Show Modal Full Screen"
 			};
 
 			modalButton.Clicked += Button_Clicked;
 
-			Content = modalButton;
-
-			_modalPage = new ContentPage();
-
-			var button = new Button
+			var stackLayout = new StackLayout
 			{
-				Text = "Pressing this raises the popped event, swiping down as well!",
 				HorizontalOptions = LayoutOptions.Center,
-				VerticalOptions = LayoutOptions.Center
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					modalButton, modalButtonFormSheet
+				}
 			};
 
-			button.Clicked += (o, a) =>
-			{
-				Navigation.PopModalAsync();
-			};
+			Content = stackLayout;
+		}
 
-			_modalPage.Content = button;
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			DisplayAlert("I have appeared!", "👋", "OK");
 		}
 
 		void Button_Clicked(object sender, EventArgs e)
 		{
-			var navigationPage = new NavigationPage(_modalPage);
-			PlatformConfiguration.iOSSpecific.Page.SetModalPresentationStyle(navigationPage.On<PlatformConfiguration.iOS>(), PlatformConfiguration.iOSSpecific.UIModalPresentationStyle.FormSheet);
-			Navigation.PushModalAsync(navigationPage);
+			Navigation.PushModalAsync(new ModalPage(false));
+		}
+
+		void Button_Clicked_FormSheet(object sender, EventArgs e)
+		{
+			Navigation.PushModalAsync(new ModalPage(true));
+		}
+
+		class ModalPage : ContentPage
+		{
+			public ModalPage(bool isFormSheet)
+			{
+				if (isFormSheet)
+					On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.FormSheet);
+
+				var button = new Button
+				{
+					Text = "Pressing this raises the popped event, swiping down as well!",
+					HorizontalOptions = LayoutOptions.Center,
+					VerticalOptions = LayoutOptions.Center
+				};
+
+				button.Clicked += (o, a) =>
+				{
+					Navigation.PopModalAsync();
+				};
+
+				Content = button;
+			}
 		}
 	}
 }
