f7dc52994be0dc5e755ce66be979927a97be32e5
Rui Marinho <me@ruimarinho.net>
2020-03-05 12:36:10 +0000

    Update App.cs

--- Xamarin.Forms.Controls/App.cs
+++ Xamarin.Forms.Controls/App.cs
@@ -34,7 +34,7 @@ namespace Xamarin.Forms.Controls
 		{
 			_testCloudService = DependencyService.Get<ITestCloudService>();
 
-			SetMainPage(new NavigationPage(new GalleryPages.CollectionViewGalleries.CarouselViewGalleries.CarouselViewGallery()));
+			SetMainPage(CreateDefaultMainPage());
 
 			//TestMainPageSwitches();
 
