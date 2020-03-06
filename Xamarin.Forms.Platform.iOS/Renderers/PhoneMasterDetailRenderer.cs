f9b70f569293f33572156975426f40426de57e09
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-03-06 14:00:19 +0100

    Fixed touch conflict between shell Flyout and SwipeView

--- Xamarin.Forms.Platform.iOS/Renderers/PhoneMasterDetailRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/PhoneMasterDetailRenderer.cs
@@ -394,7 +394,7 @@ namespace Xamarin.Forms.Platform.iOS
 
    			bool shouldReceive(UIGestureRecognizer g, UITouch t)
 			{
-				return !(t.View is UISlider) && !(IsSwipeView(t.View));
+				return !(t.View is UISlider) && !IsSwipeView(t.View);
 			}
    
 			var center = new PointF();
