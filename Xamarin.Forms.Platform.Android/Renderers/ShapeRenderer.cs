3f992acfb048ca3dd90f764b949dbe9490380090
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-03-19 13:51:23 +0100

    Updated iOS and Android ShapeRenderer size logic

--- Xamarin.Forms.Platform.Android/Renderers/ShapeRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/ShapeRenderer.cs
@@ -46,12 +46,12 @@ namespace Xamarin.Forms.Platform.Android
 
             if (args.PropertyName == VisualElement.HeightProperty.PropertyName)
             {
-                _height = (Element.HeightRequest > 0) ? Element.HeightRequest : Element.Height;
+                _height = Element.Height;
                 UpdateSize();
             }
             else if (args.PropertyName == VisualElement.WidthProperty.PropertyName)
             {
-                _width = (Element.WidthRequest > 0) ? Element.WidthRequest : Element.Width;
+                _width = Element.Width;
                 UpdateSize();
             }
             else if (args.PropertyName == Shape.AspectProperty.PropertyName)
