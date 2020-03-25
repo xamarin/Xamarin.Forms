91ea6017b0f974ff76298ae98694dd7e22ac4dc5
Clifford Agius <CliffordAgius@CA-Software.co.uk>
2020-03-25 20:29:38 +0000

    Changed the UserAgent string so that I can test and be sure that it's being correctly changed on all the devices...

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8432.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8432.cs
@@ -19,7 +19,7 @@ namespace Xamarin.Forms.Controls.Issues
 	{
 		protected override void Init()
 		{
-			string UserAgentString = "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko";
+			string UserAgentString = "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko XAMARIN Rocks";
 
 			Label header = new Label
 			{
