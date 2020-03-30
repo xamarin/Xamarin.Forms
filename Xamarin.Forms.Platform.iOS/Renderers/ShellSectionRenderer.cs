366907c7eda590bad120f090defddb39c84d61cd
Shane Neuville <shneuvil@microsoft.com>
2020-03-30 16:32:30 -0600

    Remove controller from UINavigationController stack  when page is removed

--- Xamarin.Forms.Platform.iOS/Renderers/ShellSectionRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ShellSectionRenderer.cs
@@ -105,6 +105,12 @@ namespace Xamarin.Forms.Platform.iOS
 			return false;
 		}
 
+
+		public override void AddChildViewController(UIViewController childController)
+		{
+			base.AddChildViewController(childController);
+		}
+
 		public override void ViewDidLayoutSubviews()
 		{
 			base.ViewDidLayoutSubviews();
@@ -336,15 +342,20 @@ namespace Xamarin.Forms.Platform.iOS
 			var page = e.Page;
 
 			var renderer = Platform.GetRenderer(page);
+			var viewController = renderer?.ViewController;
 
-			if (renderer != null)
+			if (viewController == null && _trackers.ContainsKey(page))
+				viewController = _trackers[page].ViewController;
+
+			if (viewController != null)
 			{
-				if (renderer.ViewController == TopViewController)
+				if (viewController == TopViewController)
 				{
 					e.Animated = false;
 					OnPopRequested(e);
 				}
-				ViewControllers = ViewControllers.Remove(renderer.ViewController);
+
+				ViewControllers = ViewControllers.Remove(viewController);
 				DisposePage(page);
 			}
 		}
@@ -371,9 +382,13 @@ namespace Xamarin.Forms.Platform.iOS
 		{
 			if (_trackers.TryGetValue(page, out var tracker))
 			{
+				if(tracker.ViewController != null && ViewControllers.Contains(tracker.ViewController))
+					ViewControllers = ViewControllers.Remove(_trackers[page].ViewController);
+
 				tracker.Dispose();
 				_trackers.Remove(page);
 			}
+
 
 			var renderer = Platform.GetRenderer(page);
 			if (renderer != null)
