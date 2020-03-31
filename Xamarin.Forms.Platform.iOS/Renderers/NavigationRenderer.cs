8dda0a78178b29bd8ec057f643d244cfe4b07b39
Yuriy Holembyovskyy <yuriy.holembyovskyy@gmail.com>
2020-03-31 19:22:23 +0300

    Add TitleView  Dispose() call for iOS

--- Xamarin.Forms.Platform.iOS/Renderers/NavigationRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/NavigationRenderer.cs
@@ -1079,6 +1079,12 @@ namespace Xamarin.Forms.Platform.iOS
 					_tracker.CollectionChanged -= TrackerOnCollectionChanged;
 					_tracker = null;
 
+					if (NavigationItem.TitleView != null)
+					{
+						NavigationItem.TitleView.Dispose();
+						NavigationItem.TitleView = null;
+					}
+
 					if (NavigationItem.RightBarButtonItems != null)
 					{
 						for (var i = 0; i < NavigationItem.RightBarButtonItems.Length; i++)
