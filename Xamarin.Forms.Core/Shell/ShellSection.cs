986f46e28be9b8a5f41935a0c1d9298eb8570dc9
Shane Neuville <shneuvil@microsoft.com>
2020-04-07 19:10:59 -0600

    - fix check

--- Xamarin.Forms.Core/Shell/ShellSection.cs
+++ Xamarin.Forms.Core/Shell/ShellSection.cs
@@ -541,7 +541,7 @@ namespace Xamarin.Forms
 					Device.BeginInvokeOnMainThread(() =>
 					{
 						var contentItems = ShellSectionController.GetItems();
-						if (contentItems.Count > 0 && CurrentItem == null)
+						if (contentItems.Count > 0 && (CurrentItem == null || !contentItems.Contains(CurrentItem)))
 							SetValueFromRenderer(CurrentItemProperty, contentItems[0]);
 					});
 				}
