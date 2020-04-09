c783e1111776ab77443feacff663d83ad23481b2
E.Z. Hart <hartez@gmail.com>
2020-04-09 09:15:19 -0600

    When in doubt, just use a button

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9686.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9686.cs
@@ -17,7 +17,7 @@ namespace Xamarin.Forms.Controls.Issues
 	public class Issue9686 : TestContentPage
 	{
 		const string Success = "Success";
-		const string Test9686 = "9686";
+		const string Run = "Run";
 
 		protected override void Init()
 		{
@@ -39,23 +39,21 @@ namespace Xamarin.Forms.Controls.Issues
 			cv.GroupHeaderTemplate = new DataTemplate(() => {
 				var label = new Label();
 				label.SetBinding(Label.TextProperty, new Binding("GroupName"));
-
-				var tgr = new TapGestureRecognizer();
-				tgr.Command = ((_9686ViewModel)BindingContext).ShowOrHideCommand;
-				tgr.SetBinding(TapGestureRecognizer.CommandParameterProperty, new Binding("."));
-
-				label.GestureRecognizers.Add(tgr);
-
 				return label;
 			});
 
 			cv.SetBinding(ItemsView.ItemsSourceProperty, new Binding(nameof(_9686ViewModel.Groups)));
 
-			var instructions = new Label { Text = $"Tap the first group header once, then again. The application doesn't crash, this test has passed." };
+			var instructions = new Label { Text = $"Tap the button once, then again. The application doesn't crash, this test has passed." };
 
 			var result = new Label { };
 
+			var button = new Button { Text  = Run, AutomationId = Run };
+			button.Command = ((_9686ViewModel)BindingContext).ShowOrHideCommand;
+			button.CommandParameter = ((_9686ViewModel)BindingContext).Groups[0];
+
 			layout.Children.Add(instructions);
+			layout.Children.Add(button);
 			layout.Children.Add(result);
 			layout.Children.Add(cv);
 
@@ -88,14 +86,15 @@ namespace Xamarin.Forms.Controls.Issues
 			{
 				ShowOrHideCommand = new Command<_9686Group>((group) => ShowOrHideItems(group));
 
-				Groups = new ObservableCollection<_9686Group>();
-
-				Groups.Add(new _9686Group($"{Test9686}", new ObservableCollection<_9686Item>()));
-				Groups.Add(new _9686Group("Group 2", new ObservableCollection<_9686Item>()));
-				Groups.Add(new _9686Group("Group 3", new ObservableCollection<_9686Item>()));
-				Groups.Add(new _9686Group("Group 4", new ObservableCollection<_9686Item>()));
-				Groups.Add(new _9686Group("Group 5", new ObservableCollection<_9686Item>()));
-				Groups.Add(new _9686Group("Group 6", new ObservableCollection<_9686Item>()));
+				Groups = new ObservableCollection<_9686Group>
+				{
+					new _9686Group("Group 1", new ObservableCollection<_9686Item>()),
+					new _9686Group("Group 2", new ObservableCollection<_9686Item>()),
+					new _9686Group("Group 3", new ObservableCollection<_9686Item>()),
+					new _9686Group("Group 4", new ObservableCollection<_9686Item>()),
+					new _9686Group("Group 5", new ObservableCollection<_9686Item>()),
+					new _9686Group("Group 6", new ObservableCollection<_9686Item>())
+				};
 			}
 
 			void ShowOrHideItems(_9686Group group)
@@ -181,10 +180,10 @@ namespace Xamarin.Forms.Controls.Issues
 		[Test]
 		public void AddRemoveEmptyGroupsShouldNotCrashOnInsert()
 		{
-			RunningApp.WaitForElement(Test9686);
-			RunningApp.Tap(Test9686);
+			RunningApp.WaitForElement(Run);
+			RunningApp.Tap(Run);
 			RunningApp.WaitForElement("Item 1");
-			RunningApp.Tap(Test9686);
+			RunningApp.Tap(Run);
 			RunningApp.WaitForElement(Success);
 		}
 #endif
