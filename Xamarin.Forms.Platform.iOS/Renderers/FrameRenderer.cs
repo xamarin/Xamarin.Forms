65a45c87d8db655dc31f197519f8fa488ebabf6c
Yuriy Holembyovskyy <yuriy.holembyovskyy@gmail.com>
2020-04-21 17:32:32 +0300

    Rebase to 4.6.0

--- Xamarin.Forms.Platform.iOS/Renderers/FrameRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/FrameRenderer.cs
@@ -1,21 +1,12 @@
 using System.ComponentModel;
 using System.Drawing;
-using CoreAnimation;
-using CoreGraphics;
-using Foundation;
 using UIKit;
 
 namespace Xamarin.Forms.Platform.iOS
 {
-	public class FrameRenderer : VisualElementRenderer<Frame>
+	public class FrameRenderer : VisualElementRenderer<Frame>, ITabStop
 	{
-		ShadowView _shadowView;
-
-		[Internals.Preserve(Conditional = true)]
-		public FrameRenderer()
-		{
-
-		}
+		UIView ITabStop.TabStop => this;
 
 		protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
 		{
@@ -32,12 +23,11 @@ namespace Xamarin.Forms.Platform.iOS
 			if (e.PropertyName == VisualElement.BackgroundColorProperty.PropertyName ||
 			    e.PropertyName == Xamarin.Forms.Frame.BorderColorProperty.PropertyName ||
 				e.PropertyName == Xamarin.Forms.Frame.HasShadowProperty.PropertyName ||
-				e.PropertyName == Xamarin.Forms.Frame.CornerRadiusProperty.PropertyName ||
-				e.PropertyName == VisualElement.IsVisibleProperty.PropertyName)
+				e.PropertyName == Xamarin.Forms.Frame.CornerRadiusProperty.PropertyName)
 				SetupLayer();
 		}
 
-		public virtual void SetupLayer()
+		void SetupLayer()
 		{
 			float cornerRadius = Element.CornerRadius;
 
@@ -51,6 +41,16 @@ namespace Xamarin.Forms.Platform.iOS
 			else
 				Layer.BackgroundColor = Element.BackgroundColor.ToCGColor();
 
+			if (Element.HasShadow)
+			{
+				Layer.ShadowRadius = 5;
+				Layer.ShadowColor = UIColor.Black.CGColor;
+				Layer.ShadowOpacity = 0.8f;
+				Layer.ShadowOffset = new SizeF();
+			}
+			else
+				Layer.ShadowOpacity = 0;
+
 			if (Element.BorderColor == Color.Default)
 				Layer.BorderColor = UIColor.Clear.CGColor;
 			else
@@ -59,91 +59,8 @@ namespace Xamarin.Forms.Platform.iOS
 				Layer.BorderWidth = 1;
 			}
 
-			if (Element.HasShadow)
-			{
-				if (_shadowView == null)
-				{
-					_shadowView = new ShadowView(Layer);
-					SetNeedsLayout();
-				}
-				_shadowView.UpdateBackgroundColor();
-				_shadowView.Layer.CornerRadius = Layer.CornerRadius;
-				_shadowView.Layer.BorderColor = Layer.BorderColor;
-				_shadowView.Hidden = !Element.IsVisible;
-			}
-			else
-			{
-				if (_shadowView != null)
-				{
-					_shadowView.RemoveFromSuperview();
-					_shadowView.Dispose();
-					_shadowView = null;
-				}
-			}
-
 			Layer.RasterizationScale = UIScreen.MainScreen.Scale;
 			Layer.ShouldRasterize = true;
-		}
-
-		public override void LayoutSubviews()
-		{
-			if (_shadowView != null)
-			{
-				if (_shadowView.Superview == null && Superview != null)
-					Superview.InsertSubviewBelow(_shadowView, this);
-
-				_shadowView?.SetNeedsLayout();
-			}
-			base.LayoutSubviews();
-		}
-
-		[Preserve(Conditional = true)]
-		class ShadowView : UIView
-		{
-			CALayer _shadowee;
-			CGRect _previousBounds;
-			CGRect _previousFrame;
-
-			[Preserve(Conditional = true)]
-			public ShadowView(CALayer shadowee)
-			{
-				_shadowee = shadowee;
-				Layer.ShadowRadius = 5;
-				Layer.ShadowColor = UIColor.Black.CGColor;
-				Layer.ShadowOpacity = 0.8f;
-				Layer.ShadowOffset = new SizeF();
-				Layer.BorderWidth = 1;
-				UserInteractionEnabled = false;
-			}
-
-			public void UpdateBackgroundColor()
-			{
-				//Putting a transparent background under any shadowee having a background with alpha < 1
-				//Giving the Shadow a background of the same color when shadowee background == 1.
-				//The latter will result in a 'darker' shadow as you would expect from something that 
-				//isn't transparent. This also mimics the look as it was before with non-transparent Frames.
-				if (_shadowee.BackgroundColor.Alpha < 1) 
-					BackgroundColor = UIColor.Clear;
-				else
-					BackgroundColor = new UIColor(_shadowee.BackgroundColor);
-			}
-
-			public override void LayoutSubviews()
-			{
-				if (_shadowee.Bounds != _previousBounds || _shadowee.Frame != _previousFrame)
-				{
-					base.LayoutSubviews();
-					SetBounds();
-				}
-			}
-
-			void SetBounds()
-			{
-				Layer.Frame = _shadowee.Frame;
-				Layer.Bounds = _shadowee.Bounds;
-				_previousBounds = _shadowee.Bounds;
-				_previousFrame = _shadowee.Frame;
-			}
 		}
 	}
 }
--- Xamarin.Forms.Platform.iOS/Renderers/PhoneMasterDetailRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/PhoneMasterDetailRenderer.cs
@@ -24,12 +24,10 @@ namespace Xamarin.Forms.Platform.iOS
 		UIGestureRecognizer _tapGesture;
 
 		VisualElementTracker _tracker;
-		bool _applyShadow;
 
 		Page Page => Element as Page;
 
 
-		[Preserve(Conditional = true)]
 		public PhoneMasterDetailRenderer()
 		{
 		}
@@ -45,10 +43,13 @@ namespace Xamarin.Forms.Platform.iOS
 					return;
 				_presented = value;
 				LayoutChildren(true);
+
 				if (value)
 					AddClickOffView();
 				else
 					RemoveClickOffView();
+
+				ToggleAccessibilityElementsHidden();
 
 				((IElementController)Element).SetValueFromRenderer(Xamarin.Forms.MasterDetailPage.IsPresentedProperty, value);
 			}
@@ -142,8 +143,6 @@ namespace Xamarin.Forms.Platform.iOS
 			UpdateBackground();
 
 			UpdatePanGesture();
-			UpdateApplyShadow(((MasterDetailPage)Element).OnThisPlatform().GetApplyShadow());
-
 		}
 
 		public override void WillRotate(UIInterfaceOrientation toInterfaceOrientation, double duration)
@@ -239,20 +238,13 @@ namespace Xamarin.Forms.Platform.iOS
 				UpdateBackground();
 			else if (e.PropertyName == Page.BackgroundImageSourceProperty.PropertyName)
 				UpdateBackground();
-			else if (e.PropertyName == PlatformConfiguration.iOSSpecific.MasterDetailPage.ApplyShadowProperty.PropertyName)
-				UpdateApplyShadow(((MasterDetailPage)Element).OnThisPlatform().GetApplyShadow());
 		}
 
 		void LayoutChildren(bool animated)
 		{
 			var frame = Element.Bounds.ToRectangleF();
 			var masterFrame = frame;
-			nfloat opacity = 1;
 			masterFrame.Width = (int)(Math.Min(masterFrame.Width, masterFrame.Height) * 0.8);
-			var detailRenderer = Platform.GetRenderer(MasterDetailPage.Detail);
-			if (detailRenderer == null)
-				return;
-			var detailView = detailRenderer.ViewController.View;
 
 			var isRTL = (Element as IVisualElementController)?.EffectiveFlowDirection.IsRightToLeft() == true;
 			if (isRTL)
@@ -264,11 +256,7 @@ namespace Xamarin.Forms.Platform.iOS
 
 			var target = frame;
 			if (Presented)
-			{
 				target.X += masterFrame.Width;
-				if (_applyShadow)
-					opacity = 0.5f;
-			}
 
 			if (isRTL)
 			{
@@ -280,16 +268,12 @@ namespace Xamarin.Forms.Platform.iOS
 				UIView.BeginAnimations("Flyout");
 				var view = _detailController.View;
 				view.Frame = target;
-				detailView.Layer.Opacity = (float)opacity;
 				UIView.SetAnimationCurve(UIViewAnimationCurve.EaseOut);
 				UIView.SetAnimationDuration(250);
 				UIView.CommitAnimations();
 			}
 			else
-			{
 				_detailController.View.Frame = target;
-				detailView.Layer.Opacity = (float)opacity;
-			}
 
 			MasterDetailPage.MasterBounds = new Rectangle(masterFrame.X, 0, masterFrame.Width, masterFrame.Height);
 			MasterDetailPage.DetailBounds = new Rectangle(0, 0, frame.Width, frame.Height);
@@ -361,7 +345,7 @@ namespace Xamarin.Forms.Platform.iOS
 			if (Forms.RespondsToSetNeedsUpdateOfHomeIndicatorAutoHidden)
 				SetNeedsUpdateOfHomeIndicatorAutoHidden();
 
-			detailRenderer.ViewController.View.Superview.BackgroundColor = Xamarin.Forms.Color.Black.ToUIColor();
+			ToggleAccessibilityElementsHidden();
 		}
 
 		void UpdateLeftBarButton()
@@ -375,11 +359,6 @@ namespace Xamarin.Forms.Platform.iOS
 			UIViewController firstPage = detailRenderer?.ViewControllers.FirstOrDefault();
 			if (firstPage != null)
 				NavigationRenderer.SetMasterLeftBarButton(firstPage, masterDetailPage);
-		}
-
-		void UpdateApplyShadow(bool value)
-		{
-			_applyShadow = value;
 		}
 
 		public override UIViewController ChildViewControllerForStatusBarHidden()
@@ -401,6 +380,17 @@ namespace Xamarin.Forms.Platform.iOS
 			}
 		}
 
+		void ToggleAccessibilityElementsHidden()
+		{
+			var masterView = _masterController?.View;
+			if (masterView != null)
+				masterView.AccessibilityElementsHidden = !Presented;
+
+			var detailView = _detailController?.View;
+			if (detailView != null)
+				detailView.AccessibilityElementsHidden = Presented;
+		}
+
 		void UpdatePanGesture()
 		{
 			var model = (MasterDetailPage)Element;
@@ -417,11 +407,11 @@ namespace Xamarin.Forms.Platform.iOS
 				return;
 			}
 
-   			bool shouldReceive(UIGestureRecognizer g, UITouch t)
+			bool shouldReceive(UIGestureRecognizer g, UITouch t)
 			{
 				return !(t.View is UISlider) && !(IsSwipeView(t.View));
 			}
-   
+
 			var center = new PointF();
 			_panGesture = new UIPanGestureRecognizer(g =>
 			{
@@ -448,11 +438,6 @@ namespace Xamarin.Forms.Platform.iOS
 							targetFrame.X = (nfloat)Math.Min(_masterController.View.Frame.Width, Math.Max(0, motion));
 
 						targetFrame.X = targetFrame.X * directionModifier;
-						if (_applyShadow)
-						{
-							var openProgress = targetFrame.X / _masterController.View.Frame.Width;
-							ApplyDetailShadow((nfloat)openProgress);
-						}
 
 						detailView.Frame = targetFrame;
 						break;
@@ -505,13 +490,6 @@ namespace Xamarin.Forms.Platform.iOS
 		void IEffectControlProvider.RegisterEffect(Effect effect)
 		{
 			VisualElementRenderer<VisualElement>.RegisterEffect(effect, View);
-		}
-
-		void ApplyDetailShadow(nfloat percent)
-		{
-			var detailView = Platform.GetRenderer(MasterDetailPage.Detail).ViewController.View;
-			var opacity = (nfloat)(0.5 + (0.5 * (1 - percent)));
-			detailView.Layer.Opacity = (float)opacity;
 		}
 	}
 }
