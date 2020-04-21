382f1ef4944d2d58825fcbfe066dd1d818a316e2
Rui Marinho <me@ruimarinho.net>
2020-04-21 12:37:49 +0100

    [Android] Fix go to the right position on load

--- Xamarin.Forms.Platform.Android/CollectionView/CarouselViewRenderer.cs
+++ Xamarin.Forms.Platform.Android/CollectionView/CarouselViewRenderer.cs
@@ -294,6 +294,7 @@ namespace Xamarin.Forms.Platform.Android
 			_oldPosition = position;
 
 			SetCurrentItem(_oldPosition);
+			Carousel.ScrollTo(_oldPosition, position: Xamarin.Forms.ScrollToPosition.Center, animate: Carousel.AnimatePositionChanges);
 		}
 
 		void UpdateVisualStates()
