49b894b79d6bcb96c9ac8ccf54ae0e4620a9da2b
Shane Neuville <shneuvil@microsoft.com>
2020-04-23 17:24:35 -0600

    - switch tests to check for colors on bitmap instead

--- Xamarin.Forms.Platform.Android.UnitTests/ToolbarExtensionsTests.cs
+++ Xamarin.Forms.Platform.Android.UnitTests/ToolbarExtensionsTests.cs
@@ -18,6 +18,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Platform.Android.UnitTests;
 using AToolBar = Android.Support.V7.Widget.Toolbar;
+using AView = Android.Views.View;
 
 [assembly: ExportRenderer(typeof(TestShell), typeof(TestShellRenderer))]
 namespace Xamarin.Forms.Platform.Android.UnitTests
@@ -36,7 +37,7 @@ namespace Xamarin.Forms.Platform.Android.UnitTests
 			};
 
 			var settings = new ToolbarSettings(sortedItems);
-			SetupToolBar(settings);
+			SetupToolBar(settings, Context);
 
 
 			int i = 0;
@@ -58,54 +59,61 @@ namespace Xamarin.Forms.Platform.Android.UnitTests
 			};
 
 			// If this doesn't crash test has passed
-			SetupToolBar(new ToolbarSettings(sortedItems));
+			SetupToolBar(new ToolbarSettings(sortedItems), Context);
 		}
 
 		[Test, Category("ToolbarExtensions")]
 		[Description("Validate ToolBarItem TextColor Changes")]
 		public void ToolBarItemsColoredCorrectlyBasedOnEnabledDisabled()
 		{
-			List<ToolbarItem> sortedItems = new List<ToolbarItem>()
+			try
 			{
-				new ToolbarItem() { IsEnabled = true, Text = "a" },
-				new ToolbarItem() { IsEnabled = true, Text = "b" },
-				new ToolbarItem() { IsEnabled = true, Text = "c" },
-			};
+				List<ToolbarItem> sortedItems = new List<ToolbarItem>()
+				{
+					new ToolbarItem() { IsEnabled = true, Text = "a" },
+					new ToolbarItem() { IsEnabled = true, Text = "b" },
+					new ToolbarItem() { IsEnabled = true, Text = "c" },
+				};
 
-			var settings = new ToolbarSettings(sortedItems) { TintColor = Color.Red };
-			SetupToolBar(settings);
-			AToolBar aToolBar = settings.ToolBar;
-			List<IMenuItem> menuItemsCreated = settings.MenuItemsCreated;
+				var settings = new ToolbarSettings(sortedItems) { TintColor = Color.Red };
+				SetupToolBar(settings, Context);
+				AToolBar aToolBar = settings.ToolBar;
+				List<IMenuItem> menuItemsCreated = settings.MenuItemsCreated;
+				Assert.IsTrue(menuItemsCreated[2].IsEnabled, "Initial state of menu Item is not enabled");
+				sortedItems[2].IsEnabled = false;
 
-			Assert.IsTrue(menuItemsCreated[2].IsEnabled, "Initial state of menu Item is not enabled");
-			sortedItems[2].IsEnabled = false;
+				Assert.IsTrue(menuItemsCreated[0].IsEnabled, "Menu Item 1 is incorrectly disabled");
+				Assert.IsTrue(menuItemsCreated[1].IsEnabled, "Menu Item 2 is incorrectly disabled");
+				Assert.IsFalse(menuItemsCreated[2].IsEnabled, "Menu Item 3 is incorrectly disabled");
 
-			Assert.IsTrue(menuItemsCreated[0].IsEnabled, "Menu Item 1 is incorrectly disabled");
-			Assert.IsTrue(menuItemsCreated[1].IsEnabled, "Menu Item 2 is incorrectly disabled");
-			Assert.IsFalse(menuItemsCreated[2].IsEnabled, "Menu Item 3 is incorrectly disabled");
+				var textViews = settings.TextViews.ToList();
+				Assert.AreEqual(3, textViews.Count, $"{textViews.Count} textviews retrieved which it should have been 3");
 
-			var textViews = settings.TextViews.ToList();
+				settings.Layout();
+				for (int i = 0; i < 3; i++)
+				{
+					global::Android.Graphics.Color androidColor;
+					if (i != 2)
+					{
+						androidColor = Color.Red.ToAndroid();
+					}
+					else
+					{
+						androidColor = Color.Red.MultiplyAlpha(0.302).ToAndroid();
+					}
 
-			for(int i = 0; i < 3; i++)
+
+					textViews[i].AssertContainsColor(androidColor);
+					Assert.AreEqual(sortedItems[i].Text, textViews[i].Text);
+				}
+			}
+			catch(Exception exc)
 			{
-				ISpanned span = (ISpanned)textViews[i].TextFormatted;
-				var color = (ForegroundColorSpan)span.GetSpans(0, span.Length(), Java.Lang.Class.FromType(typeof(ForegroundColorSpan)))[0];
-				int androidColor;
-				if (i != 2)
-				{
-					androidColor = (int)Color.Red.ToAndroid();
-				}
-				else
-				{
-					androidColor = (int)Color.Red.MultiplyAlpha(0.302).ToAndroid();
-				}
-
-				Assert.AreEqual(sortedItems[i].Text, textViews[i].Text);
-				Assert.AreEqual(androidColor, color.ForegroundColor);
+				Assert.Fail(exc.ToString());
 			}
 		}
 
-		void SetupToolBar(ToolbarSettings settings)
+		static void SetupToolBar(ToolbarSettings settings, Context context)
 		{
 			foreach(var item in settings.ToolbarItems)
 			{
@@ -113,14 +121,12 @@ namespace Xamarin.Forms.Platform.Android.UnitTests
 					item.AutomationId = item.Text;
 			}
 
-			settings.ToolBar = new AToolBar(this.Context);
-
-			Context context = this.Context;
+			settings.ToolBar = new AToolBar(context);
 
 			ToolbarExtensions.UpdateMenuItems(
 				settings.ToolBar,
 				settings.ToolbarItems,
-				Context,
+				context,
 				settings.TintColor,
 				OnToolbarItemPropertyChanged,
 				settings.MenuItemsCreated,
@@ -130,12 +136,13 @@ namespace Xamarin.Forms.Platform.Android.UnitTests
 			void OnToolbarItemPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
 			{
 				settings.ToolBar.OnToolbarItemPropertyChanged(e,
-					(ToolbarItem)sender, settings.ToolbarItems, Context, settings.TintColor, OnToolbarItemPropertyChanged,
+					(ToolbarItem)sender, settings.ToolbarItems, context, settings.TintColor, OnToolbarItemPropertyChanged,
 					settings.MenuItemsCreated,
 					settings.ToolbarItemsCreated);
 			}
 		}
 
+		[Preserve(AllMembers = true)]
 		public class ToolbarSettings
 		{
 			public ToolbarSettings(List<ToolbarItem> toolbarItems)
@@ -143,6 +150,15 @@ namespace Xamarin.Forms.Platform.Android.UnitTests
 				ToolbarItems = toolbarItems;
 				MenuItemsCreated = new List<IMenuItem>();
 				ToolbarItemsCreated = new List<ToolbarItem>();
+			}
+
+			public void Layout(int width = 800, int height = 200)
+			{
+				int widthSpec = AView.MeasureSpec.MakeMeasureSpec(width, MeasureSpecMode.Exactly);
+				int heightSpec = AView.MeasureSpec.MakeMeasureSpec(height, MeasureSpecMode.Exactly);
+
+				ToolBar.Measure(widthSpec, heightSpec);
+				ToolBar.Layout(0, 0, width, height);
 			}
 
 			public List<ToolbarItem> ToolbarItems;
