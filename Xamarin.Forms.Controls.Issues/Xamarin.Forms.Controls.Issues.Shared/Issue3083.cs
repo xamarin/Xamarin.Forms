e1561f8eaca05fe6ae3d8723df77065be730c204
Pedro Jesus <pedrojesus.cefet@gmail.com>
2020-04-23 21:58:43 -0300

    Added 3083 and 6220 to Control Gallery

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue3083.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 3083, "Label Visibility on TabbedPage", PlatformAffected.Android)]
+	class Issue3083 : TestTabbedPage
+	{
+		protected override void Init()
+		{
+			this.Children.Add(new Issue1900 { IconImageSource = "bank" });
+			this.Children.Add(new Issue1900 { IconImageSource = "bank" });
+			this.On<Android>().SetBottomToolBarLabelVisibilityMode(BottomToolBarLabelVisibilityMode.Labeled);
+			this.On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+		}
+	}
+}
---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6220.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 6220, "Label Visibility on Shell", PlatformAffected.Android)]
+	public partial class Issue6220 : TestShell
+	{
+		public Issue6220()
+		{
+#if APP
+			this.InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+		}
+	}
+}
