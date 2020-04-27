b92992761421744c18022ba81f8ff2c862af7955
Gerald Versluis <gerald@verslu.is>
2020-04-27 14:02:29 +0200

    Fix

--- Xamarin.Forms.Platform.iOS/Renderers/FrameRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/FrameRenderer.cs
@@ -1,21 +1,21 @@
 using System.ComponentModel;
 using System.Drawing;
-using CoreAnimation;
 using CoreGraphics;
-using Foundation;
 using UIKit;
 
 namespace Xamarin.Forms.Platform.iOS
 {
-	public class FrameRenderer : VisualElementRenderer<Frame>, ITabStop
+	public class FrameRenderer : ViewRenderer<Frame, UIView>, ITabStop
 	{
-		ShadowView _shadowView;
+		readonly UIView _wrapperView = new UIView();
+		UIView _actualView = new UIView();
+		CGSize _previousSize;
+
 		UIView ITabStop.TabStop => this;
 
 		[Internals.Preserve(Conditional = true)]
 		public FrameRenderer()
 		{
-
 		}
 
 		protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
@@ -23,7 +23,26 @@ namespace Xamarin.Forms.Platform.iOS
 			base.OnElementChanged(e);
 
 			if (e.NewElement != null)
+			{
+				// Add the subviews to the actual view.
+				foreach (var item in NativeView.Subviews)
+				{
+					_actualView.AddSubview(item);
+				}
+
+				if (NativeView.GestureRecognizers != null)
+				{
+					foreach (var gesture in NativeView.GestureRecognizers)
+					{
+						_actualView.AddGestureRecognizer(gesture);
+					}
+				}
+
+				_wrapperView.AddSubview(_actualView);
+				SetNativeControl(_wrapperView);
+
 				SetupLayer();
+			}
 		}
 
 		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -45,121 +64,75 @@ namespace Xamarin.Forms.Platform.iOS
 			if (cornerRadius == -1f)
 				cornerRadius = 5f; // default corner radius
 
-			Layer.CornerRadius = cornerRadius;
-			Layer.MasksToBounds = Layer.CornerRadius > 0;
+			_wrapperView.Layer.CornerRadius = cornerRadius;
+			_wrapperView.Layer.MasksToBounds = Layer.CornerRadius > 0;
 
 			if (Element.BackgroundColor == Color.Default)
-				Layer.BackgroundColor = UIColor.White.CGColor;
+				_wrapperView.Layer.BackgroundColor = UIColor.White.CGColor;
 			else
-				Layer.BackgroundColor = Element.BackgroundColor.ToCGColor();
+				_wrapperView.Layer.BackgroundColor = Element.BackgroundColor.ToCGColor();
 
 			if (Element.BorderColor == Color.Default)
-				Layer.BorderColor = UIColor.Clear.CGColor;
+				_wrapperView.Layer.BorderColor = UIColor.Clear.CGColor;
 			else
 			{
-				Layer.BorderColor = Element.BorderColor.ToCGColor();
-				Layer.BorderWidth = 1;
+				_wrapperView.Layer.BorderColor = Element.BorderColor.ToCGColor();
+				_wrapperView.Layer.BorderWidth = 1;
 			}
 
 			if (Element.HasShadow)
 			{
-				if (_shadowView == null)
-				{
-					_shadowView = new ShadowView(Layer);
-					SetNeedsLayout();
-				}
-				_shadowView.UpdateBackgroundColor();
-				_shadowView.Layer.CornerRadius = Layer.CornerRadius;
-				_shadowView.Layer.BorderColor = Layer.BorderColor;
-				_shadowView.Hidden = !Element.IsVisible;
+				_wrapperView.Layer.ShadowRadius = 5;
+				_wrapperView.Layer.ShadowColor = UIColor.Black.CGColor;
+				_wrapperView.Layer.ShadowOpacity = 0.8f;
+				_wrapperView.Layer.ShadowOffset = new SizeF();
 			}
 			else
 			{
-				if (_shadowView != null)
-				{
-					_shadowView.RemoveFromSuperview();
-					_shadowView.Dispose();
-					_shadowView = null;
-				}
+				_wrapperView.Layer.ShadowOpacity = 0;
 			}
 
-			Layer.RasterizationScale = UIScreen.MainScreen.Scale;
-			Layer.ShouldRasterize = true;
+			_wrapperView.Layer.RasterizationScale = UIScreen.MainScreen.Scale;
+			_wrapperView.Layer.ShouldRasterize = true;
 		}
 
 		public override void LayoutSubviews()
 		{
-			if (_shadowView != null)
-			{
-				if (_shadowView.Superview == null)
-					Superview.InsertSubviewBelow(_shadowView, this);
+			if (_previousSize != Bounds.Size)
+				SetNeedsDisplay();
 
-				_shadowView?.SetNeedsLayout();
-			}
 			base.LayoutSubviews();
+		}
+
+		public override void Draw(CGRect rect)
+		{
+			_actualView.Frame = Bounds;
+			_wrapperView.Frame = Bounds;
+
+			base.Draw(rect);
+
+			_previousSize = Bounds.Size;
 		}
 
 		protected override void Dispose(bool disposing)
 		{
 			base.Dispose(disposing);
+
 			if (disposing)
 			{
-				if (_shadowView != null)
+				if (_actualView != null)
 				{
-					_shadowView.RemoveFromSuperview();
-					_shadowView.Dispose();
-					_shadowView = null;
+					
+					for (var i = 0; i < _actualView.GestureRecognizers?.Length; i++)
+						_actualView.GestureRecognizers.Remove(_actualView.GestureRecognizers[i]);
+
+					for (var j = 0; j < _actualView.Subviews.Length; j++)
+						_actualView.Subviews.Remove(_actualView.Subviews[j]);
+
+					_actualView.RemoveFromSuperview();
+					_actualView.Dispose();
+					_actualView = null;
 				}
-			}
-		}
-
-
-		[Preserve(Conditional = true)]
-		class ShadowView : UIView
-		{
-			CALayer _shadowee;
-			CGRect _previousBounds;
-			CGRect _previousFrame;
-
-			[Preserve(Conditional = true)]
-			public ShadowView(CALayer shadowee)
-			{
-				_shadowee = shadowee;
-				Layer.ShadowRadius = 5;
-				Layer.ShadowColor = UIColor.Black.CGColor;
-				Layer.ShadowOpacity = 0.8f;
-				Layer.ShadowOffset = new SizeF();
-				Layer.BorderWidth = 1;
-				UserInteractionEnabled = false;
-			}
-
-			public void UpdateBackgroundColor()
-			{
-				//Putting a transparent background under any shadowee having a background with alpha < 1
-				//Giving the Shadow a background of the same color when shadowee background == 1.
-				//The latter will result in a 'darker' shadow as you would expect from something that
-				//isn't transparent. This also mimics the look as it was before with non-transparent Frames.
-				if (_shadowee.BackgroundColor.Alpha < 1)
-					BackgroundColor = UIColor.Clear;
-				else
-					BackgroundColor = new UIColor(_shadowee.BackgroundColor);
-			}
-
-			public override void LayoutSubviews()
-			{
-				if (_shadowee.Bounds != _previousBounds || _shadowee.Frame != _previousFrame)
-				{
-					base.LayoutSubviews();
-					SetBounds();
-				}
-			}
-
-			void SetBounds()
-			{
-				Layer.Frame = _shadowee.Frame;
-				Layer.Bounds = _shadowee.Bounds;
-				_previousBounds = _shadowee.Bounds;
-				_previousFrame = _shadowee.Frame;
 			}
 		}
 	}
