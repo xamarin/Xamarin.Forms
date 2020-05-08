33b6acab54ad50d71cd4c68e068947eab0f26df3
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-05-08 18:48:17 -0300

    Update Xamarin.Forms.Core/AutofitTextMode.cs
    Co-authored-by: Samantha Houts <samhouts@users.noreply.github.com>

--- Xamarin.Forms.Core/AutofitTextMode.cs
+++ Xamarin.Forms.Core/AutofitTextMode.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Xamarin.Forms.Core
+namespace Xamarin.Forms
 {
 	[TypeConverter(typeof(AutoFitTextModeConverter))]
 	public enum AutoFitTextMode
