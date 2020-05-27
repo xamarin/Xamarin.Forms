1d74641267e24cd9e1ee1310a2ee7a948ef153bd
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-05-27 11:52:13 +0200

    Added Clip implementation in Android, iOS and macOS

--- Xamarin.Forms.Controls/GalleryPages/ShapesGalleries/ShapesGallery.cs
+++ Xamarin.Forms.Controls/GalleryPages/ShapesGalleries/ShapesGallery.cs
@@ -26,8 +26,9 @@ namespace Xamarin.Forms.Controls.GalleryPages.ShapesGalleries
 					GalleryBuilder.NavButton("Path LayoutOptions Gallery", () => new PathLayoutOptionsGallery(), Navigation),
 					GalleryBuilder.NavButton("Transform Playground", () => new TransformPlaygroundGallery(), Navigation),
 					GalleryBuilder.NavButton("Path Transform using string (TypeConverter) Gallery", () => new PathTransformStringGallery(), Navigation),    
-					GalleryBuilder.NavButton("Clip Gallery", () => new ClipGallery(), Navigation)
-				}
+					GalleryBuilder.NavButton("Clip Gallery", () => new ClipGallery(), Navigation),
+                    GalleryBuilder.NavButton("Add/Remove Clip Gallery", () => new AddRemoveClipGallery(), Navigation)
+                }
             };
         }
 
