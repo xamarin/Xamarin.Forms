602524da9df7e857fac5dd3fa28e5ec54cc0616b
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-06-09 10:55:12 +0200

    Replace UWP Community Toolkit RadialGradientBrush class by WinUI 2.4 RadialGradientBrush

--- Xamarin.Forms.Platform.UAP/BrushExtensions.cs
+++ Xamarin.Forms.Platform.UAP/BrushExtensions.cs
@@ -5,6 +5,10 @@ using WGradientStop = Windows.UI.Xaml.Media.GradientStop;
 using WLinearGradientBrush = Windows.UI.Xaml.Media.LinearGradientBrush;
 using WPoint = Windows.Foundation.Point;
 
+#if UWP_18362
+using WRadialGradientBrush = Microsoft.UI.Xaml.Media.RadialGradientBrush;
+#endif
+
 namespace Xamarin.Forms.Platform.UWP
 {
 	public static class BrushExtensions
@@ -36,22 +40,25 @@ namespace Xamarin.Forms.Platform.UWP
 					EndPoint = new WPoint(p2.X, p2.Y)
 				};
 			}
-
-			if (brush is Xamarin.Forms.RadialGradientBrush radialGradientBrush)
+#if UWP_18362
+			if (brush is RadialGradientBrush radialGradientBrush)
 			{
-				var orderedStops = radialGradientBrush.GradientStops.OrderBy(x => x.Offset).ToList();
-				var gradientStopCollection = new WGradientStopCollection();
-
-				foreach (var item in orderedStops)
-					gradientStopCollection.Add(new WGradientStop { Offset = item.Offset, Color = item.Color.ToWindowsColor() });
-
-				return new RadialGradientBrush(gradientStopCollection)
+				var wRadialGradientBrush = new WRadialGradientBrush()
 				{
 					Center = new WPoint(radialGradientBrush.Center.X, radialGradientBrush.Center.Y),
 					RadiusX = radialGradientBrush.Radius,
 					RadiusY = radialGradientBrush.Radius
 				};
+
+				var orderedStops = radialGradientBrush.GradientStops.OrderBy(x => x.Offset).ToList();
+
+				foreach (var gradientStop in orderedStops)
+					wRadialGradientBrush.GradientStops.Add(
+						new WGradientStop { Color = gradientStop.Color.ToWindowsColor(), Offset = gradientStop.Offset });
+
+				return wRadialGradientBrush;
 			}
+#endif
 
 			return null;
 		}
