38d3490437dc2d9551fccaf87281fd38b443b5e5
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-06-14 11:38:36 -0300

    ♻ Remove unecessary converter

--- Xamarin.Forms.Core/AutofitTextMode.cs
+++ Xamarin.Forms.Core/AutofitTextMode.cs
@@ -1,33 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Xamarin.Forms
+﻿namespace Xamarin.Forms
 {
-	[TypeConverter(typeof(AutoFitTextModeConverter))]
 	public enum AutoFitTextMode
 	{
 		None,
 		FitToWidth
-	}
-
-	[Xaml.TypeConversion(typeof(AutoFitTextMode))]
-	public class AutoFitTextModeConverter : TypeConverter
-	{
-		public override object ConvertFromInvariantString(string value)
-		{
-			if (value != null)
-			{
-				if (value.Equals("None", StringComparison.OrdinalIgnoreCase))
-					return AutoFitTextMode.None;
-				if (value.Equals("FitToWidth", StringComparison.OrdinalIgnoreCase))
-					return AutoFitTextMode.FitToWidth;
-
-				if (Enum.TryParse(value, out TextAlignment direction))
-					return direction;
-			}
-
-			throw new InvalidOperationException($"Cannot convert \"{value}\" into {nameof(AutoFitTextMode)}");
-		}
 	}
 }
