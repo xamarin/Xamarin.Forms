5425353a8cee001f3d6ca8d6d30d7f824484455d
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-06-18 13:29:35 +0200

    Updated repro sample

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11050.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11050.xaml.cs
@@ -25,15 +25,14 @@ namespace Xamarin.Forms.Controls.Issues
 
             InitializeComponent();
 
-            BindingContext = this;
-
             Device.StartTimer(TimeSpan.FromMilliseconds(15), () =>
             {
                 DateTime dateTime = DateTime.Now;
-                SecondHandAngle = 6 * (dateTime.Second + dateTime.Millisecond / 1000.0);
-                MinuteHandAngle = 6 * dateTime.Minute + SecondHandAngle / 60;
-                HourHandAngle = 30 * (dateTime.Hour % 12) + MinuteHandAngle / 12;
-                return true;
+				secondHand.Angle = 6 * (dateTime.Second + dateTime.Millisecond / 1000.0);
+				minuteHand.Angle = 6 * dateTime.Minute + secondHand.Angle / 60;
+				hourHand.Angle = 30 * (dateTime.Hour % 12) + minuteHand.Angle / 12;
+
+				return true;
             });
 
             SizeChanged += (sender, args) =>
@@ -43,28 +42,6 @@ namespace Xamarin.Forms.Controls.Issues
                 grid.Scale = Math.Min(Width, Height) / 200;
             };
 #endif
-        }
-
-        public static readonly BindableProperty SecondHandAngleProperty = BindableProperty.Create(nameof(SecondHandAngle), typeof(double), typeof(Issue11050), default(double));
-        public static readonly BindableProperty MinuteHandAngleProperty = BindableProperty.Create(nameof(MinuteHandAngle), typeof(double), typeof(Issue11050), default(double));
-        public static readonly BindableProperty HourHandAngleProperty = BindableProperty.Create(nameof(HourHandAngle), typeof(double), typeof(Issue11050), default(double));
-
-        public double SecondHandAngle
-        {
-            get { return (double)GetValue(SecondHandAngleProperty); }
-            set { SetValue(SecondHandAngleProperty, value); }
-        }
-
-        public double MinuteHandAngle
-        {
-            get { return (double)GetValue(MinuteHandAngleProperty); }
-            set { SetValue(MinuteHandAngleProperty, value); }
-        }
-
-        public double HourHandAngle
-        {
-            get { return (double)GetValue(HourHandAngleProperty); }
-            set { SetValue(HourHandAngleProperty, value); }
         }
     }
 }
