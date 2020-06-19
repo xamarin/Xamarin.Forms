de9d92188364349220a20ea23f9b1e479eda3264
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-06-19 11:06:16 +0200

    Updated sample

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11120.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11120.xaml.cs
@@ -1,10 +1,19 @@
 ﻿using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
 namespace Xamarin.Forms.Controls.Issues
 {
+#if UITEST
+	[Category(UITestCategories.Frame)]
+#endif
 	[Preserve(AllMembers = true)]
-	[Issue(IssueTracker.Github, 11120, "[iOS] Opacity on Frame behavior change in 4.5", PlatformAffected.iOS)]
+	[Issue(IssueTracker.Github, 11120, "[Bug] IsClippedToBounds iOS not work", PlatformAffected.iOS)]
 	public partial class Issue11120 : ContentPage
 	{
 		public Issue11120()
