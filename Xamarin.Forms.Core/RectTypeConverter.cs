0af0f6b0b00304fd843c91a8585d970457c1d622
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-06-24 13:58:45 +0200

    Changes in RectTypeConverter

--- Xamarin.Forms.Core/RectTypeConverter.cs
+++ Xamarin.Forms.Core/RectTypeConverter.cs
@@ -3,6 +3,12 @@
 	[Xaml.TypeConversion(typeof(Rect))]
 	public class RectTypeConverter : RectangleTypeConverter
 	{
+		public override object ConvertFromInvariantString(string value)
+		{
+			var rectangle = base.ConvertFromInvariantString(value);
+			Rect rect = (Rectangle)rectangle;
 
+			return rect;
+		}
 	}
 }
