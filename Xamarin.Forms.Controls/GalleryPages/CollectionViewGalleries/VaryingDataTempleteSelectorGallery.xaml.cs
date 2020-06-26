1369566f3081a50514f2efb04046882d04d17ee1
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2020-06-26 15:02:11 +0200

    #10842 - ios - updated gallerypage in collectionviewgalleries which is used for testing a variying datatemplate selector with different sizes

--- Xamarin.Forms.Controls/GalleryPages/CollectionViewGalleries/VaryingDataTempleteSelectorGallery.xaml.cs
+++ Xamarin.Forms.Controls/GalleryPages/CollectionViewGalleries/VaryingDataTempleteSelectorGallery.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using Xamarin.Forms.Controls.GalleryPages.SwipeViewGalleries;
 using Xamarin.Forms.Xaml;
 
 namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries
@@ -11,6 +14,11 @@ namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries
 	public partial class VaryingDataTempleteSelectorGallery : ContentPage, INotifyPropertyChanged
 	{
 		string _index = "1";
+		string _itemsCount = "1";
+		int _counter = 6;
+		string _selectedTemplate = nameof(Plane);
+		bool _isCarTemplate;
+		bool _shouldTriggerReset;
 
 		public VaryingDataTempleteSelectorGallery()
 		{
@@ -24,17 +32,17 @@ namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries
 
 			IEnumerable<VehicleBase> CreateDefaultVehicles()
 			{
-				yield return new Plane("Plane1");
-				yield return new Plane("Plane2");
-				yield return new Car("Car1");
-				yield return new Plane("Plane3");
-				yield return new Car("Car2");
-				yield return new Plane("Plane4");
+				yield return new Plane("Plane 0");
+				yield return new Plane("Plane 1");
+				yield return new Car("Car 2");
+				yield return new Plane("Plane 3");
+				yield return new Car("Car 4");
+				yield return new Plane("Plane 5");
 			}
 		}
 
-		public ObservableCollection<VehicleBase> Items { get; set; } =
-			new ObservableCollection<VehicleBase>();
+		public ImprovedObservableCollection<VehicleBase> Items { get; set; } =
+			new ImprovedObservableCollection<VehicleBase>();
 
 		public string Index
 		{
@@ -42,22 +50,59 @@ namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries
 			set => SetValue(ref _index, value);
 		}
 
-		void InsertCar_OnClicked(object sender, EventArgs e)
+		public string ItemsCount
 		{
-			if (string.IsNullOrWhiteSpace(Index)) return;
-			if (!int.TryParse(Index, out var index)) return;
-			if (index > Items.Count || index < 0) return;
-
-			Items.Insert(index, new Car("rCar " + new Random().Next(0,1000).ToString()));
+			get => _itemsCount;
+			set => SetValue(ref _itemsCount, value);
 		}
 
-		void InsertPlane_OnClicked(object sender, EventArgs e)
+		public bool IsCarTemplate
 		{
-			if (string.IsNullOrWhiteSpace(Index)) return;
-			if (!int.TryParse(Index, out var index)) return;
-			if (index > Items.Count || index < 0) return;
+			get => _isCarTemplate;
+			set
+			{
+				SetValue(ref _isCarTemplate, value);
+				SelectedTemplate = value ?  nameof(Car) : nameof(Plane);
+			}
+		}
 
-			Items.Insert(index, new Plane("rPlane " + new Random().Next(0,1000).ToString()));
+		public string SelectedTemplate
+		{
+			get => _selectedTemplate;
+			set => SetValue(ref _selectedTemplate, value);
+		}
+
+		public bool ShouldTriggerReset
+		{
+			get => _shouldTriggerReset;
+			set => SetValue(ref _shouldTriggerReset, value);
+		}
+
+		void Insert_OnClicked(object sender, EventArgs e)
+		{
+			if (!IsValid(out var index)) return;
+
+			using (Items.Suppress(ShouldTriggerReset))
+			{
+				if(int.TryParse(ItemsCount, out var count) && count > 0)
+					Items.InsertRange(index, Enumerable.Range(0, count).Select(_ => CreateVehicle()));
+				else 
+					Items.Insert(index, CreateVehicle());
+			}
+		}
+
+		void Add_OnClicked(object sender, EventArgs e)
+		{
+			if (!IsValid(out var _)) return;
+
+			using (Items.Suppress(ShouldTriggerReset))
+			{
+				if(int.TryParse(ItemsCount, out var count) && count > 0)
+					Items.AddRange(Enumerable.Range(0, count).Select(_ => CreateVehicle()));
+				else 
+					Items.Add(CreateVehicle());
+					
+			}
 		}
 
 		void SetValue<T>(ref T backingField, in T value, [CallerMemberName] string callerName = null)
@@ -68,6 +113,69 @@ namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries
 			OnPropertyChanged(callerName);
 		}
 
+		void Remove_OnClicked(object sender, EventArgs e)
+		{
+			if(!IsValid(out var index)) return;
+
+			using (Items.Suppress(ShouldTriggerReset))
+			{
+				Items.RemoveAt(index);
+			}
+		}
+
+		VehicleBase CreateVehicle() => IsCarTemplate
+				? (VehicleBase)new Car($"{nameof(Car)} {_counter++}")
+				: new Plane($"{nameof(Plane)} {_counter++}");
+
+		bool IsValid(out int index)
+		{
+			index = -1;
+			if (string.IsNullOrWhiteSpace(Index)) return false;
+			if (!int.TryParse(Index, out index)) return false;
+			if (index > Items.Count || index < 0) return false;
+
+			return true;
+		}
+	}
+
+	public class ImprovedObservableCollection<T> : ObservableCollection<T>
+	{
+		bool _suppress;
+
+		public IDisposable Suppress(bool shouldSuppress) => new Suppressor(this, ref shouldSuppress);
+
+		protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+		{
+			if(!_suppress)
+				base.OnCollectionChanged(e);
+		}
+
+		public void InsertRange(int index, IEnumerable<T> items)
+		{
+			foreach (T item in items)
+				Insert(index, item);
+		}
+
+		public void AddRange(IEnumerable<T> items)
+		{
+			foreach (T item in items)
+				Add(item);
+		}
+
+		class Suppressor : IDisposable
+		{
+			readonly ImprovedObservableCollection<T> _source;
+			public Suppressor(ImprovedObservableCollection<T> source, ref bool shouldSuppress)
+			{
+				_source = source;
+				_source._suppress = shouldSuppress;
+			}
+			public void Dispose()
+			{
+				_source._suppress = false;
+				_source.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+			}
+		}
 	}
 
 	class VehicleTemplateSelector : DataTemplateSelector
