b05ac58e8a6456ecf53b14b5d910438b4cb0beeb
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-07-08 11:15:33 +0200

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11314.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.SwipeView)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 11314,
+		"Cannot swipe the swipeview inside a Listview[Bug]",
+		PlatformAffected.Android)]
+	public class Issue11314 : TestTabbedPage
+	{
+		public Issue11314()
+		{
+#if APP
+			Device.SetFlags(new List<string> { ExperimentalFlags.SwipeViewExperimental });
+#endif
+		}
+
+		protected override void Init()
+		{
+			Title = "Issue 11314";
+
+			var page1 = new Issue11314Page("Page 1");
+			var page2 = new Issue11314Page("Page 2");
+
+			Children.Add(page1);
+			Children.Add(page2);
+		}
+	}
+
+	public class Issue11314Page : ContentPage
+	{
+		public Issue11314Page(string title)
+		{
+			Title = title;
+
+			var layout = new StackLayout();
+
+			var swipeView = new SwipeView();
+
+			var content = new Grid
+			{
+				HeightRequest = 80,
+				BackgroundColor = Color.LightGray
+			};
+
+			content.Children.Add(new Label
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Text = "Swipe to Left"
+			});
+
+			swipeView.Content = content;
+
+			var swipeItem = new SwipeItem
+			{
+				BackgroundColor = Color.Red,
+				Text = "Text"
+			};
+
+			swipeView.RightItems = new SwipeItems
+			{
+				swipeItem
+			};
+
+			layout.Children.Add(swipeView);
+
+			Content = layout;
+		}
+	}
+}
