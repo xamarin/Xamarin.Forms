9167d5dd00b3438ecfd4f1f951284fd57a5fb887
Mike Mostachetti <mjmostachetti@gmail.com>
2020-08-08 00:30:05 -0700

    adding issue to shared issues

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9210.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 9210, "[Bug] iOS keyboard case flickers when switching entries", PlatformAffected.iOS)]
+	public class Issue9210 : TestContentPage
+	{
+		protected override void Init()
+		{
+			var stackLayout = new StackLayout();
+			stackLayout.Children.Add(new Entry() { Text="Some demo text." });
+			stackLayout.Children.Add(new Entry() { Text = "Some other demo text." });
+
+			Content = stackLayout;
+		}
+	}
+}
