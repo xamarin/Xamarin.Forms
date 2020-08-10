9d4ae2563ed30a859949f0b49a38008ffa1d384a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-08-10 12:44:45 +0200

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11571.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Xaml;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 11571, "[Bug][Brushes] LinearGradientBrush diagonal platform differences",
+		PlatformAffected.iOS)]
+	public partial class Issue11571 : TestContentPage
+	{
+		public Issue11571()
+		{
+#if APP
+			Device.SetFlags(new List<string> { ExperimentalFlags.BrushExperimental, ExperimentalFlags.ShapesExperimental });
+
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
