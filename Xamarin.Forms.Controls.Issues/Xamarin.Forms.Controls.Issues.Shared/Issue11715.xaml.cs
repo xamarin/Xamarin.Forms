6b30605be85a91e52140862b468fee4e660a9c12
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-08-10 15:54:43 +0200

    Added a new repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11715.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github,11715, "[Bug] Brushes API - Android's direction is inverted to iOS's",
+		PlatformAffected.Android)]
+	public partial class Issue11715 : TestContentPage
+	{
+		public Issue11715()
+		{
+#if APP
+			Device.SetFlags(new List<string> { ExperimentalFlags.BrushExperimental });
+
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
