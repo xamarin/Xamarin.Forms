82baa1b55bc3182fb2e3be548415b9b7c99dd6bb
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-08-14 12:07:43 +0200

    Added repro sample

---
+++ Xamarin.Forms.Controls/GalleryPages/GradientGalleries/UpdateGradientColorGallery.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Xamarin.Forms.Controls.GalleryPages.GradientGalleries
+{
+	public partial class UpdateGradientColorGallery : ContentPage
+	{
+		readonly Random _random;
+
+		public UpdateGradientColorGallery()
+		{
+			InitializeComponent();
+			_random = new Random();
+		}
+
+		void OnUpdateColorsClicked(object sender, EventArgs e)
+		{
+			GradientStop firstStop = linearBrush.GradientStops[GetRandomGradientStop()];
+			firstStop.Color = GetRandomColor();
+		}
+
+		int GetRandomGradientStop()
+		{
+			return _random.Next(3);
+		}
+
+		Color GetRandomColor()
+		{
+			return Color.FromRgb(_random.Next(256), _random.Next(256), _random.Next(256));
+		}
+	}
+}
