20eb756f27bc180499a0156a511056a9e2e21fd6
Mike Mostachetti <mjmostachetti@gmail.com>
2020-08-15 21:43:07 -0700

    fill out issue class

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9691.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9691.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using System.Linq;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
 namespace Xamarin.Forms.Controls.Issues
 {
-	public class Issue9691
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 9691, "[Bug][iOS] Keyboard hides CollectionView elements",
+		PlatformAffected.iOS)]
+	public class Issue9691 : TestContentPage
 	{
-		public Issue9691()
+		protected override void Init()
 		{
+			var collectionView = new CollectionView();
+			collectionView.ItemTemplate = new DataTemplate(() => new Editor());
+			collectionView.ItemsSource = Enumerable.Repeat(0, 20).ToList();
+
+			Content = collectionView;
 		}
 	}
 }
