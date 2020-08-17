5cc080ec5b605080c8bc8e4ff28eb82ece0c0c18
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-08-17 18:50:33 +0200

    Fixed build error

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11050.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11050.xaml.cs
@@ -12,7 +12,7 @@ using Xamarin.Forms.Core.UITests;
 namespace Xamarin.Forms.Controls.Issues
 {
 #if UITEST
-	[Category(UITestCategories.Shapes)]
+	[Category(UITestCategories.Shape)]
 #endif
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 11050, "[Bug][iOS][Android] Shapes: clock drawing erro", PlatformAffected.Android | PlatformAffected.iOS)]
