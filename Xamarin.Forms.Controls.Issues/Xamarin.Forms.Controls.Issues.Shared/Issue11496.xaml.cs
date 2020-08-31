ed8b653fb0c94f5ae5927acec46219828dce8d52
Samantha Houts <samhouts@users.noreply.github.com>
2020-08-31 16:31:41 -0700

    spaces

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11496.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11496.xaml.cs
@@ -31,26 +31,26 @@ namespace Xamarin.Forms.Controls.Issues
 			Device.SetFlags(new List<string> { ExperimentalFlags.SwipeViewExperimental });
 			InitializeComponent();
 #endif
-        }
+		}
 
-        protected override void Init()
+		protected override void Init()
 		{
 
 		}
 	}
 
-    [Preserve(AllMembers = true)]
-    public partial class Issue11496ItemControl : ContentView
-    {
+	[Preserve(AllMembers = true)]
+	public partial class Issue11496ItemControl : ContentView
+	{
 		readonly Label _label;
 
-        public Issue11496ItemControl()
-        {
-            var layout = new Grid();
+		public Issue11496ItemControl()
+		{
+			var layout = new Grid();
 
 			_label = new Label
 			{
-                HorizontalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.Center
 			};
 
@@ -64,108 +64,108 @@ namespace Xamarin.Forms.Controls.Issues
 				CommandParameter = CommandParameter
 			};
 
-            layout.GestureRecognizers.Add(tapCommand);
+			layout.GestureRecognizers.Add(tapCommand);
 
-            tapCommand.Tapped += (sender, args) =>
-            {
-                if (TestItem != null)
-                    Application.Current.MainPage.DisplayAlert("Issue11496", $"Tapped {((Issue11496Item)TestItem).Name}", "Ok");
-            };
-        }
+			tapCommand.Tapped += (sender, args) =>
+			{
+				if (TestItem != null)
+					Application.Current.MainPage.DisplayAlert("Issue11496", $"Tapped {((Issue11496Item)TestItem).Name}", "Ok");
+			};
+		}
 
-        public static readonly BindableProperty TestItemProperty =
-            BindableProperty.Create(nameof(TestItem), typeof(object), typeof(Issue11496ItemControl));
+		public static readonly BindableProperty TestItemProperty =
+			BindableProperty.Create(nameof(TestItem), typeof(object), typeof(Issue11496ItemControl));
 
-        public static readonly BindableProperty CommandProperty =
-            BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(Issue11496ItemControl));
+		public static readonly BindableProperty CommandProperty =
+			BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(Issue11496ItemControl));
 
-        public static readonly BindableProperty CommandParameterProperty =
-            BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(Issue11496ItemControl));
+		public static readonly BindableProperty CommandParameterProperty =
+			BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(Issue11496ItemControl));
 
-        public object TestItem
-        {
-            get => GetValue(TestItemProperty);
-            set => SetValue(TestItemProperty, value);
-        }
+		public object TestItem
+		{
+			get => GetValue(TestItemProperty);
+			set => SetValue(TestItemProperty, value);
+		}
 
-        public ICommand Command
-        {
-            get => (ICommand)GetValue(CommandProperty);
-            set => SetValue(CommandProperty, value);
-        }
+		public ICommand Command
+		{
+			get => (ICommand)GetValue(CommandProperty);
+			set => SetValue(CommandProperty, value);
+		}
 
-        public object CommandParameter
-        {
-            get => GetValue(CommandParameterProperty);
-            set => SetValue(CommandParameterProperty, value);
-        }
+		public object CommandParameter
+		{
+			get => GetValue(CommandParameterProperty);
+			set => SetValue(CommandParameterProperty, value);
+		}
 
 		protected override void OnBindingContextChanged()
 		{
 			base.OnBindingContextChanged();
 
-            if (TestItem != null)
-                _label.Text = ((Issue11496Item)TestItem).Name;
+			if (TestItem != null)
+				_label.Text = ((Issue11496Item)TestItem).Name;
 
-        }
+		}
 	}
 
-    [Preserve(AllMembers = true)]
-    public class Issue11496Item
-    {
-        public string Name { get; set; }
-    }
+	[Preserve(AllMembers = true)]
+	public class Issue11496Item
+	{
+		public string Name { get; set; }
+	}
 
-    [Preserve(AllMembers = true)]
-    public class Issue11496ViewModel : BindableObject
-    {
+	[Preserve(AllMembers = true)]
+	public class Issue11496ViewModel : BindableObject
+	{
 		List<Issue11496Item> _items;
 		ICommand _command;
-        object _commandParam;
+		object _commandParam;
 
-        public List<Issue11496Item> Items
-        {
-            get => _items;
-            set
-            {
-                _items = value;
-                OnPropertyChanged();
-            }
-        }
+		public List<Issue11496Item> Items
+		{
+			get => _items;
+			set
+			{
+				_items = value;
+				OnPropertyChanged();
+			}
+		}
 
-        public ICommand Command
-        {
-            get => _command;
-            set
-            {
-                _command = value;
-                OnPropertyChanged();
-            }
-        }
+		public ICommand Command
+		{
+			get => _command;
+			set
+			{
+				_command = value;
+				OnPropertyChanged();
+			}
+		}
 
-        public object CommandParameter
-        {
-            get => _commandParam;
-            set
-            {
-                _commandParam = value;
-                OnPropertyChanged();
-            }
-        }
+		public object CommandParameter
+		{
+			get => _commandParam;
+			set
+			{
+				_commandParam = value;
+				OnPropertyChanged();
+			}
+		}
 
-        public Issue11496ViewModel()
-        {
-            Items = new List<Issue11496Item>()
-            {
-                new Issue11496Item() { Name = "Test One" },
-                new Issue11496Item() { Name = "Test Two" },
-                new Issue11496Item() { Name = "Test Three" },
-                new Issue11496Item() { Name = "Test Four" },
-                new Issue11496Item() { Name = "Test Five" },
-                new Issue11496Item() { Name = "Test Six" },
-                new Issue11496Item() { Name = "Test Seven" },
-                new Issue11496Item() { Name = "Test Eight" },
-            };
-        }
-    }
+		public Issue11496ViewModel()
+		{
+			Items = new List<Issue11496Item>()
+			{
+				new Issue11496Item() { Name = "Test One" },
+				new Issue11496Item() { Name = "Test Two" },
+				new Issue11496Item() { Name = "Test Three" },
+				new Issue11496Item() { Name = "Test Four" },
+				new Issue11496Item() { Name = "Test Five" },
+				new Issue11496Item() { Name = "Test Six" },
+				new Issue11496Item() { Name = "Test Seven" },
+				new Issue11496Item() { Name = "Test Eight" },
+			};
+		}
+	}
 }
