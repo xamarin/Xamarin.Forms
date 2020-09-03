8345c300358d342764bebdd828a7b779033389ae
Seungkeun Lee <sngn.lee@samsung.com>
2020-09-03 09:28:56 +0900

    Refactoring NavigationView structure

--- Xamarin.Forms.Platform.Tizen/Shell/NavigationView.cs
+++ Xamarin.Forms.Platform.Tizen/Shell/NavigationView.cs
@@ -12,7 +12,6 @@ namespace Xamarin.Forms.Platform.Tizen
 		static EColor s_defaultBackgroundColor = EColor.White;
 
 		Box _mainLayout;
-		Scroller _scroller;
 
 		EImage _backgroundImage;
 		Aspect _bgImageAspect;
@@ -23,6 +22,7 @@ namespace Xamarin.Forms.Platform.Tizen
 
 		GenList _menu;
 		GenItemClass _templateClass;
+		GenItemClass _headerClass;
 
 		EColor _backgroundColor;
 
@@ -45,11 +45,14 @@ namespace Xamarin.Forms.Platform.Tizen
 		public Shell Shell { get; }
 
 		FlyoutHeaderBehavior _headerBehavior = FlyoutHeaderBehavior.Fixed;
+
 		public FlyoutHeaderBehavior HeaderBehavior
 		{
 			get => _headerBehavior;
 			set
 			{
+				if (_headerBehavior == value)
+					return;
 				_headerBehavior = value;
 				UpdateHeaderBehavior();
 			}
@@ -107,6 +110,9 @@ namespace Xamarin.Forms.Platform.Tizen
 			}
 		}
 
+		bool HeaderOnMenu => HeaderBehavior == FlyoutHeaderBehavior.Scroll ||
+							 HeaderBehavior == FlyoutHeaderBehavior.CollapseOnScroll;
+
 		public void BuildMenu(List<List<Element>> flyoutGroups)
 		{
 			if (!IsMenuItemChanged(flyoutGroups))
@@ -141,16 +147,6 @@ namespace Xamarin.Forms.Platform.Tizen
 		{
 			base.BackgroundColor = s_defaultBackgroundColor;
 
-			_scroller = new Scroller(parent)
-			{
-				AlignmentX = -1,
-				AlignmentY = -1,
-				WeightX = 1,
-				WeightY = 1,
-				ScrollBlock = ScrollBlock.Horizontal,
-			};
-			_scroller.Show();
-
 			_mainLayout = new Box(parent)
 			{
 				AlignmentX = -1,
@@ -161,8 +157,7 @@ namespace Xamarin.Forms.Platform.Tizen
 			_mainLayout.SetLayoutCallback(OnLayout);
 			_mainLayout.Show();
 
-			_scroller.SetContent(_mainLayout);
-			SetContent(_scroller);
+			SetContent(_mainLayout);
 
 			_menu = new GenList(parent)
 			{
@@ -192,6 +187,38 @@ namespace Xamarin.Forms.Platform.Tizen
 			{
 				GetContentHandler = GetTemplatedContent,
 			};
+
+			_headerClass = new GenItemClass(ThemeConstants.GenItemClass.Styles.Full)
+			{
+				GetContentHandler = GetHeaderContnet
+			};
+		}
+
+		bool IsMenuItemChanged(List<List<Element>> flyoutGroups)
+		{
+			if (_cachedGroups == null)
+				return true;
+
+			if (_cachedGroups.Count != flyoutGroups.Count)
+				return true;
+
+			for (int i = 0; i < flyoutGroups.Count; i++)
+			{
+				if (_cachedGroups[i].Count != flyoutGroups[i].Count)
+					return true;
+
+				for (int j = 0; j < flyoutGroups[i].Count; j++)
+				{
+					if (_cachedGroups[i][j] != flyoutGroups[i][j])
+						return true;
+				}
+			}
+			return false;
+		}
+
+		EvasObject GetHeaderContnet(object data, string part)
+		{
+			return GetNativeView(data as View);
 		}
 
 		EvasObject GetTemplatedContent(object data, string part)
@@ -232,7 +259,6 @@ namespace Xamarin.Forms.Platform.Tizen
 			string iconBinding;
 			if (bo is IMenuItemController)
 			{
-
 				if (bo is MenuItem mi && mi.Parent != null && mi.Parent.IsSet(Shell.MenuItemTemplateProperty))
 				{
 					return Shell.GetMenuItemTemplate(mi.Parent);
@@ -345,94 +371,128 @@ namespace Xamarin.Forms.Platform.Tizen
 			if (_header != null)
 			{
 				_header.MeasureInvalidated -= OnHeaderSizeChanged;
-			}
-			if (_nativeHeader != null)
-			{
-				_mainLayout.UnPack(_nativeHeader);
-				_nativeHeader.Unrealize();
-				_nativeHeader = null;
+
+				if (HeaderOnMenu)
+				{
+					ResetHeaderOnMenu();
+				}
+				else if (_nativeHeader != null)
+				{
+					_mainLayout.UnPack(_nativeHeader);
+					_nativeHeader.Unrealize();
+					_nativeHeader = null;
+				}
 			}
 
 			if (header != null)
 			{
 				header.MeasureInvalidated += OnHeaderSizeChanged;
-				var renderer = Platform.GetOrCreateRenderer(header);
-				(renderer as LayoutRenderer)?.RegisterOnLayoutUpdated();
-				_nativeHeader = renderer.NativeView;
-				_mainLayout.PackEnd(_nativeHeader);
+
+				if (HeaderOnMenu)
+				{
+					UpdateHeaderOnMenu(header);
+				}
+				else
+				{
+					var renderer = Platform.GetOrCreateRenderer(header);
+					(renderer as LayoutRenderer)?.RegisterOnLayoutUpdated();
+					_nativeHeader = renderer.NativeView;
+					_mainLayout.PackEnd(_nativeHeader);
+				}
 			}
 			_header = header;
 		}
 
 		void UpdateHeaderBehavior()
 		{
-			if (HeaderBehavior == FlyoutHeaderBehavior.Scroll || HeaderBehavior == FlyoutHeaderBehavior.CollapseOnScroll)
+			if (_header == null)
+				return;
+
+			if (HeaderOnMenu)
 			{
-				_menu.ListMode = GenListMode.Expand;
+				if (_nativeHeader != null)
+				{
+					_mainLayout.UnPack(_nativeHeader);
+					_nativeHeader.Unrealize();
+					_nativeHeader = null;
+				}
+				UpdateHeaderOnMenu(_header);
 			}
 			else
 			{
-				_menu.ListMode = GenListMode.Scroll;
+				ResetHeaderOnMenu();
+
+				if (_nativeHeader == null)
+				{
+					var renderer = Platform.GetOrCreateRenderer(_header);
+					(renderer as LayoutRenderer)?.RegisterOnLayoutUpdated();
+					_nativeHeader = renderer.NativeView;
+					_mainLayout.PackEnd(_nativeHeader);
+				}
 			}
 			OnLayout();
 		}
 
-		bool IsMenuItemChanged(List<List<Element>> flyoutGroups)
+		void UpdateHeaderOnMenu(View header)
 		{
-			if (_cachedGroups == null)
-				return true;
+			if (_menu.FirstItem != null && _menu.FirstItem.Data == header)
+				return;
 
-			if (_cachedGroups.Count != flyoutGroups.Count)
-				return true;
-
-			for (int i = 0; i < flyoutGroups.Count; i++)
+			GenListItem item = null;
+			if (_menu.Count > 0)
 			{
-				if (_cachedGroups[i].Count != flyoutGroups[i].Count)
-					return true;
-
-				for (int j = 0; j < flyoutGroups[i].Count; j++)
-				{
-					if (_cachedGroups[i][j] != flyoutGroups[i][j])
-						return true;
-				}
+				item = _menu.InsertBefore(_headerClass, header, _menu.FirstItem);
 			}
-			return false;
+			else
+			{
+				item = _menu.Append(_headerClass, header);
+			}
+			item.SelectionMode = GenItemSelectionMode.None;
+		}
+
+		void ResetHeaderOnMenu()
+		{
+			if (_menu.FirstItem != null && _menu.FirstItem.Data == _header)
+			{
+				_menu.FirstItem.Delete();
+				Platform.GetRenderer(_header)?.Dispose();
+			}
 		}
 
 		void OnHeaderSizeChanged(object sender, EventArgs e)
 		{
-			OnLayout();
+			if (HeaderOnMenu)
+			{
+				_menu.FirstItem?.Update();
+			}
+			else
+			{
+				Device.BeginInvokeOnMainThread(OnLayout);
+			}
 		}
 
 		void OnLayout()
 		{
 			if (Geometry.Width == 0 || Geometry.Height == 0)
 				return;
+
 			var bound = Geometry;
 			int headerHeight = 0;
 			if (Header != null)
 			{
-				var requestSize = Header.Measure(Forms.ConvertToScaledDP(bound.Width), Forms.ConvertToScaledDP(bound.Height));
-				headerHeight = Forms.ConvertToScaledPixel(requestSize.Request.Height);
-				var headerBound = Geometry;
-				headerBound.Height = headerHeight;
-				_nativeHeader.Geometry = headerBound;
+				if (!HeaderOnMenu)
+				{
+					var requestSize = Header.Measure(Forms.ConvertToScaledDP(bound.Width), Forms.ConvertToScaledDP(bound.Height));
+					headerHeight = Forms.ConvertToScaledPixel(requestSize.Request.Height);
+					var headerBound = Geometry;
+					headerBound.Height = headerHeight;
+					_nativeHeader.Geometry = headerBound;
+				}
 			}
 
 			bound.Y += headerHeight;
-
-			if (HeaderBehavior == FlyoutHeaderBehavior.Scroll || HeaderBehavior == FlyoutHeaderBehavior.CollapseOnScroll)
-			{
-				bound.Height = _menu.MinimumHeight;
-			}
-			else
-			{
-				bound.Height -= headerHeight;
-			}
+			bound.Height -= headerHeight;
 			_menu.Geometry = bound;
-
-			_mainLayout.MinimumWidth = _scroller.Geometry.Width;
-			_mainLayout.MinimumHeight = headerHeight + bound.Height;
 		}
 	}
 }
