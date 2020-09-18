012d1dd47234932488541f57b813d91f01a99ca1
Pedro Jesus <pedrojesus.cefet@gmail.com>
2020-09-18 20:13:18 -0300

    Changed the PlatformConfigurationRegistry to public

--- Xamarin.Forms.Core/PlatformConfigurationRegistry.cs
+++ Xamarin.Forms.Core/PlatformConfigurationRegistry.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Xamarin.Forms
 {
@@ -7,7 +8,8 @@ namespace Xamarin.Forms
 	/// Helper that handles storing and lookup of platform specifics implementations
 	/// </summary>
 	/// <typeparam name="TElement">The Element type</typeparam>
-	internal class PlatformConfigurationRegistry<TElement> : IElementConfiguration<TElement>
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public class PlatformConfigurationRegistry<TElement> : IElementConfiguration<TElement>
 		where TElement : Element
 	{
 		readonly TElement _element;
