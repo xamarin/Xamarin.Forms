0ee8b7a3ccd6031b9b6712ae95da5f465ecf4f10
Shane Neuville <shneuvil@microsoft.com>
2020-09-22 14:17:05 -0500

    - labels

--- src/Platform.Handlers/src/Xamarin.Platform.Handlers/Platform/iOS/ViewExtensions.cs
+++ src/Platform.Handlers/src/Xamarin.Platform.Handlers/Platform/iOS/ViewExtensions.cs
@@ -6,11 +6,6 @@ namespace Xamarin.Platform
 {
 	public static class ViewExtensions
 	{
-		public static void SetText(this UILabel label, string text)
-			=> label.Text = text;
-
-		public static void SetText(this UILabel label, NSAttributedString text)
-			=> label.AttributedText = text;
 
 		public static void SetBackgroundColor(this UIView view, UIColor color)
 			=> view.BackgroundColor = color;
