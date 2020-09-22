c04c053fd08e2d51346d2fe75592d4ada28f2aef
Shane Neuville <shneuvil@microsoft.com>
2020-09-22 16:47:33 -0500

    - remove mac files for now

--- src/Platform.Handlers/src/Xamarin.Platform.Handlers/Platform/iOS/ViewExtensions.cs
+++ src/Platform.Handlers/src/Xamarin.Platform.Handlers/Platform/iOS/ViewExtensions.cs
@@ -11,5 +11,22 @@ namespace Xamarin.Platform
 			=> view.BackgroundColor = color;
 		public static UIColor GetBackgroundColor(this UIView view) =>
 			view.BackgroundColor;
+
+
+		public static void UpdateIsEnabled(this UIView nativeView, IView view)
+		{
+			if (!(nativeView is UIControl uiControl))
+				return;
+
+			uiControl.Enabled = view.IsEnabled;
+		}
+
+		public static void UpdateBackgroundColor(this UIView nativeView, IView view)
+		{
+			var color = view.BackgroundColor;
+
+			if (color != null && !color.IsDefault)
+				nativeView.BackgroundColor = color.ToNative();
+		}
 	}
 }
