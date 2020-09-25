7c301843311cd3ab2d01ea01c3a45abe6f86efce
Shane Neuville <shneuvil@microsoft.com>
2020-09-25 14:03:11 -0500

    - add ui test

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12153.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12153.cs
@@ -23,7 +23,7 @@ namespace Xamarin.Forms.Controls.Issues
 				Children =
 				{
 					// Setting the font family to "Tahoma" or any other built-in Windows font would crash on UWP
-					new Label() { Text = "Four bell icons should be visible below and it shouldn't crash on UWP", FontFamily = "Tahoma", Margin = new Thickness(10)},
+					new Label() { Text = "Four bell icons should be visible below and it shouldn't crash on UWP", FontFamily = "Tahoma", Margin = new Thickness(10), AutomationId = "Success"},
 
 					new Label { FontFamily = "FontAwesome", FontSize = 50, TextColor = Color.Black, Text = "\xf0f3" },
 					new Label { FontFamily = "fa-regular-400.ttf", FontSize = 50, TextColor = Color.Black, Text = "\xf0f3" },
@@ -35,9 +35,17 @@ namespace Xamarin.Forms.Controls.Issues
 
 					new Label() { Text = "Below a PLAY icon should be visible (if the \"Segoe MDL2 Assets\" font is installed)", Margin = new Thickness(10)},
 
-					new Image() { Source = new FontImageSource{Glyph = "\xe102",FontFamily = "Segoe MDL2 Assets", Size = 50, Color = Color.Green}, HorizontalOptions = LayoutOptions.Start },
+					new Image() { Source = new FontImageSource{Glyph = "\xe102",FontFamily = "Segoe MDL2 Assets", Size = 50, Color = Color.Green}, HorizontalOptions = LayoutOptions.Start },					
 				}
 			};
 		}
+
+#if UITEST
+		[Test]
+		public void InvalidFontDoesntCauseAppToCrash()
+		{
+			RunningApp.WaitForElement("Success");
+		}
+#endif
 	}
 }
