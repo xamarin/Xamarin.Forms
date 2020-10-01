5dae23a8497eee0353fee015d8e892a3ca5fd9aa
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-10-01 10:43:58 +0200

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12315.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 12315, "[Bug] Button disappears when setting CornerRadius",
+		PlatformAffected.iOS)]
+	public partial class Issue12315 : TestContentPage
+	{
+		public Issue12315()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
