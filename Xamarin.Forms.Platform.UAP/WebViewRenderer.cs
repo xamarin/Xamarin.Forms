729b8c8f3a74be30f9aeb47ec1d1402b8ab282d4
Tim Johnson <tjohnson@aptos.com>
2020-10-01 23:49:28 -0400

    style and doc changes for #4720

--- Xamarin.Forms.Platform.UAP/WebViewRenderer.cs
+++ Xamarin.Forms.Platform.UAP/WebViewRenderer.cs
@@ -102,10 +102,10 @@ if(bases.length == 0){
 		}
 
 		/// <summary>
-		/// Override this to chose an execution mode other than the default of 'SameThread'
+		/// Set this in the constructor of a custom renderer to chose an execution mode other than the default of 'SameThread'
 		/// </summary>
 		/// <remarks>
-		/// In certain case (eg, frequent opening and closing of large websites) there may be 
+		/// This may be required in certain case (eg, frequent opening and closing of large websites) there may be 
 		/// a need to host the WebView in a different thread or process to avoid out of memory situations.
 		/// </remarks>
 		[DefaultValue(WebViewExecutionMode.SameThread)]
