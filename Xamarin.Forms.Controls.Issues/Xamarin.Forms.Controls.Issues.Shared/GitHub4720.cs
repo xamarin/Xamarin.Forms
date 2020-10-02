713ea5d2b70285527a493b4782c8e31a7458c9a8
Tim Johnson <tjohnson@aptos.com>
2020-10-01 23:45:37 -0400

    To address issue #4720: Add option for custom renderers to control the execution mode of the WebView control This is 'opt-in' as by default this commit will not change behavior of existing applciations.  to opt-in, people would need to set the ExecutionMode property in the constructor of their custom WebViewRenderer, like:
      public class MyWebViewRenderer : WebViewRenderer
      {
        public MyWebViewRenderer()
        {
          ExecutionMode = Windows.UI.Xaml.Controls.WebViewExecutionMode.SeparateProcess;
        }
      }
    
    When set as 'SeparateProcess', the memory allocated by the WebView itself is all handled in a sub-process, which ensures the main process never crashes from running out of memory here from doing things like opening and closing youtube every 5 seconds.  This behavior will likely crash when the WebView is in-process due to the huge amounts of memory that is required by that website.

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/GitHub4720.cs
@@ -0,0 +1,127 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using System.Threading;
+using System.Diagnostics;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms.Controls.GalleryPages;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.WebView)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 4720, "UWP: Webview: Memory Leak in WebView", PlatformAffected.UWP)]
+	public class GitHub4720 : TestNavigationPage
+	{
+		//shameless copy of the test case for Bugzilla39489.
+
+		protected override void Init()
+		{
+			PushAsync(new GitHub4720Content());
+		}
+
+#if UITEST
+		protected override bool Isolate => true;
+
+		[Test]
+		public async Task GitHub4720Test()
+		{
+			//4 iterations were enough to run out of memory before the fix.
+			int iterations = 10;
+
+			for (int n = 0; n < iterations; n++)
+			{
+				RunningApp.WaitForElement(q => q.Marked("New Page"));
+				RunningApp.Tap(q => q.Marked("New Page"));
+				RunningApp.WaitForElement(q => q.Marked("Close Page"));
+				await Task.Delay(2000);
+				RunningApp.Tap(q => q.Marked("Close Page"));
+			}
+		}
+
+#endif
+	}
+	public class GitHub4720WebPage : ContentPage
+	{
+		static int s_count;
+
+		public GitHub4720WebPage()
+		{
+			Interlocked.Increment(ref s_count);
+			Debug.WriteLine($"++++++++ GitHub4720WebPage : Constructor, count is {s_count}");
+
+			var label = new Label { Text = "Test case for GitHub issue #4720 -  sorry youtube." };
+
+			var button = new Button { Text = "Close Page" };
+			button.Clicked += Button_Clicked;
+
+			var wv = new WebView()
+			{				
+				Source = new UrlWebViewSource { Url = "https://www.youtube.com/" },
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				BackgroundColor = Color.Red
+				
+			};
+			Content = new StackLayout { Children = { label, button, wv } };
+		}
+		void Button_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PopAsync();
+		}
+
+		~GitHub4720WebPage()
+		{
+			Interlocked.Decrement(ref s_count);
+			Debug.WriteLine($"-------- GitHub4720WebPage: Destructor, count is {s_count}");
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class GitHub4720Content : ContentPage
+	{
+		static int s_count;
+
+		public GitHub4720Content()
+		{
+			Interlocked.Increment(ref s_count);
+			Debug.WriteLine($">>>>> GitHub4720Content GitHub4720Content : Constructor, count is {s_count}");
+
+			var button = new Button { Text = "New Page" };
+			button.Clicked += Button_Clicked;
+
+			var gcbutton = new Button { Text = "GC" };
+			gcbutton.Clicked += GCbutton_Clicked;
+
+			Content = new StackLayout { Children = { button, gcbutton } };
+		}
+
+		void GCbutton_Clicked(object sender, EventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine(">>>>>>>> Running Garbage Collection");
+			GarbageCollectionHelper.Collect();
+			System.Diagnostics.Debug.WriteLine($">>>>>>>> GC.GetTotalMemory = {GC.GetTotalMemory(true):n0}");
+		}
+
+		void Button_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new GitHub4720WebPage());
+		}
+
+		~GitHub4720Content()
+		{
+			Interlocked.Decrement(ref s_count);
+			Debug.WriteLine($">>>>> GitHub4720Content ~GitHub4720Content : Destructor, count is {s_count}");
+		}
+	}
+
+
+}
