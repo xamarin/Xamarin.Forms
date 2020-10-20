5809af26e3646fceeedae8f9ff6f2fb61afd8b6c
Domagoj Medo <domagoj.medo@moberg.is>
2020-10-20 11:58:41 +0200

    Map renderer build fix

--- Xamarin.Forms.Maps.Android/MapRenderer.cs
+++ Xamarin.Forms.Maps.Android/MapRenderer.cs
@@ -42,13 +42,6 @@ namespace Xamarin.Forms.Maps.Android
 			AutoPackage = false;
 		}
 
-		[Obsolete("This constructor is obsolete as of version 2.5. Please use MapRenderer(Context) instead.")]
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public MapRenderer()
-		{
-			AutoPackage = false;
-		}
-
 		protected Map Map => Element;
 
 		protected GoogleMap NativeMap;
