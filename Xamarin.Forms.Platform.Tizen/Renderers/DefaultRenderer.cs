95cbc89f8ff1e29a94f9ffde02406c7d3b8f4a42
JoonghyunCho <jh5.cho@samsung.com>
2020-10-21 13:11:48 +0900

    [Tizen] Updated Shapes default renderers

--- Xamarin.Forms.Platform.Tizen/Renderers/DefaultRenderer.cs
+++ Xamarin.Forms.Platform.Tizen/Renderers/DefaultRenderer.cs
@@ -17,85 +17,23 @@ namespace Xamarin.Forms.Platform.Tizen
 		}
 	}
 
-	// Following Shapes dummy renderers can be replaced to real renderers,
-	// when `InitializationOptions.UseSkiaSharp` value is set to true in an application.
-	// ```
-	// var option = new InitializationOptions(app)
-	// {
-	//         UseSkiaSharp = true
-	// };
-	// Forms.Init(option);
-	// ```
+	public class EllipseRenderer : ShapeRenderer { }
 
-	public class EllipseRenderer : VisualElementRenderer<Ellipse>
-	{
-		protected override void OnElementChanged(ElementChangedEventArgs<Ellipse> e)
-		{
-			if (NativeView == null)
-			{
-				var control = new ELayout(Forms.NativeParent);
-				SetNativeView(control);
-			}
-			base.OnElementChanged(e);
-		}
-	}
+	public class LineRenderer : ShapeRenderer { }
 
-	public class LineRenderer : VisualElementRenderer<Line>
-	{
-		protected override void OnElementChanged(ElementChangedEventArgs<Line> e)
-		{
-			if (NativeView == null)
-			{
-				var control = new ELayout(Forms.NativeParent);
-				SetNativeView(control);
-			}
-			base.OnElementChanged(e);
-		}
-	}
+	public class PathRenderer : ShapeRenderer { }
 
-	public class PathRenderer : VisualElementRenderer<Path>
-	{
-		protected override void OnElementChanged(ElementChangedEventArgs<Path> e)
-		{
-			if (NativeView == null)
-			{
-				var control = new ELayout(Forms.NativeParent);
-				SetNativeView(control);
-			}
-			base.OnElementChanged(e);
-		}
-	}
+	public class PolygonRenderer : ShapeRenderer { }
 
-	public class PolygonRenderer : VisualElementRenderer<Shapes.Polygon>
-	{
-		protected override void OnElementChanged(ElementChangedEventArgs<Shapes.Polygon> e)
-		{
-			if (NativeView == null)
-			{
-				var control = new ELayout(Forms.NativeParent);
-				SetNativeView(control);
-			}
-			base.OnElementChanged(e);
-		}
-	}
+	public class PolylineRenderer : ShapeRenderer { }
 
-	public class PolylineRenderer : VisualElementRenderer<Polyline>
-	{
-		protected override void OnElementChanged(ElementChangedEventArgs<Polyline> e)
-		{
-			if (NativeView == null)
-			{
-				var control = new ELayout(Forms.NativeParent);
-				SetNativeView(control);
-			}
-			base.OnElementChanged(e);
-		}
-	}
+	public class RectangleRenderer : ShapeRenderer { }
 
-	public class RectangleRenderer : VisualElementRenderer<Shapes.Rectangle>
+	public class ShapeRenderer : VisualElementRenderer<VisualElement>
 	{
-		protected override void OnElementChanged(ElementChangedEventArgs<Shapes.Rectangle> e)
+		protected override void OnElementChanged(ElementChangedEventArgs<VisualElement> e)
 		{
+			Log.Info("Use skia render mode (InitializationOptions.UseSkiaSharp=true) to use Shape.");
 			if (NativeView == null)
 			{
 				var control = new ELayout(Forms.NativeParent);
