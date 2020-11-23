08831cbf0b05b317ea43345f9cfc786c1e696f85
Rui Marinho <me@ruimarinho.net>
2020-11-23 18:27:14 +0000

    [Controls] Fix sample 12574

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12574.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12574.cs
@@ -121,11 +121,12 @@ namespace Xamarin.Forms.Controls.Issues
 			Title = "CarouselView Looping";
 			Items = new ObservableCollection<ModelIssue12574>();
 			LoadItemsCommand = new Command(() => ExecuteLoadItemsCommand());
-			RemoveItemsCommand = new Command(() => ExecuteRemoveItemsCommand());
+			RemoveItemsCommand = new Command(() => ExecuteRemoveItemsCommand(), () => Items.Count > 0);
 		}
 		void ExecuteRemoveItemsCommand()
 		{
 			Items.Remove(Items.Last());
+			RemoveItemsCommand.ChangeCanExecute();
 		}
 		void ExecuteLoadItemsCommand()
 		{
@@ -146,6 +147,7 @@ namespace Xamarin.Forms.Controls.Issues
 			finally
 			{
 				IsBusy = false;
+				RemoveItemsCommand.ChangeCanExecute();
 			}
 		}
 
