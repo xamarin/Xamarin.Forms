92d4c683962c02a9cba074bb673107249c1fce94
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-11-24 18:49:33 +0100

    Added UITest

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12079.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12079.cs
@@ -16,6 +16,8 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.Github, 12079, "SwipeView crash if Text not is set on SwipeItem", PlatformAffected.UWP)]
 	public class Issue12079 : TestContentPage
 	{
+		const string SwipeViewId = "SwipeViewId";
+
 		protected override void Init()
 		{
 			Title = "Issue 12079";
@@ -38,7 +40,10 @@ namespace Xamarin.Forms.Controls.Issues
 				IconImageSource = "calculator.png"
 			};
 
-			swipeItem.Invoked += (sender, e) => { DisplayAlert("SwipeView", "Invoked", "Ok"); };
+			swipeItem.Invoked += (sender, e) =>
+			{
+				DisplayAlert("Issue12079", "Invoked", "Ok");
+			};
 
 			var swipeItems = new SwipeItems { swipeItem };
 
@@ -60,16 +65,38 @@ namespace Xamarin.Forms.Controls.Issues
 
 			var swipeView = new SwipeView
 			{
+				AutomationId = SwipeViewId,
 				HeightRequest = 60,
 				WidthRequest = 300,
 				LeftItems = swipeItems,
 				Content = swipeContent
 			};
 
+			var result = new Label();
+
+			swipeView.SwipeEnded += (sender, args) =>
+			{
+				result.Text = "Success";
+			};
+
 			layout.Children.Add(instructions);
 			layout.Children.Add(swipeView);
+			layout.Children.Add(result);
 
 			Content = layout;
 		}
+
+#if UITEST
+		[Test]
+		[Category(UITestCategories.SwipeView)]
+		public void SwipeItemNoTextWindows()
+		{
+			RunningApp.WaitForElement(SwipeViewId);
+			RunningApp.SwipeLeftToRight(SwipeViewId);
+			RunningApp.Tap(SwipeViewId);
+			RunningApp.WaitForElement( c => c.Marked("Success"));
+			RunningApp.Screenshot ("The test has passed");
+		}
+#endif
 	}
 }
