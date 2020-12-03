280899b682e7683a290c4e2cb1980c9b3b7e2f1d
rachelkang <rachelkang@microsoft.com>
2020-12-03 16:10:43 -0500

    Add Issue3311.cs

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue3311.cs
@@ -0,0 +1,66 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 3311, "Issue Description", PlatformAffected.Default)]
+	public class Issue3311 : TestContentPage 
+	{
+		protected override void Init()
+		{
+
+			var formattedString = new FormattedString();
+			formattedString.Spans.Add(new Span { Text = "RTL formatted text" });
+
+			Content = new StackLayout()
+			{
+				Margin = 20,
+
+				Children =
+				{
+					new Label()
+					{
+						AutomationId = "Issue3311Label",
+						Text = "Issue 3311: RTL is not working for Label.FormattedText on iOS",
+						HorizontalTextAlignment = TextAlignment.Center,
+						FontSize = 20
+					},
+					new Label()
+					{
+						AutomationId = "Issue3311NormalTextLabel",
+						FlowDirection = FlowDirection.RightToLeft,
+						Text = "RTL normal text"
+					},
+					new Label()
+					{
+						AutomationId = "Issue3311FormattedTextLabel",
+						FlowDirection = FlowDirection.RightToLeft,
+						FormattedText = formattedString
+					}
+				}
+			};
+		}
+
+#if UITEST
+		[Test]
+		public void Issue1Test()
+		{
+			RunningApp.WaitForElement("Issue1Label");
+			// Delete this and all other UITEST sections if there is no way to automate the test. Otherwise, be sure to rename the test and update the Category attribute on the class. Note that you can add multiple categories.
+			RunningApp.Screenshot("I am at Issue1");
+			RunningApp.WaitForElement(q => q.Marked("Issue1Label"));
+			RunningApp.Screenshot("I see the Label");
+		}
+#endif
+	}
+}
