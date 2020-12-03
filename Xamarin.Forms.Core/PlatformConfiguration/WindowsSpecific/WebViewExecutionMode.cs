6840c162bd36a70dae3ff03a42d8a1b35e8ccce7
Shane Neuville <shneuvil@microsoft.com>
2020-12-03 12:07:52 -0600

    - fix tabs

--- Xamarin.Forms.Core/PlatformConfiguration/WindowsSpecific/WebViewExecutionMode.cs
+++ Xamarin.Forms.Core/PlatformConfiguration/WindowsSpecific/WebViewExecutionMode.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Xamarin.Forms.PlatformConfiguration.WindowsSpecific
 {
 	public enum WebViewExecutionMode
-	{       
+	{
 		SameThread = 0,
 		SeparateThread = 1,
 		SeparateProcess = 2
