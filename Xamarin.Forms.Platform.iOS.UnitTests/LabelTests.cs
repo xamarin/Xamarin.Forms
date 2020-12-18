bf9cd2fb938e8ebe62b87f47c16d922acd8a8c5e
rachelkang <rachelkang@microsoft.com>
2020-12-18 09:48:25 -0500

    Add relevant RTL FormattedText test

---
+++ Xamarin.Forms.Platform.iOS.UnitTests/LabelTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using UIKit;
+
+namespace Xamarin.Forms.Platform.iOS.UnitTests
+{
+	[TestFixture]
+	public class LabelTests : PlatformTestFixture
+	{
+		[Test, Category("RTL"), Category("Label")]
+		[Description("RTL should work on Label with FormattedText")]
+		public async Task LabelFormattedTextRTLWorks()
+		{
+			var formattedString = new FormattedString();
+			formattedString.Spans.Add(new Span { Text = "RTL formatted text" });
+			var label = new Label { FormattedText = formattedString };
+			label.FlowDirection = FlowDirection.RightToLeft;
+			var expected = UITextAlignment.Right;
+			var actual = await GetControlProperty(label, uiLabel => uiLabel.TextAlignment);
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+	}
+}
