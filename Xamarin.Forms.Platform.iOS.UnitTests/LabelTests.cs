dab4abdefd95b543fada759de39448cc269d18c5
rachelkang <rachelkang@microsoft.com>
2020-12-18 16:58:21 -0500

    Add LabelTest for RTL Label with LineHeight

--- Xamarin.Forms.Platform.iOS.UnitTests/LabelTests.cs
+++ Xamarin.Forms.Platform.iOS.UnitTests/LabelTests.cs
@@ -9,12 +9,24 @@ namespace Xamarin.Forms.Platform.iOS.UnitTests
 	{
 		[Test, Category("RTL"), Category("Label")]
 		[Description("RTL should work on Label with FormattedText")]
-		public async Task LabelFormattedTextRTLWorks()
+		public async Task RTLLabelWithFormattedTextWorks()
 		{
 			var formattedString = new FormattedString();
 			formattedString.Spans.Add(new Span { Text = "RTL formatted text" });
 			var label = new Label { FormattedText = formattedString };
 			label.FlowDirection = FlowDirection.RightToLeft;
+			var expected = UITextAlignment.Right;
+			var actual = await GetControlProperty(label, uiLabel => uiLabel.TextAlignment);
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		[Test, Category("RTL"), Category("Label")]
+		[Description("RTL should work on Label with LineHeight")]
+		public async Task RTLLabelWithLineHeightWorks()
+		{
+			var label = new Label { Text = "RTL text with LineHeight" };
+			label.FlowDirection = FlowDirection.RightToLeft;
+			label.LineHeight = 3;
 			var expected = UITextAlignment.Right;
 			var actual = await GetControlProperty(label, uiLabel => uiLabel.TextAlignment);
 			Assert.That(actual, Is.EqualTo(expected));
