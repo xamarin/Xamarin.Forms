a88403ab980bb5db4a9ab0f5fd656bc09cf8b7e3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-12-18 12:35:59 +0100

    Fix the issue

--- Xamarin.Forms.Platform.iOS/Renderers/SwipeViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/SwipeViewRenderer.cs
@@ -153,8 +153,14 @@ namespace Xamarin.Forms.Platform.iOS
 			if (Bounds.X < 0 || Bounds.Y < 0)
 				Bounds = new CGRect(0, 0, Bounds.Width, Bounds.Height);
 
-			if (_contentView != null && _contentView.Frame.IsEmpty)
-				_contentView.Frame = Bounds;
+			if (_contentView != null)
+			{
+				if (_contentView.Frame.IsEmpty)
+					_contentView.Frame = Bounds;
+
+				if (_originalBounds == CGRect.Empty)
+					_originalBounds = _contentView.Frame;
+			}
 		}
 
 		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -517,9 +523,6 @@ namespace Xamarin.Forms.Platform.iOS
 
 			if (items == null || items.Count == 0)
 				return;
-
-			if (_originalBounds == CGRect.Empty)
-				_originalBounds = _contentView.Frame;
 
 			int i = 0;
 			float previousWidth = 0;
@@ -1035,7 +1038,7 @@ namespace Xamarin.Forms.Platform.iOS
 			_swipeItems.Clear();
 			_swipeThreshold = 0;
 			_swipeOffset = 0;
-	
+
 			if (_actionView != null)
 			{
 				_actionView.RemoveFromSuperview();
