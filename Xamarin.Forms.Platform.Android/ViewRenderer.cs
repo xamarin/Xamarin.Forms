cb4a35569f9048f84e110841acbece7ea1deb1d1
shane <shneuvil@microsoft.com>
2020-12-29 11:04:27 -0600

    - fix invalid AutomationId with cell reuse

--- Xamarin.Forms.Platform.Android/ViewRenderer.cs
+++ Xamarin.Forms.Platform.Android/ViewRenderer.cs
@@ -47,6 +47,7 @@ namespace Xamarin.Forms.Platform.Android
 		}
 
 		ViewGroup _container;
+		bool _defaultAutomationSet;
 		string _defaultContentDescription;
 		bool? _defaultFocusable;
 		ImportantForAccessibility? _defaultImportantForAccessibility;
@@ -214,6 +215,15 @@ namespace Xamarin.Forms.Platform.Android
 			effect.SetControl(Control);
 		}
 
+		void SetupAutomationDefaults()
+		{
+			if (!_defaultAutomationSet)
+			{
+				_defaultAutomationSet = true;
+				AutomationPropertiesProvider.SetupDefaults(ControlUsedForAutomation, ref _defaultContentDescription, ref _defaultHint);
+			}
+		}
+
 		protected override void SetAutomationId(string id)
 		{
 			if (Control == null)
@@ -222,8 +232,21 @@ namespace Xamarin.Forms.Platform.Android
 				return;
 			}
 
+			SetupAutomationDefaults();
 			ContentDescription = id + "_Container";
 			AutomationPropertiesProvider.SetAutomationId(ControlUsedForAutomation, Element, id);
+		}
+
+		private protected void SetContentDescription(bool includeHint)
+		{
+			SetupAutomationDefaults();
+
+			if(includeHint)
+				AutomationPropertiesProvider.SetContentDescription(
+					ControlUsedForAutomation, Element, _defaultContentDescription, _defaultHint);
+			else
+				AutomationPropertiesProvider.SetBasicContentDescription(
+					ControlUsedForAutomation, Element, _defaultContentDescription);
 		}
 
 		protected override void SetContentDescription()
@@ -234,8 +257,7 @@ namespace Xamarin.Forms.Platform.Android
 				return;
 			}
 
-			AutomationPropertiesProvider.SetContentDescription(
-				ControlUsedForAutomation, Element, ref _defaultContentDescription, ref _defaultHint);
+			SetContentDescription(true);
 		}
 
 		protected override void SetFocusable()
