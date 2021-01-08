442fdcca6350222b97ccf82ace611c8c6ae19222
E.Z. Hart <hartez@gmail.com>
2021-01-08 11:26:49 -0700

    Restore wheels date/time picker styles in 5.0; fixes #13331

---
+++ Xamarin.Forms.Platform.iOS.UnitTests/DatePickerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Platform.iOS.UnitTests
+{
+	[TestFixture]
+	public class DatePickerTests : PlatformTestFixture 
+	{
+		[Test, Category("DatePicker")]
+		[Description("DatePicker should be using wheels-style picker")]
+		public async Task UsingWheelPicker() 
+		{
+			var datePicker = new DatePicker();
+			var expected = UIKit.UIDatePickerStyle.Wheels;
+			var actual = await GetControlProperty(datePicker, uiDatePicker => uiDatePicker.PreferredDatePickerStyle);
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+	}
+}
---
+++ Xamarin.Forms.Platform.iOS.UnitTests/TimePickerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Platform.iOS.UnitTests
+{
+	[TestFixture]
+	public class TimePickerTests : PlatformTestFixture
+	{
+		[Test, Category("TimePicker")]
+		[Description("TimePicker should be using wheels-style picker")]
+		public async Task UsingWheelPicker()
+		{
+			var timePicker = new TimePicker();
+			var expected = UIKit.UIDatePickerStyle.Wheels;
+			var actual = await GetControlProperty(timePicker, uiTimePicker => uiTimePicker.PreferredDatePickerStyle);
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+	}
+}
