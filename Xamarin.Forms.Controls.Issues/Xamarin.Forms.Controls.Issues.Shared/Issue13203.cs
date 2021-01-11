f8a835f7e545e474128b7c35b4d5c2153e3ca739
E.Z. Hart <hartez@gmail.com>
2021-01-11 16:21:58 -0700

    Automated test

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue13203.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 13203, "[Bug] [iOS] CollectionView does not bind to items if `IsVisible=False`", PlatformAffected.iOS)]
+#if UITEST
+	[NUnit.Framework.Category(UITestCategories.CollectionView)]
+#endif
+	public class Issue13203 : TestContentPage
+	{
+		const string RunTest = "RunTest";
+		const string Success = "Success";
+
+		protected override void Init()
+		{
+			var cv = new CollectionView
+			{
+				IsVisible = false,
+
+				ItemTemplate = new DataTemplate(() =>
+				{
+					var label = new Label();
+					label.SetBinding(Label.TextProperty, new Binding(nameof(Item.Text)));
+					return label;
+				})
+			};
+
+			var source = new List<Item> { new Item { Text = Success } };
+			cv.ItemsSource = source;
+
+			var button = new Button { Text = "Set IsVisible to True", AutomationId = RunTest };
+
+			var layout = new StackLayout
+			{
+				Children = { button, cv }
+			};
+
+			button.Clicked += (sender, args) => {
+				cv.IsVisible = true;
+			};
+
+			Content = layout;
+
+			Appearing += (sender, args) => { cv.IsVisible = true; };
+		}
+
+		class Item
+		{
+			public string Text { get; set; }
+		}
+
+#if UITEST
+		[Test]
+		public void SettingGroupedCollectionViewItemSourceNullShouldNotCrash()
+		{
+			RunningApp.WaitForElement(RunTest);
+			RunningApp.Tap(RunTest);
+			RunningApp.WaitForElement(Success);
+		}
+#endif
+	}
+}
+
