128a8912cb9b8f1ebd351c296832402203c67cd6
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-02-11 11:13:47 +0100

    Maintain Editor BackgroundColor when focused on UWP

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue13573.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 13573, "[Bug] Editor PlaceholderColor and BackgroundColor incorrect on UWP after upgrading to Xamarin.Forms 4.8 (from 4.5)", PlatformAffected.UWP)]
+	public partial class Issue13573 : TestContentPage
+	{
+		public Issue13573()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+		protected override void Init()
+		{
+		}
+	}
+}
