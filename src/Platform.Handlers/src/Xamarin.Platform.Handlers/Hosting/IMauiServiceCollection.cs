81279f01989dbb3797ca7b4b50548fac23a20627
Rui Marinho <me@ruimarinho.net>
2021-02-17 15:48:20 +0000

    [Hosting] Fix null reference usage on .net6

--- src/Platform.Handlers/src/Xamarin.Platform.Handlers/Hosting/IMauiServiceCollection.cs
+++ src/Platform.Handlers/src/Xamarin.Platform.Handlers/Hosting/IMauiServiceCollection.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Xamarin.Platform.Hosting
 {
 	public interface IMauiServiceCollection
-		: IServiceCollection, IDictionary<Type, Func<IServiceProvider, object>>
+		: IServiceCollection, IDictionary<Type, Func<IServiceProvider, object?>?>
 	{
 	}
 }
