e7c0b7399c3cf987a2bd6ccdc59cc93d81f77fcb
Rui Marinho <me@ruimarinho.net>
2021-02-19 15:30:23 +0000

    Cleanup

--- src/Forms/samples/Maui.Controls.Sample.Droid/Properties/AssemblyInfo.cs
+++ src/Forms/samples/Maui.Controls.Sample.Droid/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("Sample.Droid")]
+[assembly: AssemblyTitle ("Maui.Controls.Sample.Droid")]
 [assembly: AssemblyDescription ("")]
-[assembly: AssemblyCopyright ("Copyright ©  2018")]
+[assembly: AssemblyCopyright ("Copyright © 2021")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 [assembly: ComVisible (false)]
--- src/Platform.Handlers/samples/Sample.Droid/Services/DroidTextService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sample.Services;
-
-namespace Sample.Droid.Services
-{
-	public class DroidTextService : ITextService
-	{
-		public string GetText() => "Hello From Android";
-	}
-}
