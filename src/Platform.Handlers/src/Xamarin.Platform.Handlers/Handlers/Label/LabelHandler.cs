71f0dc051a0c8ae16036f327ea94954402e582cb
E.Z. Hart <hartez@gmail.com>
2021-02-19 14:41:22 -0700

    Fix rebase errors and sample app

--- src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Label/LabelHandler.cs
+++ src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Label/LabelHandler.cs
@@ -4,23 +4,9 @@
 	{
 		public static PropertyMapper<ILabel, LabelHandler> LabelMapper = new PropertyMapper<ILabel, LabelHandler>(ViewHandler.ViewMapper)
 		{
-			[nameof(ILabel.Color)] = MapColor,
+			[nameof(ILabel.TextColor)] = MapTextColor,
 			[nameof(ILabel.Text)] = MapText,
 		};
-
-		public static void MapColor(LabelHandler handler, ILabel Label)
-		{
-		}
-
-		public static void MapText(LabelHandler handler, ILabel label)
-		{
-			handler.TypedNativeView?.UpdateText(label);
-		}
-#if MONOANDROID
-		protected override NativeView CreateNativeView() => new NativeView(this.Context);
-#else
-		protected override NativeView CreateNativeView() => new NativeView();
-#endif
 
 		public LabelHandler() : base(LabelMapper)
 		{
