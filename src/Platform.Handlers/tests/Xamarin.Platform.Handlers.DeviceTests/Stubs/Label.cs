377969c8a404b272be64bb32a3866138add95982
E.Z. Hart <hartez@gmail.com>
2021-02-19 17:07:17 -0700

    Fix stubs

--- src/Platform.Handlers/tests/Xamarin.Platform.Handlers.DeviceTests/Stubs/Label.cs
+++ src/Platform.Handlers/tests/Xamarin.Platform.Handlers.DeviceTests/Stubs/Label.cs
@@ -6,6 +6,12 @@ namespace Xamarin.Platform.Handlers.DeviceTests.Stubs
 	{
 		public string Text { get; set; }
 
-		public Color TextColor { get; set; }		
+		public Color TextColor { get; set; }
+
+		public FontAttributes FontAttributes => throw new System.NotImplementedException();
+
+		public string FontFamily => throw new System.NotImplementedException();
+
+		public double FontSize => throw new System.NotImplementedException();
 	}
 }
