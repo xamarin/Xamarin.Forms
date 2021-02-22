f9479234807207134ffa711d3782d4080bdd8461
Shane Neuville <shneuvil@microsoft.com>
2021-02-22 12:22:22 -0600

    - fix link references

---
+++ src/Platform.Renderers/src/Maui.Controls.Compatibility/AppHostBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xamarin.Forms;
+using Xamarin.Platform;
+using Xamarin.Platform.Handlers;
+using Xamarin.Platform.Hosting;
+
+namespace Maui.Controls.Compatibility
+{
+	public static class AppHostBuilderExtensions
+	{
+
+		public static IAppHostBuilder RegisterCompatibilityRenderer<TControlType, TMauiType, TRenderer>(this IAppHostBuilder builder)
+			where TMauiType : IFrameworkElement
+		{
+			// register renderer with old registrar so it can get shimmed
+			// This will move to some extension method
+			Xamarin.Forms.Internals.Registrar.Registered.Register(
+				typeof(TControlType),
+				typeof(TRenderer));
+
+			builder.RegisterHandler<TMauiType, RendererToHandlerShim>();
+			return builder;
+		}
+	}
+}
