5f06e26da5486d91c8102d008e8558fe1595ebc5
Jonathan Dick <jondick@gmail.com>
2021-02-24 00:25:20 -0500

    Rename all the things!

---
+++ src/Compatibility/Maps/src/iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Internals;
+using Microsoft.Maui.Controls.Compatibility.Maps;
+using Microsoft.Maui.Controls.Compatibility.Maps.iOS;
+
+[assembly: ExportRenderer(typeof(Map), typeof(MapRenderer))]
+[assembly: Preserve]
