cf0d655e29ab7fd73e54b73b740c1c03a4c815c7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-02-25 18:22:31 +0100

    CheckBoxHandlers

--- src/Controls/samples/Controls.Sample/Pages/MainPage.cs
+++ src/Controls/samples/Controls.Sample/Pages/MainPage.cs
@@ -8,7 +8,8 @@ namespace Maui.Controls.Sample.Pages
 
 	public class MainPage : ContentPage, IPage
 	{
-		MainPageViewModel _viewModel;
+		readonly MainPageViewModel _viewModel;
+
 		public MainPage() : this(App.Current.Services.GetService<MainPageViewModel>())
 		{
 
@@ -39,7 +40,13 @@ namespace Maui.Controls.Sample.Pages
 			horizontalStack.Add(new Label { Text = "And these buttons are in a HorizontalStackLayout" });
 
 			verticalStack.Add(horizontalStack);
+
+			verticalStack.Add(new CheckBox());
+			verticalStack.Add(new CheckBox { BackgroundColor = Color.LightPink });
+			verticalStack.Add(new CheckBox { IsChecked = true, Color = Color.Aquamarine });
+
 			verticalStack.Add(new Slider());
+
 			verticalStack.Add(new Switch());
 			verticalStack.Add(new Switch() { OnColor = Color.Green });
 			verticalStack.Add(new Switch() { ThumbColor = Color.Yellow });
---
+++ src/Core/tests/DeviceTests/Handlers/CheckBox/CheckBoxHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Handlers;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class CheckBoxHandlerTests : HandlerTestBase<CheckBoxHandler>
+	{
+		[Theory(DisplayName = "[CheckBoxHandler] IsChecked Initializes Correctly")]
+		[InlineData(true)]
+		[InlineData(false)]
+		public async Task IsCheckedInitializesCorrectly(bool isChecked)
+		{
+			var checkBoxStub = new CheckBoxStub()
+			{
+				IsChecked = isChecked
+			};
+
+			await ValidatePropertyInitValue(checkBoxStub, () => checkBoxStub.IsChecked, GetNativeIsChecked, checkBoxStub.IsChecked);
+		}
+
+		[Fact(DisplayName = "[CheckBoxHandler] Color Updates Correctly")]
+		public async Task ColorUpdatesCorrectly()
+		{
+			var checkBoxStub = new CheckBoxStub()
+			{
+				Color = Color.Red,
+				IsChecked = true
+			};
+
+			await ValidateColor(checkBoxStub, Color.Red, () => checkBoxStub.Color = Color.Red);
+		}
+	}
+}
