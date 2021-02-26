4dd3a3202426ad773d640d390cdb8db8c704701c
Jonathan Dick <jondick@gmail.com>
2021-02-26 18:27:29 -0500

    Add resizetizer, add params to package script

--- .nuspec/package.ps1
+++ .nuspec/package.ps1
@@ -1,6 +1,10 @@
-& dotnet pack $PSScriptRoot\..\src\Core\src\Core-net6.csproj
-& dotnet pack $PSScriptRoot\..\src\Controls\src\Xaml\Controls.Xaml-net6.csproj
-& dotnet pack $PSScriptRoot\..\src\Controls\src\Core\Controls.Core-net6.csproj
-& dotnet pack $PSScriptRoot\..\src\Controls\src\Build.Tasks\Controls.Build.Tasks-net6.csproj
+param ($packageVersion, $configuration)
 
-& dotnet pack $PSScriptRoot\..\.nuspec\Microsoft.Maui.Package.csproj
+& dotnet pack $PSScriptRoot\..\src\Core\src\Core-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion
+& dotnet pack $PSScriptRoot\..\src\Controls\src\Xaml\Controls.Xaml-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion
+& dotnet pack $PSScriptRoot\..\src\Controls\src\Core\Controls.Core-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion
+& dotnet pack $PSScriptRoot\..\src\Controls\src\Build.Tasks\Controls.Build.Tasks-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion
+
+& dotnet pack $PSScriptRoot\..\src\SingleProject\Resizetizer\src\Resizetizer.csproj -c:$configuration -p:PackageVersion=$packageVersion
+
+& dotnet pack $PSScriptRoot\..\.nuspec\Microsoft.Maui.Package.csproj -c:$configuration -p:PackageVersion=$packageVersion
