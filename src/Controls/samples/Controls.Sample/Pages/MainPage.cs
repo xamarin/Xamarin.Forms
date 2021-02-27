0b71601d2b5003e7d1f60bd04824e3760ba01ad2
Shane Neuville <shneuvil@microsoft.com>
2021-02-26 18:13:21 -0600

    Update MainPage.cs

--- src/Controls/samples/Controls.Sample/Pages/MainPage.cs
+++ src/Controls/samples/Controls.Sample/Pages/MainPage.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Maui.Controls.Sample.Pages
 {
 
-	public class MainPage : ContentPage, IPage, IView
+	public class MainPage : ContentPage, IPage
 	{
 		MainPageViewModel _viewModel;
 		public MainPage() : this(App.Current.Services.GetService<MainPageViewModel>())
