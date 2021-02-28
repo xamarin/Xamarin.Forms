3095f97bdb01fb7f83a19e629c769fcd135f4573
Matthew Leibowitz <mattleibow@live.com>
2021-02-28 09:08:13 +0200

    Clean this up

--- src/Compatibility/Core/src/iOS/Extensions/FontExtensions.cs
+++ src/Compatibility/Core/src/iOS/Extensions/FontExtensions.cs
@@ -9,7 +9,7 @@ namespace Microsoft.Maui.Controls.Compatibility.Platform.iOS
 			=> Forms.FontManager.GetFont(self);
 
 		internal static UIFont ToUIFont(this IFontElement self)
-			=> Forms.FontManager.GetFont(self);
+			=> Forms.FontManager.GetFont(Font.OfSize(self.FontFamily, self.FontSize).WithAttributes(self.FontAttributes));
 
 		internal static bool IsDefault(this IFontElement self)
 			=> self.FontFamily == null && self.FontSize == Device.GetNamedSize(NamedSize.Default, typeof(Label), true) && self.FontAttributes == FontAttributes.None;
