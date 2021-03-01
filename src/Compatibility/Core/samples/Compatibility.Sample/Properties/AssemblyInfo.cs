b8cbaaf74e9da5b37290ef9e92882dac76dd4f92
Matthew Leibowitz <mattleibow@live.com>
2021-03-01 19:41:15 +0200

    remove compat samples

--- src/Compatibility/Core/samples/Compatibility.Sample/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Xaml;
-
-//[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-[assembly: ExportFont("Dokdo-Regular.ttf", Alias = "Cool Writing Font")]
