adf92d8d134ab7f2fc4179ad35cfa89820e3bf95
Matthew Leibowitz <mattleibow@live.com>
2021-03-01 22:49:31 +0200

    no symbols for build tasks and the head

--- .nuspec/package.ps1
+++ .nuspec/package.ps1
@@ -5,8 +5,8 @@ param ($packageVersion, $configuration)
 & dotnet pack $PSScriptRoot\..\src\Controls\src\Core\Controls.Core-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 # no symbols for build tasks
-& dotnet pack $PSScriptRoot\..\src\Controls\src\Build.Tasks\Controls.Build.Tasks-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion
-& dotnet pack $PSScriptRoot\..\src\SingleProject\Resizetizer\src\Resizetizer.csproj -c:$configuration -p:PackageVersion=$packageVersion
+& dotnet pack $PSScriptRoot\..\src\Controls\src\Build.Tasks\Controls.Build.Tasks-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=false
+& dotnet pack $PSScriptRoot\..\src\SingleProject\Resizetizer\src\Resizetizer.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=false
 
 # head package has no code to have symbols for
-& dotnet pack $PSScriptRoot\..\src\Package\Package-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion
+& dotnet pack $PSScriptRoot\..\src\Package\Package-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=false
