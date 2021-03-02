2ff5239b286eb8c79b342187e9778da2797d17d8
Matthew Leibowitz <mattleibow@live.com>
2021-03-02 05:39:07 +0200

    Try this

--- .nuspec/package.ps1
+++ .nuspec/package.ps1
@@ -1,10 +1,10 @@
-param ($packageVersion, $configuration)
+param ($configuration)
 
-& dotnet pack $PSScriptRoot\..\src\Core\src\Core-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
-& dotnet pack $PSScriptRoot\..\src\Controls\src\Xaml\Controls.Xaml-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
-& dotnet pack $PSScriptRoot\..\src\Controls\src\Core\Controls.Core-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+& dotnet pack $PSScriptRoot\..\src\Core\src\Core-net6.csproj -c:$configuration -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+& dotnet pack $PSScriptRoot\..\src\Controls\src\Xaml\Controls.Xaml-net6.csproj -c:$configuration -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+& dotnet pack $PSScriptRoot\..\src\Controls\src\Core\Controls.Core-net6.csproj -c:$configuration -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
-& dotnet pack $PSScriptRoot\..\src\Controls\src\Build.Tasks\Controls.Build.Tasks-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
-& dotnet pack $PSScriptRoot\..\src\SingleProject\Resizetizer\src\Resizetizer.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+& dotnet pack $PSScriptRoot\..\src\Controls\src\Build.Tasks\Controls.Build.Tasks-net6.csproj -c:$configuration -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+& dotnet pack $PSScriptRoot\..\src\SingleProject\Resizetizer\src\Resizetizer.csproj -c:$configuration -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
-& dotnet pack $PSScriptRoot\..\src\Package\Package-net6.csproj -c:$configuration -p:PackageVersion=$packageVersion -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+& dotnet pack $PSScriptRoot\..\src\Package\Package-net6.csproj -c:$configuration -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
