e2f721446c92dde5030f8e4f17464ae129f1f0ff
Matthew Leibowitz <mattleibow@live.com>
2021-03-03 07:43:39 +0200

    maybe

--- .nuspec/package.ps1
+++ .nuspec/package.ps1
@@ -1,4 +1,4 @@
 param ($configuration)
 
-& dotnet restore $PSScriptRoot\..\Microsoft.Maui-net6.sln -c:$configuration
+& dotnet restore $PSScriptRoot\..\Microsoft.Maui-net6.sln
 & dotnet pack $PSScriptRoot\..\Microsoft.Maui-net6.sln -c:$configuration -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -bl:$PSScriptRoot\..\artifacts\maui.binlog
