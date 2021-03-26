6f06b491c6d6900ba2f0c222c74cae2adf461078
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-26 14:53:55 +0100

    Fix exception deleting items from BindableLayout using SwipeView

--- Xamarin.Forms.Platform.Android/Renderers/SwipeViewRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/SwipeViewRenderer.cs
@@ -441,7 +441,7 @@ namespace Xamarin.Forms.Platform.Android
 				case MotionEventActions.Up:
 					handled = HandleTouchInteractions(GestureStatus.Completed, point);
 
-					if (Parent == null)
+					if (_isDisposed || Parent == null)
 						break;
 
 					Parent.RequestDisallowInterceptTouchEvent(false);
@@ -457,7 +457,7 @@ namespace Xamarin.Forms.Platform.Android
 				case MotionEventActions.Cancel:
 					handled = HandleTouchInteractions(GestureStatus.Canceled, point);
 
-					if (Parent == null)
+					if (_isDisposed || Parent == null)
 						break;
 
 					Parent.RequestDisallowInterceptTouchEvent(false);
