0a512fda8d76d3bf9a6479f3e38d3a7fada27314
Axemasta <33064621+Axemasta@users.noreply.github.com>
2021-04-05 20:00:43 +0100

    Added Issue To Control Gallery
    Added the issue PR to the control gallery, mainly so I can work on the control and have a fast reference

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12720.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.WebView)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 12720, "WebView Navigating Does Not Support Async Cancellation", PlatformAffected.All)]
+	public partial class Issue12720 : TestContentPage
+	{
+		public Issue12720()
+		{
+#if APP
+			InitializeComponent();
+			BindingContext = this;
+#endif
+		}
+
+		protected override void Init()
+		{
+			
+		}
+	}
+}
