86db84805ebfb5a2e36d064a25086782b8dc4a69
Shane Neuville <shneuvil@microsoft.com>
2021-07-05 09:42:25 -0700

    Fix iOS so if you remove more than one page it's able to remove them successfully (#14383)
    * Fix iOS Shell when removing multiple views
    
    * - ui tests and fix navigate args
    
    * - improve logic
    
    * Update ShellSection.cs
    
    * - update UI Tests

--- Xamarin.Forms.Core/Shell/ShellNavigationManager.cs
+++ Xamarin.Forms.Core/Shell/ShellNavigationManager.cs
@@ -11,7 +11,7 @@ namespace Xamarin.Forms
 		readonly Shell _shell;
 		ShellNavigatedEventArgs _accumulatedEvent;
 		bool _accumulateNavigatedEvents;
-
+		public bool AccumulateNavigatedEvents => _accumulateNavigatedEvents;
 		public event EventHandler<ShellNavigatedEventArgs> Navigated;
 		public event EventHandler<ShellNavigatingEventArgs> Navigating;
 
@@ -166,6 +166,7 @@ namespace Xamarin.Forms
 				await _shell.CurrentItem.CurrentItem.GoToAsync(navigationRequest, queryData, animate, isRelativePopping);
 			}
 
+			(_shell as IShellController).UpdateCurrentState(source);
 			_accumulateNavigatedEvents = false;
 
 			// this can be null in the event that no navigation actually took place!
