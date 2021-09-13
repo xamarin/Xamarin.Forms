7ed4f25abef5b5c675053f9635ce477f45008924
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-09-13 13:44:30 +0200

    Add sample to validate the issue

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue13371.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.CollectionView)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 13371,
+		"[Bug] CollectionView Header vs EmptyView conflict",
+		PlatformAffected.iOS)]
+	public partial class Issue13371 : TestContentPage
+	{
+		public Issue13371()
+		{
+#if APP
+			InitializeComponent();
+			BindingContext = this;
+#endif
+		}
+
+		public ObservableCollection<string> Data { get; private set; }
+
+		protected override void Init()
+		{
+			Data = new ObservableCollection<string>
+			{
+				"Item 1",
+				"Item 2",
+				"Item 3"
+			};
+		}
+	}
+}
