0e01e8d44ed2f13a15c2a3e34ca36c7e7495c445
Afshin Arani <afshin@arani.dev>
2021-09-23 17:15:05 +0200

    Added test in controls gallery for #8843
    Co-authored-by: Retheesh <sumod8898@gmail.com>

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8843.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.Navigation)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 8843, "[Bug] [macOS] Fix back button for nested nav pages",
+		PlatformAffected.macOS)]
+	public partial class Issue8843 : TestContentPage
+	{
+		public Issue8843()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		private void UpdateLabel(Label label)
+		{
+			var tapGestureRecognizer = new TapGestureRecognizer();
+			tapGestureRecognizer.Tapped += (_s, _e) =>
+			{
+				var page2 = new Issue8843_Page2();
+				SwitchToNewPage(this, page2, true);
+			};
+			label.GestureRecognizers.Add(tapGestureRecognizer);
+		}
+
+		protected override void Init()
+		{
+			Device.BeginInvokeOnMainThread(() =>
+			{
+				var mainLayout = this.FindByName<StackLayout>("mainLayout");
+				var theLabel = mainLayout.FindByName<Label>("theLabel");
+				UpdateLabel(theLabel);
+
+				// workaround for bug https://github.com/xamarin/Xamarin.Forms/issues/9526
+				theLabel.TextColor = Color.Black;
+			});
+		}
+
+		private void SwitchToNewPage(Page currentPage, Page newPage, bool navBar)
+		{
+			NavigationPage.SetHasNavigationBar(newPage, navBar);
+			var navPage = new NavigationPage(newPage);
+			NavigationPage.SetHasNavigationBar(navPage, navBar);
+			Device.BeginInvokeOnMainThread(() => DoubleCheckCompletionNonGeneric(currentPage.Navigation.PushAsync(navPage))
+			);
+		}
+		private void DoubleCheckCompletionNonGeneric(Task task)
+		{
+			task.ContinueWith((Task t) => { MaybeCrash(false, t.Exception); }, TaskContinuationOptions.OnlyOnFaulted);
+		}
+
+		private void MaybeCrash(bool canBeCancelled, Exception ex)
+		{
+			if (ex == null)
+			{
+				return;
+			}
+			else
+			{
+				Device.BeginInvokeOnMainThread(() => { throw ex; });
+			}
+		}
+	}
+}
---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8843_Page2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	public partial class Issue8843_Page2 : TestContentPage
+	{
+
+		public Issue8843_Page2()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
