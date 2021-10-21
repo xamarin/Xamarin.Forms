2b16e1b8e267f64b171ea4a01eb4579e27806be2
Gerald Versluis <gerald@verslu.is>
2021-10-21 13:31:54 +0200

    Update SafeShellTabBarAppearanceTracker.cs

--- Xamarin.Forms.Platform.iOS/Renderers/SafeShellTabBarAppearanceTracker.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/SafeShellTabBarAppearanceTracker.cs
@@ -68,8 +68,15 @@ namespace Xamarin.Forms.Platform.iOS
 
 			if (!tabBarTitleColor.IsDefault)
 			{
+				// Update colors for all variations of the appearance to also make it work for iPads, etc.
 				tabBarAppearance.StackedLayoutAppearance.Normal.TitleTextAttributes = tabBarAppearance.StackedLayoutAppearance.Selected.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarTitleColor.ToUIColor() };
 				tabBarAppearance.StackedLayoutAppearance.Normal.IconColor = tabBarAppearance.StackedLayoutAppearance.Selected.IconColor = tabBarTitleColor.ToUIColor();
+
+				tabBarAppearance.InlineLayoutAppearance.Normal.TitleTextAttributes = tabBarAppearance.InlineLayoutAppearance.Selected.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarTitleColor.ToUIColor() };
+				tabBarAppearance.InlineLayoutAppearance.Normal.IconColor = tabBarAppearance.InlineLayoutAppearance.Selected.IconColor = tabBarTitleColor.ToUIColor();
+
+				tabBarAppearance.CompactInlineLayoutAppearance.Normal.TitleTextAttributes = tabBarAppearance.CompactInlineLayoutAppearance.Selected.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarTitleColor.ToUIColor() };
+				tabBarAppearance.CompactInlineLayoutAppearance.Normal.IconColor = tabBarAppearance.CompactInlineLayoutAppearance.Selected.IconColor = tabBarTitleColor.ToUIColor();
 			}
 			
 			//Set TabBarUnselectedColor
@@ -77,8 +84,15 @@ namespace Xamarin.Forms.Platform.iOS
 
 			if (!tabBarUnselectedColor.IsDefault)
 			{
+				// Update colors for all variations of the appearance to also make it work for iPads, etc.
 				tabBarAppearance.StackedLayoutAppearance.Normal.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarUnselectedColor.ToUIColor() };
 				tabBarAppearance.StackedLayoutAppearance.Normal.IconColor = tabBarUnselectedColor.ToUIColor();
+
+				tabBarAppearance.InlineLayoutAppearance.Normal.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarUnselectedColor.ToUIColor() };
+				tabBarAppearance.InlineLayoutAppearance.Normal.IconColor = tabBarUnselectedColor.ToUIColor();
+
+				tabBarAppearance.CompactInlineLayoutAppearance.Normal.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarUnselectedColor.ToUIColor() };
+				tabBarAppearance.CompactInlineLayoutAppearance.Normal.IconColor = tabBarUnselectedColor.ToUIColor();
 			}
 
 			// Set TabBarDisabledColor
@@ -86,8 +100,15 @@ namespace Xamarin.Forms.Platform.iOS
 
 			if (!tabBarDisabledColor.IsDefault)
 			{
+				// Update colors for all variations of the appearance to also make it work for iPads, etc.
 				tabBarAppearance.StackedLayoutAppearance.Disabled.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarDisabledColor.ToUIColor() };
 				tabBarAppearance.StackedLayoutAppearance.Disabled.IconColor = tabBarDisabledColor.ToUIColor();
+
+				tabBarAppearance.InlineLayoutAppearance.Disabled.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarDisabledColor.ToUIColor() };
+				tabBarAppearance.InlineLayoutAppearance.Disabled.IconColor = tabBarDisabledColor.ToUIColor();
+
+				tabBarAppearance.CompactInlineLayoutAppearance.Disabled.TitleTextAttributes = new UIStringAttributes { ForegroundColor = tabBarDisabledColor.ToUIColor() };
+				tabBarAppearance.CompactInlineLayoutAppearance.Disabled.IconColor = tabBarDisabledColor.ToUIColor();
 			}
 			
 			tabBar.StandardAppearance = tabBar.ScrollEdgeAppearance = tabBarAppearance;
