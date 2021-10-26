ce9492e069ccb311c2511a804a55261669d24d4a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-10-26 11:26:41 +0200

    Fix MultiWindow default brush issue

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2482.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2482.cs
@@ -33,6 +33,8 @@ namespace Xamarin.Forms.Controls.Issues
 		protected override void Init()
 		{
 			var layout = new StackLayout();
+			
+			layout.Background = Brush.LightGray;
 
 			var instructions = new Label
 			{
