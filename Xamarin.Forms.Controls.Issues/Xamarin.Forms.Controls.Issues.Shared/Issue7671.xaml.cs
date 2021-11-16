d9e2ed3ff810b5ae153c02c78c87f8bbd1c6d1a8
Gerald Versluis <gerald@verslu.is>
2021-11-16 14:12:13 +0100

    Fix build

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7671.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7671.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.CustomAttributes;
+﻿using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
@@ -13,12 +6,16 @@ namespace Xamarin.Forms.Controls.Issues
 {
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.None, 7671, "Check ControlTemplate rendering", PlatformAffected.WPF)]
+#if APP
 	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
 	public partial class Issue7671 : ContentPage
 	{
 		public Issue7671()
 		{
+#if APP
 			InitializeComponent();
+#endif
 		}
 	}
 }
