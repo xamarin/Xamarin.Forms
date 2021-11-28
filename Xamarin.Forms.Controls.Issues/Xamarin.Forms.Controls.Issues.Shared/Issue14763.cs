b1d9a36f9e77352a1a1b2a238547f23082bdcad1
Christian Prähauser <christian.praehauser@omicronenergy.com>
2021-11-28 19:46:00 +0100

    Add UI test PopButtonStillVisibleAfterSwipeBack for issue 14763

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue14763.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue14763.cs
@@ -1,13 +1,23 @@
 ﻿using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
+using System.Linq;
+using System.Threading.Tasks;
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
 
 namespace Xamarin.Forms.Controls.Issues
 {
+#if UITEST
+	[NUnit.Framework.Category(Core.UITests.UITestCategories.Github10000)]
+#endif
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 14763, "[iOS] Resize on back-swipe stays when going back is cancelled", PlatformAffected.iOS)]
-	public class Issue14763 : NavigationPage
+	public class Issue14763 : TestNavigationPage
 	{
-		public Issue14763() : base(new HomePage())
+		public Issue14763() : base()
 		{ }
 
 		class HomePage : ContentPage
@@ -26,7 +36,8 @@ namespace Xamarin.Forms.Controls.Issues
 				grd.Children.Add(boxView, 0, 0);
 				var stackLayout = new StackLayout()
 				{
-					BackgroundColor = Color.Yellow
+					BackgroundColor = Color.Yellow,
+					AutomationId = "ChildLayout"
 				};
 				var btnPop = new Button {
 					Text = "Back (I get cut off)", AutomationId = "PopButtonId", Command = new Command(async () => await Navigation.PopAsync()),
@@ -46,11 +57,12 @@ namespace Xamarin.Forms.Controls.Issues
 						var page = new ContentPage
 						{
 							Title = "Detail",
-							Content = stackLayout
+							Content = stackLayout,
+							AutomationId = "ChildPage"
 						};
 
 						NavigationPage.SetHasNavigationBar(page, true);
-						await Navigation.PushAsync(page);
+						await Navigation.PushAsync(page, false);
 					})
 				};
 
@@ -62,5 +74,35 @@ namespace Xamarin.Forms.Controls.Issues
 
 			}
 		}
+
+		protected override void Init()
+		{
+			PushAsync(new HomePage());
+		}
+
+#if UITEST && __IOS__
+		[Test]
+		public async Task PopButtonStillVisibleAfterSwipeBack()
+		{
+			RunningApp.WaitForElement("NextButtonID");
+			// Navigate to child page
+			RunningApp.Tap("NextButtonID");
+			var childLayout = RunningApp.WaitForElement("ChildLayout").First();
+			RunningApp.WaitForElement("PopButtonId");
+			await Task.Delay(1000);
+			var origLayoutRect = childLayout.Rect;
+
+			// Swipe-back/Swipe in from left across 1/2 of screen width to start back nav without completing it
+			var screenBounds = RunningApp.ScreenBounds();
+			RunningApp.DragCoordinates(0, screenBounds.CenterY, screenBounds.CenterX, screenBounds.CenterY);			
+			// Wait for swipe-back and UI to settle
+			await Task.Delay(3000);
+			childLayout = RunningApp.WaitForElement("ChildLayout").First();
+			var newLayoutRect = childLayout.Rect;
+
+			// Assert that child page has same height as before
+			Assert.That(newLayoutRect.Height, Is.EqualTo(origLayoutRect.Height));
+		}
+#endif
 	}
 }
