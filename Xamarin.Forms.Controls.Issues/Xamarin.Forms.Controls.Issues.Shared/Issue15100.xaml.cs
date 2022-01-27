17c375f8f856e3ec9831bbe63e034c87a397f3bd
Heiko Wobst <wobst.heiko1010@t-online.de>
2022-01-27 16:38:37 +0100

    Added repro sample (#15100)

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue15100.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 15100, "[Bug] ScrollView's size does not consider all Margins and Paddings on Android", PlatformAffected.Android)]
+	public partial class Issue15100 : TestContentPage
+	{
+		public Issue15100()
+		{
+			InitializeComponent();
+		}
+
+		protected override void Init()
+		{
+		}
+	}
+}
