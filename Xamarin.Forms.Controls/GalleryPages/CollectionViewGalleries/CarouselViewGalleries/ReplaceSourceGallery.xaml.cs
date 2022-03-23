4644b6d15f90371724cd0e9a8891c0daeeece9eb
Oliver Brown <oliver_brown2@next.co.uk>
2022-03-23 15:48:52 +0000

    Add gallery pages for changing the itemssource of a carousel view, and hosting a carouselview inside a collecitonview

---
+++ Xamarin.Forms.Controls/GalleryPages/CollectionViewGalleries/CarouselViewGalleries/ReplaceSourceGallery.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries.CarouselViewGalleries
+{
+	public partial class ReplaceSourceGallery : ContentPage
+	{
+		public ReplaceSourceGallery()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModel();
+        }
+
+        public void BindingContextButton_Clicked(object sender, EventArgs eventArgs)
+        {
+            BindingContext = new ViewModel();
+        }
+
+        public void ItemsSourceButton_Clicked(object sender, EventArgs eventArgs)
+        {
+            ((ViewModel)BindingContext).Images = new[]
+            {
+                "cover1.jpg",
+                "oasis.jpg",
+                "photo.jpg",
+                "Vegetables.jpg",
+                "Fruits.jpg",
+                "FlowerBuds.jpg",
+                "Legumes.jpg"
+            };
+        }
+
+        public class ViewModel
+        {
+            public IList<string> Images { get; set; } = new []
+            {
+                "cover1.jpg",
+                "oasis.jpg",
+                "photo.jpg",
+                "Vegetables.jpg",
+                "Fruits.jpg",
+                "FlowerBuds.jpg",
+                "Legumes.jpg"
+            };
+        }
+    }
+}
