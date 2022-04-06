1d80c80518d57179188f87606cd9151c0fc56070
Oliver Brown <oliver_brown2@next.co.uk>
2022-04-06 11:37:58 +0100

    Add test case for 15827

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue15287.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Shapes;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 15287, "[Bug] [iOS] Changing CollectionView.ItemSizingStrategy that contains a CarouselView causes the CarouselView to size its cells incorrectly.", PlatformAffected.iOS)]
+	public partial class Issue15287 : ContentPage
+	{
+        public Issue15287()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MainCollection.ItemSizingStrategy = ItemSizingStrategy.MeasureFirstItem;
+        }
+
+        public class ViewModel
+        {
+            public List<Item> Items { get; private set; }
+
+            public ViewModel()
+            {
+                Items = Enumerable.Range(0, 30).Select(x => new Item()).ToList();
+            }
+        }
+
+        public class Item
+        {
+            public IList<string> Images { get; } = new[]
+            {
+                "cover1.jpg",
+                "oasis.jpg",
+                "photo.jpg",
+                "Vegetables.jpg",
+                "Fruits.jpg",
+                "FlowerBuds.jpg",
+                "Legumes.jpg"
+            };
+        }
+    }
+}
