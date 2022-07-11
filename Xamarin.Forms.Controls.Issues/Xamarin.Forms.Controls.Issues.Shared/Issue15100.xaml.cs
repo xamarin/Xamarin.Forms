914ff082d88101b7f0fbddc27cff0ce98171388e
Heiko Wobst <wobst.heiko1010@t-online.de>
2022-07-11 16:49:21 +0200

    fix the calculations of the needed size of a ScollView on Android (Issue 15100) (#15101)
    * Added repro sample (#15100)
    
    * Fixed measured dimension calculation of ScrollView on Android (#15100)
    
    * Fixed height of horizontal ScrollView on Android (#15100)
    
    * Update Issue15100.xaml.cs
    
    Co-authored-by: Gerald Versluis <gerald.versluis@microsoft.com>

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue15100.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 15100, "[Bug] ScrollView's size does not consider all Margins and Paddings on Android", PlatformAffected.Android)]
+	public partial class Issue15100 : TestContentPage
+	{
+		public Issue15100()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+		}
+	}
+}
